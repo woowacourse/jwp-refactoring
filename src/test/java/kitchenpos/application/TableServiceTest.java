dfbba1a60d50f985396b1405a50d3706d03cf7e8
YerinCho <forjustice9839@gmail.com>
2020-09-15 14:42:46 +0900

    test: add table service test
     add OrderTableDao test

---
+++ src/test/java/kitchenpos/application/TableServiceTest.java
@@ -0,0 +1,147 @@
+package kitchenpos.application;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import kitchenpos.dao.OrderDao;
+import kitchenpos.dao.OrderTableDao;
+import kitchenpos.domain.OrderStatus;
+import kitchenpos.domain.OrderTable;
+
+@ExtendWith(MockitoExtension.class)
+class TableServiceTest {
+    @Mock
+    private TableService tableService;
+    @Mock
+    private OrderDao orderDao;
+    @Mock
+    private OrderTableDao orderTableDao;
+    private OrderTable orderTable1;
+
+    @BeforeEach
+    void setUp() {
+        tableService = new TableService(orderDao, orderTableDao);
+        orderTable1 = new OrderTable();
+        orderTable1.setId(1L);
+        orderTable1.setNumberOfGuests(1);
+        orderTable1.setEmpty(false);
+    }
+
+    @Test
+    void create() {
+        given(orderTableDao.save(any())).willReturn(orderTable1);
+
+        OrderTable savedOrderTable = tableService.create(orderTable1);
+
+        assertThat(savedOrderTable.getId()).isEqualTo(null);
+        assertThat(savedOrderTable.getTableGroupId()).isEqualTo(null);
+        assertThat(savedOrderTable.isEmpty()).isFalse();
+    }
+
+    @Test
+    void list() {
+        OrderTable orderTable2 = new OrderTable();
+        List<OrderTable> orderTables = Arrays.asList(orderTable1, orderTable2);
+
+        given(orderTableDao.findAll()).willReturn(orderTables);
+
+        List<OrderTable> foundOrderTables = tableService.list();
+        assertThat(foundOrderTables.size()).isEqualTo(orderTables.size());
+        assertThat(foundOrderTables.get(0)).isEqualTo(orderTables.get(0));
+        assertThat(foundOrderTables.get(1)).isEqualTo(orderTables.get(1));
+    }
+
+    @Test
+    void changeEmpty() {
+        OrderTable orderTable = new OrderTable();
+        orderTable.setEmpty(true);
+
+        given(orderTableDao.findById(1L)).willReturn(java.util.Optional.ofNullable(orderTable1));
+        given(orderDao.existsByOrderTableIdAndOrderStatusIn(
+            1L, Arrays.asList(OrderStatus.COOKING.name(), OrderStatus.MEAL.name()))).willReturn(false);
+        given(orderTableDao.save(orderTable1)).willReturn(orderTable1);
+
+        assertThat(orderTable1.isEmpty()).isFalse();
+
+        OrderTable savedOrderTable = tableService.changeEmpty(1L, orderTable);
+        assertThat(savedOrderTable.isEmpty()).isTrue();
+    }
+
+    @Test
+    @DisplayName("empty 상태를 변경하기 위해서는 테이블 그룹에 포함되어있지 않아야 한다.")
+    void changeEmptyFailWhenInGroup() {
+        OrderTable orderTable = new OrderTable();
+        orderTable1.setTableGroupId(1L);
+
+        given(orderTableDao.findById(1L)).willReturn(java.util.Optional.ofNullable(orderTable1));
+
+        assertThatThrownBy(() -> tableService.changeEmpty(1L, orderTable))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("changeEmpty Error: 테이블 그룹에 포함되어있지 않아야 합니다.");
+    }
+
+    @Test
+    @DisplayName("empty 상태를 변경하기 위해서는 주문 상태가 결제 완료여야 한다.")
+    void changeEmptyFailWhenOrderStatusNotComplete() {
+        OrderTable orderTable = new OrderTable();
+        orderTable.setEmpty(true);
+
+        given(orderTableDao.findById(1L)).willReturn(java.util.Optional.ofNullable(orderTable1));
+        given(orderDao.existsByOrderTableIdAndOrderStatusIn(
+            1L, Arrays.asList(OrderStatus.COOKING.name(), OrderStatus.MEAL.name()))).willReturn(true);
+
+        assertThatThrownBy(() -> tableService.changeEmpty(1L, orderTable))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("changeEmpty Error: 주문 상태가 결제 완료여야 합니다.");
+    }
+
+    @Test
+    void changeNumberOfGuests() {
+        OrderTable orderTable = new OrderTable();
+        orderTable.setNumberOfGuests(4);
+
+        given(orderTableDao.findById(1L)).willReturn(java.util.Optional.ofNullable(orderTable1));
+        given(orderTableDao.save(orderTable1)).willReturn(orderTable1);
+
+        int guests = orderTable1.getNumberOfGuests();
+        OrderTable savedOrderTable = tableService.changeNumberOfGuests(1L, orderTable);
+        assertThat(guests).isEqualTo(1);
+        assertThat(savedOrderTable.getNumberOfGuests()).isEqualTo(4);
+
+    }
+
+    @Test
+    @DisplayName("테이블의 손님 수는 0명 이상이어야 한다")
+    void changeNumberOfGuestFailWhenUnderZero() {
+        OrderTable orderTable = new OrderTable();
+        orderTable.setNumberOfGuests(-1);
+
+        assertThatThrownBy(() -> tableService.changeNumberOfGuests(1L, orderTable))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("changeNumberOfGuest Error: 손님은 0명 이상이어야 합니다.");
+    }
+
+    @Test
+    @DisplayName("테이블의 상태는 empty가 아니어야 한다.")
+    void changeNumberOfGuestFailWhenEmpty() {
+        OrderTable orderTable = new OrderTable();
+        orderTable.setNumberOfGuests(1);
+        orderTable1.setEmpty(true);
+
+        given(orderTableDao.findById(1L)).willReturn(java.util.Optional.ofNullable(orderTable1));
+
+        assertThatThrownBy(() -> tableService.changeNumberOfGuests(1L, orderTable))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("changeNumberOfGuest Error: 테이블이 비어있습니다.");
+    }
+}
---
+++ src/test/java/kitchenpos/dao/JdbcTemplateOrderTableDaoTest.java
@@ -0,0 +1,104 @@
+package kitchenpos.dao;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import kitchenpos.domain.OrderTable;
+import kitchenpos.domain.TableGroup;
+
+@SpringBootTest
+class JdbcTemplateOrderTableDaoTest {
+    @Autowired
+    private JdbcTemplateOrderTableDao orderTableDao;
+    @Autowired
+    private JdbcTemplateTableGroupDao tableGroupDao;
+
+    @Test
+    @DisplayName("생성하는 경우")
+    void create() {
+        OrderTable orderTable = new OrderTable();
+        orderTable.setEmpty(true);
+        orderTable.setNumberOfGuests(1);
+        OrderTable savedOrderTable = orderTableDao.save(orderTable);
+
+        assertThat(savedOrderTable.getId()).isNotNull();
+        assertThat(savedOrderTable.getNumberOfGuests()).isEqualTo(orderTable.getNumberOfGuests());
+        assertThat(savedOrderTable.isEmpty()).isEqualTo(orderTable.isEmpty());
+    }
+
+    @Test
+    void findById() {
+        OrderTable foundOrderTable = orderTableDao.findById(1L).get();
+
+        assertThat(foundOrderTable.getId()).isEqualTo(1L);
+    }
+
+    @Test
+    @DisplayName("업데이트하는 경우")
+    void update() {
+        OrderTable foundOrderTable = orderTableDao.findById(1L).get();
+
+        OrderTable orderTable = new OrderTable();
+        orderTable.setId(foundOrderTable.getId());
+        orderTable.setEmpty(!foundOrderTable.isEmpty());
+
+        OrderTable savedOrderTable = orderTableDao.save(orderTable);
+
+        assertThat(savedOrderTable.getId()).isEqualTo(foundOrderTable.getId());
+        assertThat(savedOrderTable.isEmpty()).isNotEqualTo(foundOrderTable.isEmpty());
+    }
+
+    @Test
+    void findAll() {
+        OrderTable orderTable = new OrderTable();
+        List<OrderTable> orderTables = orderTableDao.findAll();
+        orderTableDao.save(orderTable);
+        List<OrderTable> savedOrderTables = orderTableDao.findAll();
+
+        assertThat(savedOrderTables.size()).isEqualTo(orderTables.size() + 1);
+    }
+
+    @Test
+    void findAllByIdIn() {
+        List<Long> ids = Arrays.asList(1L, 2L, 3L);
+        List<OrderTable> orderTables = orderTableDao.findAllByIdIn(ids);
+
+        OrderTable orderTable1 = orderTableDao.findById(1L).get();
+        OrderTable orderTable2 = orderTableDao.findById(2L).get();
+        OrderTable orderTable3 = orderTableDao.findById(3L).get();
+
+        assertThat(orderTables.get(0)).isEqualTo(orderTable1);
+        assertThat(orderTables.get(1)).isEqualTo(orderTable2);
+        assertThat(orderTables.get(2)).isEqualTo(orderTable3);
+    }
+
+    @Test
+    void findAllByTableGroupId() {
+        TableGroup tableGroup = new TableGroup();
+        OrderTable orderTable1 = orderTableDao.findById(1L).get();
+        OrderTable orderTable2 = orderTableDao.findById(2L).get();
+
+        orderTable1.setTableGroupId(1L);
+        orderTable2.setTableGroupId(1L);
+        orderTable1.setEmpty(false);
+        orderTable2.setEmpty(false);
+        tableGroup.setOrderTables(Arrays.asList(orderTable1, orderTable2));
+        tableGroup.setCreatedDate(LocalDateTime.now());
+        tableGroupDao.save(tableGroup);
+        orderTableDao.save(orderTable1);
+        orderTableDao.save(orderTable2);
+
+        List<OrderTable> orderTables = orderTableDao.findAllByTableGroupId(1L);
+
+        assertThat(orderTables.get(0)).isEqualTo(orderTableDao.findById(1L).get());
+        assertThat(orderTables.get(1)).isEqualTo(orderTableDao.findById(2L).get());
+    }
+}
