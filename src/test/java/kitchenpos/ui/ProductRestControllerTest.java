308d3288e273bf2d54b3e95b40c74a0ceec96b6d
YerinCho <forjustice9839@gmail.com>
2020-09-16 16:50:24 +0900

    test: add ProductRestControllerTest

---
+++ src/test/java/kitchenpos/ui/ProductRestControllerTest.java
@@ -0,0 +1,84 @@
+package kitchenpos.ui;
+
+import static org.hamcrest.core.StringContains.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import kitchenpos.application.ProductService;
+import kitchenpos.domain.Product;
+
+@WebMvcTest(ProductRestController.class)
+class ProductRestControllerTest {
+    private final String BASE_URL = "/api/products";
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @MockBean
+    private ProductService productService;
+    private Product product;
+
+    @BeforeEach
+    void setUp(WebApplicationContext webApplicationContext) {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+            .addFilter(new CharacterEncodingFilter("UTF-8", true))
+            .build();
+        product = new Product();
+        product.setId(1L);
+        product.setName("product");
+        product.setPrice(BigDecimal.valueOf(1000));
+    }
+
+    @Test
+    void create() throws Exception {
+        String body = objectMapper.writeValueAsString(product);
+
+        given(productService.create(any())).willReturn(product);
+
+        mockMvc.perform(post(BASE_URL)
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .accept(MediaType.APPLICATION_JSON_VALUE)
+            .content(body)
+        )
+            .andDo(print())
+            .andExpect(status().isCreated())
+            .andExpect(content().string(containsString("product")))
+            .andExpect(content().string(containsString("1")))
+            .andExpect(content().string(containsString("1000")));
+    }
+
+    @Test
+    void list() throws Exception {
+        Product product = new Product();
+        product.setName("product2");
+
+        given(productService.list()).willReturn(Arrays.asList(this.product, product));
+
+        mockMvc.perform(get(BASE_URL)
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .accept(MediaType.APPLICATION_JSON_VALUE)
+        )
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(content().string(containsString("product")))
+            .andExpect(content().string(containsString("product2")));
+    }
+}
