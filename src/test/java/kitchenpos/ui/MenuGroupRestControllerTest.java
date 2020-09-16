fab8960404f5e10bf26e675c18629e1f3dee4517
YerinCho <forjustice9839@gmail.com>
2020-09-16 14:46:22 +0900

    test: add MenuGroupRestControllerTest

---
+++ src/test/java/kitchenpos/ui/MenuGroupRestControllerTest.java
@@ -0,0 +1,72 @@
+package kitchenpos.ui;
+
+import static org.mockito.BDDMockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import kitchenpos.application.MenuGroupService;
+import kitchenpos.domain.MenuGroup;
+
+@WebMvcTest(controllers = {MenuGroupRestController.class})
+class MenuGroupRestControllerTest {
+    private final String BASE_URL = "/api/menu-groups";
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @MockBean
+    private MenuGroupService menuGroupService;
+    private MenuGroup menuGroup;
+
+    @BeforeEach
+    void setUp(WebApplicationContext webApplicationContext) {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+            .addFilter(new CharacterEncodingFilter("UTF-8", true))
+            .build();
+        menuGroup = new MenuGroup();
+        menuGroup.setName("menuGroup");
+    }
+
+    @Test
+    void create() throws Exception {
+        String body = objectMapper.writeValueAsString(menuGroup);
+
+        given(menuGroupService.create(any())).willReturn(menuGroup);
+
+        mockMvc.perform(post(BASE_URL)
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .accept(MediaType.APPLICATION_JSON_VALUE)
+            .content(body)
+        )
+            .andDo(print())
+            .andExpect(status().isCreated());
+    }
+
+    @Test
+    void list() throws Exception {
+        given(menuGroupService.list()).willReturn(new ArrayList<>());
+
+        mockMvc.perform(get(BASE_URL)
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .accept(MediaType.APPLICATION_JSON_VALUE)
+        )
+            .andDo(print())
+            .andExpect(status().isOk());
+    }
+}
