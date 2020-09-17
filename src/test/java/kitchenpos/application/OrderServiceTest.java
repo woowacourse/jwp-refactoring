8468335ca74c9ed735c74a7084b9dd1e9a367652
hwookim <gusdn9754@gmail.com>
2020-09-18 05:06:34 +0900

    test: OrderService.create() 테스트 추가

---
+++ src/test/java/kitchenpos/application/OrderServiceTest.java
@@ -0,0 +1,106 @@
+package kitchenpos.application;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import kitchenpos.dao.MenuGroupDao;
+import kitchenpos.dao.OrderTableDao;
+import kitchenpos.dao.ProductDao;
+import kitchenpos.domain.Menu;
+import kitchenpos.domain.MenuGroup;
+import kitchenpos.domain.MenuProduct;
+import kitchenpos.domain.Order;
+import kitchenpos.domain.OrderLineItem;
+import kitchenpos.domain.OrderStatus;
+import kitchenpos.domain.OrderTable;
+import kitchenpos.domain.Product;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+
+@SpringBootTest
+@Sql("/deleteAll.sql")
+class OrderServiceTest {
+
+    @Autowired
+    private OrderService orderService;
+
+    @Autowired
+    private MenuGroupDao menuGroupDao;
+    private MenuGroup menuGroup;
+
+    @Autowired
+    private ProductDao productDao;
+    private Product product;
+
+    private MenuProduct menuProduct;
+
+    @Autowired
+    private MenuService menuService;
+    private Menu menu;
+
+    @Autowired
+    private OrderTableDao tableDao;
+    private OrderTable table;
+
+    @BeforeEach
+    void setUp() {
+        menuGroup = MenuGroup.builder()
+            .name("강정메뉴")
+            .build();
+        menuGroup = menuGroupDao.save(menuGroup);
+
+        product = Product.builder()
+            .name("강정치킨")
+            .price(BigDecimal.valueOf(18_000))
+            .build();
+        product = productDao.save(product);
+
+        menuProduct = MenuProduct.builder()
+            .productId(product.getId())
+            .quantity(1)
+            .build();
+
+        menu = Menu.builder()
+            .name("강정치킨")
+            .price(BigDecimal.valueOf(18_000))
+            .menuGroupId(menuGroup.getId())
+            .menuProducts(Arrays.asList(menuProduct))
+            .build();
+        menu = menuService.create(menu);
+
+        table = OrderTable.builder()
+            .empty(false)
+            .build();
+        table = tableDao.save(table);
+    }
+
+    @DisplayName("주문 추가")
+    @Test
+    void create() {
+        OrderLineItem orderLineItem = OrderLineItem.builder()
+            .menuId(menu.getId())
+            .quantity(2)
+            .build();
+        Order order = Order.builder()
+            .orderTableId(table.getId())
+            .orderStatus(OrderStatus.COOKING.name())
+            .orderLineItems(Arrays.asList(orderLineItem))
+            .orderedTime(LocalDateTime.now())
+            .build();
+
+        Order create = orderService.create(order);
+
+        assertAll(
+            () -> assertThat(create.getId()).isNotNull(),
+            () -> assertThat(create.getOrderStatus()).isEqualTo(OrderStatus.COOKING.name()),
+            () -> assertThat(create.getOrderLineItems().get(0).getSeq()).isNotNull()
+        );
+    }
+}
