b8c2112781f41ad00b254176dd498b22e6e7cbcf
hwookim <gusdn9754@gmail.com>
2020-09-18 04:16:49 +0900

    test: TableService.list() 테스트 추가

--- src/test/java/kitchenpos/application/TableServiceTest.java
+++ src/test/java/kitchenpos/application/TableServiceTest.java
@@ -2,7 +2,9 @@ package kitchenpos.application;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
 import kitchenpos.domain.OrderTable;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,17 +17,32 @@ class TableServiceTest {
 
     @Autowired
     private TableService tableService;
+    private OrderTable table;
+
+    @BeforeEach
+    void setUp() {
+        table = OrderTable.builder()
+            .numberOfGuests(0)
+            .empty(true)
+            .build();
+    }
 
     @DisplayName("테이블 추가")
     @Test
     void create() {
-        OrderTable table = OrderTable.builder()
-            .numberOfGuests(0)
-            .empty(true)
-            .build();
-
         OrderTable create = tableService.create(table);
 
         assertThat(create.getId()).isNotNull();
+    }
+
+    @DisplayName("테이블 전체 조회")
+    @Test
+    void list() {
+        tableService.create(table);
+        tableService.create(table);
+
+        List<OrderTable> list = tableService.list();
+
+        assertThat(list).hasSize(2);
     }
 }
