923442ff9702d0d8bafc2d2df12a977f993ade2f
hwookim <gusdn9754@gmail.com>
2020-09-18 02:03:21 +0900

    test: MenuService.create() 예외 테스트 추가

--- src/test/java/kitchenpos/application/MenuServiceTest.java
+++ src/test/java/kitchenpos/application/MenuServiceTest.java
@@ -1,6 +1,7 @@
 package kitchenpos.application;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
@@ -66,5 +67,66 @@ class MenuServiceTest {
         Menu create = menuService.create(menu);
 
         assertThat(create.getId()).isNotNull();
+    }
+
+    @DisplayName("[예외] 가격이 0보다 작은 메뉴 추가")
+    @Test
+    void create_Fail_With_InvalidPrice() {
+        Menu menu = Menu.builder()
+            .name("강정치킨")
+            .price(BigDecimal.valueOf(-1))
+            .menuGroupId(menuGroup.getId())
+            .menuProducts(Arrays.asList(menuProduct))
+            .build();
+
+        assertThatThrownBy(() -> menuService.create(menu))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("[예외] 존재하지 않는 메뉴 그룹에 속한 메뉴 추가")
+    @Test
+    void create_Fail_With_NotExistMenuGroup() {
+        Menu menu = Menu.builder()
+            .name("강정치킨")
+            .price(BigDecimal.valueOf(18_000))
+            .menuGroupId(100L)
+            .menuProducts(Arrays.asList(menuProduct))
+            .build();
+
+        assertThatThrownBy(() -> menuService.create(menu))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("[예외] 존재하지 않는 상품을 포함한 메뉴 추가")
+    @Test
+    void create_Fail_With_NotExistProduct() {
+        MenuProduct menuProduct = MenuProduct.builder()
+            .productId(100L)
+            .quantity(1)
+            .build();
+
+        Menu menu = Menu.builder()
+            .name("강정치킨")
+            .price(BigDecimal.valueOf(18_000))
+            .menuGroupId(menuGroup.getId())
+            .menuProducts(Arrays.asList(menuProduct))
+            .build();
+
+        assertThatThrownBy(() -> menuService.create(menu))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("[예외] 포함한 상품의 가격 총합보다 높은 가격의 메뉴 추가")
+    @Test
+    void create_Fail_With_OverPrice() {
+        Menu menu = Menu.builder()
+            .name("강정치킨")
+            .price(BigDecimal.valueOf(100_000))
+            .menuGroupId(menuGroup.getId())
+            .menuProducts(Arrays.asList(menuProduct))
+            .build();
+
+        assertThatThrownBy(() -> menuService.create(menu))
+            .isInstanceOf(IllegalArgumentException.class);
     }
 }
