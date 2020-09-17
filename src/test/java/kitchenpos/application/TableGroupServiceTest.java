e3b06593af3760183ecc3b756de75426603e1288
hwookim <gusdn9754@gmail.com>
2020-09-18 03:47:40 +0900

    test: TableGroupService.create() 예외 테스트 추가
     - 중복 코드 setUp 메서드에서 처리

--- src/test/java/kitchenpos/application/TableGroupServiceTest.java
+++ src/test/java/kitchenpos/application/TableGroupServiceTest.java
@@ -1,6 +1,7 @@
 package kitchenpos.application;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 import java.time.LocalDateTime;
@@ -8,6 +9,8 @@ import java.util.Arrays;
 import kitchenpos.dao.OrderTableDao;
 import kitchenpos.domain.OrderTable;
 import kitchenpos.domain.TableGroup;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,27 +22,30 @@ class TableGroupServiceTest {
 
     @Autowired
     private TableGroupService tableGroupService;
+    private TableGroup tableGroup;
 
     @Autowired
     private OrderTableDao tableDao;
+    private OrderTable table1;
+    private OrderTable table2;
+
+    @BeforeEach
+    void setUp() {
+        OrderTable table = OrderTable.builder()
+            .empty(true)
+            .build();
+        table1 = tableDao.save(table);
+        table2 = tableDao.save(table);
+
+        tableGroup = TableGroup.builder()
+            .orderTables(Arrays.asList(table1, table2))
+            .createdDate(LocalDateTime.now())
+            .build();
+    }
 
     @DisplayName("테이블 그룹 추가")
     @Test
     void create() {
-        OrderTable table1 = OrderTable.builder()
-            .empty(true)
-            .build();
-        table1 = tableDao.save(table1);
-        OrderTable table2 = OrderTable.builder()
-            .empty(true)
-            .build();
-        table2 = tableDao.save(table2);
-
-        TableGroup tableGroup = TableGroup.builder()
-            .orderTables(Arrays.asList(table1, table2))
-            .createdDate(LocalDateTime.now())
-            .build();
-
         TableGroup create = tableGroupService.create(tableGroup);
 
         assertAll(
@@ -47,5 +53,69 @@ class TableGroupServiceTest {
             () -> assertThat(create.getOrderTables().get(0).isEmpty()).isFalse(),
             () -> assertThat(create.getOrderTables().get(1).isEmpty()).isFalse()
         );
+    }
+
+    @DisplayName("[예외] 2개 미만의 테이블을 포함한 테이블 그룹 추가")
+    @Test
+    void create_Fail_With_LessTable() {
+        TableGroup tableGroup = TableGroup.builder()
+            .orderTables(Arrays.asList(table1))
+            .createdDate(LocalDateTime.now())
+            .build();
+
+        assertThatThrownBy(() -> tableGroupService.create(tableGroup))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("[예외] 저장되지 않은 포함한 테이블 그룹 추가")
+    @Test
+    void create_Fail_With_NotExistTable() {
+        OrderTable notExistTable = OrderTable.builder()
+            .id(100L)
+            .empty(true)
+            .build();
+
+        TableGroup tableGroup = TableGroup.builder()
+            .orderTables(Arrays.asList(notExistTable))
+            .createdDate(LocalDateTime.now())
+            .build();
+
+        assertThatThrownBy(() -> tableGroupService.create(tableGroup))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("[예외] 빈 테이블이 아닌 테이블을 포함한 테이블 그룹 추가")
+    @Test
+    void create_Fail_With_NotEmptyTable() {
+        OrderTable emptyTable = OrderTable.builder()
+            .empty(false)
+            .build();
+        emptyTable = tableDao.save(emptyTable);
+
+        TableGroup tableGroup = TableGroup.builder()
+            .orderTables(Arrays.asList(emptyTable))
+            .createdDate(LocalDateTime.now())
+            .build();
+
+        assertThatThrownBy(() -> tableGroupService.create(tableGroup))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("[예외] 다른 그룹의 테이블을 포함한 테이블 그룹 추가")
+    @Test
+    void create_Fail_With_OthersTable() {
+        OrderTable othersTable = OrderTable.builder()
+            .empty(true)
+            .tableGroupId(tableGroup.getId())
+            .build();
+        othersTable = tableDao.save(othersTable);
+
+        TableGroup tableGroup = TableGroup.builder()
+            .orderTables(Arrays.asList(othersTable))
+            .createdDate(LocalDateTime.now())
+            .build();
+
+        assertThatThrownBy(() -> tableGroupService.create(tableGroup))
+            .isInstanceOf(IllegalArgumentException.class);
     }
 }
