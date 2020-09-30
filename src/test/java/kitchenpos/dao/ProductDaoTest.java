c10200467f87c6774b4b377d84558ddb7720d71a
jamie9504 <jamie9504@kakao.com>
2020-09-30 14:05:24 +0900

    test: ProductDaoTest 작성

---
+++ src/test/java/kitchenpos/dao/ProductDaoTest.java
@@ -0,0 +1,88 @@
+package kitchenpos.dao;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import kitchenpos.domain.Product;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+class ProductDaoTest {
+
+    private static final String TEST_PRODUCT_NAME = "테스트상품";
+    private static final BigDecimal TEST_PRODUCT_PRICE = BigDecimal.valueOf(10000);
+
+    @Autowired
+    private ProductDao productDao;
+
+    @DisplayName("Product 저장 - 성공")
+    @Test
+    void save_Success() {
+        Product product = new Product();
+        product.setName(TEST_PRODUCT_NAME);
+        product.setPrice(TEST_PRODUCT_PRICE);
+
+        Product savedProduct = productDao.save(product);
+
+        assertThat(savedProduct.getId()).isNotNull();
+        assertThat(savedProduct.getName()).isEqualTo(TEST_PRODUCT_NAME);
+        assertThat(savedProduct.getPrice()).isEqualByComparingTo(TEST_PRODUCT_PRICE);
+    }
+
+    @DisplayName("Product ID로 Product 조회 - 조회됨, ID가 존재하는 경우")
+    @Test
+    void findById_ExistsId_ReturnProduct() {
+        Product product = new Product();
+        product.setName(TEST_PRODUCT_NAME);
+        product.setPrice(TEST_PRODUCT_PRICE);
+        Product savedProduct = productDao.save(product);
+
+        Product foundProduct = productDao.findById(savedProduct.getId())
+            .orElseThrow(() -> new IllegalArgumentException("ID에 해당하는 Product가 없습니다."));
+
+        assertThat(foundProduct.getId()).isEqualTo(savedProduct.getId());
+        assertThat(foundProduct.getName()).isEqualTo(savedProduct.getName());
+        assertThat(foundProduct.getPrice()).isEqualByComparingTo(savedProduct.getPrice());
+    }
+
+    @DisplayName("Product ID로 Product 조회 - 조회되지 않음, ID가 존재하지 않는 경우")
+    @Test
+    void findById_NotExistsId_ReturnEmpty() {
+        Product product = new Product();
+        product.setName(TEST_PRODUCT_NAME);
+        product.setPrice(TEST_PRODUCT_PRICE);
+        Product savedProduct = productDao.save(product);
+
+        Optional<Product> foundProduct = productDao.findById(savedProduct.getId() + 1);
+
+        assertThat(foundProduct.isPresent()).isFalse();
+    }
+
+    @DisplayName("전체 Product 조회 - 성공")
+    @Test
+    void findAll_Success() {
+        Product product = new Product();
+        product.setName(TEST_PRODUCT_NAME);
+        product.setPrice(TEST_PRODUCT_PRICE);
+        Product savedProduct = productDao.save(product);
+
+        List<Product> products = productDao.findAll();
+
+        assertThat(products).isNotNull();
+        assertThat(products).isNotEmpty();
+
+        List<Long> productsId = products.stream()
+            .map(Product::getId)
+            .collect(Collectors.toList());
+
+        assertThat(productsId).contains(savedProduct.getId());
+    }
+}
