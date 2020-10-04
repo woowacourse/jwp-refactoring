9c128206be7050010ff17769059c7d61a8f00c1e
jamie9504 <jamie9504@kakao.com>
2020-10-05 00:20:01 +0900

    test: MenuServiceTest 작성

--- src/test/java/kitchenpos/application/KitchenPosServiceTest.java
+++ src/test/java/kitchenpos/application/KitchenPosServiceTest.java
@@ -1,14 +1,19 @@
 package kitchenpos.application;
 
+import static kitchenpos.constants.Constants.TEST_MENU_GROUP_NAME;
 import static kitchenpos.constants.Constants.TEST_ORDER_TABLE_EMPTY_FALSE;
 import static kitchenpos.constants.Constants.TEST_ORDER_TABLE_EMPTY_TRUE;
 import static kitchenpos.constants.Constants.TEST_ORDER_TABLE_NUMBER_OF_GUESTS;
 import static kitchenpos.constants.Constants.TEST_ORDER_TABLE_NUMBER_OF_GUESTS_EMPTY;
+import static kitchenpos.constants.Constants.TEST_PRODUCT_NAME;
+import static kitchenpos.constants.Constants.TEST_PRODUCT_PRICE;
 import static kitchenpos.constants.Constants.TEST_TABLE_GROUP_CREATED_DATE;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
+import kitchenpos.domain.MenuGroup;
 import kitchenpos.domain.OrderTable;
+import kitchenpos.domain.Product;
 import kitchenpos.domain.TableGroup;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -23,6 +28,12 @@ public abstract class KitchenPosServiceTest {
 
     @Autowired
     protected TableGroupService tableGroupService;
+
+    @Autowired
+    protected MenuGroupService menuGroupService;
+
+    @Autowired
+    protected ProductService productService;
 
     protected void setCreatedTableGroup(List<OrderTable> orderTables) {
         TableGroup tableGroup = new TableGroup();
@@ -52,5 +63,26 @@ public abstract class KitchenPosServiceTest {
         Long createdOrderTableId = createdOrderTable.getId();
         assertThat(createdOrderTable).isNotNull();
         return createdOrderTableId;
+    }
+
+    protected long getCreatedMenuGroupId() {
+        MenuGroup menuGroup = new MenuGroup();
+        menuGroup.setName(TEST_MENU_GROUP_NAME);
+        MenuGroup createdMenuGroup = menuGroupService.create(menuGroup);
+
+        Long createdMenuGroupId = createdMenuGroup.getId();
+        assertThat(createdMenuGroupId).isNotNull();
+        return createdMenuGroupId;
+    }
+
+    protected long getCreatedProductId() {
+        Product product = new Product();
+        product.setName(TEST_PRODUCT_NAME);
+        product.setPrice(TEST_PRODUCT_PRICE);
+        Product createdProduct = productService.create(product);
+
+        Long createdProductId = createdProduct.getId();
+        assertThat(createdProductId).isNotNull();
+        return createdProductId;
     }
 }
--- src/test/java/kitchenpos/constants/Constants.java
+++ src/test/java/kitchenpos/constants/Constants.java
@@ -14,6 +14,8 @@ public class Constants {
     public static final String TEST_MENU_NAME = "테스트메뉴";
     public static final BigDecimal TEST_MENU_PRICE = BigDecimal.valueOf(1_000);
 
+    public static final int TEST_MENU_PRODUCT_QUANTITY = 10;
+
     public static final String TEST_PRODUCT_NAME = "테스트상품";
     public static final BigDecimal TEST_PRODUCT_PRICE = BigDecimal.valueOf(10_000);
 
