03466d6b28dcb7f8dc566277c3890c9cd0a85d30
ordinCode <ysh1346@naver.com>
2020-10-04 14:20:39 +0900

    feat: Product 생성 메서드 예외 케이스 테스트 추가

--- src/test/java/kitchenpos/application/ProductServiceTest.java
+++ src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -4,10 +4,13 @@ import kitchenpos.TestObjectFactory;
 import kitchenpos.domain.Product;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 @SpringBootTest
@@ -16,10 +19,9 @@ class ProductServiceTest {
     private ProductService productService;
 
     @DisplayName("Product 생성 메서드 테스트")
-    @Test
-    void create() {
-        String name = "강정치킨";
-        int price = 17000;
+    @ParameterizedTest
+    @CsvSource({"강정치킨, 17000", "양념치킨, 0"})
+    void create(String name, int price) {
         Product product = TestObjectFactory.createProduct(name, price);
 
         Product savedProduct = productService.create(product);
@@ -28,5 +30,26 @@ class ProductServiceTest {
                 () -> assertThat(savedProduct.getName()).isEqualTo(name),
                 () -> assertThat(savedProduct.getPrice()).isEqualByComparingTo(String.valueOf(price))
         );
+    }
+
+    @DisplayName("Product 생성 - price 값이 null일 때 예외 처리")
+    @Test
+    void createWhenIllegalPrice() {
+        String name = "강정치킨";
+        Product product = new Product();
+        product.setName(name);
+
+        assertThatThrownBy(() -> productService.create(product))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("Product 생성 - price 값이 0보다 작을 때 예외 처리")
+    @Test
+    void createWhenIllegalPrice2() {
+        String name = "강정치킨";
+        Product product = TestObjectFactory.createProduct(name, -1);
+
+        assertThatThrownBy(() -> productService.create(product))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
