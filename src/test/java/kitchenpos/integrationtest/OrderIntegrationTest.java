8d10695998e3f7ea0ad72e08d40c5c856444b6e3
kuenhwi <kueniyam@naver.com>
2020-10-05 11:49:39 +0900

    test: 1 개 이상의 등록된 메뉴로 주문을 등록

---
+++ src/test/java/kitchenpos/integrationtest/OrderIntegrationTest.java
@@ -0,0 +1,31 @@
+package kitchenpos.integrationtest;
+
+import static io.restassured.RestAssured.*;
+import static kitchenpos.integrationtest.step.OrderIntegrationTestStep.*;
+
+import java.util.Map;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+
+import io.restassured.http.ContentType;
+import kitchenpos.integrationtest.common.IntegrationTest;
+
+public class OrderIntegrationTest extends IntegrationTest {
+
+	@DisplayName("1 개 이상의 등록된 메뉴로 주문을 등록할 수 있다.")
+	@Test
+	void create() {
+		Map<String, Object> requestBody = createValidOrder();
+
+		given().log().all()
+			.body(requestBody)
+			.contentType(ContentType.JSON)
+			.accept(ContentType.JSON)
+			.when()
+			.post("/api/orders")
+			.then().log().all()
+			.statusCode(HttpStatus.CREATED.value());
+	}
+}
---
+++ src/test/java/kitchenpos/integrationtest/step/OrderIntegrationTestStep.java
@@ -0,0 +1,23 @@
+package kitchenpos.integrationtest.step;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class OrderIntegrationTestStep {
+	public static Map<String, Object> createValidOrder() {
+		Map<String, Object> orderLineItem = new HashMap<>();
+		orderLineItem.put("menuId", 1);
+		orderLineItem.put("quantity", 1);
+
+		List<Map<String, Object>> orderLineItems = new ArrayList<>();
+		orderLineItems.add(orderLineItem);
+
+		Map<String, Object> requestBody = new HashMap<>();
+		requestBody.put("orderTableId", 2);
+		requestBody.put("orderLineItems", orderLineItems);
+
+		return requestBody;
+	}
+}
