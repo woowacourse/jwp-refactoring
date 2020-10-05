05dc2b7ba2ff6d94dd200f5f3b33c452243c8107
ordinCode <ysh1346@naver.com>
2020-10-05 12:35:03 +0900

    feat: MenuGroupRestController 테스트 추가

---
+++ src/test/java/kitchenpos/ui/MenuGroupRestControllerTest.java
@@ -0,0 +1,68 @@
+package kitchenpos.ui;
+
+import kitchenpos.TestObjectFactory;
+import kitchenpos.application.MenuGroupService;
+import kitchenpos.domain.MenuGroup;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = MenuGroupRestController.class)
+class MenuGroupRestControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private MenuGroupService menuGroupService;
+
+    @DisplayName("메뉴 그룹을 생성 요청 테스트")
+    @Test
+    void create() throws Exception {
+        MenuGroup menuGroup = TestObjectFactory.createMenuGroup("추천메뉴");
+        menuGroup.setId(1L);
+
+        given(menuGroupService.create(any())).willReturn(menuGroup);
+
+        mockMvc.perform(post("/api/menu-groups")
+                .content("{\n"
+                        + "  \"name\": \"추천메뉴\"\n"
+                        + "}")
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(header().string("Location", "/api/menu-groups/1"))
+                .andExpect(jsonPath("$.id", Matchers.instanceOf(Number.class)))
+                .andExpect(jsonPath("$.name", Matchers.instanceOf(String.class)));
+    }
+
+    @DisplayName("메뉴 그룹 목록을 조회하는 요청 테스트")
+    @Test
+    void list() throws Exception {
+        List<MenuGroup> menus = new ArrayList<>();
+        menus.add(TestObjectFactory.createMenuGroup("name1"));
+        menus.add(TestObjectFactory.createMenuGroup("name2"));
+        given(menuGroupService.list()).willReturn(menus);
+
+        mockMvc.perform(get("/api/menu-groups"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", Matchers.hasSize(2)));
+    }
+}
