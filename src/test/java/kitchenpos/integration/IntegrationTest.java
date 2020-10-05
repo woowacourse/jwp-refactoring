9ebc9ca23f77e420a44a372c0cc2df60fb152afd
aegis <aegis1920@gmail.com>
2020-10-05 12:55:46 +0900

    refactor: 통합테스트 간 테스트 격리를 위해 truncate.sql, init-data.sql 추가
    - data.sql -> init-data.sql로 변경

--- src/test/java/kitchenpos/integration/IntegrationTest.java
+++ src/test/java/kitchenpos/integration/IntegrationTest.java
@@ -5,8 +5,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.jdbc.Sql;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@Sql({"/truncate.sql", "/init-data.sql"})
 public abstract class IntegrationTest {
 
     @LocalServerPort
