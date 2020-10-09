9c557f07837c22a33d710d5ddc769bdecb968b47
minuyim <minu_yim@naver.com>
2020-10-10 03:56:48 +0900

    test: MenuGroupServiceTest 구현

---
+++ src/test/java/kitchenpos/application/MenuGroupServiceTest.java
@@ -0,0 +1,50 @@
+package kitchenpos.application;
+
+import static org.mockito.BDDMockito.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import kitchenpos.dao.MenuGroupDao;
+import kitchenpos.domain.MenuGroup;
+
+@ExtendWith(MockitoExtension.class)
+class MenuGroupServiceTest {
+    @InjectMocks
+    private MenuGroupService menuGroupService;
+
+    @Mock
+    private MenuGroupDao menuGroupDao;
+
+    @DisplayName("메뉴 그룹을 추가한다.")
+    @Test
+    void create() {
+        MenuGroup menuGroup = new MenuGroup();
+        menuGroup.setName("test_group");
+
+        MenuGroup savedMenuGroup = new MenuGroup();
+        savedMenuGroup.setId(1L);
+        savedMenuGroup.setName(menuGroup.getName());
+
+        given(menuGroupDao.save(any(MenuGroup.class))).willReturn(savedMenuGroup);
+
+        menuGroupService.create(menuGroup);
+
+        verify(menuGroupDao).save(menuGroup);
+    }
+
+    @DisplayName("메뉴 그룹 전체 목록을 조회한다.")
+    @Test
+    void list() {
+        menuGroupService.list();
+
+        verify(menuGroupDao).findAll();
+    }
+}
