cb49edf200d0a9e63f9dcc5c7c6f87cb6bd1cdac
김예림 <kiel0103@naver.com>
2020-10-11 01:32:21 +0900

    test: 테이블 그룹화 및 그룹 해제 테스트 작성

--- src/test/java/kitchenpos/acceptance/TableGroupAcceptanceTest.java
+++ src/test/java/kitchenpos/acceptance/TableGroupAcceptanceTest.java
@@ -1,8 +1,33 @@
 package kitchenpos.acceptance;
 
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
-class TableGroupAcceptanceTest {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import kitchenpos.domain.OrderTable;
+import kitchenpos.domain.TableGroup;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+class TableGroupAcceptanceTest extends AcceptanceTest {
+
+    private OrderTable tableA;
+    private OrderTable tableB;
+    private OrderTable tableC;
+
+    @BeforeEach
+    void setUp() {
+        tableA = createTable(0, true);
+        tableB = createTable(0, true);
+        tableC = createTable(0, true);
+    }
 
     /**
      * Feature: 테이블 그룹을 관리한다. Scenario: 테이블들을 그룹짓고, 그룹 헤제한다.
@@ -14,8 +39,60 @@ class TableGroupAcceptanceTest {
      * When 그룹지은 테이블을 그룹 헤제한다. Then 테이블 그룹이 해제된다.
      */
     @Test
+    @DisplayName("테이블 그룹 관리")
     void manageTableGroup() {
+        // 테이블 그룹짓기
+        TableGroup tableGroup = groupTables(Arrays.asList(tableA, tableB, tableC));
 
+        assertThat(tableGroup.getId()).isNotNull();
+        assertThatOrderTableBelongsTableGroup(tableA, tableGroup);
+        assertThatOrderTableBelongsTableGroup(tableB, tableGroup);
+        assertThatOrderTableBelongsTableGroup(tableC, tableGroup);
+
+        // 테이블 그룹 헤제하기
+        ungroup(tableGroup);
+    }
+
+    private void assertThatOrderTableBelongsTableGroup(OrderTable table, TableGroup tableGroup) {
+        List<Long> tableIds = tableGroup.getOrderTables()
+            .stream()
+            .map(OrderTable::getId)
+            .collect(Collectors.toList());
+        assertThat(tableIds).contains(table.getId());
+    }
+
+    private TableGroup groupTables(List<OrderTable> orderTables) {
+        Map<String, Object> body = new HashMap<>();
+
+        List<Map> tablesForGroupingRequest = new ArrayList<>();
+
+        for (OrderTable orderTable : orderTables) {
+            Map<String, Object> tableForGroupingRequest = new HashMap<>();
+            tableForGroupingRequest.put("id", orderTable.getId());
+
+            tablesForGroupingRequest.add(tableForGroupingRequest);
+        }
+        body.put("orderTables", tablesForGroupingRequest);
+
+        return
+            given()
+                .body(body)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+            .when()
+                .post("/api/table-groups")
+            .then()
+                .statusCode(HttpStatus.CREATED.value())
+                .extract().as(TableGroup.class);
+    }
+
+    private void ungroup(TableGroup tableGroup) {
+        given()
+            .accept(MediaType.APPLICATION_JSON_VALUE)
+        .when()
+            .delete("/api/table-groups/" + tableGroup.getId())
+        .then()
+            .statusCode(HttpStatus.NO_CONTENT.value());
     }
 
     /**
