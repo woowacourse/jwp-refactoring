cbdc9a8d87122480834ddbb7114d2516dca8ae76
kuenhwi <kueniyam@naver.com>
2020-10-11 17:24:49 +0900

    test: 메뉴 그룹 등록 서비스 테스트

---
+++ src/test/java/kitchenpos/service/MenuGroupServiceTest.java
@@ -0,0 +1,39 @@
+package kitchenpos.service;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+
+import kitchenpos.domain.MenuGroup;
+
+@DisplayName("MenuGroupService 단위 테스트")
+@SpringBootTest
+@Sql({"/truncate.sql", "/test_data.sql"})
+class MenuGroupServiceTest {
+
+	@Autowired
+	MenuGroupService menuGroupService;
+
+	@DisplayName("메뉴 그룹을 등록할 수 있다.")
+	@Test
+	void create() {
+		MenuGroup menuGroup = new MenuGroup();
+		menuGroup.setName("추천메뉴");
+
+		MenuGroup created = menuGroupService.create(menuGroup);
+
+		assertAll(
+			() -> assertThat(created.getId()).isEqualTo(5),
+			() -> assertThat(created.getName()).isEqualTo("추천메뉴")
+		);
+	}
+
+	@Test
+	void list() {
+	}
+}
