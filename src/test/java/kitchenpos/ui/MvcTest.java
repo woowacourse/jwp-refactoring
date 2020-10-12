e5691bb189f42d1c0e5df559a56bee7ffe47dc55
kimseonggyu1 <luckykimsk@gmail.com>
2020-10-12 12:10:01 +0900

    test: WebMvc 테스트 추상 클래스 작성

---
+++ src/test/java/kitchenpos/ui/MvcTest.java
@@ -0,0 +1,49 @@
+package kitchenpos.ui;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public abstract class MvcTest {
+
+    protected ObjectMapper objectMapper = new ObjectMapper();
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext ctx;
+
+    @BeforeEach
+    void setUpEncoding() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(ctx)
+            .addFilters(new CharacterEncodingFilter("UTF-8", true))
+            .alwaysDo(print())
+            .build();
+    }
+
+    protected ResultActions getAction(String url) throws Exception {
+        return mockMvc.perform(get(url));
+    }
+
+    protected ResultActions postAction(String url) throws Exception {
+        return mockMvc.perform(post(url));
+    }
+
+    protected ResultActions deleteAction(String url) throws Exception {
+        return mockMvc.perform(delete(url));
+    }
+
+    protected ResultActions putAction(String url) throws Exception {
+        return mockMvc.perform(put(url));
+    }
+}
