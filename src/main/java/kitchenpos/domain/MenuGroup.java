465739dee7604cdb1c6b60d0fe824c058bfd3eca
ordinCode <ysh1346@naver.com>
2020-10-15 12:57:10 +0900

    refactor: MenuGroupDao 에 jpa적용

--- src/main/java/kitchenpos/domain/MenuGroup.java
+++ src/main/java/kitchenpos/domain/MenuGroup.java
@@ -1,6 +1,14 @@
 package kitchenpos.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class MenuGroup {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
