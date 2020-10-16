f8b20de34a5856cdef68c2922075be2b6946586b
kouz <lastpuzly@gmail.com>
2020-10-16 18:17:12 +0900

    test: Table 테이블 주문 여부 변경 인수테스트 작성

--- src/test/java/kitchenpos/AcceptanceTest.java
+++ src/test/java/kitchenpos/AcceptanceTest.java
@@ -1,7 +1,6 @@
 package kitchenpos;
 
 import static java.util.Objects.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -13,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
@@ -48,9 +48,13 @@ public class AcceptanceTest {
         return Long.parseLong(id);
     }
 
-    protected Long create(String request, String uri) throws Exception {
+    protected <T> T getLastItem(List<T> items) {
+        return items.get(items.size() - 1);
+    }
+
+    protected Long post(String request, String uri) throws Exception {
         return extractId(requireNonNull(
-                mockMvc.perform(post(uri)
+                mockMvc.perform(MockMvcRequestBuilders.post(uri)
                         .content(request)
                         .contentType(MediaType.APPLICATION_JSON))
                         .andExpect(status().isCreated())
@@ -59,16 +63,28 @@ public class AcceptanceTest {
                         .getHeader(LOCATION)));
     }
 
-    protected <T> List<T> showAll(Class<T> clazz, String uri) throws Exception {
-        String content = mockMvc.perform(get(uri)
+    protected <T> List<T> getAll(Class<T> clazz, String uri) throws Exception {
+        String response = mockMvc.perform(MockMvcRequestBuilders.get(uri)
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andReturn()
                 .getResponse()
                 .getContentAsString();
 
-        return objectMapper.readValue(content,
-                objectMapper.getTypeFactory()
-                        .constructCollectionType(List.class, clazz));
+        return objectMapper.readValue(response, objectMapper.getTypeFactory()
+                .constructCollectionType(List.class, clazz));
+    }
+
+    protected <T> T put(Class<T> clazz, String request, String uri) throws Exception {
+        String response = mockMvc.perform(MockMvcRequestBuilders.put(uri)
+                .content(request)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        return objectMapper.readValue(response, clazz);
     }
 }
--- src/test/java/kitchenpos/table/TableAcceptanceTest.java
+++ src/test/java/kitchenpos/table/TableAcceptanceTest.java
@@ -21,22 +21,33 @@ public class TableAcceptanceTest extends AcceptanceTest {
      * When 테이블을 생성 요청.
      * Then 테이블이 생성 된다.
      * <p>
+     * Given 테이블이 생성 되어 있다.
      * When 테이블 전체 조회 요청.
      * Then 전체 테이블을 반환한다.
+     * <p>
+     * When 테이블 주문 여부 변경 요청.
+     * Then 변경된 테이블 정보를 반환한다.
      */
     @DisplayName("테이블 관리")
     @TestFactory
     Stream<DynamicTest> manageTable() throws Exception {
         // 테이블 생성
-        Long tableId = createTable();
-        assertThat(tableId).isNotNull();
+        Long orderTableId = createTable();
+        assertThat(orderTableId).isNotNull();
 
         return Stream.of(
                 dynamicTest("테이블 전체 조회", () -> {
-                    List<OrderTable> tables = showTables();
-                    OrderTable lastTable = tables.get(tables.size() - 1);
+                    List<OrderTable> tables = getAll(OrderTable.class, API_TABLES);
+                    OrderTable lastTable = getLastItem(tables);
 
-                    assertThat(lastTable.getId()).isEqualTo(tableId);
+                    assertThat(lastTable.getId()).isEqualTo(orderTableId);
+                }),
+                dynamicTest("테이블 주문 여부 변경", () -> {
+                    boolean empty = false;
+                    OrderTable orderTable = changeEmpty(empty, orderTableId);
+
+                    assertThat(orderTable.getId()).isEqualTo(orderTableId);
+                    assertThat(orderTable.isEmpty()).isEqualTo(empty);
                 })
         );
     }
@@ -47,10 +58,14 @@ public class TableAcceptanceTest extends AcceptanceTest {
         orderTable.setEmpty(true);
 
         String request = objectMapper.writeValueAsString(orderTable);
-        return create(request, API_TABLES);
+        return post(request, API_TABLES);
     }
 
-    private List<OrderTable> showTables() throws Exception {
-        return showAll(OrderTable.class, API_TABLES);
+    private OrderTable changeEmpty(boolean empty, Long orderTableId) throws Exception {
+        OrderTable orderTable = new OrderTable();
+        orderTable.setEmpty(empty);
+
+        String request = objectMapper.writeValueAsString(orderTable);
+        return put(OrderTable.class, request, API_TABLES + "/" + orderTableId + "/empty");
     }
 }
