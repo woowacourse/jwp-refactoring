07e0f5194189f53563486708fb3178c4097c7de3
yeonnseok <ljinsk3@gmail.com>
2020-10-20 02:55:15 +0900

    refactor: 불필요한 주석 제거 및 줄바꿈

--- src/main/java/kitchenpos/domain/repository/OrderRepository.java
+++ src/main/java/kitchenpos/domain/repository/OrderRepository.java
@@ -9,10 +9,17 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
-//    SELECT e.name, CASE WHEN (e.salary >= 100000) THEN 1 WHEN (e.salary < 100000) THEN 2 ELSE 0 END FROM Employee e
-    @Query("SELECT CASE WHEN COUNT(o) > 0 THEN TRUE ELSE FALSE END FROM Order o WHERE o.orderTable.id = (:orderTableId) AND o.orderStatus IN (:orderStatuses)")
-    boolean existsByOrderTableIdAndOrderStatusIn(@Param("orderTableId") Long orderTableId, @Param("orderStatuses") List<OrderStatus> orderStatuses);
+    @Query("SELECT CASE WHEN COUNT(o) > 0 THEN TRUE ELSE FALSE END FROM Order o "
+            + "WHERE o.orderTable.id = (:orderTableId) AND o.orderStatus IN (:orderStatuses)")
+    boolean existsByOrderTableIdAndOrderStatusIn(
+            @Param("orderTableId") Long orderTableId,
+            @Param("orderStatuses") List<OrderStatus> orderStatuses
+    );
 
-    @Query("SELECT CASE WHEN COUNT(o) > 0 THEN TRUE ELSE FALSE END FROM Order o WHERE o.orderTable.id IN (:orderTableIds) AND o.orderStatus IN (:orderStatuses)")
-    boolean existsByOrderTableIdInAndOrderStatusIn(@Param("orderTableIds") List<Long> orderTableIds, @Param("orderStatuses") List<OrderStatus> orderStatuses);
+    @Query("SELECT CASE WHEN COUNT(o) > 0 THEN TRUE ELSE FALSE END FROM Order o "
+            + "WHERE o.orderTable.id IN (:orderTableIds) AND o.orderStatus IN (:orderStatuses)")
+    boolean existsByOrderTableIdInAndOrderStatusIn(
+            @Param("orderTableIds") List<Long> orderTableIds,
+            @Param("orderStatuses") List<OrderStatus> orderStatuses
+    );
 }
