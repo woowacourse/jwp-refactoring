e427fbe8d91b44c66b2ab6303b4de28dd5055a67
lowoon <sangbo0229@gmail.com>
2020-10-19 11:36:35 +0900

    refactor: order service test use object util

--- src/test/java/kitchenpos/application/OrderServiceTest.java
+++ src/test/java/kitchenpos/application/OrderServiceTest.java
@@ -1,15 +1,20 @@
 package kitchenpos.application;
 
+import static kitchenpos.util.ObjectUtil.*;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
 
 import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -41,20 +46,12 @@ class OrderServiceTest {
     private OrderService orderService;
 
     @Test
-    @DisplayName("생성")
+    @DisplayName("주문 생성")
     void create() {
-        Order order = new Order();
-        order.setId(1L);
-        order.setOrderTableId(3L);
-        OrderLineItem orderLineItem = new OrderLineItem();
-        orderLineItem.setMenuId(2L);
-        order.setOrderLineItems(Collections.singletonList(orderLineItem));
-        OrderTable orderTable = new OrderTable();
-        orderTable.setEmpty(false);
-        Order saved = new Order();
-        saved.setId(1L);
-        saved.setOrderTableId(3L);
-        saved.setOrderStatus(OrderStatus.COOKING.name());
+        OrderLineItem orderLineItem = createOrderLineItem(null, null, 2L, 0);
+        Order order = createOrder(1L, 3L, null, null, Collections.singletonList(orderLineItem));
+        OrderTable orderTable = createOrderTable(null, null, 0, false);
+        Order saved = createOrder(1L, 3L, OrderStatus.COOKING.name(), null, Collections.emptyList());
 
         given(menuDao.countByIdIn(Collections.singletonList(2L))).willReturn(1L);
         given(orderTableDao.findById(3L)).willReturn(Optional.of(orderTable));
@@ -72,25 +69,38 @@ class OrderServiceTest {
     }
 
     @Test
-    @DisplayName("수량이 있는 메뉴가 null이거나 존재하지 않는 경우")
-    void orderLineItemDoesNotExist() {
-        Order order = new Order();
-        assertThatThrownBy(() -> orderService.create(order))
-            .isInstanceOf(IllegalArgumentException.class);
+    @DisplayName("주문 목록 조회")
+    void list() {
+        OrderLineItem orderLineItem = createOrderLineItem(null, 1L, 2L, 0);
+        Order order = createOrder(1L, 3L, null, null, Collections.singletonList(orderLineItem));
 
-        order.setOrderLineItems(Collections.emptyList());
+        given(orderDao.findAll()).willReturn(Collections.singletonList(order));
+        given(orderLineItemDao.findAllByOrderId(1L)).willReturn(Collections.singletonList(orderLineItem));
+
+        List<Order> result = orderService.list();
+
+        assertThat(result).isNotNull();
+        assertAll(
+            () -> assertThat(result).hasSize(1),
+            () -> assertThat(result.get(0)).usingRecursiveComparison()
+                .isEqualTo(order)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideOrderLineItems")
+    @DisplayName("수량이 있는 메뉴가 null이거나 존재하지 않는 경우 예외 발생")
+    void orderLineItemDoesNotExist(List<OrderLineItem> input) {
+        Order order = createOrder(null, null, null, null, input);
         assertThatThrownBy(() -> orderService.create(order))
             .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
-    @DisplayName("주문에 포함된 메뉴와 등록된 메뉴에서 조회한 것이 다른 경우")
+    @DisplayName("주문에 포함된 메뉴와 등록된 메뉴에서 조회한 것이 다른 경우 예외 발생")
     void differentCountOfOrderLineItemAndMenu() {
-        Order order = new Order();
-        order.setId(1L);
-        OrderLineItem orderLineItem = new OrderLineItem();
-        orderLineItem.setMenuId(2L);
-        order.setOrderLineItems(Collections.singletonList(orderLineItem));
+        OrderLineItem orderLineItem = createOrderLineItem(null, null, 2L, 0);
+        Order order = createOrder(1L, null, null, null, Collections.singletonList(orderLineItem));
 
         given(menuDao.countByIdIn(Collections.singletonList(2L))).willReturn(0L);
 
@@ -99,15 +109,11 @@ class OrderServiceTest {
     }
 
     @Test
-    @DisplayName("주문 테이블이 빈 경우")
+    @DisplayName("주문 테이블이 빈 경우 예외 발생")
     void emptyOrderTable() {
-        Order order = new Order();
-        order.setId(1L);
-        order.setOrderTableId(3L);
-        OrderLineItem orderLineItem = new OrderLineItem();
-        orderLineItem.setMenuId(2L);
-        order.setOrderLineItems(Collections.singletonList(orderLineItem));
-        OrderTable orderTable = new OrderTable();
+        OrderLineItem orderLineItem = createOrderLineItem(null, null, 2L, 0);
+        Order order = createOrder(1L, 3L, null, null, Collections.singletonList(orderLineItem));
+        OrderTable orderTable = createOrderTable(null, null, 0, true);
         orderTable.setEmpty(true);
 
         given(menuDao.countByIdIn(Collections.singletonList(2L))).willReturn(1L);
@@ -115,5 +121,12 @@ class OrderServiceTest {
 
         assertThatThrownBy(() -> orderService.create(order))
             .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    private static Stream<List<OrderLineItem>> provideOrderLineItems() {
+        return Stream.of(
+            null,
+            Collections.emptyList()
+        );
     }
 }
--- src/test/java/kitchenpos/util/ObjectUtil.java
+++ src/test/java/kitchenpos/util/ObjectUtil.java
@@ -1,10 +1,14 @@
 package kitchenpos.util;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import kitchenpos.domain.Menu;
 import kitchenpos.domain.MenuProduct;
+import kitchenpos.domain.Order;
+import kitchenpos.domain.OrderLineItem;
+import kitchenpos.domain.OrderTable;
 import kitchenpos.domain.Product;
 
 public class ObjectUtil {
@@ -38,5 +42,34 @@ public class ObjectUtil {
             product.setPrice(BigDecimal.valueOf(price));
         }
         return product;
+    }
+
+    public static Order createOrder(Long id, Long orderTableId, String orderStatus, LocalDateTime orderDateTime,
+        List<OrderLineItem> orderLineItems) {
+        Order order = new Order();
+        order.setId(id);
+        order.setOrderTableId(orderTableId);
+        order.setOrderStatus(orderStatus);
+        order.setOrderedTime(orderDateTime);
+        order.setOrderLineItems(orderLineItems);
+        return order;
+    }
+
+    public static OrderLineItem createOrderLineItem(Long seq, Long orderId, Long menuId, long quantity) {
+        OrderLineItem orderLineItem = new OrderLineItem();
+        orderLineItem.setSeq(seq);
+        orderLineItem.setOrderId(orderId);
+        orderLineItem.setMenuId(menuId);
+        orderLineItem.setQuantity(quantity);
+        return orderLineItem;
+    }
+
+    public static OrderTable createOrderTable(Long id, Long tableGroupId, int numberOfGuests, boolean empty) {
+        OrderTable orderTable = new OrderTable();
+        orderTable.setId(id);
+        orderTable.setTableGroupId(tableGroupId);
+        orderTable.setNumberOfGuests(numberOfGuests);
+        orderTable.setEmpty(empty);
+        return orderTable;
     }
 }
