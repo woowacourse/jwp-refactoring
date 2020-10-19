041b08b961a29e1f27a7d01786cf7c1f34a42772
taejinseok <stj930@gmail.com>
2020-10-19 22:22:50 +0900

    test: menu-group service 테스트 추가

---
+++ src/test/java/kitchenpos/application/MenuGroupServiceTest.java
@@ -0,0 +1,43 @@
+package kitchenpos.application;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import kitchenpos.domain.MenuGroup;
+
+@SpringBootTest
+@Transactional
+class MenuGroupServiceTest {
+    @Autowired
+    private MenuGroupService menuGroupService;
+
+    @DisplayName("create: 메뉴_그룹 생성 요청시, 입력 받은 이름을 기반으로 생성 하면, ID 생성 및 입력 값을 통해 생성된다.")
+    @Test
+    void create() {
+        final MenuGroup menuGroup = new MenuGroup();
+        final String menuGroupName = "치킨_두마리_세트";
+        menuGroup.setName(menuGroupName);
+
+        final MenuGroup createdMenuGroup = menuGroupService.create(menuGroup);
+        assertAll(
+                () -> assertThat(createdMenuGroup.getId()).isNotNull(),
+                () -> assertThat(createdMenuGroup.getName()).isEqualTo("치킨_두마리_세트")
+        );
+    }
+
+    @DisplayName("list: 현재 저장 되어 있는 메뉴_그룹의 목록을 반환한다.")
+    @Test
+    void list() {
+        final List<MenuGroup> menuGroups = menuGroupService.list();
+
+        assertThat(menuGroups).hasSize(4);
+    }
+}
