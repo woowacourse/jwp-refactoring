0ddb94b73f8b95152de7d4590fe0e42ffd91612b
hongbin23 <khbasd@gmail.com>
2020-10-19 23:56:55 +0900

    test: TableGroupServiceTest 테스트에서 mock 제거

--- src/test/java/kitchenpos/application/TableGroupServiceTest.java
+++ src/test/java/kitchenpos/application/TableGroupServiceTest.java
@@ -3,46 +3,40 @@ package kitchenpos.application;
 import static kitchenpos.helper.EntityCreateHelper.*;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.BDDMockito.*;
 
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.internal.verification.VerificationModeFactory;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 
 import kitchenpos.dao.OrderDao;
 import kitchenpos.dao.OrderTableDao;
 import kitchenpos.dao.TableGroupDao;
+import kitchenpos.domain.Order;
+import kitchenpos.domain.OrderStatus;
 import kitchenpos.domain.OrderTable;
 import kitchenpos.domain.TableGroup;
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+@Sql(value = "/truncate.sql")
 class TableGroupServiceTest {
 
+    @Autowired
     private TableGroupService tableGroupService;
 
-    @Mock
+    @Autowired
     private OrderTableDao orderTableDao;
 
-    @Mock
+    @Autowired
     private TableGroupDao tableGroupDao;
 
-    @Mock
+    @Autowired
     private OrderDao orderDao;
-
-    @BeforeEach
-    void setUp() {
-        tableGroupService = new TableGroupService(orderDao, orderTableDao, tableGroupDao);
-    }
 
     @DisplayName("주문 테이블이 비어있거나 1개일 시 단체지정을 하면 예외를 발생한다.")
     @Test
@@ -63,9 +57,12 @@ class TableGroupServiceTest {
     @DisplayName("주문 테이블이 중복되거나 없을 시 단체지정을 하면 예외를 발생한다.")
     @Test
     void createWhenOrderTableIsNullOrDuplicated() {
-        OrderTable orderTable = createOrderTable(1L, true, null, 3);
+        //todo
+        OrderTable orderTable = createOrderTable(null, true, null, 3);
         TableGroup tableGroup = createTableGroup(1L, LocalDateTime.now(), Arrays.asList(orderTable, orderTable));
-        given(orderTableDao.findAllByIdIn(anyList())).willReturn(Arrays.asList(orderTable));
+
+        orderTableDao.save(orderTable);
+
         assertThatThrownBy(
             () -> tableGroupService.create(tableGroup)
         ).isInstanceOf(IllegalArgumentException.class);
@@ -94,43 +91,69 @@ class TableGroupServiceTest {
     @DisplayName("단체 지정을 한다.")
     @Test
     void create() {
-        OrderTable orderTable = createOrderTable(1L, true, null, 3);
-        OrderTable orderTable2 = createOrderTable(2L, true, null, 3);
-        TableGroup tableGroup = createTableGroup(1L, LocalDateTime.now(), Arrays.asList(orderTable, orderTable2));
+        //todo
+        OrderTable orderTable = createOrderTable(null, true, null, 3);
+        OrderTable orderTable2 = createOrderTable(null, true, null, 3);
 
-        given(orderTableDao.findAllByIdIn(anyList())).willReturn(Arrays.asList(orderTable, orderTable2));
-        given(tableGroupDao.save(any(TableGroup.class))).willReturn(tableGroup);
-        given(orderTableDao.save(any(OrderTable.class))).willReturn(orderTable);
+        OrderTable savedOrderTable1 = orderTableDao.save(orderTable);
+        OrderTable savedOrderTable2 = orderTableDao.save(orderTable2);
+
+        TableGroup tableGroup = createTableGroup(null, LocalDateTime.now(),
+            Arrays.asList(savedOrderTable1, savedOrderTable2));
 
         TableGroup savedTableGroup = tableGroupService.create(tableGroup);
 
         assertAll(
-            () -> assertThat(savedTableGroup.getId()).isEqualTo(1L),
-            () -> assertThat(savedTableGroup.getOrderTables()).containsAll(Arrays.asList(orderTable, orderTable2))
+            () -> assertThat(savedTableGroup.getId()).isNotNull(),
+            () -> assertThat(savedTableGroup.getOrderTables()).hasSize(2)
         );
     }
 
     @DisplayName("주문 테이블 중 주문이 안 들어가거나 계산 완료되지 않은 테이블이 있을 경우 단체지정 해제할 때 예외를 발생한다.")
     @Test
-    void test() {
-        given(orderDao.existsByOrderTableIdInAndOrderStatusIn(anyList(), anyList())).willReturn(true);
+    void ungroupWhenNotStatusOrderCompletion() {
+        OrderTable orderTable = createOrderTable(null, true, null, 3);
+        OrderTable orderTable2 = createOrderTable(null, true, null, 3);
+
+        OrderTable savedOrderTable1 = orderTableDao.save(orderTable);
+        OrderTable savedOrderTable2 = orderTableDao.save(orderTable2);
+
+        TableGroup tableGroup = createTableGroup(null, LocalDateTime.now(),
+            Arrays.asList(savedOrderTable1, savedOrderTable2));
+
+        TableGroup savedTableGroup = tableGroupService.create(tableGroup);
+
+        Order order = createOrder(null, LocalDateTime.now(), Collections.emptyList(), OrderStatus.MEAL,
+            savedOrderTable1.getId());
+        orderDao.save(order);
+
         assertThatThrownBy(
-            () -> tableGroupService.ungroup(1L)
+            () -> tableGroupService.ungroup(savedTableGroup.getId())
         ).isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("단체 지정을 해제한다.")
     @Test
     void ungroup() {
-        OrderTable orderTable = createOrderTable(1L, true, 1L, 3);
-        OrderTable orderTable2 = createOrderTable(2L, true, 1L, 3);
+        OrderTable orderTable = createOrderTable(null, true, null, 3);
+        OrderTable orderTable2 = createOrderTable(null, true, null, 3);
 
-        List<OrderTable> orderTables = Arrays.asList(orderTable, orderTable2);
-        given(orderDao.existsByOrderTableIdInAndOrderStatusIn(anyList(), anyList())).willReturn(false);
+        OrderTable savedOrderTable1 = orderTableDao.save(orderTable);
+        OrderTable savedOrderTable2 = orderTableDao.save(orderTable2);
 
-        tableGroupService.ungroup(1L);
+        TableGroup tableGroup = createTableGroup(null, LocalDateTime.now(),
+            Arrays.asList(savedOrderTable1, savedOrderTable2));
 
-        verify(orderTableDao, VerificationModeFactory.atMost(orderTables.size()))
-            .save(any());
+        TableGroup savedTableGroup = tableGroupService.create(tableGroup);
+
+        Order order = createOrder(null, LocalDateTime.now(), Collections.emptyList(), OrderStatus.COMPLETION,
+            savedOrderTable1.getId());
+        orderDao.save(order);
+
+        tableGroupService.ungroup(savedTableGroup.getId());
+
+        OrderTable actual = orderTableDao.findById(savedOrderTable1.getId()).get();
+
+        assertThat(actual.getTableGroupId()).isNull();
     }
 }
