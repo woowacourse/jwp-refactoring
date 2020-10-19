659091110201fb3674bdbca551069fbe4e92a420
yejiahn <yeppilisa@naver.com>
2020-10-19 15:19:04 +0900

    test: MenuGroupService 테스트에서 mock 제거

--- src/test/java/kitchenpos/application/MenuGroupServiceTest.java
+++ src/test/java/kitchenpos/application/MenuGroupServiceTest.java
@@ -1,43 +1,29 @@
 package kitchenpos.application;
 
-import static kitchenpos.helper.EntityCreateHelper.*;
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.BDDMockito.*;
+import static kitchenpos.helper.EntityCreateHelper.createMenuGroup;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
-import java.util.Arrays;
 import java.util.List;
-
-import org.junit.jupiter.api.BeforeEach;
+import kitchenpos.domain.MenuGroup;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 
-import kitchenpos.dao.MenuGroupDao;
-import kitchenpos.domain.MenuGroup;
-
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+@Sql(value = "/truncate.sql")
 class MenuGroupServiceTest {
 
+    @Autowired
     private MenuGroupService menuGroupService;
-
-    @Mock
-    private MenuGroupDao menuGroupDao;
-
-    @BeforeEach
-    void setUp() {
-        menuGroupService = new MenuGroupService(menuGroupDao);
-    }
 
     @DisplayName("메뉴 그룹을 등록한다.")
     @Test
     void createMenu() {
         MenuGroup menuGroup = createMenuGroup(1L, "음료");
 
-        given(menuGroupDao.save(any(MenuGroup.class))).willReturn(menuGroup);
         MenuGroup savedMenuGroup = menuGroupService.create(menuGroup);
 
         assertAll(
@@ -51,8 +37,7 @@ class MenuGroupServiceTest {
     void list() {
         MenuGroup menuGroup = createMenuGroup(1L, "음료");
 
-        given(menuGroupDao.findAll()).willReturn(Arrays.asList(menuGroup));
-
+        MenuGroup savedMenuGroup = menuGroupService.create(menuGroup);
         List<MenuGroup> menuGroups = menuGroupService.list();
 
         assertAll(
