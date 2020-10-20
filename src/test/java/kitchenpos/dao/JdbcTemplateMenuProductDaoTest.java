90644793257a63c432e765bd8d7b9c59603d59cb
황준호 <42054054+hwanghe159@users.noreply.github.com>
2020-10-20 14:35:40 +0900

    test: JdbcTemplateMenuProductDaoTest 작성

---
+++ src/test/java/kitchenpos/dao/JdbcTemplateMenuProductDaoTest.java
@@ -0,0 +1,42 @@
+package kitchenpos.dao;
+
+import kitchenpos.domain.MenuProduct;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+class JdbcTemplateMenuProductDaoTest extends DaoTest {
+
+    @DisplayName("전체조회 테스트")
+    @Test
+    void findAllTest() {
+        List<MenuProduct> menuProducts = jdbcTemplateMenuProductDao.findAll();
+
+        assertAll(
+            () -> assertThat(menuProducts).hasSize(2),
+            () -> assertThat(menuProducts.get(0)).usingRecursiveComparison().isEqualTo(MENU_PRODUCT_1),
+            () -> assertThat(menuProducts.get(1)).usingRecursiveComparison().isEqualTo(MENU_PRODUCT_2)
+        );
+    }
+
+    @DisplayName("단건조회 예외 테스트: id에 해당하는 메뉴가 존재하지 않을때")
+    @Test
+    void findByIdFailByNotExistTest() {
+        Optional<MenuProduct> menuProduct = jdbcTemplateMenuProductDao.findById(-1L);
+
+        assertThat(menuProduct).isEmpty();
+    }
+
+    @DisplayName("단건조회 테스트")
+    @Test
+    void findByIdTest() {
+        MenuProduct menuProduct = jdbcTemplateMenuProductDao.findById(MENU_PRODUCT_SEQ_1).get();
+
+        assertThat(menuProduct).usingRecursiveComparison().isEqualTo(MENU_PRODUCT_1);
+    }
+}
