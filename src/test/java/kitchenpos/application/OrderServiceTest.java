efb8cceda0f779ec2150be75e8b0ad8d2b21c539
taejinseok <stj930@gmail.com>
2020-10-20 16:29:29 +0900

    test: 주문 추가 예외 상황, 주문 상태 변경 테스트 케이스 추가

--- src/test/java/kitchenpos/application/OrderServiceTest.java
+++ src/test/java/kitchenpos/application/OrderServiceTest.java
@@ -7,6 +7,7 @@ import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.List;
 
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import kitchenpos.dao.OrderDao;
+import kitchenpos.dao.OrderLineItemDao;
 import kitchenpos.dao.OrderTableDao;
 import kitchenpos.domain.Order;
 import kitchenpos.domain.OrderLineItem;
@@ -32,6 +34,9 @@ class OrderServiceTest {
     @Autowired
     private OrderDao orderDao;
 
+    @Autowired
+    private OrderLineItemDao orderLineItemDao;
+
     @DisplayName("list: 전체 주문 목록을 조회한다.")
     @Test
     void list() {
@@ -49,14 +54,17 @@ class OrderServiceTest {
         order.setOrderStatus(OrderStatus.COOKING.name());
         order.setOrderTableId(savedTable.getId());
         order.setOrderLineItems(Collections.singletonList(orderLineItem));
-        orderDao.save(order);
+        final Order savedOrder = orderDao.save(order);
+        final Long savedOrderId = savedOrder.getId();
+        orderLineItem.setOrderId(savedOrderId);
+        orderLineItemDao.save(orderLineItem);
 
         final List<Order> list = orderService.list();
 
         assertThat(list).hasSize(1);
     }
 
-    @DisplayName("create: 주문을 생성한다.")
+    @DisplayName("create: 점유중인 테이블에서 메뉴 중복이 없는 하나 이상의 상품 주문시, 주문 추가 후, 생성된 주문 객체를 반환한다.")
     @Test
     void create() {
         final OrderLineItem orderLineItem = new OrderLineItem();
@@ -69,8 +77,6 @@ class OrderServiceTest {
         final OrderTable savedTable = orderTableDao.save(orderTable);
 
         final Order order = new Order();
-        order.setOrderedTime(LocalDateTime.of(2020, 10, 10, 20, 30));
-        order.setOrderStatus(OrderStatus.COOKING.name());
         order.setOrderTableId(savedTable.getId());
         order.setOrderLineItems(Collections.singletonList(orderLineItem));
 
@@ -83,5 +89,131 @@ class OrderServiceTest {
                 () -> assertThat(savedOrder.getOrderTableId()).isEqualTo(savedTable.getId()),
                 () -> assertThat(savedOrder.getOrderLineItems()).hasSize(1)
         );
+    }
+
+    @DisplayName("create: 점유중인 테이블에서 주문 상품이 없는 경우, 주문 추가 실패 후, IllegalArgumentException 발생")
+    @Test
+    void create_fail_if_order_contains_no_order_line_item() {
+        final OrderTable orderTable = new OrderTable();
+        orderTable.setEmpty(false);
+        orderTable.setNumberOfGuests(5);
+        final OrderTable savedTable = orderTableDao.save(orderTable);
+
+        final Order order = new Order();
+        order.setOrderTableId(savedTable.getId());
+        order.setOrderLineItems(Collections.emptyList());
+
+        assertThatThrownBy(() -> orderService.create(order))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("create: 점유중이 아닌 테이블에서 주문 요청시, 주문 추가 실패 후, IllegalArgumentException 발생")
+    @Test
+    void create_fail_if_table_is_empty() {
+        final OrderLineItem orderLineItem = new OrderLineItem();
+        orderLineItem.setMenuId(1L);
+        orderLineItem.setQuantity(3);
+
+        final OrderTable orderTable = new OrderTable();
+        orderTable.setEmpty(true);
+        orderTable.setNumberOfGuests(0);
+        final OrderTable savedTable = orderTableDao.save(orderTable);
+
+        final Order order = new Order();
+        order.setOrderTableId(savedTable.getId());
+        order.setOrderLineItems(Collections.singletonList(orderLineItem));
+
+        assertThatThrownBy(() -> orderService.create(order))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("create: 점유중인 테이블에서 중복된 중복 메뉴를 포함한 상품 들 주문 요청시, 주문 추가 실패 후, IllegalArgumentException 발생")
+    @Test
+    void create_fail_if_order_line_item_contains_duplicate_menu() {
+        final OrderLineItem firstOrderLineItem = new OrderLineItem();
+        firstOrderLineItem.setMenuId(1L);
+        firstOrderLineItem.setQuantity(1);
+
+        final OrderLineItem secondOrderLineItem = new OrderLineItem();
+        secondOrderLineItem.setMenuId(1L);
+        secondOrderLineItem.setQuantity(2);
+
+        final OrderTable orderTable = new OrderTable();
+        orderTable.setEmpty(true);
+        orderTable.setNumberOfGuests(0);
+        final OrderTable savedTable = orderTableDao.save(orderTable);
+
+        final Order order = new Order();
+        order.setOrderTableId(savedTable.getId());
+        order.setOrderLineItems(Lists.list(firstOrderLineItem, secondOrderLineItem));
+
+        assertThatThrownBy(() -> orderService.create(order))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("changeOrderStatus: 완료 되지 않는 주문의 경우, 주문 상태의 변경 요청시, 상태 변경 후, 변경된 주문 객체를 반환한다.")
+    @Test
+    void changeOrderStatus() {
+        final OrderLineItem orderLineItem = new OrderLineItem();
+        orderLineItem.setMenuId(1L);
+        orderLineItem.setQuantity(3);
+
+        final OrderTable orderTable = new OrderTable();
+        orderTable.setEmpty(false);
+        orderTable.setNumberOfGuests(5);
+        final OrderTable savedTable = orderTableDao.save(orderTable);
+
+        final Order order = new Order();
+        order.setOrderTableId(savedTable.getId());
+        order.setOrderLineItems(Collections.singletonList(orderLineItem));
+        order.setOrderStatus(OrderStatus.COOKING.name());
+        order.setOrderedTime(LocalDateTime.of(2020, 10, 10, 20, 40));
+
+        final Order savedOrder = orderDao.save(order);
+        final Long savedOrderId = savedOrder.getId();
+        orderLineItem.setOrderId(savedOrderId);
+        orderLineItemDao.save(orderLineItem);
+
+        final Order newOrder = new Order();
+        newOrder.setOrderStatus(OrderStatus.MEAL.name());
+
+        final Order updatedOrder = orderService.changeOrderStatus(savedOrderId, newOrder);
+        assertAll(
+                () -> assertThat(updatedOrder.getId()).isEqualTo(savedOrderId),
+                () -> assertThat(updatedOrder.getOrderedTime()).isNotNull(),
+                () -> assertThat(updatedOrder.getOrderStatus()).isEqualTo("MEAL"),
+                () -> assertThat(updatedOrder.getOrderTableId()).isEqualTo(savedTable.getId()),
+                () -> assertThat(updatedOrder.getOrderLineItems()).hasSize(1)
+        );
+    }
+
+    @DisplayName("changeOrderStatus: 이미 완료한 주문의 상태의 변경 요청시, 상태 변경 실패 후, IllegalArgumentException 발생.")
+    @Test
+    void changeOrderStatus_fail_if_order_status_is_completion() {
+        final OrderLineItem orderLineItem = new OrderLineItem();
+        orderLineItem.setMenuId(1L);
+        orderLineItem.setQuantity(3);
+
+        final OrderTable orderTable = new OrderTable();
+        orderTable.setEmpty(false);
+        orderTable.setNumberOfGuests(5);
+        final OrderTable savedTable = orderTableDao.save(orderTable);
+
+        final Order order = new Order();
+        order.setOrderTableId(savedTable.getId());
+        order.setOrderLineItems(Collections.singletonList(orderLineItem));
+        order.setOrderStatus(OrderStatus.COMPLETION.name());
+        order.setOrderedTime(LocalDateTime.of(2020, 10, 10, 20, 40));
+
+        final Order savedOrder = orderDao.save(order);
+        final Long savedOrderId = savedOrder.getId();
+        orderLineItem.setOrderId(savedOrderId);
+        orderLineItemDao.save(orderLineItem);
+
+        final Order newOrder = new Order();
+        newOrder.setOrderStatus(OrderStatus.MEAL.name());
+
+        assertThatThrownBy(() -> orderService.changeOrderStatus(savedOrderId, newOrder))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
