88123d9c13cd414cbdd29340866c0b77216f5d97
황준호 <42054054+hwanghe159@users.noreply.github.com>
2020-10-20 14:23:49 +0900

    test: DaoTest, JdbcTemplateMenuDaoTest 작성

--- src/test/java/kitchenpos/dao/JdbcTemplateProductDaoTest.java
+++ src/test/java/kitchenpos/dao/JdbcTemplateProductDaoTest.java
@@ -1,36 +1,16 @@
 package kitchenpos.dao;
 
 import kitchenpos.domain.Product;
-import kitchenpos.fixture.TestFixture;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
-import org.springframework.test.context.jdbc.Sql;
 
-import javax.sql.DataSource;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
-@JdbcTest
-@Sql({"classpath:truncate.sql"})
-class JdbcTemplateProductDaoTest extends TestFixture {
-
-    private JdbcTemplateProductDao jdbcTemplateProductDao;
-
-    @Autowired
-    private DataSource dataSource;
-
-    @BeforeEach
-    void setUp() {
-        jdbcTemplateProductDao = new JdbcTemplateProductDao(dataSource);
-
-        jdbcTemplateProductDao.save(PRODUCT_1);
-        jdbcTemplateProductDao.save(PRODUCT_2);
-    }
+class JdbcTemplateProductDaoTest extends DaoTest {
 
     @DisplayName("전체조회 테스트")
     @Test
@@ -42,6 +22,14 @@ class JdbcTemplateProductDaoTest extends TestFixture {
             () -> assertThat(products.get(0)).usingRecursiveComparison().isEqualTo(PRODUCT_1),
             () -> assertThat(products.get(1)).usingRecursiveComparison().isEqualTo(PRODUCT_2)
         );
+    }
+
+    @DisplayName("단건조회 예외 테스트: id에 해당하는 제품이 존재하지 않을때")
+    @Test
+    void findByIdFailByNotExistTest() {
+        Optional<Product> product = jdbcTemplateProductDao.findById(-1L);
+
+        assertThat(product).isEmpty();
     }
 
     @DisplayName("단건조회 테스트")
