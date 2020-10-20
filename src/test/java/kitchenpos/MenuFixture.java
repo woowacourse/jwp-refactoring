7a4c61fcc0b9c56f2a6eb5700546f74fc8483560
hj <a301dks@naver.com>
2020-10-20 17:34:54 +0900

    feat: implement menu fixture for test

---
+++ src/test/java/kitchenpos/MenuFixture.java
@@ -0,0 +1,56 @@
+package kitchenpos;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import kitchenpos.domain.Menu;
+import kitchenpos.domain.MenuProduct;
+
+public class MenuFixture {
+    public static Menu createMenuWithoutId() {
+        final Menu menu = new Menu();
+        final MenuProduct menuProduct = new MenuProduct();
+        menu.setName("후라이드+후라이드");
+        menu.setPrice(BigDecimal.valueOf(19000));
+        menu.setMenuGroupId(1L);
+        menuProduct.setProductId(1L);
+        menuProduct.setQuantity(2);
+        menu.setMenuProducts(new ArrayList<>(Collections.singletonList(menuProduct)));
+
+        return menu;
+    }
+
+    public static Menu createMenuWithId(final Long id) {
+        final Menu menu = new Menu();
+        final MenuProduct menuProduct = new MenuProduct();
+        menu.setId(id);
+        menu.setName("후라이드+후라이드");
+        menu.setPrice(BigDecimal.valueOf(19000));
+        menu.setMenuGroupId(1L);
+        menuProduct.setProductId(1L);
+        menuProduct.setQuantity(2);
+        menu.setMenuProducts(new ArrayList<>(Collections.singletonList(menuProduct)));
+
+        return menu;
+    }
+
+    public static Menu createMinusPriceMenu() {
+        final Menu menu = new Menu();
+        final MenuProduct menuProduct = new MenuProduct();
+        menu.setName("후라이드+후라이드");
+        menu.setPrice(BigDecimal.valueOf(-10000));
+        menu.setMenuGroupId(1L);
+        menuProduct.setProductId(1L);
+        menuProduct.setQuantity(2);
+        menu.setMenuProducts(new ArrayList<>(Collections.singletonList(menuProduct)));
+
+        return menu;
+    }
+
+    public static List<Menu> createMenuList() {
+        return Arrays.asList(createMenuWithId(1L), createMenuWithId(2L));
+    }
+}
