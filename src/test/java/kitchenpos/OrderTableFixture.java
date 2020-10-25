093b3dc37f3e89d5a8b97a8b488a6a9795fc59a0
hj <a301dks@naver.com>
2020-10-25 14:35:03 +0900

    feat: implement order table fixture

---
+++ src/test/java/kitchenpos/OrderTableFixture.java
@@ -0,0 +1,31 @@
+package kitchenpos;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+
+import kitchenpos.domain.OrderTable;
+
+public class OrderTableFixture {
+    public static OrderTable createOrderTableWithoutId() {
+        final OrderTable orderTable = new OrderTable();
+        orderTable.setId(null);
+        orderTable.setTableGroupId(null);
+        orderTable.setNumberOfGuests(2);
+        orderTable.setEmpty(false);
+        return orderTable;
+    }
+
+    public static OrderTable createOrderTableWithId(final Long orderTableId) {
+        final OrderTable orderTable = new OrderTable();
+        orderTable.setId(orderTableId);
+        orderTable.setTableGroupId(null);
+        orderTable.setNumberOfGuests(2);
+        orderTable.setEmpty(false);
+        return orderTable;
+    }
+
+    public static List<OrderTable> createOrderTables() {
+        return Arrays.asList(createOrderTableWithId(1L), createOrderTableWithId(2L));
+    }
+}
