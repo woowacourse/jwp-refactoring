f34fc2996e4e568f7632b38be311d31808d22255
hj <a301dks@naver.com>
2020-10-25 14:17:41 +0900

    test: add ProductRestControllerTest

---
+++ src/test/java/kitchenpos/ui/ProductRestControllerTest.java
@@ -0,0 +1,71 @@
+package kitchenpos.ui;
+
+import static kitchenpos.ProductFixture.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import kitchenpos.application.ProductService;
+import kitchenpos.domain.Product;
+
+@WebMvcTest(ProductRestController.class)
+class ProductRestControllerTest {
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private ProductService productService;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp(WebApplicationContext webApplicationContext) {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+            .addFilters(new CharacterEncodingFilter("UTF-8", true))
+            .alwaysDo(print())
+            .build();
+    }
+
+    @DisplayName("정상적인 상품 생성 요청에 created 상태로 응답하는지 확인한다.")
+    @Test
+    void createTest() throws Exception {
+        final Product savedProduct = createProductWithId(1L);
+        given(productService.create(any(Product.class))).willReturn(savedProduct);
+
+        mockMvc.perform(post("/api/products")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsBytes(createProductWithoutId()))
+        )
+            .andExpect(status().isCreated())
+            .andExpect(content().bytes(objectMapper.writeValueAsBytes(savedProduct)))
+            .andExpect(header().exists("Location"));
+    }
+
+    @DisplayName("정상적인 상품 리스트 요청에 ok상태로 응답하는지 확인한다.")
+    @Test
+    void listTest() throws Exception {
+        final List<Product> products = createProducts();
+        given(productService.list()).willReturn(products);
+
+        mockMvc.perform(get("/api/products"))
+            .andExpect(status().isOk())
+            .andExpect(content().bytes(objectMapper.writeValueAsBytes(products)));
+    }
+}
