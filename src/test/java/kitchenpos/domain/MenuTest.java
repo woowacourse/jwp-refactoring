534b9c1bd321ccfcc351282d07ece51485969362
yeonnseok <ljinsk3@gmail.com>
2020-10-28 01:06:37 +0900

    test: Menu 테스트 추가

--- src/test/java/kitchenpos/domain/MenuTest.java
+++ src/test/java/kitchenpos/domain/MenuTest.java
@@ -3,17 +3,34 @@ package kitchenpos.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 class MenuTest {
 
-    @DisplayName("Menu 생성 실패 - ")
+    @DisplayName("Menu 생성 실패 - 가격이 null일 경우")
     @Test
-    public void createFail() {
+    public void createFailPriceNull() {
+        assertThatThrownBy(() -> new Menu("파스타", null, new MenuGroup("양식")))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 
+    @DisplayName("Menu 생성 실패 - 가격이 0보다 작을 경우")
+    @Test
+    public void createFailPriceMinus() {
+        assertThatThrownBy(() -> new Menu("파스타", BigDecimal.valueOf(-1), new MenuGroup("양식")))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("Menu 생성")
     @Test
     public void createMenu() {
+        Menu menu = new Menu("파스타", BigDecimal.valueOf(10_000L), new MenuGroup("양식"));
 
+        assertThat(menu).isNotNull();
+        assertThat(menu.getPrice()).isEqualByComparingTo(BigDecimal.valueOf(10_000L));
+        assertThat(menu.getMenuGroup().getName()).isEqualTo("양식");
     }
 }
