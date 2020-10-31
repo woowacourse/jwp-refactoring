29670abb52709a51f43a356be6ad821701a5c693
김예림 <kiel0103@naver.com>
2020-10-31 16:03:51 +0900

    feat: Product 생성자 추가

--- src/main/java/kitchenpos/domain/Product.java
+++ src/main/java/kitchenpos/domain/Product.java
@@ -1,11 +1,36 @@
 package kitchenpos.domain;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public class Product {
+
     private Long id;
     private String name;
     private BigDecimal price;
+
+    public Product() {
+
+    }
+
+    public Product(String name, BigDecimal price) {
+        validateName(name);
+        validatePrice(price);
+        this.name = name;
+        this.price = price;
+    }
+
+    private void validateName(String name) {
+        if (Objects.isNull(name)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validatePrice(BigDecimal price) {
+        if (Objects.isNull(price) || price.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException();
+        }
+    }
 
     public Long getId() {
         return id;
