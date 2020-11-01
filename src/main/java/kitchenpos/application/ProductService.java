f22d114cb158b8f81b96c063080ac8addc12518f
juyeongjung <juyeong.dev@gmail.com>
2020-11-01 14:56:57 +0900

    refactor: ProductResponse 객체 분리

--- src/main/java/kitchenpos/application/ProductService.java
+++ src/main/java/kitchenpos/application/ProductService.java
@@ -1,9 +1,11 @@
 package kitchenpos.application;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import kitchenpos.dao.ProductDao;
 import kitchenpos.domain.Product;
 import kitchenpos.dto.ProductRequest;
+import kitchenpos.dto.ProductResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,7 +22,9 @@ public class ProductService {
         return productDao.save(request.toEntity());
     }
 
-    public List<Product> list() {
-        return productDao.findAll();
+    public List<ProductResponse> list() {
+        return productDao.findAll().stream()
+            .map(ProductResponse::of)
+            .collect(Collectors.toList());
     }
 }
