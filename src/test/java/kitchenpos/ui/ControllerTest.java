3a3c672193b064f917f12d68e06ec69b5026bcb8
Eun Seok <jeseok95@gmail.com>
2020-11-03 16:55:35 +0900

    feat: ProductRestControllerTest 통합테스트 구현

---
+++ src/test/java/kitchenpos/ui/ControllerTest.java
@@ -0,0 +1,37 @@
+package kitchenpos.ui;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@SpringBootTest
+@Transactional
+@AutoConfigureMockMvc
+public class ControllerTest {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    protected MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .addFilter(new CharacterEncodingFilter("UTF-8", true))
+                .alwaysDo(print())
+                .build();
+    }
+}
---
+++ src/test/java/kitchenpos/ui/ProductRestControllerTest.java
@@ -0,0 +1,57 @@
+package kitchenpos.ui;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
+
+import kitchenpos.common.TestObjectUtils;
+import kitchenpos.domain.Product;
+
+class ProductRestControllerTest extends ControllerTest {
+
+    @DisplayName("상품을 등록할 수 있다.")
+    @Test
+    void createTest() throws Exception {
+        String createProduct = objectMapper.writeValueAsString(
+                TestObjectUtils.createProduct(null, "참치뱃살", BigDecimal.valueOf(30000)));
+
+        MvcResult mvcResult = mockMvc.perform(
+                post("/api/products")
+                        .content(createProduct)
+                        .contentType(MediaType.APPLICATION_JSON)
+        )
+                .andExpect(status().isCreated())
+                .andReturn();
+
+        Product product = objectMapper.readValue(mvcResult.getResponse().getContentAsString(),
+                Product.class);
+
+        assertThat(product.getId()).isEqualTo(7L);
+    }
+
+    @DisplayName("상품을 조회 할 수 있다.")
+    @Test
+    void listTest() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(
+                get("/api/products")
+        )
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andReturn();
+
+        List<Product> products = objectMapper.readValue(
+                mvcResult.getResponse().getContentAsString(),
+                objectMapper.getTypeFactory().constructCollectionType(List.class, Product.class));
+
+        assertThat(products.size()).isEqualTo(6);
+    }
+}
