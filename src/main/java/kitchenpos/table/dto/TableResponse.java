d16b971c5899a91690001ec2eecafb274ea4afd1
hongbin23 <khbasd@gmail.com>
2020-11-03 12:24:12 +0900

    fix: TableGroup이 비어있음을 고려하여 TableREsponse 수정

--- src/main/java/kitchenpos/table/dto/TableResponse.java
+++ src/main/java/kitchenpos/table/dto/TableResponse.java
@@ -1,33 +1,50 @@
 package kitchenpos.table.dto;
 
+import java.time.LocalDateTime;
+import java.util.Objects;
+
 import kitchenpos.table.domain.Table;
 
 public class TableResponse {
 
     private final Long id;
-    private final TableGroupResponse tableGroupResponse;
+    private final Long tableId;
+    private final LocalDateTime tableCreateDate;
     private final Integer tableNumberOfGuests;
     private final boolean empty;
 
-    private TableResponse(Long id, TableGroupResponse tableGroupResponse, Integer tableNumberOfGuests, boolean empty) {
+    public TableResponse(Long id, Long tableId, LocalDateTime tableCreateDate, Integer tableNumberOfGuests,
+        boolean empty) {
         this.id = id;
-        this.tableGroupResponse = tableGroupResponse;
+        this.tableId = tableId;
+        this.tableCreateDate = tableCreateDate;
         this.tableNumberOfGuests = tableNumberOfGuests;
         this.empty = empty;
     }
 
     public static TableResponse from(Table table) {
-        TableGroupResponse tableGroupResponse = TableGroupResponse.from(table.getTableGroup());
-        return new TableResponse(table.getId(), tableGroupResponse,
+        Long tableId = null;
+        LocalDateTime tableCreatedDate = null;
+
+        if (Objects.nonNull(table.getTableGroup())) {
+            tableId = table.getTableGroup().getId();
+            tableCreatedDate = table.getTableGroup().getCreatedDate();
+        }
+
+        return new TableResponse(table.getId(), tableId, tableCreatedDate,
             table.getTableNumberOfGuests().getNumberOfGuests(), table.getTableEmpty().getEmpty());
+    }
+
+    public Long getTableId() {
+        return tableId;
+    }
+
+    public LocalDateTime getTableCreateDate() {
+        return tableCreateDate;
     }
 
     public Long getId() {
         return id;
-    }
-
-    public TableGroupResponse getTableGroupReponse() {
-        return tableGroupResponse;
     }
 
     public Integer getTableNumberOfGuests() {
