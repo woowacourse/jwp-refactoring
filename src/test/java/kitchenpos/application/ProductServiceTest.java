92f17395d19f07ef07b3c6b5379be408155f881c
dd <dqrd123@gmail.com>
2020-11-04 14:59:26 +0900

    feat: ProductServiceTest

---
+++ src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -0,0 +1,82 @@
+package kitchenpos.application;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import kitchenpos.dao.ProductDao;
+import kitchenpos.domain.Product;
+
+@ExtendWith(MockitoExtension.class)
+class ProductServiceTest {
+
+    private ProductService productService;
+
+    @Mock
+    private ProductDao productDao;
+
+    private Product product1;
+    private Product product2;
+    private Product nullPriceProduct;
+    private Product negativePriceProduct;
+
+    @BeforeEach
+    void setUp() {
+        productService = new ProductService(productDao);
+        product1 = new Product();
+        product1.setName("chicken");
+        product1.setPrice(BigDecimal.valueOf(18000));
+
+        product2 = new Product();
+        product2.setId(1L);
+        product2.setName("chicken");
+        product2.setPrice(BigDecimal.valueOf(18000));
+
+        nullPriceProduct = new Product();
+        nullPriceProduct.setId(2L);
+        nullPriceProduct.setName("pizza");
+
+        negativePriceProduct = new Product();
+        negativePriceProduct.setId(3L);
+        negativePriceProduct.setName("coke");
+        product2.setPrice(BigDecimal.valueOf(-1000));
+    }
+
+    @DisplayName("정상적으로 Product를 생성한다.")
+    @Test
+    void create() {
+        when(productDao.save(product1)).thenReturn(product2);
+
+        assertThat(productService.create(product1))
+            .isEqualToComparingFieldByField(product2);
+    }
+
+    @DisplayName("가격이 null이거나 음수인 Product를 생성요청하면 예외를 반환한다.")
+    @Test
+    void createIllegalPriceProduct() {
+        assertThatThrownBy(()->productService.create(nullPriceProduct))
+            .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(()->productService.create(negativePriceProduct))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("정상적으로 저장된 Product를 불러온다.")
+    @Test
+    void list() {
+        when(productDao.findAll()).thenReturn(Arrays.asList(product1, product2));
+
+        assertThat(productService.list())
+            .usingRecursiveComparison()
+            .isEqualTo(Arrays.asList(product1, product2));
+    }
+}
