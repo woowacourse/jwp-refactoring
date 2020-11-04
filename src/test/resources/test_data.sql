ea6349ab50e8f8ee8a8df4fe067debb948898369
toneyparky <qkrtmddhks95@gmail.com>
2020-11-04 12:55:37 +0900

    feat: 테스트 기본 데이터를 위한 sql 및 interface 추가

---
+++ src/test/resources/test_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO product (id, name, price) VALUES (1, '후라이드', 16000);
+INSERT INTO product (id, name, price) VALUES (2, '양념치킨', 16000);
+INSERT INTO product (id, name, price) VALUES (3, '반반치킨', 16000);
+INSERT INTO product (id, name, price) VALUES (4, '통구이', 16000);
+INSERT INTO product (id, name, price) VALUES (5, '간장치킨', 17000);
+INSERT INTO product (id, name, price) VALUES (6, '순살치킨', 17000);
