4cacf04a21f5afd02b83a25fbaf50b8b1fb9927b
icyMojito <dev.icymojito@gmail.com>
2020-11-07 21:53:57 +0900

    feat: MenuGroup 관련 요청과 응답을 담당하는 dto 객체 생성

---
+++ src/main/java/kitchenpos/dto/MenuGroupCreateRequest.java
@@ -0,0 +1,25 @@
+package kitchenpos.dto;
+
+import kitchenpos.domain.MenuGroup;
+
+import javax.validation.constraints.NotBlank;
+
+public class MenuGroupCreateRequest {
+    @NotBlank(message = "메뉴 그룹의 이름은 반드시 존재해야 합니다!")
+    private String name;
+
+    public MenuGroupCreateRequest() {
+    }
+
+    public MenuGroupCreateRequest(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public MenuGroup toMenuGroup() {
+        return new MenuGroup(this.name);
+    }
+}
---
+++ src/main/java/kitchenpos/dto/MenuGroupResponse.java
@@ -0,0 +1,28 @@
+package kitchenpos.dto;
+
+import kitchenpos.domain.MenuGroup;
+
+public class MenuGroupResponse {
+    private Long id;
+    private String name;
+
+    public MenuGroupResponse(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public static MenuGroupResponse from(MenuGroup menuGroup) {
+        Long id = menuGroup.getId();
+        String name = menuGroup.getName();
+
+        return new MenuGroupResponse(id, name);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
