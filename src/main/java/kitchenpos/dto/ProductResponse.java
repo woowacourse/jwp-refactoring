bfdb213831d9014e40e65441c951d9266db7666f
icyMojito <dev.icymojito@gmail.com>
2020-11-09 22:11:16 +0900

    feat: Product 관련 요청과 응답을 담당하는 dto 객체 생성

---
+++ src/main/java/kitchenpos/dto/ProductResponse.java
@@ -0,0 +1,37 @@
+package kitchenpos.dto;
+
+import kitchenpos.domain.Product;
+
+import java.math.BigDecimal;
+
+public class ProductResponse {
+    private Long id;
+    private String name;
+    private BigDecimal price;
+
+    public ProductResponse(Long id, String name, BigDecimal price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public static ProductResponse from(Product product) {
+        Long id = product.getId();
+        String name = product.getName();
+        BigDecimal price = product.getPrice();
+
+        return new ProductResponse(id, name, price);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+}
