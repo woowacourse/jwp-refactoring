816eb1c6a1a0b5c13217e9504dfc90ba39c79000
JunSeong <ebseud6135@naver.com>
2020-11-09 23:56:54 +0900

    refactor: Jpa Repository로 수정

---
+++ src/main/java/kitchenpos/dao/OrderRepository.java
@@ -0,0 +1,7 @@
+package kitchenpos.dao;
+
+import kitchenpos.domain.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
