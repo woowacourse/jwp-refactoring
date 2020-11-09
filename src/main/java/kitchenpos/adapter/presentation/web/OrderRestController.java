4de4794bdef661b091c953dd85a93a76a6b6db94
kouz <lastpuzly@gmail.com>
2020-11-09 16:17:30 +0900

    refactor: OrderCreateRequest 생성

--- src/main/java/kitchenpos/adapter/presentation/web/OrderRestController.java
+++ src/main/java/kitchenpos/adapter/presentation/web/OrderRestController.java
@@ -3,19 +3,16 @@ package kitchenpos.adapter.presentation.web;
 import static kitchenpos.adapter.presentation.web.OrderRestController.*;
 
 import java.net.URI;
-import java.util.List;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import kitchenpos.application.OrderService;
-import kitchenpos.domain.entity.Order;
+import kitchenpos.application.dto.OrderCreateRequest;
+import kitchenpos.application.dto.OrderResponse;
 
 @RequestMapping(API_ORDERS)
 @RestController
@@ -29,26 +26,26 @@ public class OrderRestController {
     }
 
     @PostMapping
-    public ResponseEntity<Order> create(@RequestBody final Order order) {
-        final Order created = orderService.create(order);
+    public ResponseEntity<OrderResponse> create(@RequestBody final OrderCreateRequest order) {
+        final OrderResponse created = orderService.create(order);
         final URI uri = URI.create(API_ORDERS + "/" + created.getId());
         return ResponseEntity.created(uri)
                 .body(created)
                 ;
     }
 
-    @GetMapping
-    public ResponseEntity<List<Order>> list() {
-        return ResponseEntity.ok()
-                .body(orderService.list())
-                ;
-    }
-
-    @PutMapping("/{orderId}/order-status")
-    public ResponseEntity<Order> changeOrderStatus(
-            @PathVariable final Long orderId,
-            @RequestBody final Order order
-    ) {
-        return ResponseEntity.ok(orderService.changeOrderStatus(orderId, order));
-    }
+    // @GetMapping
+    // public ResponseEntity<List<Order>> list() {
+    //     return ResponseEntity.ok()
+    //             .body(orderService.list())
+    //             ;
+    // }
+    //
+    // @PutMapping("/{orderId}/order-status")
+    // public ResponseEntity<Order> changeOrderStatus(
+    //         @PathVariable final Long orderId,
+    //         @RequestBody final Order order
+    // ) {
+    //     return ResponseEntity.ok(orderService.changeOrderStatus(orderId, order));
+    // }
 }
--- src/main/java/kitchenpos/domain/entity/Order.java
+++ src/main/java/kitchenpos/domain/entity/Order.java
@@ -4,51 +4,50 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import kitchenpos.domain.OrderLineItem;
+import kitchenpos.domain.OrderStatus;
+import kitchenpos.domain.service.OrderCreateService;
 
 public class Order {
-    private Long id;
-    private Long orderTableId;
+    private final Long id;
+    private final Long orderTableId;
     private String orderStatus;
     private LocalDateTime orderedTime;
-    private List<OrderLineItem> orderLineItems;
+    private final List<OrderLineItem> orderLineItems;
+
+    public Order(Long id, Long orderTableId, String orderStatus, LocalDateTime orderedTime,
+            List<OrderLineItem> orderLineItems) {
+        this.id = id;
+        this.orderTableId = orderTableId;
+        this.orderStatus = orderStatus;
+        this.orderedTime = orderedTime;
+        this.orderLineItems = orderLineItems;
+    }
+
+    public Order create(OrderCreateService orderCreateService) {
+        orderCreateService.validate(orderLineItems, orderTableId);
+
+        this.orderStatus = OrderStatus.COOKING.name();
+        this.orderedTime = LocalDateTime.now();
+        return this;
+    }
 
     public Long getId() {
         return id;
-    }
-
-    public void setId(final Long id) {
-        this.id = id;
     }
 
     public Long getOrderTableId() {
         return orderTableId;
     }
 
-    public void setOrderTableId(final Long orderTableId) {
-        this.orderTableId = orderTableId;
-    }
-
     public String getOrderStatus() {
         return orderStatus;
-    }
-
-    public void setOrderStatus(final String orderStatus) {
-        this.orderStatus = orderStatus;
     }
 
     public LocalDateTime getOrderedTime() {
         return orderedTime;
     }
 
-    public void setOrderedTime(final LocalDateTime orderedTime) {
-        this.orderedTime = orderedTime;
-    }
-
     public List<OrderLineItem> getOrderLineItems() {
         return orderLineItems;
-    }
-
-    public void setOrderLineItems(final List<OrderLineItem> orderLineItems) {
-        this.orderLineItems = orderLineItems;
     }
 }
