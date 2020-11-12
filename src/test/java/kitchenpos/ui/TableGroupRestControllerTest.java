48062def04d89c0c3f7b975346ac0f60fcf0bfd2
lalize <printf@kakao.com>
2020-11-12 19:47:02 +0900

    feat(test): add TableGroupRestControllerTest

---
+++ src/test/java/kitchenpos/ui/TableGroupRestControllerTest.java
@@ -0,0 +1,77 @@
+package kitchenpos.ui;
+
+import static kitchenpos.fixture.OrderTableFixture.createOrderTable;
+import static kitchenpos.fixture.TableGroupFixture.createTableGroup;
+import static kitchenpos.fixture.TableGroupFixture.createTableGroupRequest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+import kitchenpos.application.TableGroupService;
+import kitchenpos.dao.OrderTableDao;
+import kitchenpos.dao.TableGroupDao;
+import kitchenpos.domain.OrderTable;
+import kitchenpos.domain.TableGroup;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+
+public class TableGroupRestControllerTest extends AbstractControllerTest {
+    @Autowired
+    private OrderTableDao orderTableDao;
+
+    @Autowired
+    private TableGroupDao tableGroupDao;
+
+    @Autowired
+    private TableGroupService tableGroupService;
+
+    @DisplayName("단체 지정을 생성할 수 있다.")
+    @Test
+    void create() throws Exception {
+        List<OrderTable> orderTables = Arrays.asList(
+            orderTableDao.save(createOrderTable(null, true, 0, null)),
+            orderTableDao.save(createOrderTable(null, true, 0, null))
+        );
+
+        TableGroup tableGroupRequest = createTableGroupRequest(orderTables);
+
+        mockMvc.perform(post("/api/table-groups")
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .content(objectMapper.writeValueAsString(tableGroupRequest))
+        )
+            .andDo(print())
+            .andExpect(status().isCreated())
+            .andExpect(jsonPath("$.id").exists())
+            .andExpect(jsonPath("$.createdDate").exists())
+            .andExpect(jsonPath("$.orderTables").isNotEmpty());
+    }
+
+    @DisplayName("단체 지정을 해제할 수 있다.")
+    @Test
+    void ungroup() throws Exception {
+        List<OrderTable> orderTables = Arrays.asList(
+            orderTableDao.save(createOrderTable(null, true, 0, null)),
+            orderTableDao.save(createOrderTable(null, true, 0, null))
+        );
+        TableGroup tableGroup = tableGroupDao
+            .save(createTableGroup(null, LocalDateTime.now(), orderTables));
+        orderTables.forEach(it -> {
+            it.setTableGroupId(tableGroup.getId());
+            orderTableDao.save(it);
+        });
+
+        mockMvc.perform(delete("/api/table-groups/{id}", tableGroup.getId()))
+            .andDo(print())
+            .andExpect(status().isNoContent());
+
+        assertThat(orderTableDao.findAllByTableGroupId(tableGroup.getId())).isEmpty();
+    }
+}
