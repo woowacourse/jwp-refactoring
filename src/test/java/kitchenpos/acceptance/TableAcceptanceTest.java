869bc5c4a1a89196d85b70bc74a5c6b5c19d169d
lxxjn0 <ljy3510@gmail.com>
2020-11-13 00:54:58 +0900

    feat: TableAcceptanceTest 주문 테이블의 방문 손님 수 설정 시나리오 테스트 작성

--- src/test/java/kitchenpos/acceptance/TableAcceptanceTest.java
+++ src/test/java/kitchenpos/acceptance/TableAcceptanceTest.java
@@ -1,5 +1,6 @@
 package kitchenpos.acceptance;
 
+import static io.restassured.RestAssured.*;
 import static kitchenpos.ui.TableRestController.*;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -11,7 +12,10 @@ import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.TestFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import kitchenpos.domain.OrderTable;
 
 class TableAcceptanceTest extends AcceptanceTest {
@@ -87,7 +91,105 @@ class TableAcceptanceTest extends AcceptanceTest {
                                     .contains(createdOrderTable.getId())
                             ;
                         }
+                ),
+                dynamicTest(
+                        "주문 테이블을 빈 테이블로 설정한다",
+                        () -> {
+                            // Given
+                            final OrderTable orderTable = new OrderTable();
+                            orderTable.setNumberOfGuests(0);
+                            orderTable.setEmpty(false);
+
+                            final OrderTable createdOrderTable = create(TABLE_REST_API_URI,
+                                    orderTable, OrderTable.class);
+
+                            // When
+                            final OrderTable emptyOrderTable = new OrderTable();
+                            emptyOrderTable.setEmpty(true);
+
+                            final OrderTable changedOrderTable = changeEmpty(
+                                    createdOrderTable.getId(), emptyOrderTable);
+
+                            // Then
+                            assertAll(
+                                    () -> assertThat(changedOrderTable)
+                                            .extracting(OrderTable::getId)
+                                            .isEqualTo(createdOrderTable.getId())
+                                    ,
+                                    () -> assertThat(changedOrderTable)
+                                            .extracting(OrderTable::isEmpty)
+                                            .isEqualTo(true)
+                            );
+                        }
+                ),
+                dynamicTest(
+                        "주문 테이블의 방문한 손님 수를 설정한다",
+                        () -> {
+                            // Given
+                            final OrderTable orderTable = new OrderTable();
+                            orderTable.setNumberOfGuests(0);
+                            orderTable.setEmpty(false);
+
+                            final OrderTable createdOrderTable = create(TABLE_REST_API_URI,
+                                    orderTable, OrderTable.class);
+
+                            // When
+                            final OrderTable setOrderTable = new OrderTable();
+                            orderTable.setNumberOfGuests(4);
+
+                            final OrderTable changedOrderTable = changeNumberOfGuests(
+                                    createdOrderTable.getId(), setOrderTable);
+
+                            // Then
+                            assertAll(
+                                    () -> assertThat(changedOrderTable)
+                                            .extracting(OrderTable::getId)
+                                            .isEqualTo(createdOrderTable.getId())
+                                    ,
+                                    () -> assertThat(changedOrderTable)
+                                            .extracting(OrderTable::getNumberOfGuests)
+                                            .isEqualTo(setOrderTable.getNumberOfGuests())
+                            );
+                        }
                 )
         );
+    }
+
+    private OrderTable changeEmpty(final Long orderTableId, final OrderTable emptyOrderTable)
+            throws JsonProcessingException {
+        final String request = objectMapper.writeValueAsString(emptyOrderTable);
+
+        // @formatter:off
+        return
+                given()
+                        .accept(MediaType.APPLICATION_JSON_VALUE)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .body(request)
+                .when()
+                        .put(TABLE_REST_API_URI + "/{orderTableId}/empty", orderTableId)
+                .then()
+                        .log().all()
+                        .statusCode(HttpStatus.OK.value())
+                        .extract().as(OrderTable.class);
+        // @formatter:on
+    }
+
+    private OrderTable changeNumberOfGuests(final Long orderTableId, final OrderTable setOrderTable)
+            throws JsonProcessingException {
+        final String request = objectMapper.writeValueAsString(setOrderTable);
+
+        // @formatter:off
+        return
+                given()
+                        .accept(MediaType.APPLICATION_JSON_VALUE)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .body(request)
+                .when()
+                        .put(TABLE_REST_API_URI + "/{orderTableId}/number-of-guests", orderTableId)
+                .then()
+                        .log().all()
+                        .statusCode(HttpStatus.OK.value())
+                        .extract().as(OrderTable.class);
+        // @formatter:on
     }
 }
