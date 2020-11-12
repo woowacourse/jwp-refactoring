1cc57814d30bb7e6c3977ecbe5ad225232da2cc6
yebink <yebin825@gmail.com>
2020-11-12 14:14:04 +0900

    refactor : MenuRepository.countByIdIn 변경

--- src/main/java/kitchenpos/menu/repository/MenuRepository.java
+++ src/main/java/kitchenpos/menu/repository/MenuRepository.java
@@ -2,12 +2,9 @@ package kitchenpos.menu.repository;
 
 import kitchenpos.menu.domain.Menu;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface MenuRepository extends JpaRepository<Menu, Long> {
-    @Query("SELECT COUNT(m.id) FROM Menu m WHERE m.id IN (:menuIds)")
-    int countByIdIn(@Param(value = "menuIds") List<Long> menuIds);
+    long countByIdIn(List<Long> menuIds);
 }
