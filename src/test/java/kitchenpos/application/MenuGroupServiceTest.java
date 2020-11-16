bb3fea27d9bd85c11dc34007641ac9b9665b567a
taejinseok <stj930@gmail.com>
2020-11-16 22:39:10 +0900

    refactor: 메뉴 그룹 서비스 테스트에서 이름사이 underscore 제거

--- src/test/java/kitchenpos/application/MenuGroupServiceTest.java
+++ src/test/java/kitchenpos/application/MenuGroupServiceTest.java
@@ -20,22 +20,22 @@ class MenuGroupServiceTest {
     @Autowired
     private MenuGroupService menuGroupService;
 
-    @DisplayName("create: 메뉴_그룹 생성 요청시, 입력 받은 이름을 기반으로 생성 하면, ID 생성 및 입력 값을 통해 생성된다.")
+    @DisplayName("create: 메뉴 그룹 생성 요청시, 입력 받은 이름을 기반으로 생성 하면, ID 생성 및 입력 값을 통해 생성된다.")
     @Test
     void create() {
-        MenuGroup createdMenuGroup = menuGroupService.create(createMenuGroup("치킨_두마리_세트"));
+        MenuGroup createdMenuGroup = menuGroupService.create(createMenuGroup("치킨 두마리 세트"));
 
         assertAll(
                 () -> assertThat(createdMenuGroup.getId()).isNotNull(),
-                () -> assertThat(createdMenuGroup.getName()).isEqualTo("치킨_두마리_세트")
+                () -> assertThat(createdMenuGroup.getName()).isEqualTo("치킨 두마리 세트")
         );
     }
 
-    @DisplayName("list: 현재 저장 되어 있는 메뉴_그룹의 목록을 반환한다.")
+    @DisplayName("list: 현재 저장 되어 있는 메뉴그룹의 목록을 반환한다.")
     @Test
     void list() {
-        menuGroupService.create(createMenuGroup("치킨_세트"));
-        menuGroupService.create(createMenuGroup("치킨_단품"));
+        menuGroupService.create(createMenuGroup("치킨세트"));
+        menuGroupService.create(createMenuGroup("치킨단품"));
         List<MenuGroup> menuGroups = menuGroupService.list();
 
         assertThat(menuGroups).hasSize(2);
