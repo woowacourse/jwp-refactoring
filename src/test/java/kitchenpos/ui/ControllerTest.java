804dc6620e59a55bfe45607acdcb6e8808cd0fa7
taejinseok <stj930@gmail.com>
2020-11-16 22:35:43 +0900

    refactor: controller 통합 테스트 utf-8 인코딩 필터 적용

--- src/test/java/kitchenpos/ui/ControllerTest.java
+++ src/test/java/kitchenpos/ui/ControllerTest.java
@@ -12,12 +12,16 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD, scripts = "/truncate.sql")
 public abstract class ControllerTest {
+
+    private static final String UTF_8 = "UTF-8";
+
     private MockMvc mockMvc;
 
     @Autowired
@@ -26,6 +30,8 @@ public abstract class ControllerTest {
     @BeforeEach
     void setUp(WebApplicationContext webApplicationContext) {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .alwaysDo(print())
+                .addFilters(new CharacterEncodingFilter(UTF_8))
                 .build();
     }
 
@@ -33,28 +39,24 @@ public abstract class ControllerTest {
         return mockMvc.perform(post(url)
                 .accept(MediaType.APPLICATION_JSON)
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(entity)))
-                .andDo(print());
+                .content(objectMapper.writeValueAsString(entity)));
     }
 
     ResultActions findList(String url) throws Exception {
         return mockMvc.perform(get(url)
                 .accept(MediaType.APPLICATION_JSON)
-                .contentType(MediaType.APPLICATION_JSON))
-                .andDo(print());
+                .contentType(MediaType.APPLICATION_JSON));
     }
 
     <T> ResultActions updateByPathIdAndBody(String url, Long id, T body) throws Exception {
         return mockMvc.perform(put(url, id)
                 .accept(MediaType.APPLICATION_JSON)
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(body)))
-                .andDo(print());
+                .content(objectMapper.writeValueAsString(body)));
     }
 
     ResultActions deleteByPathId(String url, Long id) throws Exception {
-        return mockMvc.perform(delete(url, id))
-                .andDo(print());
+        return mockMvc.perform(delete(url, id));
     }
 }
 
