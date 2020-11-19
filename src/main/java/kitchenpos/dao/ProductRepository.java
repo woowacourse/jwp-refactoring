f6086f7cc7bef4f69d3b1b234f398fea64cfc7d8
Hamliet <hamliet@hanyang.ac.kr>
2020-11-20 06:35:48 +0900

    refactor: 코드 리팩토링

--- src/main/java/kitchenpos/dao/ProductRepository.java
+++ src/main/java/kitchenpos/dao/ProductRepository.java
@@ -17,54 +17,5 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-//    private static final String TABLE_NAME = "product";
-//    private static final String KEY_COLUMN_NAME = "id";
-//
-//    private final NamedParameterJdbcTemplate jdbcTemplate;
-//    private final SimpleJdbcInsert jdbcInsert;
-//
-//    public JdbcTemplateProductRepository(final DataSource dataSource) {
-//        jdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
-//        jdbcInsert = new SimpleJdbcInsert(dataSource)
-//                .withTableName(TABLE_NAME)
-//                .usingGeneratedKeyColumns(KEY_COLUMN_NAME)
-//        ;
-//    }
-//
-//    @Override
-//    public Product save(final Product entity) {
-//        final SqlParameterSource parameters = new BeanPropertySqlParameterSource(entity);
-//        final Number key = jdbcInsert.executeAndReturnKey(parameters);
-//        return select(key.longValue());
-//    }
-//
-//    @Override
-//    public Optional<Product> findById(final Long id) {
-//        try {
-//            return Optional.of(select(id));
-//        } catch (final EmptyResultDataAccessException e) {
-//            return Optional.empty();
-//        }
-//    }
-//
-//    @Override
-//    public List<Product> findAll() {
-//        final String sql = "SELECT id, name, price FROM product";
-//        return jdbcTemplate.query(sql, (resultSet, rowNumber) -> toEntity(resultSet));
-//    }
-//
-//    private Product select(final Long id) {
-//        final String sql = "SELECT id, name, price FROM product WHERE id = (:id)";
-//        final SqlParameterSource parameters = new MapSqlParameterSource()
-//                .addValue("id", id);
-//        return jdbcTemplate.queryForObject(sql, parameters, (resultSet, rowNumber) -> toEntity(resultSet));
-//    }
-//
-//    private Product toEntity(final ResultSet resultSet) throws SQLException {
-//        final Product entity = new Product();
-//        entity.setId(resultSet.getLong(KEY_COLUMN_NAME));
-//        entity.setName(resultSet.getString("name"));
-//        entity.setPrice(resultSet.getBigDecimal("price"));
-//        return entity;
-//    }
+
 }
--- src/main/java/kitchenpos/dao/TableGroupRepository.java
+++ src/main/java/kitchenpos/dao/TableGroupRepository.java
@@ -18,53 +18,5 @@ import java.util.List;
 import java.util.Optional;
 
 public interface TableGroupRepository extends JpaRepository<TableGroup, Long> {
-//    private static final String TABLE_NAME = "table_group";
-//    private static final String KEY_COLUMN_NAME = "id";
-//
-//    private final NamedParameterJdbcTemplate jdbcTemplate;
-//    private final SimpleJdbcInsert jdbcInsert;
-//
-//    public TableGroupRepository(final DataSource dataSource) {
-//        jdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
-//        jdbcInsert = new SimpleJdbcInsert(dataSource)
-//                .withTableName(TABLE_NAME)
-//                .usingGeneratedKeyColumns(KEY_COLUMN_NAME)
-//        ;
-//    }
-//
-//    @Override
-//    public TableGroup save(final TableGroup entity) {
-//        final SqlParameterSource parameters = new BeanPropertySqlParameterSource(entity);
-//        final Number key = jdbcInsert.executeAndReturnKey(parameters);
-//        return select(key.longValue());
-//    }
-//
-//    @Override
-//    public Optional<TableGroup> findById(final Long id) {
-//        try {
-//            return Optional.of(select(id));
-//        } catch (final EmptyResultDataAccessException e) {
-//            return Optional.empty();
-//        }
-//    }
-//
-//    @Override
-//    public List<TableGroup> findAll() {
-//        final String sql = "SELECT id, created_date FROM table_group";
-//        return jdbcTemplate.query(sql, (resultSet, rowNumber) -> toEntity(resultSet));
-//    }
-//
-//    private TableGroup select(final Long id) {
-//        final String sql = "SELECT id, created_date FROM table_group WHERE id = (:id)";
-//        final SqlParameterSource parameters = new MapSqlParameterSource()
-//                .addValue("id", id);
-//        return jdbcTemplate.queryForObject(sql, parameters, (resultSet, rowNumber) -> toEntity(resultSet));
-//    }
-//
-//    private TableGroup toEntity(final ResultSet resultSet) throws SQLException {
-//        final TableGroup entity = new TableGroup();
-//        entity.setId(resultSet.getLong(KEY_COLUMN_NAME));
-//        entity.setCreatedDate(resultSet.getObject("created_date", LocalDateTime.class));
-//        return entity;
-//    }
+
 }
