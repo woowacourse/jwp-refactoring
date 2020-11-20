e697a700035a4091fa449b264ac2c4b6421d9a9b
kimseonggyu1 <luckykimsk@gmail.com>
2020-11-20 16:25:49 +0900

    refactor: TableGroupService 그룹 해제 개선
    - OrderDao에 불필요한 메서드 제거
    - 커스텀 예외 생성
    - 도메인으로 이동 가능한 로직 이동

--- src/test/java/kitchenpos/dao/OrderDaoTest.java
+++ src/test/java/kitchenpos/dao/OrderDaoTest.java
@@ -1,16 +1,17 @@
 package kitchenpos.dao;
 
-import kitchenpos.domain.Order;
-import kitchenpos.domain.OrderStatus;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import kitchenpos.domain.Order;
+import kitchenpos.domain.OrderStatus;
 
 class OrderDaoTest extends DaoTest {
 
@@ -47,20 +48,21 @@ class OrderDaoTest extends DaoTest {
         );
     }
 
+    @DisplayName("테이블 id로 검색 테스트")
+    @Test
+    void findByTableIds() {
+        List<Order> byTableIds = orderDao.findByTableIds(Arrays.asList(TABLE_ID_1, TABLE_ID_2));
+
+        for (Order byTableId : byTableIds) {
+            System.out.println(byTableId.getId());
+        }
+    }
+
     @DisplayName("해당 테이블에 해당 상태들의 주문이 있는지 조회 테스트")
     @Test
     void existsByTableIdAndOrderStatusInTest() {
         boolean isExist = orderDao
             .existsByTableIdAndOrderStatusIn(TABLE_ID_1, Arrays.asList(ORDER_STATUS_1));
-
-        assertThat(isExist).isTrue();
-    }
-
-    @DisplayName("해당 테이블들에 해당 상태들의 주문이 있는지 조회 테스트")
-    @Test
-    void existsByTableIdInAndOrderStatusInTest() {
-        boolean isExist = orderDao
-            .existsByTableIdInAndOrderStatusIn(Arrays.asList(TABLE_ID_1), Arrays.asList(ORDER_STATUS_1));
 
         assertThat(isExist).isTrue();
     }
