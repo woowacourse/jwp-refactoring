66cc52ba436daaf16540a6bd1b0475250c4f1229
Hamliet <hamliet@hanyang.ac.kr>
2020-11-20 11:24:39 +0900

    refactor: 코드 리팩토링

--- src/main/java/kitchenpos/ui/ProductRestController.java
+++ src/main/java/kitchenpos/ui/ProductRestController.java
@@ -1,5 +1,7 @@
 package kitchenpos.ui;
 
+import java.net.URI;
+import java.util.List;
 import kitchenpos.application.ProductService;
 import kitchenpos.domain.Product;
 import org.springframework.http.ResponseEntity;
@@ -7,9 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.net.URI;
-import java.util.List;
 
 @RestController
 public class ProductRestController {
