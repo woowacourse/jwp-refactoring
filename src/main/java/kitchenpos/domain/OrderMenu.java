64dea16ef80a2febf3f4081344a9a2b8e08901be
황준호 <42054054+hwanghe159@users.noreply.github.com>
2020-11-20 10:42:47 +0900

    refactor: OrderMenu setter 제거

--- src/main/java/kitchenpos/domain/OrderMenu.java
+++ src/main/java/kitchenpos/domain/OrderMenu.java
@@ -15,35 +15,26 @@ public class OrderMenu {
         this.quantity = quantity;
     }
 
-    public Long getSeq() {
-        return seq;
+    public OrderMenu(Long seq, Long orderId, Long menuId, long quantity) {
+        this.seq = seq;
+        this.orderId = orderId;
+        this.menuId = menuId;
+        this.quantity = quantity;
     }
 
-    public void setSeq(final Long seq) {
-        this.seq = seq;
+    public Long getSeq() {
+        return seq;
     }
 
     public Long getOrderId() {
         return orderId;
     }
 
-    public void setOrderId(final Long orderId) {
-        this.orderId = orderId;
-    }
-
     public Long getMenuId() {
         return menuId;
     }
 
-    public void setMenuId(final Long menuId) {
-        this.menuId = menuId;
-    }
-
     public long getQuantity() {
         return quantity;
-    }
-
-    public void setQuantity(final long quantity) {
-        this.quantity = quantity;
     }
 }
