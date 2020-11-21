0ecffd41930da24984c447ef6dc6f3325bdc3c45
김예림 <kiel0103@naver.com>
2020-11-21 20:30:15 +0900

    refactor: 예측하지 못한 예외 발생시 에러로그를 남기고 500에러를 응답하도록 구현

--- src/main/java/kitchenpos/exception/GlobalExceptionHandler.java
+++ src/main/java/kitchenpos/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package kitchenpos.exception;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -17,5 +18,12 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         logger.info(e.getMessage());
 
         return ResponseEntity.badRequest().build();
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Void> handleBadRequest(Exception e) {
+        logger.error(e.getMessage());
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
     }
 }
