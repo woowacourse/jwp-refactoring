0e77d4b09d5f30fbdb191b718861f8d981984d01
jnsorn <jnsorn@gmail.com>
2020-11-24 23:46:33 +0900

    test : 주문 테이블의 목록 조회 기능 테스트 작성

--- src/test/java/kitchenpos/application/TableServiceTest.java
+++ src/test/java/kitchenpos/application/TableServiceTest.java
@@ -3,15 +3,20 @@ package kitchenpos.application;
 import static kitchenpos.KitchenposTestHelper.*;
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.List;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import kitchenpos.dao.OrderTableDao;
 import kitchenpos.domain.OrderTable;
 
 class TableServiceTest extends ServiceTest {
     @Autowired
     private TableService tableService;
+    @Autowired
+    private OrderTableDao orderTableDao;
 
     @DisplayName("주문 테이블을 등록할 수 있다.")
     @Test
@@ -22,4 +27,16 @@ class TableServiceTest extends ServiceTest {
 
         assertThat(saved.getId()).isNotNull();
     }
+
+    @DisplayName("주문 테이블의 목록을 조회할 수 있다.")
+    @Test
+    void list() {
+        OrderTable orderTable = new OrderTable();
+        orderTableDao.save(orderTable);
+
+        List<OrderTable> orderTables = tableService.list();
+
+        assertThat(orderTables).hasSize(1);
+    }
+
 }
