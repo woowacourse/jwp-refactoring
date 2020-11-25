497ddfdc92fc1820596c371329d29fb9d9757021
jaeju.jang <jjj0611@gmail.com>
2020-11-26 05:34:29 +0900

    test : MenuVerifier test 작성

---
+++ src/test/java/kitchenpos/domain/MenuVerifierTest.java
@@ -0,0 +1,23 @@
+package kitchenpos.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.math.BigDecimal;
+import java.util.Collections;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class MenuVerifierTest {
+    @DisplayName("메뉴 등록 시, 메뉴에 속한 상품 금액의 합은 메뉴의 가격보다 크거나 같아야 한다.")
+    @Test
+    void create_OverSumOfProductsPrice_ThrownException() {
+        Product product = new Product(1L, "양념치킨", BigDecimal.valueOf(18_000));
+        MenuProduct menuProduct = new MenuProduct(null, null, product.getId(), 1);
+
+        assertThatThrownBy(
+            () -> MenuVerifier.validateMenuPrice(BigDecimal.valueOf(18_001), Collections.singletonList(menuProduct),
+                Collections.singletonList(product)))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+}
