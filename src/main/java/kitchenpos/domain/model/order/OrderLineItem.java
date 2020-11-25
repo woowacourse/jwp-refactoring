1be4b5ffa54be03ac7dd4353b98c7238605cb629
kouz <lastpuzly@gmail.com>
2020-11-25 19:41:15 +0900

    refactor: OrderLineItem equals(), hashCode() 추가

--- src/main/java/kitchenpos/domain/model/order/OrderLineItem.java
+++ src/main/java/kitchenpos/domain/model/order/OrderLineItem.java
@@ -1,5 +1,7 @@
 package kitchenpos.domain.model.order;
 
+import java.util.Objects;
+
 import javax.persistence.Entity;
 
 import kitchenpos.domain.model.IdentifiedValueObject;
@@ -7,7 +9,6 @@ import kitchenpos.domain.model.IdentifiedValueObject;
 @Entity
 public class OrderLineItem extends IdentifiedValueObject {
     private Long menuId;
-
     private long quantity;
 
     protected OrderLineItem() {
@@ -24,5 +25,21 @@ public class OrderLineItem extends IdentifiedValueObject {
 
     public long getQuantity() {
         return quantity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        OrderLineItem that = (OrderLineItem)o;
+        return quantity == that.quantity &&
+                Objects.equals(menuId, that.menuId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(menuId, quantity);
     }
 }
