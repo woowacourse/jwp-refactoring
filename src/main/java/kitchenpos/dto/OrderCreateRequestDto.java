294f837dface8684b1490d289e2f5d04ee604502
jaeju.jang <jjj0611@gmail.com>
2020-11-25 22:45:40 +0900

    refactor : OrderService의 create 관련 로직을 Order로 이동

---
+++ src/main/java/kitchenpos/dto/OrderCreateRequestDto.java
@@ -0,0 +1,33 @@
+package kitchenpos.dto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import kitchenpos.domain.Order;
+import kitchenpos.domain.OrderStatus;
+
+public class OrderCreateRequestDto {
+    private Long orderTableId;
+    private List<OrderLineCreateRequestDto> orderLineCreateRequests;
+
+    private OrderCreateRequestDto() {
+    }
+
+    public OrderCreateRequestDto(Long orderTableId,
+        List<OrderLineCreateRequestDto> orderLineCreateRequests) {
+        this.orderTableId = orderTableId;
+        this.orderLineCreateRequests = orderLineCreateRequests;
+    }
+
+    public Long getOrderTableId() {
+        return orderTableId;
+    }
+
+    public List<OrderLineCreateRequestDto> getOrderLineCreateRequests() {
+        return orderLineCreateRequests;
+    }
+
+    public Order toEntity() {
+        return new Order(null, orderTableId, OrderStatus.COOKING.name(), LocalDateTime.now());
+    }
+}
