7bc00ff756275dbd5e08fc7d13ef478485581409
jnsorn <jnsorn@gmail.com>
2020-11-25 23:32:39 +0900

    refactor : Order에 setter 없애기

--- src/main/java/kitchenpos/domain/Order.java
+++ src/main/java/kitchenpos/domain/Order.java
@@ -8,45 +8,10 @@ public class Order {
     private String orderStatus;
     private LocalDateTime orderedTime;
 
-    public Order() {
-    }
-
     public Order(Long id, Long orderTableId, String orderStatus, LocalDateTime orderedTime) {
         this.id = id;
         this.orderTableId = orderTableId;
         this.orderStatus = orderStatus;
-        this.orderedTime = orderedTime;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(final Long id) {
-        this.id = id;
-    }
-
-    public Long getOrderTableId() {
-        return orderTableId;
-    }
-
-    public void setOrderTableId(final Long orderTableId) {
-        this.orderTableId = orderTableId;
-    }
-
-    public String getOrderStatus() {
-        return orderStatus;
-    }
-
-    public void setOrderStatus(final String orderStatus) {
-        this.orderStatus = orderStatus;
-    }
-
-    public LocalDateTime getOrderedTime() {
-        return orderedTime;
-    }
-
-    public void setOrderedTime(final LocalDateTime orderedTime) {
         this.orderedTime = orderedTime;
     }
 
@@ -59,5 +24,21 @@ public class Order {
             throw new IllegalArgumentException("이미 결제가 끝난 주문은 변경할 수 없습니다.");
         }
         this.orderStatus = orderStatus.name();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getOrderTableId() {
+        return orderTableId;
+    }
+
+    public String getOrderStatus() {
+        return orderStatus;
+    }
+
+    public LocalDateTime getOrderedTime() {
+        return orderedTime;
     }
 }
