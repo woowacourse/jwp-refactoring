b537b55600c661828de36a53d55e24b670de65a3
jnsorn <jnsorn@gmail.com>
2020-11-25 23:30:42 +0900

    refactor : MenuGroup에 setter 없애기

--- src/main/java/kitchenpos/domain/MenuGroup.java
+++ src/main/java/kitchenpos/domain/MenuGroup.java
@@ -4,9 +4,6 @@ public class MenuGroup {
     private Long id;
     private String name;
 
-    public MenuGroup() {
-    }
-
     public MenuGroup(Long id, String name) {
         this.id = id;
         this.name = name;
@@ -16,15 +13,7 @@ public class MenuGroup {
         return id;
     }
 
-    public void setId(final Long id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(final String name) {
-        this.name = name;
     }
 }
