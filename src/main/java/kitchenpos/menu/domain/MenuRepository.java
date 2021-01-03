2bb33bf7ed1ed7c3df4d588c069cafd7f95b3e5d
SeYun <ksy90101@naver.com>
2021-01-03 14:37:32 +0900

    refactor: 사용하지 않은 쿼리 메소드 삭제

--- src/main/java/kitchenpos/menu/domain/MenuRepository.java
+++ src/main/java/kitchenpos/menu/domain/MenuRepository.java
@@ -1,15 +1,8 @@
 package kitchenpos.menu.domain;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MenuRepository extends JpaRepository<Menu, Long> {
-    @Query("SELECT m from Menu m join fetch m.menuProducts")
-    List<Menu> findAllWithMenuProducts();
-
-    long countByIdIn(List<Long> ids);
 }
