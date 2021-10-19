19cf4ed2981a4dec418235f318ba4e676e6b2e3b
kimjunseo <air.junseo@gmail.com>
2021-10-20 03:06:44 +0900

    test: MenuGroupRestController 단위 테스트 추가

---
+++ src/test/java/kitchenpos/ui/ControllerTest.java
@@ -0,0 +1,46 @@
+package kitchenpos.ui;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import kitchenpos.application.MenuGroupService;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@WebMvcTest({MenuGroupRestController.class})
+public abstract class ControllerTest {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @MockBean
+    protected MenuGroupService menuGroupService;
+
+    @BeforeEach
+    void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                                      .addFilter(new CharacterEncodingFilter("UTF-8", true))
+                                      .alwaysDo(print())
+                                      .build();
+    }
+
+    protected String objectToJsonString(Object object) throws JsonProcessingException {
+        return objectMapper.writeValueAsString(object);
+    }
+}
---
+++ src/test/java/kitchenpos/ui/MenuGroupRestControllerTest.java
@@ -0,0 +1,59 @@
+package kitchenpos.ui;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import kitchenpos.domain.MenuGroup;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@DisplayName("MenuGroupRestController 단위 테스트")
+class MenuGroupRestControllerTest extends ControllerTest {
+
+    @Test
+    @DisplayName("메뉴 그룹을 생성할 수 있다.")
+    void create() throws Exception {
+        // given
+        MenuGroup 추천메뉴 = new MenuGroup("추천 메뉴");
+        MenuGroup expected = new MenuGroup(1L, "추천 메뉴");
+        given(menuGroupService.create(any(MenuGroup.class))).willReturn(expected);
+
+        // when
+        ResultActions response = mockMvc.perform(post("/api/menu-groups")
+                .content(objectToJsonString(추천메뉴))
+                .contentType(MediaType.APPLICATION_JSON));
+
+        // then
+        response.andExpect(status().isCreated())
+                .andExpect(header().string("location", "/api/menu-groups/" + expected.getId()))
+                .andExpect(content().json(objectToJsonString(expected)));
+    }
+
+    @Test
+    @DisplayName("전체 메뉴 그룹을 조회할 수 있다.")
+    void list() throws Exception {
+        // given
+        MenuGroup 추천메뉴 = new MenuGroup(1L, "추천 메뉴");
+        MenuGroup 세트메뉴 = new MenuGroup(2L, "세트 메뉴");
+        List<MenuGroup> expected = Arrays.asList(추천메뉴, 세트메뉴);
+        given(menuGroupService.list()).willReturn(expected);
+
+        // when
+        ResultActions response = mockMvc.perform(get("/api/menu-groups")
+                .contentType(MediaType.APPLICATION_JSON));
+
+        // then
+        response.andExpect(status().isOk())
+                .andExpect(content().json(objectToJsonString(expected)));
+    }
+}
