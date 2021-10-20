f092351669dd5c0e1ee6250b674fdf5324cc898a
kimjunseo <air.junseo@gmail.com>
2021-10-20 11:17:43 +0900

    test: MenuService 단위 테스트 추가

--- src/main/java/kitchenpos/domain/Menu.java
+++ src/main/java/kitchenpos/domain/Menu.java
@@ -10,6 +10,29 @@ public class Menu {
     private Long menuGroupId;
     private List<MenuProduct> menuProducts;
 
+    public Menu() {
+    }
+
+    public Menu(String name, Long menuGroupId, List<MenuProduct> menuProducts) {
+        this(null, name, null, menuGroupId, menuProducts);
+    }
+
+    public Menu(String name, int price, Long menuGroupId, List<MenuProduct> menuProducts) {
+        this(null, name, price, menuGroupId, menuProducts);
+    }
+
+    public Menu(Long id, String name, int price, Long menuGroupId, List<MenuProduct> menuProducts) {
+        this(id, name, BigDecimal.valueOf(price), menuGroupId, menuProducts);
+    }
+
+    public Menu(Long id, String name, BigDecimal price, Long menuGroupId, List<MenuProduct> menuProducts) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.menuGroupId = menuGroupId;
+        this.menuProducts = menuProducts;
+    }
+
     public Long getId() {
         return id;
     }
