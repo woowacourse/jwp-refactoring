f387da1707fbde9ed742a117731ab47790826a16
pomo <bosl95@naver.com>
2021-10-22 16:31:19 +0900

    feat: MenuGroupServiceTest 테스트 코드 작성

---
+++ src/test/java/kitchenpos/application/MenuGroupServiceTest.java
@@ -0,0 +1,71 @@
+package kitchenpos.application;
+
+import kitchenpos.dao.MenuGroupDao;
+import kitchenpos.domain.MenuGroup;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+class MenuGroupServiceTest {
+
+    @Mock
+    private MenuGroupDao menuGroupDao;
+
+    @InjectMocks
+    private MenuGroupService menuGroupService;
+
+    private MenuGroup menuGroup;
+
+    @BeforeEach
+    void setUp() {
+        menuGroup = new MenuGroup();
+        menuGroup.setName("추천 메뉴");
+    }
+
+    @DisplayName("메뉴 분류를 위한 그룹을 등록할 수 있다.")
+    @Test
+    void create() {
+        // given
+        MenuGroup savedMenuGroup = 메뉴_그룹을_저장한다(1L, menuGroup.getName());
+        given(menuGroupDao.save(menuGroup)).willReturn(savedMenuGroup);
+
+        // when
+        MenuGroup result = menuGroupService.create(menuGroup);
+
+        // then
+        assertThat(result).isEqualTo(savedMenuGroup);
+    }
+
+    @DisplayName("메뉴 분류를 위한 그룹을 조회할 수 있다.")
+    @Test
+    void list() {
+        // given
+        MenuGroup savedMenuGroup1 = 메뉴_그룹을_저장한다(1L, menuGroup.getName());
+        MenuGroup savedMenuGroup2 = 메뉴_그룹을_저장한다(2L, menuGroup.getName());
+
+        // when
+        given(menuGroupDao.findAll()).willReturn(Arrays.asList(savedMenuGroup1, savedMenuGroup2));
+        List<MenuGroup> result = menuGroupService.list();
+
+        // then
+        assertThat(result).containsExactly(savedMenuGroup1, savedMenuGroup2);
+    }
+
+    private MenuGroup 메뉴_그룹을_저장한다(long id, String groupName) {
+        MenuGroup savedMenuGroup = new MenuGroup();
+        savedMenuGroup.setId(id);
+        savedMenuGroup.setName(groupName);
+        return savedMenuGroup;
+    }
+}
