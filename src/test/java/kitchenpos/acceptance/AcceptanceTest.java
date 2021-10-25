395dca3cad0fa1e9eda57d28e38219f066dc3dbc
seovalue <seominjeong.dev@gmail.com>
2021-10-25 17:51:56 +0900

    refactor: 불필요한 profile 제거

--- src/test/java/kitchenpos/acceptance/AcceptanceTest.java
+++ src/test/java/kitchenpos/acceptance/AcceptanceTest.java
@@ -7,13 +7,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
-@ActiveProfiles("test")
 public class AcceptanceTest {
     @LocalServerPort
     int port;
