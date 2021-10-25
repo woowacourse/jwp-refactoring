2ca178000f0ef7d7940ed34dcf787c103c303360
livenow14 <xowjd41@naver.com>
2021-10-26 00:10:07 +0900

    feat: H2 옵션 수정
    서버 모드를 활성화 하여, intellij에서 확인하게 하였음.

---
+++ src/main/java/kitchenpos/config/H2ServerConfig.java
@@ -0,0 +1,18 @@
+package kitchenpos.config;
+
+import org.h2.tools.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.sql.SQLException;
+
+@Configuration
+@Profile("default")
+public class H2ServerConfig {
+
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public Server H2DatabaseServer() throws SQLException {
+        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9091");
+    }
+}
