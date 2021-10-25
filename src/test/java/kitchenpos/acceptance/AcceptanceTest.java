c7406dad6fcc75893a181ed4ac58c83c8299db05
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2021-10-26 02:37:52 +0900

    refactor: DirtiesContext를 제거한 테스트 격리 전략 수립

--- src/test/java/kitchenpos/acceptance/AcceptanceTest.java
+++ src/test/java/kitchenpos/acceptance/AcceptanceTest.java
@@ -4,14 +4,10 @@ import kitchenpos.dao.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.jdbc.Sql;
 
-import static org.springframework.test.annotation.DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD;
-
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@DirtiesContext(classMode = BEFORE_EACH_TEST_METHOD)
-@Sql("classpath:/clear-database.sql")
+@Sql("classpath:/database-cleanup.sql")
 public abstract class AcceptanceTest {
 
     @Autowired
