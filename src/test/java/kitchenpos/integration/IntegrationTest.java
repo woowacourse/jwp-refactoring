89e55c2ddfca4fd3b7c8d50c093a275b4ac1f55b
Daeun Lee <leede0418@likelion.org>
2021-10-27 00:58:45 +0900

    test: MenuGroup 통합 테스트 작성

--- src/test/java/kitchenpos/integration/IntegrationTest.java
+++ src/test/java/kitchenpos/integration/IntegrationTest.java
@@ -1,5 +1,6 @@
 package kitchenpos.integration;
 
+import kitchenpos.application.MenuGroupService;
 import kitchenpos.application.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,4 +10,7 @@ public abstract class IntegrationTest {
 
     @Autowired
     protected ProductService productService;
+
+    @Autowired
+    protected MenuGroupService menuGroupService;
 }
