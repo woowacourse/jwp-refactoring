a65dd638a2e5f83fc226f573bfa541c073a511a9
jaeseongDev <qkrwotjd1445@naver.com>
2021-10-26 21:23:14 +0900

    test: MenuProductDao 테스트 코드 추가

---
+++ src/test/java/kitchenpos/dao/MenuProductDaoTest.java
@@ -0,0 +1,71 @@
+package kitchenpos.dao;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.math.BigDecimal;
+import java.util.List;
+import kitchenpos.domain.Menu;
+import kitchenpos.domain.MenuProduct;
+import kitchenpos.domain.Product;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class MenuProductDaoTest extends DaoTest {
+    @Autowired
+    private MenuProductDao menuProductDao;
+
+    @Autowired
+    private MenuDao menuDao;
+
+    @Autowired
+    private ProductDao productDao;
+
+    @Test
+    void save() throws Exception {
+        Menu menu = menuDao.save(new Menu("메뉴명", new BigDecimal(100), 1L));
+        Product product = productDao.save(new Product("상품명", new BigDecimal(100)));
+        MenuProduct savedMenuProduct = menuProductDao.save(new MenuProduct(menu.getId(), product.getId(), 10));
+        MenuProduct foundMenuProduct = menuProductDao
+            .findById(savedMenuProduct.getSeq())
+            .orElseThrow(() -> new Exception());
+        assertThat(savedMenuProduct.getSeq()).isEqualTo(foundMenuProduct.getSeq());
+        assertThat(savedMenuProduct.getProductId()).isEqualTo(foundMenuProduct.getProductId());
+        assertThat(savedMenuProduct.getMenuId()).isEqualTo(foundMenuProduct.getMenuId());
+        assertThat(savedMenuProduct.getQuantity()).isEqualTo(foundMenuProduct.getQuantity());
+    }
+
+    @Test
+    void findById() throws Exception {
+        Menu menu = menuDao.save(new Menu("메뉴명", new BigDecimal(100), 1L));
+        Product product = productDao.save(new Product("상품명", new BigDecimal(100)));
+        MenuProduct savedMenuProduct = menuProductDao.save(new MenuProduct(menu.getId(), product.getId(), 10));
+        MenuProduct foundMenuProduct = menuProductDao
+            .findById(savedMenuProduct.getSeq())
+            .orElseThrow(() -> new Exception());
+        assertThat(savedMenuProduct.getSeq()).isEqualTo(foundMenuProduct.getSeq());
+        assertThat(savedMenuProduct.getProductId()).isEqualTo(foundMenuProduct.getProductId());
+        assertThat(savedMenuProduct.getMenuId()).isEqualTo(foundMenuProduct.getMenuId());
+        assertThat(savedMenuProduct.getQuantity()).isEqualTo(foundMenuProduct.getQuantity());
+    }
+
+    @Test
+    void findAll() {
+        Menu menu = menuDao.save(new Menu("메뉴명", new BigDecimal(100), 1L));
+        Product product = productDao.save(new Product("상품명", new BigDecimal(100)));
+        menuProductDao.save(new MenuProduct(menu.getId(), product.getId(), 10));
+        menuProductDao.save(new MenuProduct(menu.getId(), product.getId(), 10));
+        List<MenuProduct> menuProducts = menuProductDao.findAll();
+        assertThat(menuProducts).hasSize(2);
+    }
+
+    @Test
+    void findAllByMenuId() {
+        Menu menu = menuDao.save(new Menu("메뉴명", new BigDecimal(100), 1L));
+        Product product = productDao.save(new Product("상품명", new BigDecimal(100)));
+        menuProductDao.save(new MenuProduct(menu.getId(), product.getId(), 10));
+        menuProductDao.save(new MenuProduct(menu.getId(), product.getId(), 10));
+        menuProductDao.save(new MenuProduct(menu.getId(), product.getId(), 10));
+        List<MenuProduct> menuProducts = menuProductDao.findAllByMenuId(menu.getId());
+        assertThat(menuProducts).hasSize(3);
+    }
+}
