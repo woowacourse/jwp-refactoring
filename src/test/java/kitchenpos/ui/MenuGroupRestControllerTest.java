5381fe8cc206b9d67b0c5397fcd9b660fe905f88
gomding <backchida@gmail.com>
2021-10-27 00:09:40 +0900

    docs(README.md): 키친 포스 요구사항 다듬기

---
+++ src/test/java/kitchenpos/ui/MenuGroupRestControllerTest.java
@@ -0,0 +1,20 @@
+package kitchenpos.ui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@WebMvcTest
+class MenuGroupRestControllerTest {
+
+    @
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void list() {
+    }
+}
