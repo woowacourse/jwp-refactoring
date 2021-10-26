0eeebeef4baeba6e3f1820970f34486efc1b20f3
Daeun Lee <leede0418@likelion.org>
2021-10-27 03:43:54 +0900

    test: Order 통합 테스트 작성

--- src/test/java/kitchenpos/integration/IntegrationTest.java
+++ src/test/java/kitchenpos/integration/IntegrationTest.java
@@ -2,7 +2,9 @@ package kitchenpos.integration;
 
 import kitchenpos.application.MenuGroupService;
 import kitchenpos.application.MenuService;
+import kitchenpos.application.OrderService;
 import kitchenpos.application.ProductService;
+import kitchenpos.application.TableService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -17,4 +19,10 @@ public abstract class IntegrationTest {
 
     @Autowired
     protected MenuService menuService;
+
+    @Autowired
+    protected OrderService orderService;
+
+    @Autowired
+    protected TableService tableService;
 }
