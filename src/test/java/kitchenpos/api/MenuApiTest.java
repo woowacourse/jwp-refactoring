1171c13b9c186f4145a51eef8331ed5049f0520e
ROOT <junroot0909@gmail.com>
2021-10-28 03:17:30 +0900

    test: 메뉴 관련 테스트 추가

---
+++ src/test/java/kitchenpos/api/MenuApiTest.java
@@ -0,0 +1,110 @@
+package kitchenpos.api;
+
+import static org.assertj.core.api.Assertions.as;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import kitchenpos.dao.JdbcTemplateMenuDao;
+import kitchenpos.dao.JdbcTemplateMenuGroupDao;
+import kitchenpos.dao.JdbcTemplateMenuProductDao;
+import kitchenpos.dao.JdbcTemplateProductDao;
+import kitchenpos.domain.Menu;
+import kitchenpos.domain.MenuGroup;
+import kitchenpos.domain.MenuProduct;
+import kitchenpos.domain.Product;
+import kitchenpos.generator.MenuGenerator;
+import kitchenpos.generator.MenuGroupGenerator;
+import kitchenpos.generator.ProductGenerator;
+import org.assertj.core.util.BigDecimalComparator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+public class MenuApiTest extends ApiTest {
+
+    private static final String BASE_URL = "/api/menus";
+
+    @Autowired
+    private JdbcTemplateMenuGroupDao menuGroupDao;
+
+    @Autowired
+    private JdbcTemplateProductDao productDao;
+
+    @Autowired
+    private JdbcTemplateMenuDao menuDao;
+
+    @Autowired
+    private JdbcTemplateMenuProductDao menuProductDao;
+
+    private MenuGroup menuGroup;
+    private List<Product> products;
+    private List<Menu> menus;
+    private List<MenuProduct> menuProducts;
+
+    @Override
+    @BeforeEach
+    void setUp() throws java.sql.SQLException {
+        super.setUp();
+        products = new ArrayList<>();
+        menus = new ArrayList<>();
+        menuProducts = new ArrayList<>();
+
+        menuGroup = menuGroupDao.save(MenuGroupGenerator.newInstance("두마리메뉴"));
+
+        products.add(productDao.save(ProductGenerator.newInstance("후라이드", 16000)));
+        products.add(productDao.save(ProductGenerator.newInstance("양념치킨", 16000)));
+
+        menus.add(menuDao.save(MenuGenerator.newInstance("후라이드치킨", 16000, menuGroup.getId(), Collections.emptyList())));
+        menus.add(menuDao.save(MenuGenerator.newInstance("양념치킨", 16000, menuGroup.getId(), Collections.emptyList())));
+
+        menuProducts.add(menuProductDao.save(MenuGenerator.newMenuProduct(menus.get(0).getId(), products.get(0).getId(), 1)));
+        menuProducts.add(menuProductDao.save(MenuGenerator.newMenuProduct(menus.get(1).getId(), products.get(1).getId(), 1)));
+    }
+
+    @DisplayName("메뉴 등록")
+    @Test
+    void postMenu() {
+        MenuProduct menuProduct = MenuGenerator.newMenuProduct(products.get(0).getId(), 2);
+        Menu request = MenuGenerator.newInstance("후라이드+후라이드", 19000, menuGroup.getId(), Collections.singletonList(menuProduct));
+
+        ResponseEntity<Menu> responseEntity = testRestTemplate.postForEntity(BASE_URL, request, Menu.class);
+        Menu response = responseEntity.getBody();
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        assertThat(response.getId()).isNotNull();
+        assertThat(response.getId()).isEqualTo(response.getMenuProducts().get(0).getMenuId());
+        assertThat(response).usingComparatorForType(BigDecimalComparator.BIG_DECIMAL_COMPARATOR, BigDecimal.class)
+            .usingRecursiveComparison()
+            .ignoringFields("id", "menuProducts.seq", "menuProducts.menuId")
+            .isEqualTo(request);
+    }
+
+    @DisplayName("메뉴 목록 조회")
+    @Test
+    void getMenus() {
+        ResponseEntity<Menu[]> responseEntity = testRestTemplate.getForEntity(BASE_URL, Menu[].class);
+        Menu[] response = responseEntity.getBody();
+
+        List<Menu> expected = new ArrayList<>();
+        for (int i = 0; i < 2; i++) {
+            Menu menu = new Menu();
+            menu.setId(menus.get(i).getId());
+            menu.setName(menus.get(i).getName());
+            menu.setPrice(menus.get(i).getPrice());
+            menu.setMenuGroupId(menuGroup.getId());
+            menu.setMenuProducts(Collections.singletonList(menuProducts.get(i)));
+            expected.add(menu);
+        }
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response).hasSize(2);
+        assertThat(response).usingRecursiveFieldByFieldElementComparator()
+            .containsAll(expected);
+    }
+}
---
+++ src/test/java/kitchenpos/application/MenuServiceTest.java
@@ -0,0 +1,107 @@
+package kitchenpos.application;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import kitchenpos.dao.MenuDao;
+import kitchenpos.dao.MenuGroupDao;
+import kitchenpos.dao.MenuProductDao;
+import kitchenpos.dao.ProductDao;
+import kitchenpos.domain.Menu;
+import kitchenpos.domain.MenuProduct;
+import kitchenpos.domain.Product;
+import kitchenpos.generator.MenuGenerator;
+import kitchenpos.generator.ProductGenerator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+public class MenuServiceTest extends ServiceTest {
+
+    @Mock
+    private MenuDao menuDao;
+    @Mock
+    private MenuGroupDao menuGroupDao;
+    @Mock
+    private MenuProductDao menuProductDao;
+    @Mock
+    private ProductDao productDao;
+    @InjectMocks
+    private MenuService menuService;
+
+    @DisplayName("메뉴 등록")
+    @Test
+    void create() {
+        Menu menuToCreate = MenuGenerator.newInstance("후라이드+후라이드", 19000, 1L, Collections.singletonList(MenuGenerator.newMenuProduct(1L, 2)));
+        when(menuDao.save(menuToCreate)).thenAnswer(invocation -> {
+            Menu menu = invocation.getArgument(0);
+            menu.setId(2L);
+            return menu;
+        });
+        when(menuProductDao.save(any(MenuProduct.class))).thenAnswer(invocation -> {
+            MenuProduct menuProduct = invocation.getArgument(0);
+            menuProduct.setSeq(3L);
+            return menuProduct;
+        });
+        when(menuGroupDao.existsById(menuToCreate.getMenuGroupId())).thenReturn(true);
+        when(productDao.findById(1L)).thenAnswer(invocation -> Optional.of(ProductGenerator.newInstance(1L, "후라이드", 16000)));
+
+        Menu actual = menuService.create(menuToCreate);
+
+        Menu expected = new Menu();
+        expected.setId(2L);
+        expected.setName(menuToCreate.getName());
+        expected.setPrice(menuToCreate.getPrice());
+        expected.setMenuGroupId(1L);
+        expected.setMenuProducts(Collections.singletonList(MenuGenerator.newMenuProduct(3L, 2L, 1L, 2)));
+        verify(menuDao, times(1)).save(menuToCreate);
+        verify(menuProductDao, times(1)).save(any(MenuProduct.class));
+        assertThat(actual).usingRecursiveComparison()
+            .isEqualTo(expected);
+    }
+
+    @DisplayName("가격이 음수인 메뉴 등록시 예외 처리")
+    @Test
+    void createWithNegativePrice() {
+        Menu menuToCreate = MenuGenerator.newInstance("후라이드+후라이드", -1, 1L, Collections.singletonList(MenuGenerator.newMenuProduct(1L, 2)));
+
+        assertThatThrownBy(() -> menuService.create(menuToCreate)).isExactlyInstanceOf(IllegalArgumentException.class);
+    }
+    
+    @DisplayName("메뉴 가격이 메뉴 상품 가격 합보다 크면 예외 처리")
+    @Test
+    void createWhenPriceOfMenuIsGreaterThanTotalPriceOfMenuProduct() {
+        Menu menuToCreate = MenuGenerator.newInstance("후라이드+후라이드", 33000, 1L, Collections.singletonList(MenuGenerator.newMenuProduct(1L, 2)));
+        when(menuGroupDao.existsById(menuToCreate.getMenuGroupId())).thenReturn(true);
+        when(productDao.findById(1L)).thenAnswer(invocation -> Optional.of(ProductGenerator.newInstance(1L, "후라이드", 16000)));
+
+        assertThatThrownBy(() -> menuService.create(menuToCreate)).isExactlyInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("등록되지 않은 메뉴 그룹으로 메뉴 등록시 예외 처리")
+    @Test
+    void createWithNotFoundMenuGroup() {
+        Menu menuToCreate = MenuGenerator.newInstance("후라이드+후라이드", 19000, 1L, Collections.singletonList(MenuGenerator.newMenuProduct(1L, 2)));
+        when(menuGroupDao.existsById(menuToCreate.getMenuGroupId())).thenReturn(false);
+
+        assertThatThrownBy(() -> menuService.create(menuToCreate)).isExactlyInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("등록되지 않은 상품으로 메뉴 등록시 예외 처리")
+    @Test
+    void createWithNotFoundProduct() {
+        Menu menuToCreate = MenuGenerator.newInstance("후라이드+후라이드", 19000, 1L, Collections.singletonList(MenuGenerator.newMenuProduct(1L, 2)));
+        when(menuGroupDao.existsById(menuToCreate.getMenuGroupId())).thenReturn(true);
+        when(productDao.findById(1L)).thenAnswer(invocation -> Optional.empty());
+
+        assertThatThrownBy(() -> menuService.create(menuToCreate)).isExactlyInstanceOf(IllegalArgumentException.class);
+    }
+}
