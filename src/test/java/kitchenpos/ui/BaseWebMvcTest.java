add83d48042a9f839144abcc441198e747784818
gomding <backchida@gmail.com>
2021-10-27 21:56:18 +0900

    test(BaseWebMvcTest.class): Service들을 Mocking 하도록 구성

--- src/test/java/kitchenpos/ui/BaseWebMvcTest.java
+++ src/test/java/kitchenpos/ui/BaseWebMvcTest.java
@@ -2,9 +2,24 @@ package kitchenpos.ui;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import kitchenpos.application.MenuGroupService;
+import kitchenpos.application.MenuService;
+import kitchenpos.application.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@WebMvcTest(controllers = {
+        MenuGroupRestController.class,
+        MenuRestController.class,
+        ProductRestController.class
+})
 public class BaseWebMvcTest {
 
     @Autowired
@@ -13,7 +28,29 @@ public class BaseWebMvcTest {
     @Autowired
     protected ObjectMapper objectMapper;
 
+    @MockBean
+    protected MenuGroupService menuGroupService;
+
+    @MockBean
+    protected MenuService menuService;
+
+    @MockBean
+    protected ProductService productService;
+
     protected String parseJson(Object object) throws JsonProcessingException {
         return objectMapper.writeValueAsString(object);
+    }
+
+    protected MockHttpServletRequestBuilder getMethodRequestBase(String url) {
+        return get(url)
+                .contentType(MediaType.APPLICATION_JSON)
+                .characterEncoding("utf-8");
+    }
+
+    protected MockHttpServletRequestBuilder postMethodRequestBase(String url, String content) {
+        return post(url)
+                .contentType(MediaType.APPLICATION_JSON)
+                .characterEncoding("utf-8")
+                .content(content);
     }
 }
