ac3c48b1e11fd636e34dc05d7cdf24e0b25d77e5
Sehwan-Jang <whilter08@gmail.com>
2021-10-27 22:37:13 +0900

    test: 상품 서비스 단위 테스트 작성

--- src/test/java/kitchenpos/fixtures/ProductFixtures.java
+++ src/test/java/kitchenpos/fixtures/ProductFixtures.java
@@ -7,13 +7,26 @@ public class ProductFixtures {
 
     private static final String PRODUCT_NAME = "기본 상품명";
     private static final int PRICE = 10000;
+    private static final long PRODUCT_ID = 1L;
+
+    public static Product createProduct(
+        Long id,
+        String name,
+        long price
+    ) {
+        Product product = new Product();
+        product.setId(id);
+        product.setName(name);
+        product.setPrice(BigDecimal.valueOf(price * 100, 2));
+        return product;
+    }
 
     public static Product createProduct() {
-        Product product = new Product();
-        product.setId(1L);
-        product.setName(PRODUCT_NAME);
-        product.setPrice(BigDecimal.valueOf(PRICE * 100, 2));
-        return product;
+        return createProduct(PRODUCT_ID, PRODUCT_NAME, PRICE);
+    }
+
+    public static Product createProduct(long price) {
+        return createProduct(PRODUCT_ID, PRODUCT_NAME, price);
     }
 
 }
