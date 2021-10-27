879c39c5291212c2d39960d90e5eeae545b9eaa6
seovalue <seominjeong.dev@gmail.com>
2021-10-28 00:50:38 +0900

    refactor: TableGroup setter 메서드 제거

--- src/main/java/kitchenpos/domain/TableGroup.java
+++ src/main/java/kitchenpos/domain/TableGroup.java
@@ -1,6 +1,7 @@
 package kitchenpos.domain;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 public class TableGroup {
@@ -8,27 +9,37 @@ public class TableGroup {
     private LocalDateTime createdDate;
     private List<OrderTable> orderTables;
 
-    public Long getId() {
-        return id;
+    public TableGroup(List<OrderTable> orderTables) {
+        this(null, LocalDateTime.now(), orderTables);
     }
 
-    public void setId(final Long id) {
+    public TableGroup(Long id, LocalDateTime createdDate) {
+        this(id, createdDate, new ArrayList<>());
+    }
+
+    public TableGroup(LocalDateTime createdDate, List<OrderTable> orderTables) {
+        this(null, createdDate, orderTables);
+    }
+
+    public TableGroup(Long id, LocalDateTime createdDate, List<OrderTable> orderTables) {
         this.id = id;
+        this.createdDate = createdDate;
+        this.orderTables = orderTables;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public LocalDateTime getCreatedDate() {
         return createdDate;
     }
 
-    public void setCreatedDate(final LocalDateTime createdDate) {
-        this.createdDate = createdDate;
-    }
-
     public List<OrderTable> getOrderTables() {
         return orderTables;
     }
 
-    public void setOrderTables(final List<OrderTable> orderTables) {
-        this.orderTables = orderTables;
+    public void addOrderTables(List<OrderTable> orderTables) {
+        this.orderTables.addAll(orderTables);
     }
 }
