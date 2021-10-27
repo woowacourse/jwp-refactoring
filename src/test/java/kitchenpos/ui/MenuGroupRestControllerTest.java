9ccad6843a50a9b48848d9e9ca4e6be6a48627e8
gomding <backchida@gmail.com>
2021-10-27 21:54:58 +0900

    test(MenuRestControllerTest.java): MenuRestController 테스트 작성

--- src/test/java/kitchenpos/ui/MenuGroupRestControllerTest.java
+++ src/test/java/kitchenpos/ui/MenuGroupRestControllerTest.java
@@ -1,14 +1,11 @@
 package kitchenpos.ui;
 
-import kitchenpos.application.MenuGroupService;
 import kitchenpos.domain.MenuGroup;
-import kitchenpos.ui.factory.MenuGroupFactory;
+import kitchenpos.ui.factory.MenuGroupBuilder;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.ResultActions;
 
 import java.util.Arrays;
@@ -18,41 +15,43 @@ import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.core.Is.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(MenuGroupRestController.class)
 class MenuGroupRestControllerTest extends BaseWebMvcTest {
 
-    @MockBean
-    MenuGroupService service;
-
     MenuGroup menuGroup1;
     MenuGroup menuGroup2;
 
     @BeforeEach
     void setUp() {
-        this.menuGroup1 = MenuGroupFactory.create(1L, "추천 메뉴");
-        this.menuGroup2 = MenuGroupFactory.create(2L, "사이드 메뉴");
+        this.menuGroup1 = new MenuGroupBuilder()
+                .id(1L)
+                .name("추천 메뉴")
+                .build();
+        this.menuGroup2 = new MenuGroupBuilder()
+                .id(2L)
+                .name("사이드 메뉴")
+                .build();
+        ;
     }
 
     @DisplayName("POST /api/menu-groups -> 메뉴 그룹을 생성한다.")
     @Test
     void create() throws Exception {
         // given
-        MenuGroup requestMenuGroup = MenuGroupFactory.create(null, "추천 메뉴");
+        MenuGroup requestMenuGroup = new MenuGroupBuilder()
+                .id(null)
+                .name("추천 메뉴")
+                .build();
         String content = super.parseJson(requestMenuGroup);
 
-        given(service.create(any(MenuGroup.class)))
+        given(menuGroupService.create(any(MenuGroup.class)))
                 .willReturn(menuGroup1);
 
         // when
-        ResultActions actions = mvc.perform(post("/api/menu-groups")
-                .contentType(MediaType.APPLICATION_JSON)
-                .characterEncoding("utf-8")
-                .content(content));
+        ResultActions actions = mvc.perform(postMethodRequestBase("/api/menu-groups", content));
 
         // then
         actions.andExpect(status().isCreated())
@@ -68,12 +67,10 @@ class MenuGroupRestControllerTest extends BaseWebMvcTest {
         // given
         List<MenuGroup> menuGroups = Arrays.asList(menuGroup1, menuGroup2);
 
-        given(service.list()).willReturn(menuGroups);
+        given(menuGroupService.list()).willReturn(menuGroups);
 
         // when
-        ResultActions actions = mvc.perform(get("/api/menu-groups")
-                .contentType(MediaType.APPLICATION_JSON)
-                .characterEncoding("utf-8"));
+        ResultActions actions = mvc.perform(getMethodRequestBase("/api/menu-groups"));
 
         // then
         actions.andExpect(status().isOk())
