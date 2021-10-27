0fa8db7c9955526ad71ecf9e0ef29616f6d42989
seovalue <seominjeong.dev@gmail.com>
2021-10-28 01:02:52 +0900

    refactor: Product 요청/응답 DTO 추가

---
+++ src/main/java/kitchenpos/ui/dto/ProductRequest.java
@@ -0,0 +1,36 @@
+package kitchenpos.ui.dto;
+
+import kitchenpos.domain.Product;
+
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+
+public class ProductRequest {
+    @NotNull
+    private String name;
+    @NotNull
+    private BigDecimal price;
+
+    private ProductRequest() {}
+
+    private ProductRequest(String name, BigDecimal price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public static ProductRequest from(String name, BigDecimal price) {
+        return new ProductRequest(name, price);
+    }
+
+    public Product toProduct() {
+        return new Product(null, name, price);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+}
---
+++ src/main/java/kitchenpos/ui/dto/ProductResponse.java
@@ -0,0 +1,47 @@
+package kitchenpos.ui.dto;
+
+import kitchenpos.domain.Product;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ProductResponse {
+    private Long id;
+    private String name;
+    private BigDecimal price;
+
+    private ProductResponse() {}
+
+    private ProductResponse(Long id, String name, BigDecimal price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public static ProductResponse from(Product product) {
+        return new ProductResponse(
+                product.getId(),
+                product.getName(),
+                product.getPrice()
+        );
+    }
+
+    public static List<ProductResponse> from(List<Product> products) {
+        return products.stream()
+                .map(ProductResponse::from)
+                .collect(Collectors.toList());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+}
