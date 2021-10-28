7d4ed6ce2c02836e93d452b6afdb51afb592cf82
NewWisdom <wlgp2500@gmail.com>
2021-10-29 00:51:15 +0900

    test(menu): 메뉴 그룹 테스트 구현

--- src/test/java/kitchenpos/MenuFixture.java
+++ src/test/java/kitchenpos/MenuFixture.java
@@ -1,29 +1,41 @@
 package kitchenpos;
 
 import kitchenpos.domain.Menu;
+import kitchenpos.domain.MenuGroup;
 import kitchenpos.domain.MenuProduct;
 import kitchenpos.domain.Product;
 
-import java.awt.*;
 import java.math.BigDecimal;
 import java.util.Collections;
 import java.util.List;
 
 public class MenuFixture {
-    private static final String NAME = "육회초밥";
+    private static final String MENU_NAME = "육회초밥";
+    private static final String MENU_GROUP_NAME = "나혼자세트";
     private static final BigDecimal PRICE = BigDecimal.valueOf(15900);
     private static final Long MENU_GROUP_ID = 1L;
 
+    public static MenuGroup createMenuGroup() {
+        return createMenuGroup(null);
+    }
+
+    public static MenuGroup createMenuGroup(Long id) {
+        MenuGroup menuGroup = new MenuGroup();
+        menuGroup.setId(id);
+        menuGroup.setName(MENU_GROUP_NAME);
+        return menuGroup;
+    }
+
     public static Menu createMenu() {
-        return createMenu(NAME, PRICE, MENU_GROUP_ID, Collections.emptyList());
+        return createMenu(MENU_NAME, PRICE, MENU_GROUP_ID, Collections.emptyList());
     }
 
     public static Menu createMenu(Long id) {
-        return createMenu(id, NAME, PRICE, MENU_GROUP_ID, Collections.emptyList());
+        return createMenu(id, MENU_NAME, PRICE, MENU_GROUP_ID, Collections.emptyList());
     }
 
     public static Menu createMenu(String name, BigDecimal price, Long menuGroupId, List<MenuProduct> menuProducts) {
-        return createMenu(null, name, price, menuGroupId,menuProducts);
+        return createMenu(null, name, price, menuGroupId, menuProducts);
     }
 
     public static Menu createMenu(Long id, String name, BigDecimal price, Long menuGroupId, List<MenuProduct> menuProducts) {
