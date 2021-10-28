780645fb4e948c3608ff4ab9cb3798dcd9e998df
gomding <backchida@gmail.com>
2021-10-28 23:56:23 +0900

    feat(test.resources): 테스트용 리소스와 데이터베이스 초기화 sql 작성

---
+++ src/test/resources/data-initialization-h2.sql
@@ -0,0 +1,8 @@
+delete from table_group;
+delete from menu_product;
+delete from menu;
+delete from product;
+delete from menu_group;
+delete from order_line_item;
+delete from order_table;
+delete from orders;
