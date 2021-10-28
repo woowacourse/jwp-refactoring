27229d4222e25d0310a801f5d2d6211a66be261b
Seed <hsik0225@gmail.com>
2021-10-29 03:14:28 +0900

    refactor: 불필요한 @DirtiesContext 제거

--- src/test/java/kitchenpos/application/ServiceTest.java
+++ src/test/java/kitchenpos/application/ServiceTest.java
@@ -5,9 +5,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
-import org.springframework.test.annotation.DirtiesContext;
 
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 @JdbcTest(includeFilters = {
         @ComponentScan.Filter(
                 type = FilterType.ANNOTATION,
