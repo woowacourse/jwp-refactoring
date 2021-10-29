4bf82b4d58ebf37a3b3dbe9568967e7999ad5f9c
NewWisdom <wlgp2500@gmail.com>
2021-10-30 02:31:50 +0900

    test(menu): 메뉴 api 테스트 구현

--- src/test/java/kitchenpos/ui/ControllerTest.java
+++ src/test/java/kitchenpos/ui/ControllerTest.java
@@ -2,10 +2,8 @@ package kitchenpos.ui;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(ProductRestController.class)
 public abstract class ControllerTest {
 
     @Autowired
--- src/test/java/kitchenpos/ui/MenuRestControllerTest.java
+++ src/test/java/kitchenpos/ui/MenuRestControllerTest.java
@@ -1,7 +1,7 @@
 package kitchenpos.ui;
 
-import kitchenpos.application.ProductService;
-import kitchenpos.domain.Product;
+import kitchenpos.application.MenuService;
+import kitchenpos.domain.Menu;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -11,7 +11,7 @@ import org.springframework.http.MediaType;
 import java.util.Arrays;
 import java.util.List;
 
-import static kitchenpos.ProductFixture.createProduct;
+import static kitchenpos.MenuFixture.createMenu;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -19,34 +19,34 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(ProductRestController.class)
-class ProductRestControllerTest extends ControllerTest {
+@WebMvcTest(MenuRestController.class)
+class MenuRestControllerTest extends ControllerTest {
 
     @MockBean
-    private ProductService productService;
+    private MenuService menuService;
 
-    @DisplayName("상품을 생성할 수 있다.")
+    @DisplayName("메뉴를 생성할 수 있다.")
     @Test
     void create() throws Exception {
-        Product product = createProduct();
-        Product savedProduct = createProduct(1L);
-        when(productService.create(any())).thenReturn(savedProduct);
+        Menu menu = createMenu();
+        Menu savedMenu = createMenu(1L);
+        when(menuService.create(any())).thenReturn(savedMenu);
 
-        mockMvc.perform(post("/api/products")
-                .content(objectMapper.writeValueAsString(product))
+        mockMvc.perform(post("/api/menus")
+                .content(objectMapper.writeValueAsString(menu))
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated())
-                .andExpect(content().json(objectMapper.writeValueAsString(savedProduct)));
+                .andExpect(content().json(objectMapper.writeValueAsString(savedMenu)));
     }
 
-    @DisplayName("상품 목록을 조회할 수 있다.")
+    @DisplayName("메뉴 목록을 조회할 수 있다.")
     @Test
     void list() throws Exception {
-        List<Product> products = Arrays.asList(createProduct(1L), createProduct(2L));
-        when(productService.list()).thenReturn(products);
+        List<Menu> menus = Arrays.asList(createMenu(1L), createMenu(2L));
+        when(menuService.list()).thenReturn(menus);
 
-        mockMvc.perform(get("/api/products"))
+        mockMvc.perform(get("/api/menus"))
                 .andExpect(status().isOk())
-                .andExpect(content().json(objectMapper.writeValueAsString(products)));
+                .andExpect(content().json(objectMapper.writeValueAsString(menus)));
     }
 }
