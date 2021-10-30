24b2ba01dc6e058525e0a71e5a46745a0e2b81bb
pomo <bosl95@naver.com>
2021-10-30 14:37:47 +0900

    feat: TableController 테스트 구현

--- src/main/java/kitchenpos/ui/TableRestController.java
+++ src/main/java/kitchenpos/ui/TableRestController.java
@@ -20,26 +20,20 @@ public class TableRestController {
     public ResponseEntity<OrderTable> create(@RequestBody final OrderTable orderTable) {
         final OrderTable created = tableService.create(orderTable);
         final URI uri = URI.create("/api/tables/" + created.getId());
-        return ResponseEntity.created(uri)
-                .body(created)
-                ;
+        return ResponseEntity.created(uri).body(created);
     }
 
     @GetMapping("/api/tables")
     public ResponseEntity<List<OrderTable>> list() {
-        return ResponseEntity.ok()
-                .body(tableService.list())
-                ;
+        return ResponseEntity.ok().body(tableService.list());
     }
 
     @PutMapping("/api/tables/{orderTableId}/empty")
     public ResponseEntity<OrderTable> changeEmpty(
             @PathVariable final Long orderTableId,
-            @RequestBody final OrderTable orderTable
-    ) {
-        return ResponseEntity.ok()
-                .body(tableService.changeEmpty(orderTableId, orderTable))
-                ;
+            @RequestBody final OrderTable orderTable)
+    {
+        return ResponseEntity.ok().body(tableService.changeEmpty(orderTableId, orderTable));
     }
 
     @PutMapping("/api/tables/{orderTableId}/number-of-guests")
@@ -47,8 +41,6 @@ public class TableRestController {
             @PathVariable final Long orderTableId,
             @RequestBody final OrderTable orderTable
     ) {
-        return ResponseEntity.ok()
-                .body(tableService.changeNumberOfGuests(orderTableId, orderTable))
-                ;
+        return ResponseEntity.ok().body(tableService.changeNumberOfGuests(orderTableId, orderTable));
     }
 }
---
+++ src/test/java/kitchenpos/ui/TableRestControllerTest.java
@@ -0,0 +1,123 @@
+package kitchenpos.ui;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import kitchenpos.application.KitchenPosTestFixture;
+import kitchenpos.application.TableService;
+import kitchenpos.domain.OrderTable;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ContextConfiguration(classes = TableRestController.class)
+@WebMvcTest(TableRestController.class)
+class TableRestControllerTest extends KitchenPosTestFixture {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private TableService tableService;
+
+    private OrderTable firstTable;
+    private OrderTable secondTable;
+
+    @BeforeEach
+    void setUp() {
+        firstTable = 주문_테이블을_저장한다(1L, 1L, 3, false);
+        secondTable = 주문_테이블을_저장한다(2L, 1L, 3, true);
+    }
+
+    @Test
+    void create() throws Exception {
+        // given
+        // when
+        given(tableService.create(any(OrderTable.class))).willReturn(firstTable);
+
+        // then
+        mvc.perform(post("/api/tables")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(firstTable)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id", is(firstTable.getId().intValue())))
+                .andExpect(jsonPath("$.tableGroupId", is(firstTable.getTableGroupId().intValue())))
+                .andExpect(jsonPath("$.numberOfGuests", is(firstTable.getNumberOfGuests())))
+                .andExpect(jsonPath("$.empty", is(firstTable.isEmpty())));
+    }
+
+    @Test
+    void list() throws Exception {
+        // given
+        List<OrderTable> orderTables = Arrays.asList(firstTable, secondTable);
+
+        // when
+        given(tableService.list()).willReturn(orderTables);
+
+        // then
+        mvc.perform(MockMvcRequestBuilders.get("/api/tables")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].id", is(firstTable.getId().intValue())))
+                .andExpect(jsonPath("$[1].id", is(secondTable.getId().intValue())));
+    }
+
+    @Test
+    void changeEmpty() throws Exception {
+        // given
+        // when
+        secondTable.setEmpty(true);
+        given(tableService.changeEmpty(any(Long.class), any(OrderTable.class))).willReturn(firstTable);
+
+        // then
+        mvc.perform(put("/api/tables/{orderTableId}/empty", 1)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(firstTable)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(firstTable.getId().intValue())))
+                .andExpect(jsonPath("$.tableGroupId", is(firstTable.getTableGroupId().intValue())))
+                .andExpect(jsonPath("$.numberOfGuests", is(firstTable.getNumberOfGuests())))
+                .andExpect(jsonPath("$.empty", is(firstTable.isEmpty())));
+    }
+
+    @Test
+    void changeNumberOfGuests() throws Exception {
+        // given
+        // when
+        firstTable.setNumberOfGuests(7);
+        given(tableService.changeNumberOfGuests(any(Long.class), any(OrderTable.class))).willReturn(firstTable);
+
+        // then
+        mvc.perform(put("/api/tables/{orderTableId}/number-of-guests", 1L)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(firstTable)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(firstTable.getId().intValue())))
+                .andExpect(jsonPath("$.tableGroupId", is(firstTable.getTableGroupId().intValue())))
+                .andExpect(jsonPath("$.numberOfGuests", is(firstTable.getNumberOfGuests())))
+                .andExpect(jsonPath("$.empty", is(firstTable.isEmpty())));
+    }
+}
