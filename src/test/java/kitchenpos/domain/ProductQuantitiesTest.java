b108fca581b0de06a4ae1b487ab84b09588b07ba
seovalue <seominjeong.dev@gmail.com>
2021-10-31 21:39:47 +0900

    refactor: ProductQuantities 단위테스트 추가

--- src/test/java/kitchenpos/domain/ProductQuantitiesTest.java
+++ src/test/java/kitchenpos/domain/ProductQuantitiesTest.java
@@ -4,31 +4,60 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class ProductQuantitiesTest {
 
+    private BigDecimal expectedTotalPrice;
+    private ProductQuantities productQuantities;
+
     @BeforeEach
     void setUp() {
+        BigDecimal priceOfChicken = BigDecimal.valueOf(10_000);
+        long quantityOfChicken = 10L;
+
+        BigDecimal priceOfPizza = BigDecimal.valueOf(15_000);
+        long quantityOfPizza = 5L;
+
+        expectedTotalPrice = BigDecimal.ZERO;
+        expectedTotalPrice = expectedTotalPrice.add(priceOfChicken.multiply(BigDecimal.valueOf(quantityOfChicken)))
+                .add(priceOfPizza.multiply(BigDecimal.valueOf(quantityOfPizza)));
+
+
+        List<ProductQuantity> productQuantityList = Arrays.asList(
+                new ProductQuantity(
+                        new Product(1L, "대왕치킨", priceOfChicken),
+                        quantityOfChicken
+                ),
+                new ProductQuantity(
+                        new Product(1L, "대왕피자", BigDecimal.valueOf(15_000)),
+                        quantityOfPizza
+                )
+        );
+
+        productQuantities = new ProductQuantities(productQuantityList);
     }
 
-    @DisplayName("")
+    @DisplayName("포함된 제품에 수량을 곱한 전체 가격을 계산한다.")
     @Test
     void totalPrice() {
-        // given
-
-        // when
-
-        // then
+        // when - then
+        assertThat(productQuantities.totalPrice()).isEqualTo(expectedTotalPrice);
     }
 
-    @DisplayName("")
+    @DisplayName("MenuProduct로 그룹화한다.")
     @Test
     void groupToMenuProduct() {
         // given
+        Menu menu = new Menu(1L, "대왕메뉴", BigDecimal.valueOf(175_000), new MenuGroup("대왕그룹"));
+        List<MenuProduct> menuProducts
+                = productQuantities.groupToMenuProduct(menu);
 
-        // when
-
-        // then
+        // when - then
+        assertThat(menuProducts).hasSize(2);
     }
 }
