90b5db413b90793e60d6eb16564748ead09b367b
MyaGya <38939015+MyaGya@users.noreply.github.com>
2021-10-31 17:27:16 +0900

    feat: 상품 테스트 코드 추가

---
+++ src/test/java/kitchenpos/ProductServiceTest.java
@@ -0,0 +1,46 @@
+package kitchenpos;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.math.BigDecimal;
+import kitchenpos.application.ProductService;
+import kitchenpos.domain.Product;
+import kitchenpos.fixture.ProductFixture;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+
+public class ProductServiceTest extends ServiceTest {
+
+    @InjectMocks
+    private ProductService productService;
+
+    @DisplayName("상품을 생성할 수 있다")
+    @Test
+    void create() {
+        productService.create(ProductFixture.product());
+    }
+
+    @DisplayName("상품 가격이 null일 수 없다")
+    @Test
+    void createNull() {
+        Product product = new Product();
+
+        assertThatThrownBy(() -> productService.create(product)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("상품 가격이 0보다 작을 수 없다")
+    @Test
+    void createMinus() {
+        Product product = new Product(0L, "name", BigDecimal.valueOf(-1));
+
+        assertThatThrownBy(() -> productService.create(product)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("상품 가격이 0보다 작을 수 없다")
+    @Test
+    void findAll() {
+
+        productService.list();
+    }
+}
