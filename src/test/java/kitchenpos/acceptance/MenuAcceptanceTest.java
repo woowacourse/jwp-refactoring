1b5d21071161bea1601d6777bcf0e5ee742bd96c
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2021-11-01 17:49:55 +0900

    test(MenuAcceptanceTest): 테스트 내용 보강

--- src/test/java/kitchenpos/acceptance/MenuAcceptanceTest.java
+++ src/test/java/kitchenpos/acceptance/MenuAcceptanceTest.java
@@ -59,6 +59,8 @@ class MenuAcceptanceTest extends AcceptanceTest {
         MenuResponse 응답된_메뉴 = response.getBody();
         assertThat(응답된_메뉴.getName()).isEqualTo("두마리메뉴_후라이드_양념치킨");
         assertThat(응답된_메뉴.getMenuProducts()).hasSize(2);
+        assertThat(응답된_메뉴.getMenuProducts().get(0).getProductId()).isEqualTo(후라이드치킨.getId());
+        assertThat(응답된_메뉴.getMenuProducts().get(1).getProductId()).isEqualTo(양념치킨.getId());
     }
 
     @DisplayName("메뉴 그룹 카테고리를 생성 시, 메뉴 그룹의 가격 총합이 개별 메뉴 가격의 총합보다 크다면 예외가 발생한다.")
