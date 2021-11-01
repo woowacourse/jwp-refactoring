fbbcb0f8f8e0c0e092de06616d3ba3e7bb065cb7
Bepoz <45073750+Be-poz@users.noreply.github.com>
2021-11-01 19:58:16 +0900

    refactor: 도메인 연관관계 필드 추가2

--- src/main/java/kitchenpos/domain/MenuGroup.java
+++ src/main/java/kitchenpos/domain/MenuGroup.java
@@ -22,23 +22,11 @@ public class MenuGroup {
         this.name = name;
     }
 
-    public MenuGroup(String name) {
-        this(null, name);
-    }
-
     public Long getId() {
         return id;
     }
 
-    public void setId(final Long id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(final String name) {
-        this.name = name;
     }
 }
