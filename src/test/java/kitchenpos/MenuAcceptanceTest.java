d5ddd9dd83dc353d406a49c553780a85f44d9d8b
unluckyjung <ybook2012@gmail.com>
2021-11-03 02:04:41 +0900

    test: 실패하는 메뉴 통합테스트 구현

--- src/test/java/kitchenpos/MenuAcceptanceTest.java
+++ src/test/java/kitchenpos/MenuAcceptanceTest.java
@@ -12,7 +12,10 @@ import kitchenpos.domain.Menu;
 import kitchenpos.domain.MenuProduct;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -35,7 +38,7 @@ public class MenuAcceptanceTest extends ApplicationTest {
 
     @DisplayName("메뉴 등록에 성공하면 201 응답을 받는다.")
     @Test
-    void createMenuGroup() {
+    void createMenu() {
         //when
         ExtractableResponse<Response> response = 메뉴_추가(기본_메뉴);
 
@@ -45,7 +48,7 @@ public class MenuAcceptanceTest extends ApplicationTest {
 
     @DisplayName("전체 메뉴를 불러오는데 성공하면, 200 응답을 받는다.")
     @Test
-    void getMenuGroup() {
+    void getMenus() {
 
         //given
         ExtractableResponse<Response> response = 전체_메뉴_조회();
@@ -55,7 +58,6 @@ public class MenuAcceptanceTest extends ApplicationTest {
 
         //then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
-        assertThat(menus).hasSize(6);  // defaultData 6
     }
 
     private List<MenuProduct> getMenuProducts(final Long productId, final long quantity) {
@@ -66,13 +68,72 @@ public class MenuAcceptanceTest extends ApplicationTest {
         return Collections.singletonList(menuProduct);
     }
 
+    @DisplayName("잘못된 ")
+    @Nested
+    class FailTest {
+
+        @DisplayName("가격으로 메뉴 등록을 시도하면 500 응답을 받는다.")
+        @ValueSource(longs = {-1, 30000})
+        @ParameterizedTest
+        void createMenuWithWrongPrice(final long wrongPrice) {
+            //given
+            Menu menu = new Menu();
+            menu.setName("포츈후라이드");
+            menu.setPrice(BigDecimal.valueOf(wrongPrice));
+            menu.setMenuGroupId(1L);
+            menu.setMenuProducts(getMenuProducts(1L, 1));
+
+            //when
+            ExtractableResponse<Response> response = 메뉴_추가(menu);
+
+            //then
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        }
+
+        @DisplayName("그룹 id로 메뉴 등록을 시도하면 500 응답을 받는다.")
+        @ValueSource(longs = {-1, 30000})
+        @ParameterizedTest
+        void createMenuWithWrongGroupId(final Long wrongGroupId) {
+            //given
+            Menu menu = new Menu();
+            menu.setName("포츈후라이드");
+            menu.setPrice(BigDecimal.valueOf(1000));
+            menu.setMenuGroupId(wrongGroupId);
+            menu.setMenuProducts(getMenuProducts(1L, 1));
+
+            //when
+            ExtractableResponse<Response> response = 메뉴_추가(menu);
+
+            //then
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        }
+
+        @DisplayName("상품 정보로 메뉴 등록을 시도하면 500 응답을 받는다.")
+        @ValueSource(longs = {-1, 30000})
+        @ParameterizedTest
+        void createMenuWithWrongProductId(final Long wrongProductId) {
+            //given
+            Menu menu = new Menu();
+            menu.setName("포츈후라이드");
+            menu.setPrice(BigDecimal.valueOf(1000));
+            menu.setMenuGroupId(1L);
+            menu.setMenuProducts(getMenuProducts(wrongProductId, 1));
+
+            //when
+            ExtractableResponse<Response> response = 메뉴_추가(menu);
+
+            //then
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        }
+    }
+
     private ExtractableResponse<Response> 메뉴_추가(final Menu menu) {
         return RestAssured
             .given().log().all()
             .body(menu)
             .contentType(MediaType.APPLICATION_JSON_VALUE)
             .post("/api/menus")
-            .then()
+            .then().log().all()
             .extract();
     }
 
