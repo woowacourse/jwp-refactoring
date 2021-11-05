d44e6c1e7eb5322c6927f9f4642c32681c315a86
ecsimsw <ecsimsw@gmail.com>
2021-11-05 17:41:38 +0900

    feat : MenuServiceTest 모킹 제거

--- src/test/java/kitchenpos/application/MenuServiceTest.java
+++ src/test/java/kitchenpos/application/MenuServiceTest.java
@@ -1,21 +1,17 @@
 package kitchenpos.application;
 
 import static kitchenpos.fixture.MenuFixture.createMenu;
+import static kitchenpos.fixture.MenuFixture.createMenuProduct;
+import static kitchenpos.fixture.MenuGroupFixture.createMenuGroup;
 import static kitchenpos.fixture.ProductFixture.createProduct;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
 
 import java.math.BigDecimal;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
-import kitchenpos.ServiceTest;
-import kitchenpos.dao.MenuDao;
 import kitchenpos.dao.MenuGroupDao;
-import kitchenpos.dao.MenuProductDao;
 import kitchenpos.dao.ProductDao;
 import kitchenpos.domain.Menu;
 import kitchenpos.domain.MenuProduct;
@@ -24,60 +20,67 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.mockito.AdditionalAnswers;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
 
-@ServiceTest
+@Sql("classpath:db/test/truncate.sql")
+@ActiveProfiles("test")
+@SpringBootTest
 class MenuServiceTest {
 
-    @Mock
-    private MenuDao mockMenuDao;
+    @Autowired
+    private MenuGroupDao menuGroupDao;
 
-    @Mock
-    private MenuGroupDao mockMenuGroupDao;
+    @Autowired
+    private ProductDao productDao;
 
-    @Mock
-    private MenuProductDao mockMenuProductDao;
-
-    @Mock
-    private ProductDao mockProductDao;
-
-    @InjectMocks
+    @Autowired
     private MenuService menuService;
+    private Long menuGroupId;
+    private List<MenuProduct> menuProducts;
+
+    @BeforeEach
+    void setUp() {
+        menuGroupId = menuGroupDao.save(createMenuGroup()).getId();
+        Product product = productDao.save(createProduct());
+        menuProducts = Arrays.asList(
+                createMenuProduct(1L, null, product.getId(), 1),
+                createMenuProduct(2L, null, product.getId(), 1)
+        );
+    }
 
     @DisplayName("메뉴 생성")
     @Nested
     class CreateMenu {
 
-        @BeforeEach
-        void setUp() {
-            when(mockMenuDao.save(any())).then(AdditionalAnswers.returnsFirstArg());
-            when(mockProductDao.save(any())).then(AdditionalAnswers.returnsFirstArg());
-            when(mockMenuGroupDao.existsById(any())).thenReturn(true);
-            when(mockProductDao.findById(any())).thenReturn(Optional.of(createProduct()));
-        }
-
         @DisplayName("메뉴를 생성한다.")
         @Test
         void create() {
-            Menu menu = createMenu();
+            Menu menu = createMenu(menuGroupId, menuProducts);
             Menu savedMenu = menuService.create(menu);
-            assertThat(savedMenu).isEqualTo(menu);
+            assertAll(
+                    () -> assertThat(savedMenu.getId()).isNotNull(),
+                    () -> assertThat(savedMenu.getName()).isEqualTo(menu.getName()),
+                    () -> assertThat(savedMenu.getPrice().compareTo(menu.getPrice())).isEqualTo(0),
+                    () -> assertThat(savedMenu.getMenuGroupId()).isEqualTo(menu.getMenuGroupId()),
+                    () -> assertThat(savedMenu.getMenuProducts()).hasSize(menu.getMenuProducts().size())
+            );
         }
 
         @DisplayName("메뉴 가격은 음수일 수 없다.")
         @Test
         void createWithInvalidPrice1() {
             BigDecimal price = BigDecimal.valueOf(-1);
-            Menu menu = createMenu(price);
+            Menu menu = createMenu(price, menuGroupId, menuProducts);
             assertThatThrownBy(() -> menuService.create(menu)).isInstanceOf(IllegalArgumentException.class);
         }
 
         @DisplayName("메뉴 상품 가격의 합보다 큰 가격으로 메뉴를 생성할 수 없다.")
         @Test
         void createWithInvalidPrice2() {
-            Menu menu = createMenu();
+            Menu menu = createMenu(menuGroupId, menuProducts);
             BigDecimal price = sumOfProductPrice(menu.getMenuProducts()).add(BigDecimal.ONE);
             menu.setPrice(price);
             assertThatThrownBy(() -> menuService.create(menu)).isInstanceOf(IllegalArgumentException.class);
@@ -86,15 +89,15 @@ class MenuServiceTest {
         @DisplayName("존재하지 않는 메뉴 그룹에 메뉴를 생성할 수 없다.")
         @Test
         void createWithInvalidMenuGroup() {
-            Menu menu = createMenu();
-            when(mockMenuGroupDao.existsById(menu.getId())).thenReturn(false);
+            Long menuGroupId = Long.MAX_VALUE;
+            Menu menu = createMenu(menuGroupId, menuProducts);
             assertThatThrownBy(() -> menuService.create(menu)).isInstanceOf(IllegalArgumentException.class);
         }
 
         private BigDecimal sumOfProductPrice(List<MenuProduct> menuProducts) {
             BigDecimal sum = BigDecimal.ZERO;
             for (MenuProduct menuProduct : menuProducts) {
-                Product product = mockProductDao.findById(menuProduct.getProductId()).get();
+                Product product = productDao.findById(menuProduct.getProductId()).get();
                 sum = sum.add(product.getPrice().multiply(BigDecimal.valueOf(menuProduct.getQuantity())));
             }
             return sum;
@@ -104,13 +107,13 @@ class MenuServiceTest {
     @DisplayName("메뉴 목록을 출력한다.")
     @Test
     void list() {
-        Menu menu = createMenu();
-        when(mockMenuDao.findAll()).thenReturn(Collections.singletonList(menu));
-        when(mockMenuProductDao.findAllByMenuId(menu.getId())).thenReturn(menu.getMenuProducts());
+        Menu menu1 = menuService.create(createMenu(menuGroupId, menuProducts));
+        Menu menu2 = menuService.create(createMenu(menuGroupId, menuProducts));
+
         List<Menu> list = menuService.list();
         assertAll(
-                () -> assertThat(list).hasSize(1),
-                () -> assertThat(list).contains(menu)
+                () -> assertThat(list).hasSize(2),
+                () -> assertThat(list.stream().map(Menu::getId)).contains(menu1.getId(), menu2.getId())
         );
     }
 }
