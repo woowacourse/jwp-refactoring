e3aaa5ac627b45095cac039dd7e888b39c91af3a
youngbin <thisisyoungbin@gmail.com>
2021-11-05 19:20:08 +0900

    refactor: TableGroupServiceTest 수정

--- src/test/java/kitchenpos/TableGroupServiceTest.java
+++ src/test/java/kitchenpos/TableGroupServiceTest.java
@@ -3,23 +3,28 @@ package kitchenpos;
 import kitchenpos.application.TableGroupService;
 import kitchenpos.dao.OrderDao;
 import kitchenpos.dao.OrderTableDao;
+import kitchenpos.dao.TableGroupDao;
 import kitchenpos.domain.Order;
 import kitchenpos.domain.OrderStatus;
 import kitchenpos.domain.OrderTable;
 import kitchenpos.domain.TableGroup;
+import kitchenpos.fixture.OrderFixture;
+import kitchenpos.fixture.OrderTableFixture;
+import kitchenpos.fixture.TableGroupFixture;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
-import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
+@Transactional
 @DisplayName("TableGroupService 테스트")
 class TableGroupServiceTest {
 
@@ -30,24 +35,20 @@ class TableGroupServiceTest {
     private OrderDao orderDao;
 
     @Autowired
-    private OrderTableDao tableDao;
+    private OrderTableDao orderTableDao;
 
-    private OrderTable table1;
-    private OrderTable table2;
+    private OrderTable orderTable1;
+    private OrderTable orderTable2;
     private TableGroup tableGroup;
 
     @BeforeEach
     void setUp() {
-        OrderTable table = OrderTable.builder()
-                .empty(true)
-                .build();
-        table1 = tableDao.save(table);
-        table2 = tableDao.save(table);
+        OrderTable orderTable1 = OrderTableFixture.create(null, null, 1, true);
+        OrderTable orderTable2 = OrderTableFixture.create(null, null, 1, true);
+        this.orderTable1 = orderTableDao.save(orderTable1);
+        this.orderTable2 = orderTableDao.save(orderTable2);
 
-        tableGroup = TableGroup.builder()
-                .orderTables(Arrays.asList(table1, table2))
-                .createdDate(LocalDateTime.now())
-                .build();
+        tableGroup = TableGroupFixture.create(this.orderTable1, this.orderTable2);
     }
 
     @DisplayName("테이블 그룹 추가 - 성공")
@@ -65,19 +66,10 @@ class TableGroupServiceTest {
     @Test
     void createFailure() {
         //given
-        OrderTable notEmptyTable = OrderTable.builder()
-                .empty(false)
-                .build();
-        OrderTable table2 = OrderTable.builder()
-                .empty(true)
-                .build();
-        tableDao.save(notEmptyTable);
-        tableDao.save(table2);
+        OrderTable notEmptyTable = OrderTableFixture.create();
+        orderTableDao.save(notEmptyTable);
         //when
-        TableGroup tableGroup = TableGroup.builder()
-                .orderTables(Arrays.asList(notEmptyTable, table2))
-                .createdDate(LocalDateTime.now())
-                .build();
+        TableGroup tableGroup = TableGroupFixture.create(orderTable1, notEmptyTable);
         //then
         assertThatThrownBy(() -> tableGroupService.create(tableGroup))
                 .isInstanceOf(IllegalArgumentException.class);
@@ -87,15 +79,8 @@ class TableGroupServiceTest {
     @Test
     void createFailureWhenInvalidOrderTablesSize() {
         //given
-        OrderTable notEmptyTable = OrderTable.builder()
-                .empty(true)
-                .build();
-        tableDao.save(notEmptyTable);
         //when
-        TableGroup tableGroup = TableGroup.builder()
-                .orderTables(Arrays.asList(notEmptyTable))
-                .createdDate(LocalDateTime.now())
-                .build();
+        TableGroup tableGroup = TableGroupFixture.create(orderTable1);
         //then
         assertThatThrownBy(() -> tableGroupService.create(tableGroup))
                 .isInstanceOf(IllegalArgumentException.class);
@@ -105,18 +90,10 @@ class TableGroupServiceTest {
     @Test
     void createFailureWhenNotExistTable() {
         //given
-        OrderTable notExistTable = OrderTable.builder()
-                .id(100L)
-                .empty(true)
-                .build();
-        OrderTable table1 = OrderTable.builder()
-                .empty(true)
-                .build();
+        OrderTable orderTable1 = OrderTableFixture.create(null, 99L, 1, true);
+        OrderTable orderTable2 = OrderTableFixture.create(null, 99L, 1, true);
+        TableGroup tableGroup = TableGroupFixture.create(orderTable1, orderTable2);
         //when
-        TableGroup tableGroup = TableGroup.builder()
-                .orderTables(Arrays.asList(notExistTable, table1))
-                .createdDate(LocalDateTime.now())
-                .build();
         //then
         assertThatThrownBy(() -> tableGroupService.create(tableGroup))
                 .isInstanceOf(IllegalArgumentException.class);
@@ -125,19 +102,12 @@ class TableGroupServiceTest {
     @DisplayName("테이블 그룹 추가 - 실패 - 다른 그룹의 테이블을 포함한 테이블 그룹 추가")
     @Test
     void create_Fail_With_OthersTable() {
-        OrderTable othersTable = OrderTable.builder()
-                .empty(true)
-                .build();
-        OrderTable table = OrderTable.builder()
-                .empty(true)
-                .build();
-        tableDao.save(othersTable);
-        tableDao.save(table);
+        OrderTable othersTable = OrderTableFixture.create();
+        OrderTable table = OrderTableFixture.create();
+        orderTableDao.save(othersTable);
+        orderTableDao.save(table);
 
-        TableGroup tableGroup = TableGroup.builder()
-                .orderTables(Arrays.asList(othersTable, table))
-                .createdDate(LocalDateTime.now())
-                .build();
+        TableGroup tableGroup = TableGroupFixture.create(othersTable, table);
 
         assertThatThrownBy(() -> tableGroupService.create(tableGroup))
                 .isInstanceOf(IllegalArgumentException.class);
@@ -150,8 +120,8 @@ class TableGroupServiceTest {
         TableGroup savedTableGroup = tableGroupService.create(tableGroup);
         //when
         tableGroupService.ungroup(savedTableGroup.getId());
-        OrderTable ungroupedTable1 = tableDao.findById(table1.getId()).get();
-        OrderTable ungroupedTable2 = tableDao.findById(table2.getId()).get();
+        OrderTable ungroupedTable1 = orderTableDao.findById(orderTable1.getId()).get();
+        OrderTable ungroupedTable2 = orderTableDao.findById(orderTable2.getId()).get();
         //then
         assertThat(ungroupedTable1.getTableGroupId()).isNull();
         assertThat(ungroupedTable2.getTableGroupId()).isNull();
@@ -162,11 +132,7 @@ class TableGroupServiceTest {
     @DisplayName("TableGroup 해제 테스트 - 실패 - 조리, 식사 중인 테이블을 포함한 테이블 그룹 해제")
     @Test
     void ungroup_Fail_With_TableInProgress() {
-        Order order = Order.builder()
-                .orderTableId(table1.getId())
-                .orderStatus(OrderStatus.COOKING.name())
-                .orderedTime(LocalDateTime.now())
-                .build();
+        Order order = OrderFixture.create(null, orderTable1.getId(), OrderStatus.COOKING.name(), LocalDateTime.now(), OrderFixture.orderLineItems());
         orderDao.save(order);
 
         TableGroup savedTableGroup = tableGroupService.create(tableGroup);
