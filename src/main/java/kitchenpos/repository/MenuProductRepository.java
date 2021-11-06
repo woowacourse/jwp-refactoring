12611947fa310b6845eddba6fcef49e6f9b0ea91
youngbin <thisisyoungbin@gmail.com>
2021-11-07 01:23:20 +0900

    refactor: MenuProductRepository 내 findAllByMenuId 메서드 추가

--- src/main/java/kitchenpos/repository/MenuProductRepository.java
+++ src/main/java/kitchenpos/repository/MenuProductRepository.java
@@ -2,9 +2,13 @@ package kitchenpos.repository;
 
 import kitchenpos.domain.MenuProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface MenuProductRepository extends JpaRepository<MenuProduct, Long> {
-
+    @Query("select mp from MenuProduct mp where mp.menu.id = :menuId")
+    List<MenuProduct> findAllByMenuId(Long menuId);
 }
