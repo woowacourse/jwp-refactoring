901341cf0f1fc8dcbc1d87311d636982b4232b8b
youngbin <thisisyoungbin@gmail.com>
2021-11-06 23:28:42 +0900

    feat: MenuRepository, MenuProductRepository 구현

---
+++ src/main/java/kitchenpos/repository/MenuProductRepository.java
@@ -0,0 +1,10 @@
+package kitchenpos.repository;
+
+import kitchenpos.domain.MenuProduct;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MenuProductRepository extends JpaRepository<MenuProduct, Long> {
+
+}
