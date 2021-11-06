3ab37096f238dcb1995b3f03877d781df351b004
youngbin <thisisyoungbin@gmail.com>
2021-11-06 23:29:08 +0900

    feat: Menu 관련 DTO 구현(request, response)

---
+++ src/main/java/kitchenpos/ui/dto/MenuRequest.java
@@ -0,0 +1,46 @@
+package kitchenpos.ui.dto;
+
+import kitchenpos.domain.Menu;
+import kitchenpos.domain.MenuGroup;
+import kitchenpos.domain.MenuProduct;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class MenuRequest {
+
+    private String name;
+    private BigDecimal price;
+    private Long menuGroupId;
+    private List<MenuProduct> menuProducts;
+
+    public MenuRequest() {
+    }
+
+    public MenuRequest(String name, BigDecimal price, Long menuGroupId, List<MenuProduct> menuProducts) {
+        this.name = name;
+        this.price = price;
+        this.menuGroupId = menuGroupId;
+        this.menuProducts = menuProducts;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public Long getMenuGroupId() {
+        return menuGroupId;
+    }
+
+    public List<MenuProduct> getMenuProducts() {
+        return menuProducts;
+    }
+
+    public Menu toEntity() {
+        return new Menu(name, price);
+    }
+}
---
+++ src/main/java/kitchenpos/ui/dto/MenuResponse.java
@@ -0,0 +1,51 @@
+package kitchenpos.ui.dto;
+
+import kitchenpos.domain.Menu;
+import kitchenpos.domain.MenuProduct;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class MenuResponse {
+
+    private Long id;
+    private String name;
+    private BigDecimal price;
+    private Long menuGroupId;
+    private List<MenuProduct> menuProducts;
+
+    public MenuResponse() {
+    }
+
+    public MenuResponse(Long id, String name, BigDecimal price, Long menuGroupId, List<MenuProduct> menuProducts) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.menuGroupId = menuGroupId;
+        this.menuProducts = menuProducts;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public Long getMenuGroupId() {
+        return menuGroupId;
+    }
+
+    public List<MenuProduct> getMenuProducts() {
+        return menuProducts;
+    }
+
+    public MenuResponse from(Menu menu) {
+        return new MenuResponse(menu.getId(), menu.getName(), menu.getPrice(), menu.getMenuGroupId(), menu.getMenuProducts());
+    }
+}
