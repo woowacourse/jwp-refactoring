049b4c9445661e1178a439d8cb08e7c21b04c35c
youngbin <thisisyoungbin@gmail.com>
2021-11-07 03:25:12 +0900

    refactor: OrderTable 엔티티 설정

--- src/main/java/kitchenpos/domain/OrderTable.java
+++ src/main/java/kitchenpos/domain/OrderTable.java
@@ -1,40 +1,73 @@
 package kitchenpos.domain;
 
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
 public class OrderTable {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Long tableGroupId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private TableGroup tableGroup;
     private int numberOfGuests;
     private boolean empty;
+
+    public OrderTable() {
+    }
+
+    public OrderTable(Integer numberOfGuest, Boolean empty) {
+        this(null, null, numberOfGuest, empty);
+    }
+
+    public OrderTable(Long id, TableGroup tableGroup, int numberOfGuests, boolean empty) {
+        this.id = id;
+        this.tableGroup = tableGroup;
+        this.numberOfGuests = numberOfGuests;
+        this.empty = empty;
+    }
+
+    public void checkNotGrouped() {
+        if (Objects.nonNull(tableGroup)) {
+            throw new IllegalArgumentException("테이블 그룹이 존재합니다.");
+        }
+    }
+
+    public void changeEmpty(Boolean empty) {
+        this.empty = empty;
+    }
+
+    public void changeNumberOfGuests(Integer numberOfGuest) {
+        validateEmpty();
+        validateNumberOfGuests();
+        this.numberOfGuests = numberOfGuest;
+    }
+
+    private void validateEmpty() {
+        if (empty) {
+            throw new IllegalArgumentException("빈 테이블 입니다.");
+        }
+    }
+
+    private void validateNumberOfGuests() {
+        if (numberOfGuests < 0) {
+            throw new IllegalArgumentException("변경할 수 없는 손님수 입니다.");
+        }
+    }
 
     public Long getId() {
         return id;
     }
 
-    public void setId(final Long id) {
-        this.id = id;
-    }
-
     public Long getTableGroupId() {
-        return tableGroupId;
-    }
-
-    public void setTableGroupId(final Long tableGroupId) {
-        this.tableGroupId = tableGroupId;
+        return tableGroup.getId();
     }
 
     public int getNumberOfGuests() {
         return numberOfGuests;
     }
 
-    public void setNumberOfGuests(final int numberOfGuests) {
-        this.numberOfGuests = numberOfGuests;
-    }
-
     public boolean isEmpty() {
         return empty;
-    }
-
-    public void setEmpty(final boolean empty) {
-        this.empty = empty;
     }
 }
