390a4e3c06c37a383fd6d018458f96a360340183
youngbin <thisisyoungbin@gmail.com>
2021-11-06 21:54:21 +0900

    refactor: Product 엔티티 설정 및 Setter 제거

--- src/main/java/kitchenpos/domain/Product.java
+++ src/main/java/kitchenpos/domain/Product.java
@@ -1,33 +1,44 @@
 package kitchenpos.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.math.BigDecimal;
+import java.util.Objects;
 
+@Entity
 public class Product {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private BigDecimal price;
 
-    public Long getId() {
-        return id;
+    public Product() {
     }
 
-    public void setId(final Long id) {
-        this.id = id;
+    public Product(String name, BigDecimal price) {
+        validatePrice(price);
+        this.name = name;
+        this.price = price;
+    }
+
+    private void validatePrice(BigDecimal price) {
+        if (Objects.isNull(price) || price.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("잘못된 요청입니다.");
+        }
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(final String name) {
-        this.name = name;
-    }
-
     public BigDecimal getPrice() {
         return price;
-    }
-
-    public void setPrice(final BigDecimal price) {
-        this.price = price;
     }
 }
