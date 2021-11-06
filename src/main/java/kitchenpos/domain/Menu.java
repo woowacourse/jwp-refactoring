ce67cdf9b6f7a39c4b140189580dd647709cdade
youngbin <thisisyoungbin@gmail.com>
2021-11-07 00:51:12 +0900

    refactor: Menu 및 MenuProduct 엔티티 설정 및 Setter 제거

--- src/main/java/kitchenpos/domain/Menu.java
+++ src/main/java/kitchenpos/domain/Menu.java
@@ -1,52 +1,78 @@
 package kitchenpos.domain;
 
+import javax.persistence.*;
 import java.math.BigDecimal;
-import java.util.List;
+import java.util.Objects;
 
+@Entity
 public class Menu {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private BigDecimal price;
-    private Long menuGroupId;
-    private List<MenuProduct> menuProducts;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private MenuGroup menuGroup;
+
+    public Menu() {
+    }
+
+    public Menu(String name, BigDecimal price, MenuGroup menuGroup) {
+        this(null, name, price, menuGroup);
+    }
+
+    public Menu(Long id, String name, BigDecimal price, MenuGroup menuGroup) {
+        validateName(name);
+        validatePrice(price);
+        validateMenuGroup(menuGroup);
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.menuGroup = menuGroup;
+    }
+
+    private void validateMenuGroup(MenuGroup menuGroup) {
+        if (Objects.isNull(menuGroup)) {
+            throw new IllegalArgumentException("존재하지 않는 메뉴 그룹입니다.");
+        }
+    }
+
+    private void validateName(String name) {
+        if (Objects.isNull(name) || name.isEmpty()) {
+            throw new IllegalArgumentException("유효하지 않은 메뉴 이름입니다.");
+        }
+    }
+
+    private void validatePrice(BigDecimal price) {
+        if (Objects.isNull(price) || price.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("유효하지 않은 가격입니다.");
+        }
+    }
+
+    public void validateTotalPrice(BigDecimal totalPrice) {
+        if (this.price.compareTo(totalPrice) > 0) {
+            throw new IllegalArgumentException("메뉴가격은 상품가격보다 클 수 없습니다.");
+        }
+    }
 
     public Long getId() {
         return id;
-    }
-
-    public void setId(final Long id) {
-        this.id = id;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(final String name) {
-        this.name = name;
-    }
-
     public BigDecimal getPrice() {
         return price;
     }
 
-    public void setPrice(final BigDecimal price) {
-        this.price = price;
+    public MenuGroup getMenuGroup() {
+        return menuGroup;
     }
 
     public Long getMenuGroupId() {
-        return menuGroupId;
-    }
-
-    public void setMenuGroupId(final Long menuGroupId) {
-        this.menuGroupId = menuGroupId;
-    }
-
-    public List<MenuProduct> getMenuProducts() {
-        return menuProducts;
-    }
-
-    public void setMenuProducts(final List<MenuProduct> menuProducts) {
-        this.menuProducts = menuProducts;
+        return menuGroup.getId();
     }
 }
