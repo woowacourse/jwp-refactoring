2d20378faa500a6bc57fd1ac99f773e0192f5e83
ROOT <junroot0909@gmail.com>
2021-11-24 16:08:25 +0900

    refactor: cascadeType을 ALL로 변경

--- src/main/java/kitchenpos/menu/domain/MenuProducts.java
+++ src/main/java/kitchenpos/menu/domain/MenuProducts.java
@@ -11,7 +11,7 @@ import javax.persistence.OneToMany;
 @Embeddable
 public class MenuProducts implements Iterable<MenuProduct> {
 
-    @OneToMany(cascade = CascadeType.PERSIST)
+    @OneToMany(cascade = CascadeType.ALL)
     @JoinColumn(name = "menu_id")
     private final List<MenuProduct> elements;
 
