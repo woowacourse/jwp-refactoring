475284e905ac3a2ee76d665a0718745517141ae1
perenok <perenoke@gmail.com>
2021-11-25 15:16:07 +0900

    chore: 안 쓰는 메서드 삭제

--- src/main/java/kitchenpos/menu/domain/MenuRepository.java
+++ src/main/java/kitchenpos/menu/domain/MenuRepository.java
@@ -1,10 +1,8 @@
 package kitchenpos.menu.domain;
 
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MenuRepository extends JpaRepository<Menu, Long> {
 
-    Long countByIdIn(final List<Long> ids);
 }
 
