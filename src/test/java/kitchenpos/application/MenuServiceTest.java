f0cb5e1cfd3f8c229f1211386b8ce43d445a23fa
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-10-19 17:17:12 +0900

    test: MenuService create price 음수 예외 테스트 추가

--- src/test/java/kitchenpos/application/MenuServiceTest.java
+++ src/test/java/kitchenpos/application/MenuServiceTest.java
@@ -2,6 +2,7 @@ package kitchenpos.application;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import kitchenpos.domain.Menu;
 import org.junit.jupiter.api.Nested;
@@ -24,6 +25,19 @@ class MenuServiceTest {
         class 가격이_null이_입력될_경우 {
 
             private final Menu menu = new Menu("파닭", null, 1L, new ArrayList<>());
+
+            @Test
+            void 예외가_발생한다() {
+                assertThatThrownBy(() -> menuService.create(menu))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage("가격은 양의 정수만 들어올 수 있습니다.");
+            }
+        }
+
+        @Nested
+        class 가격이_음수가_입력될_경우 {
+
+            private final Menu menu = new Menu("파닭", new BigDecimal(-1), 1L, new ArrayList<>());
 
             @Test
             void 예외가_발생한다() {
