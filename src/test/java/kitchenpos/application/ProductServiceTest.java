2c568a14aa47f7c92f4dd5be8fc6d5b6db261c4c
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-10-19 19:12:32 +0900

    test: ProductService create price 성공 테스트 추가

--- src/test/java/kitchenpos/application/ProductServiceTest.java
+++ src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -1,5 +1,6 @@
 package kitchenpos.application;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.math.BigDecimal;
@@ -47,6 +48,18 @@ class ProductServiceTest {
                 assertThatThrownBy(() -> productService.create(product))
                         .isInstanceOf(IllegalArgumentException.class)
                         .hasMessage("가격은 양의 정수만 들어올 수 있습니다.");
+            }
+        }
+
+        @Nested
+        class 상품을_정상적으로_생성가능한_경우 {
+
+            private final Product product = new Product("후라이드", BigDecimal.valueOf(16000));
+
+            @Test
+            void 저장된_상품이_반환된다() {
+                Product actual = productService.create(product);
+                assertThat(actual).isNotNull();
             }
         }
     }
