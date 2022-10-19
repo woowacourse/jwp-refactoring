805bbbf17d3a8f36031d4ddd86a5716cd142b66a
Richard JEON <ztzy1907@gmail.com>
2022-10-19 11:59:50 +0900

    docs: 요구사항 분석 문서 작성 완료

--- src/main/java/kitchenpos/application/MenuGroupService.java
+++ src/main/java/kitchenpos/application/MenuGroupService.java
@@ -1,11 +1,10 @@
 package kitchenpos.application;
 
+import java.util.List;
 import kitchenpos.dao.MenuGroupDao;
 import kitchenpos.domain.MenuGroup;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Service
 public class MenuGroupService {
@@ -17,6 +16,7 @@ public class MenuGroupService {
 
     @Transactional
     public MenuGroup create(final MenuGroup menuGroup) {
+        //TODO name 필드가 null이 아니고 공백이 아닌 문자가 1개 이상 있어야 할 듯 하다
         return menuGroupDao.save(menuGroup);
     }
 
