1b4d91cc2df347c35d0b508a92e791411b0d39fb
Richard JEON <ztzy1907@gmail.com>
2022-10-19 20:46:29 +0900

    refactor: TableGroupService 주석 제거 및 유효성 검증 로직 추가

--- src/main/java/kitchenpos/application/TableGroupService.java
+++ src/main/java/kitchenpos/application/TableGroupService.java
@@ -30,16 +30,12 @@ public class TableGroupService {
 
     @Transactional
     public TableGroup create(final TableGroup tableGroup) {
-        // 매개변수로 받은 TableGroup에서는 주문 테이블 목록만 받는다.
-        // 여러 테이블을 단체 계산을 위해 묶는 과정이다.
         final List<OrderTable> orderTables = tableGroup.getOrderTables();
 
-        // 비어있거나 2보다 작으면 그루핑 의미가 없다. 예외를 던진다.
         if (CollectionUtils.isEmpty(orderTables) || orderTables.size() < 2) {
             throw new IllegalArgumentException();
         }
 
-        // 테이블 아이디만 추출해 리스트로 만들고 모두 실재하는지 검증한다
         final List<Long> orderTableIds = orderTables.stream()
                 .map(OrderTable::getId)
                 .collect(Collectors.toList());
@@ -50,9 +46,6 @@ public class TableGroupService {
             throw new IllegalArgumentException();
         }
 
-        // 묶어달라고 요청한 테이블 중 하나라도 이미 다른 그룹으로 묶여졌다면 예외를 던진다
-        // 주문 불가 상태가 아니면 예외를 던진다.... 묶으려면 주문 불가 상태로 변경해야 한다는 건가?
-        // 테이블 상태는 쿠킹이나 밀 상태면 변경 불가능한데... 어떻게 돌아가는거지...??
         for (final OrderTable savedOrderTable : savedOrderTables) {
             if (!savedOrderTable.isEmpty() || Objects.nonNull(savedOrderTable.getTableGroupId())) {
                 throw new IllegalArgumentException();
@@ -63,7 +56,6 @@ public class TableGroupService {
 
         final TableGroup savedTableGroup = tableGroupDao.save(tableGroup);
 
-        // 생성된 그룹 아이디를 테이블들에 할당하고, 주문 가능하게 empty 상태를 false로 변경하여 업데이트한다
         final Long tableGroupId = savedTableGroup.getId();
         for (final OrderTable savedOrderTable : savedOrderTables) {
             savedOrderTable.setTableGroupId(tableGroupId);
@@ -77,24 +69,28 @@ public class TableGroupService {
 
     @Transactional
     public void ungroup(final Long tableGroupId) {
-        // 그룹 아이디로 모든 테이블 조회 후 아이디 리스트로 추출
         final List<OrderTable> orderTables = orderTableDao.findAllByTableGroupId(tableGroupId);
+        validateTableGroupId(tableGroupId, orderTables);
 
         final List<Long> orderTableIds = orderTables.stream()
                 .map(OrderTable::getId)
                 .collect(Collectors.toList());
 
-        // 하나라도 조리 시작, 식사중 상태인 테이블이 있으면 예외
         if (orderDao.existsByOrderTableIdInAndOrderStatusIn(
                 orderTableIds, Arrays.asList(OrderStatus.COOKING.name(), OrderStatus.MEAL.name()))) {
             throw new IllegalArgumentException();
         }
 
-        // 모든 테이블의 그룹 아이디 할당을 제거하고, 주문 가능한 상태로 변경하고 저장
         for (final OrderTable orderTable : orderTables) {
             orderTable.setTableGroupId(null);
             orderTable.setEmpty(false);
             orderTableDao.save(orderTable);
+        }
+    }
+
+    private void validateTableGroupId(final Long tableGroupId, final List<OrderTable> orderTables) {
+        if (orderTables.isEmpty()) {
+            throw new IllegalArgumentException("유효하지 않은 그룹 아이디 : " + tableGroupId);
         }
     }
 }
