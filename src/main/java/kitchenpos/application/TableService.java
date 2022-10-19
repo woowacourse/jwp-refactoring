a12165fc4dab61d910088b5f460b6a871fe6293a
Richard JEON <ztzy1907@gmail.com>
2022-10-19 21:44:24 +0900

    refactor: TableService 테이블 생성 시 인원 수 유효성 검증 로직 추가

--- src/main/java/kitchenpos/application/TableService.java
+++ src/main/java/kitchenpos/application/TableService.java
@@ -24,9 +24,15 @@ public class TableService {
     public OrderTable create(final OrderTable orderTable) {
         orderTable.setId(null);
         orderTable.setTableGroupId(null);
-        // TODO numberOfGuests 가 음수여선 안된다는 유효성 검증 추가 필요
+        validateNumberOfGuests(orderTable.getNumberOfGuests());
 
         return orderTableDao.save(orderTable);
+    }
+
+    private void validateNumberOfGuests(final int numberOfGuests) {
+        if (numberOfGuests < 0) {
+            throw new IllegalArgumentException("유효하지 않은 고객 인원 수 : " + numberOfGuests);
+        }
     }
 
     public List<OrderTable> list() {
@@ -35,22 +41,18 @@ public class TableService {
 
     @Transactional
     public OrderTable changeEmpty(final Long orderTableId, final OrderTable orderTable) {
-        // 아이디로 테이블을 찾는다. 없으면 예외를 던진다.
         final OrderTable savedOrderTable = orderTableDao.findById(orderTableId)
                 .orElseThrow(IllegalArgumentException::new);
 
-        // 찾은 테이블에 그룹 아이디가 없어야 한다. 있으면 예외를 던진다.
         if (Objects.nonNull(savedOrderTable.getTableGroupId())) {
             throw new IllegalArgumentException();
         }
 
-        // 찾은 테이블에 COOKING, MEAL 상태인 주문이 없어야 한다. 있으면 예외를 던진다.
         if (orderDao.existsByOrderTableIdAndOrderStatusIn(
                 orderTableId, Arrays.asList(OrderStatus.COOKING.name(), OrderStatus.MEAL.name()))) {
             throw new IllegalArgumentException();
         }
 
-        // 주문테이블을 비운다. body로 받은 값을 여기서 한 번 쓴다. true/false이다.
         savedOrderTable.setEmpty(orderTable.isEmpty());
 
         return orderTableDao.save(savedOrderTable);
@@ -58,25 +60,19 @@ public class TableService {
 
     @Transactional
     public OrderTable changeNumberOfGuests(final Long orderTableId, final OrderTable orderTable) {
-        // RequestBody로 받은 값을 사용하는 유일한 곳이다. 변경할 테이블 고객 수만 받는다.
         final int numberOfGuests = orderTable.getNumberOfGuests();
 
-        // 음수로 변경할 순 없다. 0은 가능하다.
         if (numberOfGuests < 0) {
             throw new IllegalArgumentException();
         }
 
-        // PathVariable로 받은 테이블이 존재하지 않으면 예외를 던진다
         final OrderTable savedOrderTable = orderTableDao.findById(orderTableId)
                 .orElseThrow(IllegalArgumentException::new);
 
-        // 찾은 테이블이 주문 불가 상태 테이블일 경우 고객 수 변경은 잘못된 요청이다. 예외를 던진다
-        // IllegalStateException이 어울릴 것 같은데?
         if (savedOrderTable.isEmpty()) {
             throw new IllegalArgumentException();
         }
 
-        // 고객수를 변경하고 업데이트한다
         savedOrderTable.setNumberOfGuests(numberOfGuests);
 
         return orderTableDao.save(savedOrderTable);
