a2785ba6e413242b28d880d37276d50bca287d03
Richard JEON <ztzy1907@gmail.com>
2022-10-19 19:17:34 +0900

    test: 서비스 레이어 테스트를 위한 부모 클래스 ServiceTest 추가

---
+++ src/test/java/kitchenpos/application/ServiceTest.java
@@ -0,0 +1,17 @@
+package kitchenpos.application;
+
+import kitchenpos.DatabaseCleaner;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ServiceTest {
+    @Autowired
+    private DatabaseCleaner databaseCleaner;
+
+    @BeforeEach
+    void setUp() {
+        databaseCleaner.clear();
+    }
+}
