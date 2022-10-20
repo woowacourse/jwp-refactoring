928639cf7d5aa04d63c74de0e1f67e0a0bd0158e
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-10-20 11:05:31 +0900

    test: TableGroupService create 주문테이블 1개 테스트 추가

--- src/test/java/kitchenpos/application/TableGroupServiceTest.java
+++ src/test/java/kitchenpos/application/TableGroupServiceTest.java
@@ -4,7 +4,9 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
 import kitchenpos.SpringServiceTest;
+import kitchenpos.domain.OrderTable;
 import kitchenpos.domain.TableGroup;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -24,6 +26,25 @@ class TableGroupServiceTest {
                 assertThatThrownBy(() -> tableGroupService.create(tableGroup))
                         .isInstanceOf(IllegalArgumentException.class)
                         .hasMessage("주문 테이블 2개 이상인 경우 단체 지정이 가능합니다.");
+            }
+        }
+
+        @Nested
+        class 주문테이블이_1개인_경우 extends SpringServiceTest {
+
+            private final TableGroup tableGroup = new TableGroup(LocalDateTime.now(), createOneTableGroups());
+
+            @Test
+            void 예외가_발생한다() {
+                assertThatThrownBy(() -> tableGroupService.create(tableGroup))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage("주문 테이블 2개 이상인 경우 단체 지정이 가능합니다.");
+            }
+
+            private List<OrderTable> createOneTableGroups() {
+                List<OrderTable> orderTables = new ArrayList<>();
+                orderTables.add(new OrderTable(1, true));
+                return orderTables;
             }
         }
     }
