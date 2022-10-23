eabbf2f72049f03391fdaee899babbeab6f5d33a
yeon-06 <rnjstldus2@gmail.com>
2022-10-23 15:42:53 +0900

    test: 메뉴 생성 실패 케이스 추가

--- src/test/java/kitchenpos/application/MenuServiceTest.java
+++ src/test/java/kitchenpos/application/MenuServiceTest.java
@@ -1,15 +1,19 @@
 package kitchenpos.application;
 
 import static kitchenpos.fixture.MenuTestFixture.떡볶이;
+import static kitchenpos.fixture.ProductFixture.공짜_어묵국물;
 import static kitchenpos.fixture.ProductFixture.불맛_떡볶이;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import kitchenpos.dao.MenuDao;
 import kitchenpos.dao.MenuGroupDao;
+import kitchenpos.dao.MenuProductDao;
 import kitchenpos.dao.ProductDao;
 import kitchenpos.domain.Menu;
 import kitchenpos.domain.MenuGroup;
@@ -35,6 +39,9 @@ class MenuServiceTest {
     @Autowired
     private ProductDao productDao;
 
+    @Autowired
+    private MenuProductDao menuProductDao;
+
     @Test
     void 메뉴_정상_생성() {
         // given
@@ -48,21 +55,89 @@ class MenuServiceTest {
         Menu savedMenu = menuService.create(menu);
 
         // then
-        Optional<Menu> actual = menuDao.findById(savedMenu.getId());
-        assertThat(actual).isNotEmpty();
+        Optional<Menu> actualMenu = menuDao.findById(savedMenu.getId());
+        assertThat(actualMenu).isNotEmpty();
+    }
+
+    @Test
+    void 메뉴의_가격은_0_미만은_메뉴_생성_불가능() {
+        // given
+        MenuGroup menuGroup = menuGroupDao.save(MenuGroupFixture.분식.toEntity());
+        Product product = productDao.save(불맛_떡볶이.toEntity());
+        List<MenuProduct> menuProducts = 메뉴_상품_목록_생성(product);
+
+        Menu menu = 떡볶이.toEntity(menuGroup.getId(), menuProducts);
+        menu.setPrice(BigDecimal.valueOf(-1));
+
+        // when & then
+        assertThatThrownBy(() -> menuService.create(menu))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 존재하지_않는_메뉴_그룹으로_메뉴_생성_불가능() {
+        // given
+        Product product = productDao.save(불맛_떡볶이.toEntity());
+        List<MenuProduct> menuProducts = 메뉴_상품_목록_생성(product);
+
+        Menu menu = 떡볶이.toEntity(100L, menuProducts);
+        menu.setPrice(BigDecimal.valueOf(-1));
+
+        // when & then
+        assertThatThrownBy(() -> menuService.create(menu))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 상품_합계가_0_이하라면_메뉴_생성_불가능() {
+        // given
+        MenuGroup menuGroup = menuGroupDao.save(MenuGroupFixture.분식.toEntity());
+        Product product = productDao.save(공짜_어묵국물.toEntity());
+        List<MenuProduct> menuProducts = 메뉴_상품_목록_생성(product);
+
+        Menu menu = 떡볶이.toEntity(menuGroup.getId(), menuProducts);
+        menu.setPrice(BigDecimal.valueOf(-1));
+
+        // when & then
+        assertThatThrownBy(() -> menuService.create(menu))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 메뉴의_가격은_상품의_합계보다_작거나_같음() {
+        // given
+        MenuGroup menuGroup = menuGroupDao.save(MenuGroupFixture.분식.toEntity());
+        Product product = productDao.save(불맛_떡볶이.toEntity());
+
+        long quantity = 4;
+        List<MenuProduct> menuProducts = 메뉴_상품_목록_생성(quantity, product);
+        BigDecimal menuProductsSum = product.getPrice().multiply(BigDecimal.valueOf(quantity));
+
+        Menu menu = 떡볶이.toEntity(menuGroup.getId(), menuProducts);
+        menu.setPrice(menuProductsSum.add(BigDecimal.valueOf(1000)));
+
+        // when & then
+        assertThatThrownBy(() -> menuService.create(menu))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     private List<MenuProduct> 메뉴_상품_목록_생성(final Product... products) {
         return Arrays.stream(products)
-                .map(this::메뉴_상품_생성)
+                .map(it -> 메뉴_상품_생성(it, 3))
                 .collect(Collectors.toList());
     }
 
-    private MenuProduct 메뉴_상품_생성(final Product product) {
+    private List<MenuProduct> 메뉴_상품_목록_생성(final long quantity, final Product... products) {
+        return Arrays.stream(products)
+                .map(it -> 메뉴_상품_생성(it, quantity))
+                .collect(Collectors.toList());
+    }
+
+    private MenuProduct 메뉴_상품_생성(final Product product, final long quantity) {
         MenuProduct menuProduct = new MenuProduct();
         menuProduct.setMenuId(null);
         menuProduct.setProductId(product.getId());
-        menuProduct.setQuantity(3);
+        menuProduct.setQuantity(quantity);
         return menuProduct;
     }
 }
--- src/test/java/kitchenpos/fixture/ProductFixture.java
+++ src/test/java/kitchenpos/fixture/ProductFixture.java
@@ -8,6 +8,7 @@ public enum ProductFixture {
     불맛_떡볶이("불맛 떡볶이", BigDecimal.valueOf(8000)),
     짜장맛_떡볶이("짜장맛 떡볶이", BigDecimal.valueOf(7000)),
     카레맛_떡볶이("카레맛 떡볶이", BigDecimal.valueOf(9000)),
+    공짜_어묵국물("어묵 국물", BigDecimal.ZERO),
     ;
 
     private final String name;
@@ -23,5 +24,9 @@ public enum ProductFixture {
         product.setName(name);
         product.setPrice(price);
         return product;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
     }
 }
