6c95c44df56ba9be6ae218c5c4df5f5187f48395
hyunrrr <whgusrms96@gmail.com>
2022-10-23 19:28:25 +0900

    chore: 테스트를 위한 db migration 파일 추가

---
+++ src/test/resources/db/migration/V2__Insert_default_data.sql
@@ -0,0 +1,7 @@
+INSERT INTO product (name, price) VALUES ('맘모스빵', 3000);
+INSERT INTO product (name, price) VALUES ('팥빵', 1000);
+INSERT INTO product (name, price) VALUES ('소보로빵', 2000);
+
+INSERT INTO menu_group (name) VALUES ('베이커리');
+
+INSERT INTO orders ()
