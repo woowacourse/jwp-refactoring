4fcd5cfbb3b88afafc2adb76f34ce5ef55c34676
jurlring <78091011+jurlring@users.noreply.github.com>
2022-10-23 22:10:00 +0900

    test: 공통 어노테이션 추상화

--- src/test/java/kitchenpos/application/ProductServiceTest.java
+++ src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -7,13 +7,9 @@ import java.math.BigDecimal;
 import java.util.List;
 import kitchenpos.domain.Product;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestConstructor;
-import org.springframework.test.context.TestConstructor.AutowireMode;
 
 @SuppressWarnings("NonAsciiCharacters")
-@TestConstructor(autowireMode = AutowireMode.ALL)
-@SpringBootTest
+@ApplicationTest
 class ProductServiceTest {
 
     private final ProductService productService;
