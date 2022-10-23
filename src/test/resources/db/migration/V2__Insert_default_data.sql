64edc2ed07e4fd0fb79823954f0fe1889459e808
hyunrrr <whgusrms96@gmail.com>
2022-10-23 19:36:22 +0900

    chore: 잘못된 sql 수정

--- src/test/resources/db/migration/V2__Insert_default_data.sql
+++ src/test/resources/db/migration/V2__Insert_default_data.sql
@@ -3,5 +3,3 @@ INSERT INTO product (name, price) VALUES ('팥빵', 1000);
 INSERT INTO product (name, price) VALUES ('소보로빵', 2000);
 
 INSERT INTO menu_group (name) VALUES ('베이커리');
-
-INSERT INTO orders ()
