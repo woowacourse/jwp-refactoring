3e63ac8d9a53f6bd8524e2483cff20ae2f53d73d
pup-paw <wldusdl0310@gmail.com>
2022-10-23 19:33:34 +0900

    refactor: menu product을 setter 대신 생성자를 통해 초기화 하도록 수정

--- src/main/java/kitchenpos/domain/MenuProduct.java
+++ src/main/java/kitchenpos/domain/MenuProduct.java
@@ -1,17 +1,20 @@
 package kitchenpos.domain;
 
 public class MenuProduct {
-    private Long seq;
+    private final Long seq;
     private Long menuId;
-    private Long productId;
-    private long quantity;
+    private final Long productId;
+    private final long quantity;
+
+    public MenuProduct(Long seq, Long menuId, Long productId, long quantity) {
+        this.seq = seq;
+        this.menuId = menuId;
+        this.productId = productId;
+        this.quantity = quantity;
+    }
 
     public Long getSeq() {
         return seq;
-    }
-
-    public void setSeq(final Long seq) {
-        this.seq = seq;
     }
 
     public Long getMenuId() {
@@ -26,15 +29,7 @@ public class MenuProduct {
         return productId;
     }
 
-    public void setProductId(final Long productId) {
-        this.productId = productId;
-    }
-
     public long getQuantity() {
         return quantity;
-    }
-
-    public void setQuantity(final long quantity) {
-        this.quantity = quantity;
     }
 }
