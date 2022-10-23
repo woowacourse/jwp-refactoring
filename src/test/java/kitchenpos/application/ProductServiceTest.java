4baf1b700a1450b6975a3b5d26da64c4958f40e3
sinb57 <sinb57@naver.com>
2022-10-23 21:20:35 +0900

    test: ProductServiceTest 테스트 작성

---
+++ src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -0,0 +1,99 @@
+package kitchenpos.application;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import kitchenpos.dao.ProductDao;
+import kitchenpos.domain.Product;
+
+@Transactional
+@SpringBootTest
+class ProductServiceTest {
+
+    private final ProductService productService;
+    private final ProductDao productDao;
+
+    @Autowired
+    public ProductServiceTest(final ProductService productService, final ProductDao productDao) {
+        this.productService = productService;
+        this.productDao = productDao;
+    }
+
+    @Nested
+    @TestInstance(Lifecycle.PER_CLASS)
+    class CreateTest {
+
+        @ParameterizedTest
+        @ValueSource(ints = {1, 2})
+        void createWithPositivePrice(final int price) {
+            Product expected = new Product("탕수육", price);
+            Product actual = productDao.save(expected);
+
+            assertThat(actual.getId()).isPositive();
+            assertProductEqualsWithoutId(actual, expected);
+        }
+
+        @Test
+        void createWithZeroPrice() {
+            Product expected = new Product("탕수육", 0);
+            Product actual = productDao.save(expected);
+
+            assertThat(actual.getId()).isPositive();
+            assertProductEqualsWithoutId(actual, expected);
+        }
+
+        @ParameterizedTest
+        @ValueSource(ints = {-1, -2})
+        void createWithNegativePrice(final int price) {
+            Product product = new Product("탕수육", price);
+
+            assertThatThrownBy(() -> productService.create(product))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        private void assertProductEqualsWithoutId(final Product actual, final Product expected) {
+            assertThat(actual.getName()).isEqualTo(expected.getName());
+            assertThat(actual.getPrice()).isEqualByComparingTo(expected.getPrice());
+        }
+    }
+
+    @Test
+    void list() {
+        final List<Product> expected = Map.of("자장면", 4500, "짬뽕", 5000, "탕수육", 10000)
+                .entrySet()
+                .stream()
+                .map(entry -> new Product(entry.getKey(), entry.getValue()))
+                .map(productDao::save)
+                .collect(Collectors.toUnmodifiableList());
+        final List<Product> actual = productService.list();
+
+        assertAllMatches(actual, expected);
+    }
+
+    private void assertAllMatches(final List<Product> actualList, final List<Product> expectedList) {
+        final int expectedSize = actualList.size();
+        assertThat(expectedList).hasSize(expectedSize);
+
+        for (int i = 0; i < expectedSize; i++) {
+            final var actual = actualList.get(i);
+            final var expected = expectedList.get(i);
+
+            assertThat(actual).usingRecursiveComparison()
+                    .isEqualTo(expected);
+        }
+    }
+}
