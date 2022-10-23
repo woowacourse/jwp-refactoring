3f7a3c5599844a237637fdc3f57960cc09f37333
leokim <kunga077@gmail.com>
2022-10-23 17:33:16 +0900

    chore: 테스트용 더미 데이터 추가

--- src/main/resources/db/migration/V2__Insert_default_data.sql
+++ src/main/resources/db/migration/V2__Insert_default_data.sql
@@ -32,3 +32,5 @@ INSERT INTO order_table (id, number_of_guests, empty) VALUES (5, 0, true);
 INSERT INTO order_table (id, number_of_guests, empty) VALUES (6, 0, true);
 INSERT INTO order_table (id, number_of_guests, empty) VALUES (7, 0, true);
 INSERT INTO order_table (id, number_of_guests, empty) VALUES (8, 0, true);
+
+INSERT INTO table_group (created_date) VALUES (now());
