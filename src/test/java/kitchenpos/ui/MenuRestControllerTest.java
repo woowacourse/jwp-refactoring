35839aaac86e44107c5e12dfc193ed128430796f
cjlee38 <cjlee38@github.com>
2022-10-24 01:37:02 +0900

    test: add tests for menu controller

---
+++ src/test/java/kitchenpos/ui/MenuRestControllerTest.java
@@ -0,0 +1,60 @@
+package kitchenpos.ui;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.math.BigDecimal;
+import java.util.List;
+import kitchenpos.application.MenuService;
+import kitchenpos.domain.Menu;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
+
+@WebMvcTest(MenuRestController.class)
+class MenuRestControllerTest extends ControllerTest {
+
+    @MockBean
+    private MenuService menuService;
+
+    @Test
+    void create() throws Exception {
+        // given
+        long id = 1L;
+        Menu menu = new Menu("메뉴", BigDecimal.valueOf(1000), 1L, List.of());
+        menu.setId(id);
+        given(menuService.create(any())).willReturn(menu);
+
+        // when
+        ResultActions actions = mockMvc.perform(post("/api/menus")
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .content(objectMapper.writeValueAsString(menu))
+        );
+
+        // then
+        actions.andExpect(status().isCreated())
+                .andExpect(header().string("Location", "/api/menus/" + id));
+    }
+
+    @Test
+    void list() throws Exception {
+        // given
+        Menu menu = new Menu("메뉴", BigDecimal.valueOf(1000), 1L, List.of());
+        given(menuService.list()).willReturn(List.of(menu));
+
+        // when
+        ResultActions actions = mockMvc.perform(get("/api/menus"));
+
+        // then
+        actions.andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(List.of(menu))));
+    }
+}
