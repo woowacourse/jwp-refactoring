a3edd5d53be5ef33f4bfb5a97312137ceb2e8821
SuyeonChoi <tndus970120@gmail.com>
2022-10-24 03:13:44 +0900

    test(MenuServiceTest): MenuService 테스트 작성

---
+++ src/test/java/kitchenpos/application/MenuServiceTest.java
@@ -0,0 +1,60 @@
+package kitchenpos.application;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.math.BigDecimal;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import kitchenpos.domain.Menu;
+import kitchenpos.domain.MenuGroup;
+import kitchenpos.domain.MenuProduct;
+import kitchenpos.domain.Product;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("MenuService 클래스의")
+class MenuServiceTest extends ServiceTest {
+
+    @Test
+    @DisplayName("create 메서드는 메뉴를 생성한다.")
+    void create() {
+        // given
+        MenuGroup menuGroup = menuGroupService.create(createMenuGroup("반마리치킨"));
+        Product product = productService.create(createProduct("크림치킨", BigDecimal.valueOf(10000.00)));
+        MenuProduct menuProduct = createMenuProduct(product.getId(), 1);
+        Menu menu = createMenu("크림치킨", BigDecimal.valueOf(10000.00), menuGroup.getId(),
+                Collections.singletonList(menuProduct));
+
+        // when
+        Menu savedMenu = menuService.create(menu);
+
+        // then
+        Optional<Menu> actual = menuDao.findById(savedMenu.getId());
+        assertThat(actual).isPresent();
+    }
+
+    @Test
+    @DisplayName("list 메서드는 모든 메뉴를 조회한다.")
+    void list() {
+        // given
+        MenuGroup menuGroup = menuGroupService.create(createMenuGroup("반마리치킨"));
+        Product product = productService.create(createProduct("크림치킨", BigDecimal.valueOf(15000.00)));
+        saveMenu("크림치킨", menuGroup, product);
+        saveMenu("크림어니언치킨", menuGroup, product);
+        saveMenu("크림치즈치킨", menuGroup, product);
+
+        // when
+        List<Menu> menus = menuService.list();
+
+        // then
+        assertThat(menus).hasSize(9);
+    }
+
+    private Menu saveMenu(String menuName, MenuGroup menuGroup, Product product) {
+        MenuProduct menuProduct = createMenuProduct(product.getId(), 1);
+        return menuService.create(
+                createMenu(menuName, product.getPrice().multiply(BigDecimal.valueOf(menuProduct.getQuantity())),
+                        menuGroup.getId(), Collections.singletonList(menuProduct)));
+    }
+}
--- src/test/java/kitchenpos/application/ServiceTest.java
+++ src/test/java/kitchenpos/application/ServiceTest.java
@@ -1,9 +1,13 @@
 package kitchenpos.application;
 
 import java.math.BigDecimal;
+import java.util.List;
+import kitchenpos.dao.MenuDao;
 import kitchenpos.dao.MenuGroupDao;
 import kitchenpos.dao.ProductDao;
+import kitchenpos.domain.Menu;
 import kitchenpos.domain.MenuGroup;
+import kitchenpos.domain.MenuProduct;
 import kitchenpos.domain.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,10 +24,16 @@ abstract class ServiceTest {
     protected ProductService productService;
 
     @Autowired
+    protected MenuService menuService;
+
+    @Autowired
     protected MenuGroupDao menuGroupDao;
 
     @Autowired
     protected ProductDao productDao;
+
+    @Autowired
+    protected MenuDao menuDao;
 
     protected MenuGroup createMenuGroup(String name) {
         MenuGroup menuGroup = new MenuGroup();
@@ -36,5 +46,21 @@ abstract class ServiceTest {
         product.setName(name);
         product.setPrice(price);
         return product;
+    }
+
+    protected Menu createMenu(String name, BigDecimal price, Long menuGroupId, List<MenuProduct> menuProducts) {
+        Menu menu = new Menu();
+        menu.setName(name);
+        menu.setPrice(price);
+        menu.setMenuGroupId(menuGroupId);
+        menu.setMenuProducts(menuProducts);
+        return menu;
+    }
+
+    protected MenuProduct createMenuProduct(Long productId, long quantity) {
+        MenuProduct menuProduct = new MenuProduct();
+        menuProduct.setProductId(productId);
+        menuProduct.setQuantity(quantity);
+        return menuProduct;
     }
 }
