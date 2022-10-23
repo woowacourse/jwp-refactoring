0b7f14eaba6a112593d7862b46ace08de7ecf132
cjlee38 <cjlee38@github.com>
2022-10-24 01:44:06 +0900

    test: add tests for order controller

---
+++ src/test/java/kitchenpos/ui/OrderRestControllerTest.java
@@ -0,0 +1,80 @@
+package kitchenpos.ui;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import kitchenpos.application.OrderService;
+import kitchenpos.domain.Order;
+import kitchenpos.domain.OrderStatus;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
+
+@WebMvcTest(OrderRestController.class)
+class OrderRestControllerTest extends ControllerTest {
+
+    @MockBean
+    private OrderService orderService;
+
+    @Test
+    void create() throws Exception {
+        // given
+        long id = 1L;
+        Order order = new Order(1L, OrderStatus.COOKING.name(), LocalDateTime.now(), List.of());
+        order.setId(id);
+
+        given(orderService.create(any())).willReturn(order);
+
+        // when
+        ResultActions actions = mockMvc.perform(post("/api/orders")
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .content(objectMapper.writeValueAsString(order))
+        );
+
+        // then
+        actions.andExpect(status().isCreated())
+                .andExpect(header().string("Location", "/api/orders/" + id));
+    }
+
+    @Test
+    void list() throws Exception {
+        // given
+        Order order = new Order(1L, OrderStatus.COOKING.name(), LocalDateTime.now(), List.of());
+        given(orderService.list()).willReturn(List.of(order));
+
+        // when
+        ResultActions actions = mockMvc.perform(get("/api/orders"));
+
+        // then
+        actions.andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(List.of(order))));
+    }
+
+    @Test
+    void changeOrderStatus() throws Exception {
+        // given
+        Order order = new Order(1L, OrderStatus.COOKING.name(), LocalDateTime.now(), List.of());
+        given(orderService.changeOrderStatus(any(), any())).willReturn(order);
+
+        // when
+        ResultActions actions = mockMvc.perform(put("/api/orders/{orderId}/order-status", 1L)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .content(objectMapper.writeValueAsString(order))
+        );
+
+        // then
+        actions.andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(order)));
+    }
+}
