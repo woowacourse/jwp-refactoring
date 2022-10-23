e4dc62e37e615db08b5b08f8b6dbf396762842cf
sinb57 <sinb57@naver.com>
2022-10-24 01:44:29 +0900

    test: TableTest 테스트 추가

---
+++ src/test/java/kitchenpos/application/TableServiceTest.java
@@ -0,0 +1,159 @@
+package kitchenpos.application;
+
+import kitchenpos.dao.OrderDao;
+import kitchenpos.dao.OrderTableDao;
+import kitchenpos.domain.Order;
+import kitchenpos.domain.OrderTable;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@ServiceTest
+class TableServiceTest {
+
+    private final TableService tableService;
+    private final OrderDao orderDao;
+    private final OrderTableDao orderTableDao;
+
+    @Autowired
+    public TableServiceTest(final TableService tableService, final OrderDao orderDao,
+                            final OrderTableDao orderTableDao) {
+        this.tableService = tableService;
+        this.orderDao = orderDao;
+        this.orderTableDao = orderTableDao;
+    }
+
+    @Test
+    void create() {
+        final var expected = new OrderTable(null, 1, false);
+        final var actual = tableService.create(expected);
+
+        assertThat(actual.getId()).isPositive();
+        assertThat(actual).usingRecursiveComparison()
+                .ignoringFields("id")
+                .isEqualTo(expected);
+    }
+
+    @Test
+    void list() {
+        final List<OrderTable> expected = Stream.of(
+                        new OrderTable(null, 1, false),
+                        new OrderTable(null, 5, false),
+                        new OrderTable(null, 10, true))
+                .map(orderTableDao::save)
+                .collect(Collectors.toUnmodifiableList());
+        final List<OrderTable> actual = tableService.list();
+
+        assertThat(actual).usingRecursiveComparison()
+                .isEqualTo(expected);
+    }
+
+    @Test
+    void changeEmpty() {
+        final var expected = orderTableDao.save(new OrderTable(null,1,false));
+        expected.setEmpty(true);
+
+        final var actual = tableService.changeEmpty(expected.getId(), expected);
+        assertThat(actual).usingRecursiveComparison()
+                .isEqualTo(expected);
+    }
+
+    @Test
+    void changeEmptyWithNonExistOrderTable() {
+        final var nonExistOrderTableId = 0L;
+
+        final var orderTable = new OrderTable(null,1,false);
+
+        assertThatThrownBy(() -> tableService.changeEmpty(nonExistOrderTableId, orderTable))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("주문 테이블을 찾을 수 없습니다.");
+    }
+
+    @Test
+    void changeEmptyWithAssignedTableGroup() {
+        final var tableGroupId = 1L;
+
+        final var orderTable = orderTableDao.save(new OrderTable(tableGroupId, 1, false));
+        orderTable.setEmpty(true);
+
+        final var orderTableId = orderTable.getId();
+        assertThatThrownBy(() -> tableService.changeEmpty(orderTableId, orderTable))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("단체 지정된 테이블입니다.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"COOKING", "MEAL"})
+    void changeEmptyWithAlreadyAssignedOrderTable(final String orderStatus) {
+        final var orderTable = orderTableDao.save(new OrderTable(null, 1, false));
+        final var orderTableId = orderTable.getId();
+
+        orderDao.save(new Order(orderTableId, orderStatus, LocalDateTime.now(), Collections.emptyList()));
+
+        assertThatThrownBy(() -> tableService.changeEmpty(orderTableId, orderTable))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("계산이 완료되지 않은 테이블입니다.");
+    }
+
+    @Test
+    void changeNumberOfGuests() {
+        final var expected = 10;
+
+        final var orderTable = orderTableDao.save(new OrderTable(null, 1, false));
+
+        orderTable.setNumberOfGuests(expected);
+        tableService.changeNumberOfGuests(orderTable.getId(), orderTable);
+
+        final var actual = orderTable.getNumberOfGuests();
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void changeNumberOfGuestsWithNegative() {
+        final var negativeNumberOfGuests = -1;
+
+        final var orderTable = new OrderTable(null, 1, false);
+        final var savedOrderTable = orderTableDao.save(orderTable);
+
+        orderTable.setNumberOfGuests(negativeNumberOfGuests);
+
+        final var orderTableId = savedOrderTable.getId();
+        assertThatThrownBy(() -> tableService.changeNumberOfGuests(orderTableId, orderTable))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("손님 수는 음수가 될 수 없습니다.");
+    }
+
+    @Test
+    void changeNumberOfGuestsWithNonExistOrderTable() {
+        final var nonExistOrderTableId = 0L;
+
+        final var orderTable = orderTableDao.save(new OrderTable(null, 1, false));
+
+        assertThatThrownBy(() -> tableService.changeNumberOfGuests(nonExistOrderTableId, orderTable))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("주문 테이블을 찾을 수 없습니다.");
+    }
+
+    @Test
+    void changeNumberOfGuestsWithEmptyOrderTable() {
+        final var isEmpty = true;
+
+        final var orderTable = new OrderTable(null, 1, isEmpty);
+        final var savedOrderTable = orderTableDao.save(orderTable);
+
+        final var orderTableId = savedOrderTable.getId();
+        assertThatThrownBy(() -> tableService.changeNumberOfGuests(orderTableId, orderTable))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("주문 테이블이 비어있습니다.");
+    }
+}
