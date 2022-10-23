3ff6b19c3c286ea9a38bdbab4e15160f61c5f18f
jurlring <78091011+jurlring@users.noreply.github.com>
2022-10-23 23:27:40 +0900

    feat: 생성자 추가

--- src/main/java/kitchenpos/domain/Order.java
+++ src/main/java/kitchenpos/domain/Order.java
@@ -11,6 +11,17 @@ public class Order {
     private LocalDateTime orderedTime;
     private List<OrderLineItem> orderLineItems;
 
+    public Order(Long orderTableId, String orderStatus, LocalDateTime orderedTime,
+                 List<OrderLineItem> orderLineItems) {
+        this.orderTableId = orderTableId;
+        this.orderStatus = orderStatus;
+        this.orderedTime = orderedTime;
+        this.orderLineItems = orderLineItems;
+    }
+
+    public Order() {
+    }
+
     public Long getId() {
         return id;
     }
