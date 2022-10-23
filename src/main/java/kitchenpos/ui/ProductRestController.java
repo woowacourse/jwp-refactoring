5f8718b1cc348e892bb85e9e351eb66b0dd5d16b
김찬 <gmelddl514@gmail.com>
2022-10-24 00:42:28 +0900

    test: Product test 작성

--- src/main/java/kitchenpos/ui/ProductRestController.java
+++ src/main/java/kitchenpos/ui/ProductRestController.java
@@ -3,6 +3,7 @@ package kitchenpos.ui;
 import java.net.URI;
 import java.util.List;
 import kitchenpos.application.ProductService;
+import kitchenpos.application.request.ProductCreateRequest;
 import kitchenpos.domain.Product;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,8 +20,8 @@ public class ProductRestController {
     }
 
     @PostMapping("/api/products")
-    public ResponseEntity<Product> create(@RequestBody final Product product) {
-        final Product created = productService.create(product);
+    public ResponseEntity<Product> create(@RequestBody final ProductCreateRequest request) {
+        final Product created = productService.create(request);
         final URI uri = URI.create("/api/products/" + created.getId());
         return ResponseEntity.created(uri)
                 .body(created);
---
+++ src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -0,0 +1,88 @@
+package kitchenpos.application;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import java.math.BigDecimal;
+import java.util.List;
+import kitchenpos.application.request.ProductCreateRequest;
+import kitchenpos.domain.Product;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+class ProductServiceTest {
+
+    @Autowired
+    private ProductService productService;
+
+    @Nested
+    class create_메서드는 {
+
+        @Nested
+        class 가격이_null인_경우 {
+            private final ProductCreateRequest request = new ProductCreateRequest("상품", null);
+
+            @Test
+            void 예외가_발생한다() {
+                assertThatThrownBy(() -> productService.create(request))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage("가격은 null 혹은 0 미만일 수 없습니다.");
+            }
+        }
+
+        @Nested
+        class 가격이_0_미만일_경우 {
+
+            private static final int INVALID_PRICE = -1000;
+
+            private final ProductCreateRequest request = new ProductCreateRequest("상품", new BigDecimal(INVALID_PRICE));
+
+            @Test
+            void 예외가_발생한다() {
+                assertThatThrownBy(() -> productService.create(request))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage("가격은 null 혹은 0 미만일 수 없습니다.");
+            }
+        }
+
+        @Nested
+        class 정상적인_입력을_받을_경우 {
+
+            private final ProductCreateRequest request = new ProductCreateRequest("상품", new BigDecimal(10000));
+
+            @Test
+            void Product를_생성하고_반환한다() {
+                final Product actual = productService.create(request);
+
+                assertAll(
+                        () -> assertThat(actual.getId()).isNotNull(),
+                        () -> assertThat(actual.getName()).isEqualTo("상품"),
+                        () -> assertThat(actual.getPrice()).isEqualByComparingTo(new BigDecimal(10000))
+                );
+            }
+        }
+    }
+
+    @Nested
+    class list_메서드는 {
+
+        @Nested
+        class 호출하는_경우 {
+
+            private static final int EXPECT_SIZE = 6;
+
+            @Test
+            void Product의_목록을_반환한다() {
+                final List<Product> actual = productService.list();
+
+                assertThat(actual).hasSize(EXPECT_SIZE);
+            }
+        }
+    }
+}
