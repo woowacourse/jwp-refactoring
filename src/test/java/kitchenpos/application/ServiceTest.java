2c2608b096309587843c4176c8a9eea694ea885a
BeomsooKim <kbsat@naver.com>
2022-10-23 15:17:11 +0900

    test: 테스트를 위한 ServiceTest 구성

---
+++ src/test/java/kitchenpos/application/ServiceTest.java
@@ -0,0 +1,60 @@
+package kitchenpos.application;
+
+import kitchenpos.dao.MenuDao;
+import kitchenpos.dao.MenuGroupDao;
+import kitchenpos.dao.MenuProductDao;
+import kitchenpos.dao.OrderDao;
+import kitchenpos.dao.OrderLineItemDao;
+import kitchenpos.dao.OrderTableDao;
+import kitchenpos.dao.ProductDao;
+import kitchenpos.dao.TableGroupDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+public abstract class ServiceTest {
+
+    @Autowired
+    protected MenuGroupService menuGroupService;
+
+    @Autowired
+    protected MenuService menuService;
+
+    @Autowired
+    protected OrderService orderService;
+
+    @Autowired
+    protected ProductService productService;
+
+    @Autowired
+    protected TableGroupService tableGroupService;
+
+    @Autowired
+    protected TableService tableService;
+
+    @Autowired
+    protected MenuDao menuDao;
+
+    @Autowired
+    protected MenuGroupDao menuGroupDao;
+
+    @Autowired
+    protected MenuProductDao menuProductDao;
+
+    @Autowired
+    protected OrderDao orderDao;
+
+    @Autowired
+    protected OrderLineItemDao orderLineItemDao;
+
+    @Autowired
+    protected OrderTableDao orderTableDao;
+
+    @Autowired
+    protected ProductDao productDao;
+
+    @Autowired
+    protected TableGroupDao tableGroupDao;
+}
