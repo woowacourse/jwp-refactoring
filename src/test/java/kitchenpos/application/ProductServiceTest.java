9588fc0e7879a0d7b8d25a5e75ae6e6283c805e2
corinne <yoo77hyeon@gmail.com>
2022-10-24 15:42:44 +0900

    test: productServiceTest 추가

---
+++ src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -0,0 +1,36 @@
+package kitchenpos.application;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.math.BigDecimal;
+import kitchenpos.domain.Product;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceTest {
+
+    @Autowired
+    private ProductService productService;
+
+    @Test
+    void 제품을_저장할_때_가격이_음수이면_예외를_발생한다() {
+        // given
+        final Product product = new Product("제품명", new BigDecimal(-1));
+
+        // when, then
+        assertThatThrownBy(() -> productService.create(product))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 제품을_저장할_때_가격이_null이면_예외를_발생한다() {
+        // given
+        final Product product = new Product("제품명", null);
+
+        // when, then
+        assertThatThrownBy(() -> productService.create(product))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
