c1a016d892eb969f4358a7fd71052fd5e9d912e4
syoun602 <syoun602@gmail.com>
2022-10-24 20:25:09 +0900

    test: 상품 Service 테스트 작성

---
+++ src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -0,0 +1,73 @@
+package kitchenpos.application;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.math.BigDecimal;
+import java.util.List;
+import kitchenpos.domain.Product;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+@SuppressWarnings("NonAsciiCharacters")
+class ProductServiceTest extends ServiceTest {
+
+    @Nested
+    class create_메서드는 {
+
+        @Nested
+        class 유효한_상품이_입력되면 {
+
+            private final Product product = new Product("치킨", new BigDecimal(100));
+
+            @Test
+            void 상품을_반환한다() {
+                final Product savedProduct = productService.create(product);
+
+                assertThat(savedProduct.getId()).isNotNull();
+            }
+        }
+
+        @Nested
+        class 가격이_null인_상품이_입력되면 {
+
+            private final Product product = new Product("치킨", null);
+
+            @Test
+            void 예외가_발생한다() {
+                assertThatThrownBy(() -> productService.create(product))
+                        .isInstanceOf(IllegalArgumentException.class);
+            }
+        }
+
+        @Nested
+        class 가격이_0_미만인_상품이_입력되면 {
+
+            private final Product product = new Product("치킨", new BigDecimal(-1));
+
+            @Test
+            void 예외가_발생한다() {
+                assertThatThrownBy(() -> productService.create(product))
+                        .isInstanceOf(IllegalArgumentException.class);
+            }
+        }
+    }
+
+    @Nested
+    class list_메서드는 {
+
+        @Nested
+        class 호출되면 {
+
+            private final Product product = new Product("치킨", new BigDecimal(100));
+
+            @Test
+            void 모든_상품을_반환한다() {
+                productService.create(product);
+                final List<Product> products = productService.list();
+
+                assertThat(products).hasSize(1);
+            }
+        }
+    }
+}
---
+++ src/test/java/kitchenpos/application/ServiceTest.java
@@ -0,0 +1,16 @@
+package kitchenpos.application;
+
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Transactional
+class ServiceTest {
+
+    @Autowired
+    protected MenuService menuService;
+
+    @Autowired
+    protected ProductService productService;
+}
