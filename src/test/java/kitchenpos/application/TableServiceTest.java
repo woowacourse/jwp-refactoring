e1b355c3af83afe7c5870cd1fe120d3b792ee9f6
hyewoncc <hyewoncc@gmail.com>
2022-10-25 02:34:37 +0900

    feat: 예외 처리 케이스 추가

--- src/test/java/kitchenpos/application/TableServiceTest.java
+++ src/test/java/kitchenpos/application/TableServiceTest.java
@@ -4,8 +4,21 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
+import kitchenpos.dao.MenuDao;
+import kitchenpos.dao.MenuGroupDao;
+import kitchenpos.dao.OrderDao;
+import kitchenpos.dao.ProductDao;
+import kitchenpos.domain.Menu;
+import kitchenpos.domain.MenuGroup;
+import kitchenpos.domain.MenuProduct;
+import kitchenpos.domain.Order;
+import kitchenpos.domain.OrderLineItem;
+import kitchenpos.domain.OrderStatus;
 import kitchenpos.domain.OrderTable;
+import kitchenpos.domain.Product;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +31,18 @@ class TableServiceTest {
 
     @Autowired
     private TableService tableService;
+
+    @Autowired
+    private ProductDao productDao;
+
+    @Autowired
+    private MenuGroupDao menuGroupDao;
+
+    @Autowired
+    private MenuDao menuDao;
+
+    @Autowired
+    private OrderDao orderDao;
 
     @DisplayName("주문 테이블 등록")
     @Test
@@ -44,6 +69,33 @@ class TableServiceTest {
                 () -> assertThat(result.getId()).isEqualTo(table.getId()),
                 () -> assertThat(result.isEmpty()).isEqualTo(updatedTable.isEmpty())
         );
+    }
+
+    @DisplayName("등록된 주문 테이블의 주문 상태가 COMPLETION이 아닐 경우, 빈 테이블 상태 변경 시 예외 발생")
+    @Test
+    void changeEmpty_orderStatusIsNotCompletion_throwsException() {
+
+        final var pizza = productDao.save(new Product("피자", new BigDecimal(10_000)));
+        final var pizzaInMenu = new MenuProduct(pizza.getId(), 1);
+
+        final var italian = menuGroupDao.save(new MenuGroup("양식"));
+        final var pizzaMenu = menuDao.save(
+                new Menu("싼 피자", new BigDecimal(9_000), italian.getId(), List.of(pizzaInMenu)));
+
+        final var orderItem = new OrderLineItem(pizzaMenu.getId(), 1);
+
+        final var table = tableService.create(new OrderTable(2));
+        final var order = new Order(table.getId(), List.of(orderItem));
+        order.setOrderedTime(LocalDateTime.now());
+        order.setOrderStatus(OrderStatus.MEAL.name());
+        orderDao.save(order);
+
+        final var changed = new OrderTable(2);
+        changed.setEmpty(true);
+
+        assertThatThrownBy(
+                () -> tableService.changeEmpty(table.getId(), changed)
+        ).isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("단체 지정된 주문 테이블의 빈 테이블 여부 상태 변경 시 예외 발생")
