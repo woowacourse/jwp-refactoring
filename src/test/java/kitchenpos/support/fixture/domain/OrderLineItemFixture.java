693bc8a74684b4cdc49f4ab4c6cb431da56f5c98
dwl21 <nggus5@naver.com>
2022-10-24 18:29:15 +0900

    test: OrderLineItem Fixture 생성

---
+++ src/test/java/kitchenpos/support/fixture/domain/OrderLineItemFixture.java
@@ -0,0 +1,33 @@
+package kitchenpos.support.fixture.domain;
+
+import kitchenpos.domain.OrderLineItem;
+
+public enum OrderLineItemFixture {
+
+    ONE(1L),
+    TWO(2L)
+    ;
+
+    private final Long quantity;
+
+    OrderLineItemFixture(Long quantity) {
+        this.quantity = quantity;
+    }
+
+    public OrderLineItem getOrderLineItem(Long orderId, Long menuId) {
+        OrderLineItem orderLineItem = new OrderLineItem();
+        orderLineItem.setOrderId(orderId);
+        orderLineItem.setMenuId(menuId);
+        orderLineItem.setQuantity(quantity);
+        return orderLineItem;
+    }
+
+    public OrderLineItem getOrderLineItem(Long seq, Long orderId, Long menuId) {
+        OrderLineItem orderLineItem = new OrderLineItem();
+        orderLineItem.setSeq(seq);
+        orderLineItem.setOrderId(orderId);
+        orderLineItem.setMenuId(menuId);
+        orderLineItem.setQuantity(quantity);
+        return orderLineItem;
+    }
+}
