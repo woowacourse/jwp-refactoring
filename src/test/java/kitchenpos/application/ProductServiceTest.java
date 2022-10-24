eb790f11867e7088e3358387a77c65d3a2e8c7ad
yeon-06 <rnjstldus2@gmail.com>
2022-10-24 16:10:10 +0900

    test: 상품 목록 조회 테스트 추가

--- src/test/java/kitchenpos/application/ProductServiceTest.java
+++ src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -1,10 +1,13 @@
 package kitchenpos.application;
 
+import static kitchenpos.fixture.ProductFixture.불맛_떡볶이;
 import static kitchenpos.fixture.ProductFixture.카레맛_떡볶이;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 import kitchenpos.dao.ProductDao;
 import kitchenpos.domain.Product;
@@ -43,5 +46,20 @@ class ProductServiceTest {
         // when & then
         assertThatThrownBy(() -> productService.create(product))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 상품_목록_조회() {
+        // given
+        Product curryTteokbokki = productDao.save(카레맛_떡볶이.toEntity());
+        Product fireTteokbokki = productDao.save(불맛_떡볶이.toEntity());
+
+        // when
+        List<Product> products = productService.list();
+
+        // then
+        assertThat(products)
+                .usingRecursiveComparison()
+                .isEqualTo(Arrays.asList(curryTteokbokki, fireTteokbokki));
     }
 }
