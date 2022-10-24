38876cfdaca0f0d5596495cbe1c0de91dec348d0
hyeonbinsa <shb1833@gmail.com>
2022-10-24 22:14:10 +0900

    refactor: TableServiceTest 추가 구현

--- src/test/java/kitchenpos/application/TableServiceTest.java
+++ src/test/java/kitchenpos/application/TableServiceTest.java
@@ -1,11 +1,16 @@
 package kitchenpos.application;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import kitchenpos.dao.OrderDao;
 import kitchenpos.dao.OrderTableDao;
+import kitchenpos.domain.Order;
+import kitchenpos.domain.OrderStatus;
 import kitchenpos.domain.OrderTable;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -51,5 +56,42 @@ public class TableServiceTest {
 
         // then
         assertThat(products.size()).isEqualTo(8);
+    }
+
+    @Test
+    @DisplayName("주문 테이블 상태를 변경한다.")
+    void changeEmpty() {
+        // given
+        OrderTable orderTable = new OrderTable();
+        orderTable.setTableGroupId(null);
+        orderTable.setNumberOfGuests(0);
+        orderTable.setEmpty(true);
+        OrderTable savedOrderTable = orderTableDao.save(orderTable);
+        // when
+        OrderTable changeOrderTable = tableService.changeEmpty(savedOrderTable.getId(), orderTable);
+
+        // then
+        assertThat(changeOrderTable.isEmpty()).isEqualTo(true);
+    }
+
+    @Test
+    @DisplayName("주문 테이블 상태를 변경 시 주문 상태가 cooking이나 meal이면 예외를 반환한다.")
+    void changeEmpty_WhenOrderStatusCooking() {
+        // given
+        OrderTable orderTable = new OrderTable();
+        orderTable.setTableGroupId(null);
+        orderTable.setNumberOfGuests(0);
+        orderTable.setEmpty(true);
+
+        OrderTable savedOrderTable = orderTableDao.save(orderTable);
+
+        Order order= new Order();
+        order.setOrderedTime(LocalDateTime.now());
+        order.setOrderTableId(savedOrderTable.getId());
+        order.setOrderStatus(OrderStatus.COOKING.name());
+        order.setOrderLineItems(new ArrayList<>());
+        orderDao.save(order);
+        // when
+        assertThatThrownBy(()->tableService.changeEmpty(savedOrderTable.getId(), orderTable)).isInstanceOf(IllegalArgumentException.class);
     }
 }
