29054e7afd18b91e5980495880997bf557dc9ed3
Hongdonggeon <ghd700@naver.com>
2022-10-25 03:28:50 +0900

    test: 주문 테이블 방문자 수 테스트 코드 추가

--- src/test/java/kitchenpos/application/TableServiceTest.java
+++ src/test/java/kitchenpos/application/TableServiceTest.java
@@ -11,10 +11,12 @@ import java.util.Arrays;
 import java.util.List;
 import kitchenpos.dao.OrderDao;
 import kitchenpos.dao.OrderTableDao;
+import kitchenpos.dao.TableGroupDao;
 import kitchenpos.domain.Order;
 import kitchenpos.domain.OrderLineItem;
 import kitchenpos.domain.OrderStatus;
 import kitchenpos.domain.OrderTable;
+import kitchenpos.domain.TableGroup;
 import kitchenpos.support.OrderTableFixtures;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -31,6 +33,9 @@ public class TableServiceTest {
 
     @Autowired
     private OrderTableDao orderTableDao;
+
+    @Autowired
+    private TableGroupDao tableGroupDao;
 
     @Autowired
     private OrderDao orderDao;
@@ -76,12 +81,14 @@ public class TableServiceTest {
     @Test
     void changeEmpty_tableGroupIdNonNull_throwsException() {
         // given
-        final OrderTable orderTable = ORDER_TABLE1.create();
+        final OrderTable orderTable = ORDER_TABLE_NOT_EMPTY.createWithIdNull();
+        final TableGroup tableGroup = new TableGroup(null, LocalDateTime.now(), Arrays.asList(orderTable));
+        tableGroupDao.save(tableGroup);
         orderTable.setTableGroupId(1L);
         final OrderTable savedOrderTable = orderTableDao.save(orderTable);
 
         // when, then
-        assertThatThrownBy(() -> tableService.changeEmpty(1L, savedOrderTable))
+        assertThatThrownBy(() -> tableService.changeEmpty(savedOrderTable.getId(), savedOrderTable))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -112,6 +119,52 @@ public class TableServiceTest {
         assertAll(
                 () -> assertThat(changedOrderTable.getId()).isNotNull(),
                 () -> assertThat(changedOrderTable.isEmpty()).isFalse()
+        );
+    }
+
+    @DisplayName("주문 테이블의 방문자 수를 0보다 작은수로 변경할 수 없다.")
+    @Test
+    void changeNumberOfGuests_numberOfGuestsLessThanZero_throwsException() {
+        // given
+        final OrderTable orderTable = ORDER_TABLE1.create();
+        orderTable.setNumberOfGuests(-1);
+
+        // when, then
+        assertThatThrownBy(() -> tableService.changeNumberOfGuests(1L, orderTable))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("orderTableId가 존재하지 않은 경우 예외가 발생한다.")
+    @Test
+    void changeNumberOfGuests_notExistOrderTableId_throwsException() {
+        // when, then
+        assertThatThrownBy(() -> tableService.changeNumberOfGuests(9L, ORDER_TABLE1.create()))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("주문 테이블이 비어있는 경우 예외가 발생한다.")
+    @Test
+    void changeNumberOfGuests_orderTableEmpty_throwsException() {
+        // when, then
+        assertThatThrownBy(() -> tableService.changeNumberOfGuests(1L, ORDER_TABLE1.create()))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("주문 테이블의 방문자 수를 정상적으로 변경한다.")
+    @Test
+    void changeNumberOfGuests() {
+        // given
+        final OrderTable savedOrderTable = orderTableDao.save(ORDER_TABLE_NOT_EMPTY.createWithIdNull());
+        final OrderTable orderTable = ORDER_TABLE1.create();
+        orderTable.setNumberOfGuests(3);
+
+        // when
+        final OrderTable updatedOrderTable = tableService.changeNumberOfGuests(savedOrderTable.getId(), orderTable);
+
+        // then
+        assertAll(
+                () -> assertThat(updatedOrderTable.getId()).isNotNull(),
+                () -> assertThat(updatedOrderTable.getNumberOfGuests()).isEqualTo(3)
         );
     }
 }
