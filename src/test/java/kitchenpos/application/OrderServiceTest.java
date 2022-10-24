7ce967e8e5921c4fede4d74a9f15fd0df27266d1
Hongdonggeon <ghd700@naver.com>
2022-10-24 23:43:51 +0900

    test: Order 상태 변경

--- src/test/java/kitchenpos/application/OrderServiceTest.java
+++ src/test/java/kitchenpos/application/OrderServiceTest.java
@@ -4,9 +4,11 @@ import static kitchenpos.support.OrderTableFixtures.ORDER_TABLE_NOT_EMPTY;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import kitchenpos.dao.OrderDao;
 import kitchenpos.dao.OrderTableDao;
 import kitchenpos.domain.Order;
 import kitchenpos.domain.OrderLineItem;
@@ -27,6 +29,9 @@ public class OrderServiceTest {
 
     @Autowired
     private OrderTableDao orderTableDao;
+
+    @Autowired
+    private OrderDao orderDao;
 
     @DisplayName("OrderLineItems이 비어있는 경우 예외가 발생한다.")
     @Test
@@ -93,5 +98,53 @@ public class OrderServiceTest {
 
         // then
         assertThat(orders).usingRecursiveFieldByFieldElementComparator().isEqualTo(expected);
+    }
+
+    @DisplayName("OrderId가 존재하지 않으면 예외가 발생한다.")
+    @Test
+    void changeOrderStatus_notExistOrder_throwsException() {
+        // given
+        final OrderTable orderTable = orderTableDao.save(ORDER_TABLE_NOT_EMPTY.createWithIdNull());
+        final OrderLineItem orderLineItem = new OrderLineItem(1L, 3);
+        final Order order = new Order(orderTable.getId(), OrderStatus.COOKING.name(), Arrays.asList(orderLineItem));
+
+        // when, then
+        assertThatThrownBy(() -> orderService.changeOrderStatus(1L, order))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("Order의 상태가 Completion이면 예외가 발생한다.")
+    @Test
+    void changeOrderStatus_orderStatusCompletion_throwsException() {
+        // given
+        final OrderTable orderTable = orderTableDao.save(ORDER_TABLE_NOT_EMPTY.createWithIdNull());
+        final OrderLineItem orderLineItem = new OrderLineItem(1L, 3);
+        final Order order = new Order(null, orderTable.getId(), OrderStatus.COMPLETION.name(), LocalDateTime.now(),
+                Arrays.asList(orderLineItem));
+        final Order savedOrder = orderDao.save(order);
+
+        // when, then
+        assertThatThrownBy(() -> orderService.changeOrderStatus(savedOrder.getId(), order))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("Order의 상태를 Cooking에서 meal로 변경한다.")
+    @Test
+    void changeOrderStatus() {
+        // given
+        final OrderTable orderTable = orderTableDao.save(ORDER_TABLE_NOT_EMPTY.createWithIdNull());
+        final OrderLineItem orderLineItem = new OrderLineItem(1L, 3);
+        final Order order = new Order(null, orderTable.getId(), OrderStatus.COOKING.name(), LocalDateTime.now(),
+                Arrays.asList(orderLineItem));
+        final Order savedOrder = orderDao.save(order);
+
+        final Order newOrder = new Order(null, orderTable.getId(), OrderStatus.MEAL.name(), LocalDateTime.now(),
+                Arrays.asList(orderLineItem));
+
+        // when
+        final Order updatedOrder = orderService.changeOrderStatus(savedOrder.getId(), newOrder);
+
+        // then
+        assertThat(updatedOrder.getOrderStatus()).isEqualTo(OrderStatus.MEAL.name());
     }
 }
