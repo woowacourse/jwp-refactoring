ecfbec6a960396ed2afc9e5c7d86b0b394e0237c
koo <gudonghee2000@gmail.com>
2022-10-24 17:19:49 +0900

    test: 메뉴를 등록할 때, 가격이 잘못되면 예외를 발생하는 테스트 코드 작성

--- src/test/java/kitchenpos/application/MenuServiceTest.java
+++ src/test/java/kitchenpos/application/MenuServiceTest.java
@@ -4,9 +4,11 @@ import static kitchenpos.common.fixtures.MenuGroupFixtures.루나세트_이름;
 import static kitchenpos.common.fixtures.ProductFixtures.야채곱창_가격;
 import static kitchenpos.common.fixtures.ProductFixtures.야채곱창_이름;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 import com.sun.tools.javac.util.List;
+import java.math.BigDecimal;
 import kitchenpos.common.builder.MenuBuilder;
 import kitchenpos.common.builder.MenuGroupBuilder;
 import kitchenpos.common.builder.MenuProductBuilder;
@@ -18,6 +20,8 @@ import kitchenpos.domain.Product;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -33,18 +37,25 @@ public class MenuServiceTest {
     @Autowired
     private MenuGroupService menuGroupService;
 
-    private Product 야채곱창;
     private MenuGroup 루나세트;
+    private MenuProduct 루나_야채곱창;
 
     @BeforeEach
     void setUp() {
-        야채곱창 = new ProductBuilder()
+        Product 야채곱창 = new ProductBuilder()
                 .name(야채곱창_이름)
                 .price(야채곱창_가격)
                 .build();
+        야채곱창 = productService.create(야채곱창);
 
         루나세트 = new MenuGroupBuilder()
                 .name(루나세트_이름)
+                .build();
+        루나세트 = menuGroupService.create(루나세트);
+
+        루나_야채곱창 = new MenuProductBuilder()
+                .productId(야채곱창.getId())
+                .quantity(1)
                 .build();
     }
 
@@ -52,17 +63,10 @@ public class MenuServiceTest {
     @Test
     void 메뉴를_등록한다() {
         // given
-        Product 저장된_야채곱창 = productService.create(야채곱창);
-        MenuGroup 저장된_루나세트 = menuGroupService.create(루나세트);
-
-        MenuProduct 루나_야채곱창 = new MenuProductBuilder()
-                .productId(저장된_야채곱창.getId())
-                .quantity(1)
-                .build();
-
-        Menu 야채곱창_메뉴 = new MenuBuilder().name(야채곱창_이름)
+        Menu 야채곱창_메뉴 = new MenuBuilder()
+                .name(야채곱창_이름)
                 .price(야채곱창_가격)
-                .menuGroupId(저장된_루나세트.getId())
+                .menuGroupId(루나세트.getId())
                 .menuProducts(List.of(루나_야채곱창))
                 .build();
 
@@ -75,5 +79,38 @@ public class MenuServiceTest {
                 () -> assertThat(actual.getName()).isEqualTo(야채곱창_이름),
                 () -> assertThat(actual.getPrice()).isEqualTo(야채곱창_가격)
         );
+    }
+
+    @DisplayName("메뉴를 등록할 때, 가격이 0원 보다 작으면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {-1, -2, -100})
+    void 메뉴를_등록할_때_가격이_0원_보다_작으면_예외가_발생한다(int 잘못된_가격) {
+        // given
+        Menu 야채곱창_메뉴 = new MenuBuilder()
+                .name(야채곱창_이름)
+                .price(BigDecimal.valueOf(잘못된_가격))
+                .menuGroupId(루나세트.getId())
+                .menuProducts(List.of(루나_야채곱창))
+                .build();
+
+        // when & then
+        assertThatThrownBy(() -> menuService.create(야채곱창_메뉴))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("메뉴를 등록할 때, 가격이 null 이면 예외가 발생한다.")
+    @Test
+    void 메뉴를_등록할_때_가격이_null_이면_예외가_발생한다() {
+        // given
+        Menu 야채곱창_메뉴 = new MenuBuilder()
+                .name(야채곱창_이름)
+                .price(null)
+                .menuGroupId(루나세트.getId())
+                .menuProducts(List.of(루나_야채곱창))
+                .build();
+
+        // when & then
+        assertThatThrownBy(() -> menuService.create(야채곱창_메뉴))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
