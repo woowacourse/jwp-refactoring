63ebaa52ecb05df15b350ef7e545b6789b6621db
progress0407 <progress0407@gmail.com>
2022-10-25 00:41:50 +0900

    test(IntegrationServiceTest): 1. @Transactional 추가 2. 클래스 용도에 대한 주석 추가

--- src/test/java/kitchenpos/support/IntegrationServiceTest.java
+++ src/test/java/kitchenpos/support/IntegrationServiceTest.java
@@ -7,13 +7,24 @@ import kitchenpos.application.ProductService;
 import kitchenpos.application.TableGroupService;
 import kitchenpos.application.TableService;
 import kitchenpos.dao.OrderDao;
+import kitchenpos.dao.OrderLineItemDao;
 import kitchenpos.dao.OrderTableDao;
 import kitchenpos.dao.TableGroupDao;
 import org.junit.jupiter.api.AfterEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
+/**
+ * 각 서비스 테스트의 최상단에 붙입니다.
+ *
+ * 만일 테스트 구조가 Nested Class를 가지면서 (DCI 등)
+ *
+ * Dao로 추가한 테스트 케이스를 롤백하고자 할 경우 이 클래스를 상속해야 합니다.
+s */
 @SpringBootTest
+@Transactional // Transaction Rollback in Test
 abstract public class IntegrationServiceTest {
 
     // Service's
@@ -40,6 +51,8 @@ abstract public class IntegrationServiceTest {
 
 
     // DAO's
+    @Autowired
+    protected OrderLineItemDao orderLineItemDao;
     @Autowired
     protected OrderDao orderDao;
 
