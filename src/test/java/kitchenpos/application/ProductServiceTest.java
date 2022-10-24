874a167ddf79d974da05773a6a80b5b3fa0ea02b
Gyuchool <balance0591@naver.com>
2022-10-24 20:25:16 +0900

    test: add ProductServiceTest

---
+++ src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -0,0 +1,34 @@
+package kitchenpos.application;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.math.BigDecimal;
+import kitchenpos.dao.ProductDao;
+import kitchenpos.domain.Product;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@SpringBootTest
+class ProductServiceTest {
+
+    @Autowired
+    private ProductService productService;
+
+    @Test
+    void create() {
+        Product product = productService.create(new Product("제육", BigDecimal.ONE));
+
+        assertThat(product.getId()).isNotNull();
+    }
+
+    @Test
+    void list() {
+        productService.create(new Product("제육", BigDecimal.ONE));
+
+        assertThat(productService.list()).hasSize(1);
+    }
+}
