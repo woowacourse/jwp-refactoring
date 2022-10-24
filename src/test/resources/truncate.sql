c7088d9582d755d4910d6e5f51058ede303ccd72
bomi <bonita9931@gmail.com>
2022-10-24 15:42:56 +0900

    chore: test에서 flyway를 사용하지 않도록 설정

---
+++ src/test/resources/truncate.sql
@@ -0,0 +1,14 @@
+SET
+foreign_key_checks = 0;
+
+TRUNCATE TABLE orders;
+TRUNCATE TABLE order_line_item;
+TRUNCATE TABLE menu;
+TRUNCATE TABLE menu_group;
+TRUNCATE TABLE menu_product;
+TRUNCATE TABLE order_table;
+TRUNCATE TABLE table_group;
+TRUNCATE TABLE product;
+
+SET
+foreign_key_checks = 1;
