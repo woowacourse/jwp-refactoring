6b719f13f4621f569a9555ba2fb598c6d31f9ef1
dwl21 <nggus5@naver.com>
2022-10-24 16:16:17 +0900

    test: truncate tables sql 작성

---
+++ src/test/resources/truncate.sql
@@ -0,0 +1,12 @@
+SET FOREIGN_KEY_CHECKS = 0;
+
+TRUNCATE TABLE orders;
+TRUNCATE TABLE order_line_item;
+TRUNCATE TABLE menu;
+TRUNCATE TABLE menu_group;
+TRUNCATE TABLE menu_product;
+TRUNCATE TABLE order_table;
+TRUNCATE TABLE table_group;
+TRUNCATE TABLE product;
+
+SET FOREIGN_KEY_CHECKS = 1;
