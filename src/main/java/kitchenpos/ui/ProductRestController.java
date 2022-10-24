6c55e897edd75ba00ce013c0a1e80013d2756fcd
shindong96 <2006peter@naver.com>
2022-10-24 16:52:20 +0900

    test: ProductService test 구현

--- src/main/java/kitchenpos/ui/ProductRestController.java
+++ src/main/java/kitchenpos/ui/ProductRestController.java
@@ -31,7 +31,7 @@ public class ProductRestController {
     @GetMapping("/api/products")
     public ResponseEntity<List<Product>> list() {
         return ResponseEntity.ok()
-                .body(productService.list())
+                .body(productService.findAll())
                 ;
     }
 }
