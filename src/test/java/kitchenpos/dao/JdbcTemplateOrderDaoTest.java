317ae69704aaff689f335a64f7cfc248021c9439
dwl21 <nggus5@naver.com>
2022-10-24 18:55:21 +0900

    test: Order Dao 테스트 추가 작성

--- src/test/java/kitchenpos/dao/JdbcTemplateOrderDaoTest.java
+++ src/test/java/kitchenpos/dao/JdbcTemplateOrderDaoTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
 import kitchenpos.domain.Order;
+import kitchenpos.domain.OrderStatus;
 import kitchenpos.domain.OrderTable;
 import kitchenpos.domain.TableGroup;
 import kitchenpos.support.fixture.domain.OrderFixture;
@@ -79,7 +80,86 @@ class JdbcTemplateOrderDaoTest extends JdbcTemplateTest{
         void success() {
             List<Order> orders = jdbcTemplateOrderDao.findAll();
 
-            assertThat(orders).hasSize(2);
+            assertThat(orders).hasSize(8);
+        }
+    }
+
+    @Nested
+    @DisplayName("existsByOrderTableIdAndOrderStatusIn 메서드는")
+    class ExistsByOrderTableIdAndOrderStatusIndById {
+
+        private OrderTable orderTable;
+
+        @BeforeEach
+        void setUp() {
+            TableGroup tableGroup = jdbcTemplateTableGroupDao.save(TableGroupFixture.getTableGroup());
+            orderTable = jdbcTemplateOrderTableDao.save(OrderTableFixture.GUEST_ONE.getOrderTable(tableGroup.getId()));
+            jdbcTemplateOrderDao.save(OrderFixture.COMPLETION.getOrder(orderTable.getId()));
+        }
+
+        @Test
+        @DisplayName("OrderTable ID와 orderStatus 목록을 받아 일치하는 Order가 있으면 true를 반환한다.")
+        void success_true() {
+            Long orderTableId = orderTable.getId();
+            OrderStatus orderStatus = OrderStatus.COMPLETION;
+
+            Boolean actual = jdbcTemplateOrderDao.existsByOrderTableIdAndOrderStatusIn(orderTableId,
+                List.of(orderStatus.name()));
+
+            assertThat(actual).isTrue();
+        }
+
+        @Test
+        @DisplayName("OrderTable ID와 orderStatus 목록을 받아 일치하는 Order가 없으면 false를 반환한다.")
+        void success_false() {
+            Long orderTableId = orderTable.getId();
+            OrderStatus orderStatus = OrderStatus.COOKING;
+
+            Boolean actual = jdbcTemplateOrderDao.existsByOrderTableIdAndOrderStatusIn(orderTableId,
+                List.of(orderStatus.name()));
+
+            assertThat(actual).isFalse();
+        }
+    }
+
+    @Nested
+    @DisplayName("existsByOrderTableIdInAndOrderStatusIn 메서드는")
+    class ExistsByOrderTableIdInAndOrderStatusIn {
+
+        private OrderTable orderTable1;
+        private OrderTable orderTable2;
+
+        @BeforeEach
+        void setUp() {
+            TableGroup tableGroup = jdbcTemplateTableGroupDao.save(TableGroupFixture.getTableGroup());
+            orderTable1 = jdbcTemplateOrderTableDao.save(OrderTableFixture.GUEST_ONE.getOrderTable(tableGroup.getId()));
+            orderTable2 = jdbcTemplateOrderTableDao.save(OrderTableFixture.GUEST_ONE.getOrderTable(tableGroup.getId()));
+            jdbcTemplateOrderDao.save(OrderFixture.COMPLETION.getOrder(orderTable1.getId()));
+            jdbcTemplateOrderDao.save(OrderFixture.COMPLETION.getOrder(orderTable1.getId()));
+        }
+
+        @Test
+        @DisplayName("OrderTable ID 목록과 orderStatus 목록을 받아 일치하는 Order가 있으면 true를 반환한다.")
+        void success_true() {
+            List<Long> orderTableIds = List.of(orderTable1.getId(), orderTable2.getId());
+            OrderStatus orderStatus = OrderStatus.COMPLETION;
+
+            Boolean actual = jdbcTemplateOrderDao.existsByOrderTableIdInAndOrderStatusIn(orderTableIds,
+                List.of(orderStatus.name()));
+
+            assertThat(actual).isTrue();
+        }
+
+        @Test
+        @DisplayName("OrderTable ID와 orderStatus 목록을 받아 일치하는 Order가 없으면 false를 반환한다.")
+        void success_false() {
+            List<Long> orderTableIds = List.of(orderTable2.getId());
+            OrderStatus orderStatus = OrderStatus.COMPLETION;
+
+            Boolean actual = jdbcTemplateOrderDao.existsByOrderTableIdInAndOrderStatusIn(orderTableIds,
+                List.of(orderStatus.name()));
+
+            assertThat(actual).isFalse();
         }
     }
 }
