3aacbc75eb553ed82ac5a93aa367666f038bdd65
youngwooyoo <fbduddn97@naver.com>
2022-10-24 18:54:32 +0900

    feat: MenuGroupService test 추가

--- src/test/java/kitchenpos/Fixture.java
+++ src/test/java/kitchenpos/Fixture.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 import kitchenpos.domain.Menu;
+import kitchenpos.domain.MenuGroup;
 import kitchenpos.domain.MenuProduct;
 import kitchenpos.domain.Order;
 import kitchenpos.domain.OrderLineItem;
@@ -13,12 +14,16 @@ import kitchenpos.domain.Product;
 public class Fixture {
 
     private static final OrderLineItem ORDER_LINE_ITEM = new OrderLineItem(1L, 1L);
-    public static final Order ORDER =  new Order(1L,
-            OrderStatus.COOKING.name(), LocalDateTime.now(), List.of(ORDER_LINE_ITEM));
+    public static final Order ORDER =  new Order(1L, OrderStatus.COOKING.name(),
+            LocalDateTime.now(), List.of(ORDER_LINE_ITEM));
 
-    public static final MenuProduct MENU_PRODUCT = new MenuProduct(1L, 1L, 1L, 2L);
+    public static final MenuProduct MENU_PRODUCT = new MenuProduct(1L, 1L,
+            1L, 2L);
     public static final Menu MENU =  new Menu(1L, "후라이드+후라이드",
             BigDecimal.valueOf(19000), 1L, List.of(MENU_PRODUCT));
 
-    public static final Product PRODUCT = new Product("후라이드 치킨",  BigDecimal.valueOf(10000));
+    public static final Product PRODUCT = new Product("후라이드 치킨",
+            BigDecimal.valueOf(10000));
+
+    public static final MenuGroup MENU_GROUP = new MenuGroup(1L, "추천메뉴");
 }
