6795f70bb92e8629c6b498cc3e4b5dbb7a4465fe
yeon-06 <rnjstldus2@gmail.com>
2022-10-24 16:06:58 +0900

    test: 상품 가격이 0원 미만인 경우 실패하는 테스트 추가

--- src/test/java/kitchenpos/application/ProductServiceTest.java
+++ src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -2,7 +2,9 @@ package kitchenpos.application;
 
 import static kitchenpos.fixture.ProductFixture.카레맛_떡볶이;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.math.BigDecimal;
 import java.util.Optional;
 import kitchenpos.dao.ProductDao;
 import kitchenpos.domain.Product;
@@ -30,5 +32,16 @@ class ProductServiceTest {
         // then
         Optional<Product> actual = productDao.findById(savedProduct.getId());
         assertThat(actual).isNotEmpty();
+    }
+
+    @Test
+    void 가격이_0원_미만인_상품_생성_불가능() {
+        // given
+        Product product = 카레맛_떡볶이.toEntity();
+        product.setPrice(BigDecimal.valueOf(-1));
+
+        // when & then
+        assertThatThrownBy(() -> productService.create(product))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
