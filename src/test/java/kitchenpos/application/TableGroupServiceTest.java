8dabfcfe7df75d949a5a0593005bc7d827e850d4
jurlring <78091011+jurlring@users.noreply.github.com>
2022-10-24 15:28:26 +0900

    test: 테이블그룹 통합테스트시 기존 데이터 사용하지 않도록 테스트 수정

--- src/test/java/kitchenpos/application/TableGroupServiceTest.java
+++ src/test/java/kitchenpos/application/TableGroupServiceTest.java
@@ -3,9 +3,16 @@ package kitchenpos.application;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
+import kitchenpos.domain.Menu;
+import kitchenpos.domain.MenuGroup;
+import kitchenpos.domain.MenuProduct;
+import kitchenpos.domain.Order;
+import kitchenpos.domain.OrderLineItem;
 import kitchenpos.domain.OrderTable;
+import kitchenpos.domain.Product;
 import kitchenpos.domain.TableGroup;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -17,25 +24,31 @@ class TableGroupServiceTest {
     private final TableGroupService tableGroupService;
     private final TableService tableService;
     private final OrderService orderService;
+    private final ProductService productService;
+    private final MenuService menuService;
+    private final MenuGroupService menuGroupService;
 
     TableGroupServiceTest(TableGroupService tableGroupService, TableService tableService,
-                          OrderService orderService) {
+                          OrderService orderService, ProductService productService,
+                          MenuService menuService, MenuGroupService menuGroupService) {
         this.tableGroupService = tableGroupService;
         this.tableService = tableService;
         this.orderService = orderService;
+        this.productService = productService;
+        this.menuService = menuService;
+        this.menuGroupService = menuGroupService;
     }
 
     @Test
     void 테이블그룹을_생성한다() {
-        List<OrderTable> orderTables = tableService.list();
-        TableGroup tableGroup = new TableGroup(LocalDateTime.now(), List.of(orderTables.get(0), orderTables.get(1)));
+        TableGroup tableGroup = new TableGroup(null, List.of(빈테이블을_생성한다(), 빈테이블을_생성한다()));
 
         assertThat(tableGroupService.create(tableGroup)).isInstanceOf(TableGroup.class);
     }
 
     @Test
     void 테이블그룹을_생성할때_테이블이_없는_경우_예외를_발생시킨다() {
-        TableGroup tableGroup = new TableGroup(LocalDateTime.now(), null);
+        TableGroup tableGroup = new TableGroup(null, null);
 
         assertThatThrownBy(() -> tableGroupService.create(tableGroup))
                 .isExactlyInstanceOf(IllegalArgumentException.class);
@@ -43,8 +56,7 @@ class TableGroupServiceTest {
 
     @Test
     void 테이블그룹을_생성할때_테이블이_2개보다_적은_경우_예외를_발생시킨다() {
-        List<OrderTable> orderTables = tableService.list();
-        TableGroup tableGroup = new TableGroup(LocalDateTime.now(), List.of(orderTables.get(0)));
+        TableGroup tableGroup = new TableGroup(null, List.of(빈테이블을_생성한다()));
 
         assertThatThrownBy(() -> tableGroupService.create(tableGroup))
                 .isExactlyInstanceOf(IllegalArgumentException.class);
@@ -52,8 +64,7 @@ class TableGroupServiceTest {
 
     @Test
     void 테이블그룹을_생성할때_테이블이_저장된_정보와_다를_경우_예외를_발생시킨다() {
-        TableGroup tableGroup = new TableGroup(LocalDateTime.now(),
-                List.of(new OrderTable(-1L, null, 0, true), new OrderTable(-2L, null, 0, true)));
+        TableGroup tableGroup = new TableGroup(null, List.of(new OrderTable(-1L, null, 0, true), 빈테이블을_생성한다()));
 
         assertThatThrownBy(() -> tableGroupService.create(tableGroup))
                 .isExactlyInstanceOf(IllegalArgumentException.class);
@@ -61,11 +72,10 @@ class TableGroupServiceTest {
 
     @Test
     void 테이블그룹을_생성할때_테이블이_비어있지_않은_경우_예외를_발생시킨다() {
-        List<OrderTable> orderTables = tableService.list();
-        OrderTable orderTable = orderTables.get(2);
+        OrderTable orderTable = 빈테이블을_생성한다();
         orderTable.setEmpty(false);
         tableService.changeEmpty(orderTable.getId(), orderTable);
-        TableGroup tableGroup = new TableGroup(LocalDateTime.now(), List.of(orderTable, orderTables.get(1)));
+        TableGroup tableGroup = new TableGroup(null, List.of(orderTable, 빈테이블을_생성한다()));
 
         assertThatThrownBy(() -> tableGroupService.create(tableGroup))
                 .isExactlyInstanceOf(IllegalArgumentException.class);
@@ -73,20 +83,53 @@ class TableGroupServiceTest {
 
     @Test
     void 테이블그룹을_해제한다() {
-        List<OrderTable> orderTables = tableService.list();
-        TableGroup tableGroup = new TableGroup(LocalDateTime.now(), List.of(orderTables.get(2), orderTables.get(3)));
+        TableGroup tableGroup = new TableGroup(LocalDateTime.now(), List.of(빈테이블을_생성한다(), 빈테이블을_생성한다()));
         TableGroup savedGroup = tableGroupService.create(tableGroup);
 
         Assertions.assertDoesNotThrow(() -> tableGroupService.ungroup(savedGroup.getId()));
     }
 
     @Test
-    void 테이블그룹을_해제할때_조리중이거나_식사중인_주문이_있는_경우_예외를_발생시킨다() {
-        List<OrderTable> orderTables = tableService.list();
-        TableGroup tableGroup = new TableGroup(LocalDateTime.now(), List.of(orderTables.get(0), orderTables.get(1)));
+    void 테이블그룹을_해제할때_조리중인_주문이_있는_경우_예외를_발생시킨다() {
+        OrderTable orderTable = 빈테이블을_생성한다();
+        TableGroup tableGroup = new TableGroup(LocalDateTime.now(), List.of(orderTable, 빈테이블을_생성한다()));
         TableGroup savedTableGroup = tableGroupService.create(tableGroup);
+        Order order = new Order(orderTable.getId(), null, null,
+                List.of(new OrderLineItem(1L, null, 메뉴를_저장한다().getId(), 1)));
+        orderService.create(order);
 
         assertThatThrownBy(() -> tableGroupService.ungroup(savedTableGroup.getId()))
                 .isExactlyInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 테이블그룹을_해제할때_식사중인_주문이_있는_경우_예외를_발생시킨다() {
+        OrderTable orderTable = 빈테이블을_생성한다();
+        TableGroup tableGroup = new TableGroup(LocalDateTime.now(), List.of(orderTable, 빈테이블을_생성한다()));
+        TableGroup savedTableGroup = tableGroupService.create(tableGroup);
+        Order order = new Order(orderTable.getId(), null, null,
+                List.of(new OrderLineItem(1L, null, 메뉴를_저장한다().getId(), 1)));
+        Order savedOrder = orderService.create(order);
+        savedOrder.setOrderStatus("MEAL");
+        orderService.changeOrderStatus(savedOrder.getId(), savedOrder);
+
+        assertThatThrownBy(() -> tableGroupService.ungroup(savedTableGroup.getId()))
+                .isExactlyInstanceOf(IllegalArgumentException.class);
+    }
+
+    OrderTable 빈테이블을_생성한다() {
+        OrderTable orderTable = new OrderTable(null, 0, true);
+        return tableService.create(orderTable);
+    }
+
+    Menu 메뉴를_저장한다() {
+        Product jwt_후라이드 = productService.create(new Product("JWT 후라이드", new BigDecimal(100_000)));
+        Product jwt_양념 = productService.create(new Product("JWT 양념", new BigDecimal(100_000)));
+        MenuProduct 후라이드 = new MenuProduct(1L, null, jwt_후라이드.getId(), 1);
+        MenuProduct 양념 = new MenuProduct(2L, null, jwt_양념.getId(), 1);
+        MenuGroup menuGroup = menuGroupService.create(new MenuGroup("추천메뉴"));
+        Menu menu = new Menu("반반치킨", new BigDecimal(200_000), menuGroup.getId(), List.of(후라이드, 양념));
+
+        return menuService.create(menu);
     }
 }
