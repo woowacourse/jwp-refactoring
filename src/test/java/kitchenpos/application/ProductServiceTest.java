4bad845f006213a566dcff8088b2c02a9944a0ae
2yujeong <yjfflq4872@gmail.com>
2022-10-24 16:26:09 +0900

    test: ProductService 테스트 작성

---
+++ src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -0,0 +1,79 @@
+package kitchenpos.application;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.tuple;
+
+import java.math.BigDecimal;
+import java.util.List;
+import kitchenpos.domain.Product;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+
+@SpringBootTest
+@Sql("/init_schema.sql")
+class ProductServiceTest {
+
+    @Autowired
+    private ProductService productService;
+
+    @Nested
+    class 상품_등록_메소드는 {
+
+        @Test
+        void 입력받은_상품을_저장한다() {
+            // given
+            Product newProduct = new Product();
+            newProduct.setName("상품1");
+            newProduct.setPrice(BigDecimal.valueOf(10000));
+
+            // when
+            productService.create(newProduct);
+
+            // then
+            List<Product> savedProducts = productService.list();
+            assertThat(savedProducts).extracting(Product::getName, (product) -> product.getPrice().intValue())
+                    .contains(tuple("상품1", 10000));
+        }
+
+        @Test
+        void 상품_가격이_음수면_예외가_발생한다() {
+            // given
+            Product product = new Product();
+            product.setName("상품1");
+            product.setPrice(BigDecimal.valueOf(-1));
+
+            // when & then
+            assertThatThrownBy(() -> productService.create(product))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+    }
+
+    @Test
+    void 상품_목록_조회_메소드는_모든_상품의_id_이름_가격을_조회한다() {
+        // given
+        Product product1 = 상품을_등록한다("상품1", 10000);
+        Product product2 = 상품을_등록한다("상품2", 20000);
+        productService.create(product1);
+        productService.create(product2);
+
+        // when
+        final List<Product> products = productService.list();
+
+        // then
+        assertThat(products).extracting("name", "price.intValueExact")
+                .hasSize(2)
+                .contains(tuple("상품1", 10000), tuple("상품2", 20000));
+    }
+
+    private Product 상품을_등록한다(final String name, final int price) {
+        Product product = new Product();
+        product.setName(name);
+        product.setPrice(BigDecimal.valueOf(price));
+
+        return product;
+    }
+}
