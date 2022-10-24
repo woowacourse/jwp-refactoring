97d8dc7681fddd7e8dd6779f2a1e47a7ecc92315
koo <gudonghee2000@gmail.com>
2022-10-24 15:59:30 +0900

    test: 상품을 등록하는 기능에 대한 통합테스트 작성

--- src/test/java/kitchenpos/application/ProductServiceTest.java
+++ src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -1,24 +1,40 @@
 package kitchenpos.application;
 
-import java.math.BigDecimal;
+import static kitchenpos.common.fixtures.ProductFixtures.야채곱창_가격;
+import static kitchenpos.common.fixtures.ProductFixtures.야채곱창_이름;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
 import kitchenpos.common.builder.ProductBuilder;
 import kitchenpos.domain.Product;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ProductServiceTest {
+
+    @Autowired
+    private ProductService productService;
 
     @DisplayName("상품을 등록한다.")
     @Test
     void 상품을_등록한다() {
         // given
         Product 야채곱창 = new ProductBuilder()
-                .name("야채곱창")
-                .price(new BigDecimal(10000))
+                .name(야채곱창_이름)
+                .price(야채곱창_가격)
                 .build();
 
+        // when
+        Product actual = productService.create(야채곱창);
 
+        // then
+        assertAll(
+                () -> assertThat(actual.getId()).isNotNull(),
+                () -> assertThat(actual.getName()).isEqualTo(야채곱창_이름),
+                () -> assertThat(actual.getPrice()).isEqualTo(야채곱창_가격)
+        );
     }
 }
---
+++ src/test/java/kitchenpos/common/fixtures/ProductFixtures.java
@@ -0,0 +1,9 @@
+package kitchenpos.common.fixtures;
+
+import java.math.BigDecimal;
+
+public class ProductFixtures {
+
+    public static String 야채곱창_이름 = "야채곱창";
+    public static BigDecimal 야채곱창_가격 = new BigDecimal("10000.00");
+}
