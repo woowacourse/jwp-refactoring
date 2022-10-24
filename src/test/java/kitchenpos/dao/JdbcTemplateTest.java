0e618390b66691a2f7c5bf04981b5def9c905a0e
dwl21 <nggus5@naver.com>
2022-10-24 15:22:18 +0900

    test: JdbcTemplate을 테스트 할 수 있는 환경 구축

---
+++ src/test/java/kitchenpos/dao/JdbcTemplateTest.java
@@ -0,0 +1,20 @@
+package kitchenpos.dao;
+
+import javax.sql.DataSource;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+
+@JdbcTest
+public class JdbcTemplateTest {
+
+    @Autowired
+    private DataSource dataSource;
+
+    protected JdbcTemplateProductDao jdbcTemplateProductDao;
+
+    @BeforeEach
+    void setUp() {
+        jdbcTemplateProductDao = new JdbcTemplateProductDao(dataSource);
+    }
+}
