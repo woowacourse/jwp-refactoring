dfb91292832dd3c9a31cec46a039d76829ba9de9
Gyuchool <balance0591@naver.com>
2022-10-25 15:48:48 +0900

    refactor: add ProductServiceTest Exception

--- src/test/java/kitchenpos/application/ProductServiceTest.java
+++ src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -1,10 +1,16 @@
 package kitchenpos.application;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.math.BigDecimal;
+import java.util.stream.Stream;
 import kitchenpos.domain.Product;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.NullSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,6 +22,7 @@ class ProductServiceTest {
     @Autowired
     private ProductService productService;
 
+    @DisplayName("상품을 생성한다.")
     @Test
     void create() {
         Product product = productService.create(new Product("제육", BigDecimal.ONE));
@@ -23,6 +30,20 @@ class ProductServiceTest {
         assertThat(product.getId()).isNotNull();
     }
 
+    @DisplayName("상품의 가격이 0이거나 없으면 예외가 발새한다")
+    @MethodSource
+    @NullSource
+    @ParameterizedTest
+    void createFailureWhenPriceIsNullOrNegative(BigDecimal price) {
+        assertThatThrownBy(() -> productService.create(new Product("제육 볶음", price)))
+                .isExactlyInstanceOf(IllegalArgumentException.class);
+    }
+
+    public static Stream<BigDecimal> createFailureWhenPriceIsNullOrNegative() {
+        return Stream.of(BigDecimal.valueOf(-1L), BigDecimal.valueOf(-100L));
+    }
+
+    @DisplayName("상품의 목록을 조회한다.")
     @Test
     void list() {
         productService.create(new Product("제육", BigDecimal.ONE));
