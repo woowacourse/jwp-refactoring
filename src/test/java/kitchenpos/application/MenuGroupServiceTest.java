2a78f5e74de2965a30e77657247a61ac83f1aa95
hyewoncc <hyewoncc@gmail.com>
2022-10-26 00:00:05 +0900

    refactor: 테스트를 요구 사항의 의도에 맞게 개선

--- src/test/java/kitchenpos/application/MenuGroupServiceTest.java
+++ src/test/java/kitchenpos/application/MenuGroupServiceTest.java
@@ -2,8 +2,6 @@ package kitchenpos.application;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.List;
-import java.util.stream.Collectors;
 import kitchenpos.domain.MenuGroup;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -31,8 +29,6 @@ class MenuGroupServiceTest {
     @DisplayName("모든 메뉴 그룹 조회")
     @Test
     void list() {
-        final var existingMenuGroups = menuGroupService.list();
-
         final var korean = new MenuGroup("한식");
         final var japanese = new MenuGroup("일식");
 
@@ -40,13 +36,6 @@ class MenuGroupServiceTest {
         menuGroupService.create(japanese);
 
         final var result = menuGroupService.list();
-        final var filteredResult = filterNewlyCreated(existingMenuGroups, result);
-        assertThat(filteredResult).containsExactly(korean, japanese);
-    }
-
-    private List<MenuGroup> filterNewlyCreated(final List<MenuGroup> olds, final List<MenuGroup> news) {
-        return news.stream()
-                .filter(menuGroup -> !olds.contains(menuGroup))
-                .collect(Collectors.toList());
+        assertThat(result).contains(korean, japanese);
     }
 }
