f2dfc0aff9663a6ef5a98198c9cbf0ca13c8686e
koo <gudonghee2000@gmail.com>
2022-10-25 19:32:21 +0900

    test: TableServiceTest의 테스트 코드 전체 수정
    ProductDao 객체 활용
    ProductServiceTest 내부에 fixture 메서드 생성

--- src/test/java/kitchenpos/application/TableServiceTest.java
+++ src/test/java/kitchenpos/application/TableServiceTest.java
@@ -1,11 +1,15 @@
 package kitchenpos.application;
 
+import static kitchenpos.common.fixtures.OrderTableFixtures.빈_테이블;
+import static kitchenpos.common.fixtures.OrderTableFixtures.사용중인_테이블;
+import static kitchenpos.common.fixtures.OrderTableFixtures.테이블_1번_손님_수;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 import java.util.List;
 import kitchenpos.common.builder.OrderTableBuilder;
+import kitchenpos.dao.OrderTableDao;
 import kitchenpos.domain.OrderTable;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,16 +21,13 @@ class TableServiceTest extends ServiceTest {
     private TableService tableService;
 
     @Autowired
-    private TableGroupService tableGroupService;
+    private OrderTableDao orderTableDao;
 
     @DisplayName("주문 테이블을 등록한다.")
     @Test
     void 주문_테이블을_등록한다() {
         // given
-        OrderTable 야채곱창_주문_테이블 = new OrderTableBuilder()
-                .numberOfGuests(3)
-                .empty(false)
-                .build();
+        OrderTable 야채곱창_주문_테이블 = 주문_테이블_생성(테이블_1번_손님_수, 사용중인_테이블);
 
         // when
         OrderTable actual = tableService.create(야채곱창_주문_테이블);
@@ -34,7 +35,7 @@ class TableServiceTest extends ServiceTest {
         // then
         assertAll(
                 () -> assertThat(actual.getId()).isNotNull(),
-                () -> assertThat(actual.getNumberOfGuests()).isEqualTo(3)
+                () -> assertThat(actual.getNumberOfGuests()).isEqualTo(테이블_1번_손님_수)
         );
     }
 
@@ -42,32 +43,25 @@ class TableServiceTest extends ServiceTest {
     @Test
     void 주문_테이블_목록을_조회한다() {
         // given
-        OrderTable 야채곱창_주문_테이블 = new OrderTableBuilder()
-                .numberOfGuests(3)
-                .empty(false)
-                .build();
-        야채곱창_주문_테이블 = tableService.create(야채곱창_주문_테이블);
+        OrderTable 야채곱창_주문_테이블 = 주문_테이블_생성(테이블_1번_손님_수, 사용중인_테이블);
+        orderTableDao.save(야채곱창_주문_테이블);
 
         // when
         List<OrderTable> 주문_테이블들 = tableService.list();
 
         // then
-        assertThat(주문_테이블들).extracting(OrderTable::getId)
-                .contains(야채곱창_주문_테이블.getId());
+        assertThat(주문_테이블들).hasSize(1);
     }
 
     @DisplayName("주문 테이블을 빈 테이블로 변경한다.")
     @Test
     void 주문_테이블을_빈_테이블로_변경한다() {
         // given
-        OrderTable 야채곱창_주문_테이블 = new OrderTableBuilder()
-                .numberOfGuests(3)
-                .empty(false)
-                .build();
-        야채곱창_주문_테이블 = tableService.create(야채곱창_주문_테이블);
+        OrderTable 야채곱창_주문_테이블 = 주문_테이블_생성(테이블_1번_손님_수, 사용중인_테이블);
+        야채곱창_주문_테이블 = orderTableDao.save(야채곱창_주문_테이블);
 
         // when
-        OrderTable actual = tableService.changeEmpty(야채곱창_주문_테이블.getId(), new OrderTable(true));
+        OrderTable actual = tableService.changeEmpty(야채곱창_주문_테이블.getId(), new OrderTable(빈_테이블));
 
         // then
         assertThat(actual.isEmpty()).isTrue();
@@ -80,7 +74,7 @@ class TableServiceTest extends ServiceTest {
         Long 잘못된_주문_테이블_아이디 = -1L;
 
         // when & then
-        assertThatThrownBy(() -> tableService.changeEmpty(잘못된_주문_테이블_아이디, new OrderTable(true)))
+        assertThatThrownBy(() -> tableService.changeEmpty(잘못된_주문_테이블_아이디, new OrderTable(빈_테이블)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -88,14 +82,13 @@ class TableServiceTest extends ServiceTest {
     @Test
     void 주문_테이블의_방문한_손님_수를_변경한다() {
         // given
-        OrderTable 야채곱창_주문_테이블 = new OrderTableBuilder()
-                .numberOfGuests(3)
-                .empty(false)
-                .build();
-        야채곱창_주문_테이블 = tableService.create(야채곱창_주문_테이블);
+        OrderTable 야채곱창_주문_테이블 = 주문_테이블_생성(테이블_1번_손님_수, 사용중인_테이블);
+        야채곱창_주문_테이블 = orderTableDao.save(야채곱창_주문_테이블);
+
+        int 변경할_테이블_1번_손님_수 = 5;
 
         // when
-        OrderTable actual = tableService.changeNumberOfGuests(야채곱창_주문_테이블.getId(), new OrderTable(5));
+        OrderTable actual = tableService.changeNumberOfGuests(야채곱창_주문_테이블.getId(), new OrderTable(변경할_테이블_1번_손님_수));
 
         // then
         assertThat(actual.getNumberOfGuests()).isEqualTo(5);
@@ -105,13 +98,11 @@ class TableServiceTest extends ServiceTest {
     @Test
     void 주문_테이블의_방문한_손님_수를_변경할_때_변경할_손님_수가_0명_미만이면_예외를_발생한다() {
         // given
-        OrderTable 야채곱창_주문_테이블 = new OrderTableBuilder()
-                .numberOfGuests(3)
-                .empty(false)
-                .build();
-        야채곱창_주문_테이블 = tableService.create(야채곱창_주문_테이블);
-
+        OrderTable 야채곱창_주문_테이블 = 주문_테이블_생성(테이블_1번_손님_수, 사용중인_테이블);
+        야채곱창_주문_테이블 = orderTableDao.save(야채곱창_주문_테이블);
         Long 야채곱창_주문_테이블_아이디 = 야채곱창_주문_테이블.getId();
+
+        int 잘못된_손님_수 = -1;
 
         // when & then
         assertThatThrownBy(() -> tableService.changeNumberOfGuests(야채곱창_주문_테이블_아이디, new OrderTable(-1)))
@@ -125,7 +116,14 @@ class TableServiceTest extends ServiceTest {
         Long 잘못된_주문_테이블_아이디 = -1L;
 
         // when & then
-        assertThatThrownBy(() -> tableService.changeNumberOfGuests(잘못된_주문_테이블_아이디, new OrderTable(-1)))
+        assertThatThrownBy(() -> tableService.changeNumberOfGuests(잘못된_주문_테이블_아이디, new OrderTable(5)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    private OrderTable 주문_테이블_생성(final int numberOfGuests, final boolean empty) {
+        return new OrderTableBuilder()
+                .numberOfGuests(numberOfGuests)
+                .empty(empty)
+                .build();
     }
 }
---
+++ src/test/java/kitchenpos/common/fixtures/OrderTableFixtures.java
@@ -0,0 +1,8 @@
+package kitchenpos.common.fixtures;
+
+public class OrderTableFixtures {
+
+    public static int 테이블_1번_손님_수 = 4;
+    public static boolean 사용중인_테이블 = false;
+    public static boolean 빈_테이블 = true;
+}
