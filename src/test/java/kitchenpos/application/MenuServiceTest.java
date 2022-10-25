f1ab9e09b5ebeb41c408e552f2a7f1262ce657b7
progress0407 <progress0407@gmail.com>
2022-10-25 14:30:32 +0900

    refactor(Fixture): MenuServiceTest를 픽스쳐로 리펙터링

--- src/test/java/kitchenpos/application/MenuServiceTest.java
+++ src/test/java/kitchenpos/application/MenuServiceTest.java
@@ -1,28 +1,23 @@
 package kitchenpos.application;
 
+import static java.util.Arrays.asList;
+import static kitchenpos.support.Fixture.createMenu;
+import static kitchenpos.support.Fixture.createMenu;
+import static kitchenpos.support.Fixture.createMenuWithProduct;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.util.Lists.emptyList;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.List;
 import kitchenpos.domain.Menu;
 import kitchenpos.domain.MenuProduct;
 import kitchenpos.support.IntegrationServiceTest;
-import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class MenuServiceTest extends IntegrationServiceTest {
-
-    private static final List<MenuProduct> MENU_PRODUCTS;
-
-    static  {
-        final List<MenuProduct> menuProducts = new ArrayList<>();
-        menuProducts.add(new MenuProduct(1L, 1L));
-        MENU_PRODUCTS = menuProducts;
-    }
 
     @Nested
     class create_메서드는 {
@@ -32,7 +27,7 @@ class MenuServiceTest extends IntegrationServiceTest {
 
             private final BigDecimal NULL_PRICE = null;
 
-            private final Menu menu = new Menu("후라이드치킨", NULL_PRICE, 1L, new ArrayList<>());
+            private final Menu menu = new Menu("후라이드치킨", NULL_PRICE, 1L, emptyList());
 
             @Test
             void 예외가_발생한다() {
@@ -45,9 +40,10 @@ class MenuServiceTest extends IntegrationServiceTest {
         @Nested
         class 가격에_음수를_입력될_경우 {
 
-            private final BigDecimal MINUS_PRICE = BigDecimal.valueOf(-1);
+            private final int MINUS_PRICE = -1;
 
-            private final Menu menu = new Menu("후라이드치킨", MINUS_PRICE, 1L, Lists.emptyList());
+            private final Menu menu = createMenu(MINUS_PRICE);
+
 
             @Test
             void 예외가_발생한다() {
@@ -60,14 +56,10 @@ class MenuServiceTest extends IntegrationServiceTest {
         @Nested
         class 없는_메뉴_그룹의_id가_입력될_경우 {
 
-            private final long NOT_FOUND_MENU_GROUP_ID = -1L;
+            private final long NOT_EXIST_MENU_GROUP_ID = -1L;
 
-            private final Menu menu = new Menu(
-                    "후라이드치킨",
-                    BigDecimal.valueOf(10_000),
-                    NOT_FOUND_MENU_GROUP_ID,
-                    Lists.emptyList()
-            );
+            private final Menu menu = createMenu(10_000, NOT_EXIST_MENU_GROUP_ID);
+
 
             @Test
             void 예외가_발생한다() {
@@ -79,11 +71,14 @@ class MenuServiceTest extends IntegrationServiceTest {
         }
 
         @Nested
-        class 메뉴상품의_상품id가_존재하지_않는_경우 {
+        class 메뉴상품의_상품ID가_존재하지_않는_경우 {
 
-            private static final long NOT_EXIST_MENU_PRODUCT_NUMBER = 0L;
+            private static final long NOT_EXIST_MENU_PRODUCT_NUMBER = -1L;
 
-            private final Menu menu = new Menu("후라이드치킨", BigDecimal.valueOf(16000), 1L, errorMenuProducts());
+            private final List<MenuProduct> ERROR_MENU_PRODUCTS = asList(new MenuProduct(NOT_EXIST_MENU_PRODUCT_NUMBER, 1));
+
+            private final Menu menu = createMenu(16_000, ERROR_MENU_PRODUCTS);
+
 
             @Test
             void 예외가_발생한다() {
@@ -91,20 +86,14 @@ class MenuServiceTest extends IntegrationServiceTest {
                         .isInstanceOf(IllegalArgumentException.class)
                         .hasMessage("메뉴상품의 상품ID가 존재하지 않습니다.");
             }
-
-            private List<MenuProduct> errorMenuProducts() {
-                final List<MenuProduct> menuProducts = new ArrayList<>();
-                menuProducts.add(new MenuProduct(NOT_EXIST_MENU_PRODUCT_NUMBER, 1));
-                return menuProducts;
-            }
         }
 
         @Nested
         class 메뉴_가격이_상품가격과_메뉴상품양의_곱보다_큰_경우 {
 
-            private final BigDecimal ERROR_MENU_PRICE = BigDecimal.valueOf(9_900_000);
+            private final int ERROR_MENU_PRICE = 9_900_000;
 
-            private final Menu menu = new Menu("후라이드치킨", ERROR_MENU_PRICE, 1L, MENU_PRODUCTS);
+            private final Menu menu = createMenu(ERROR_MENU_PRICE);
 
             @Test
             void 예외가_발생한다() {
@@ -117,7 +106,7 @@ class MenuServiceTest extends IntegrationServiceTest {
         @Nested
         class 정상적으로_메뉴를_생성가능한_경우 {
 
-            private final Menu menu = new Menu("후라이드치킨", BigDecimal.valueOf(16_000), 1L, MENU_PRODUCTS);
+            private final Menu menu = createMenuWithProduct(16_000, 1L);
 
             @Test
             void 저장된_메뉴가_반환된다() {
---
+++ src/test/java/kitchenpos/support/Fixture.java
@@ -0,0 +1,36 @@
+package kitchenpos.support;
+
+import static java.util.Arrays.asList;
+import static org.assertj.core.util.Lists.emptyList;
+
+import java.math.BigDecimal;
+import java.util.List;
+import kitchenpos.domain.Menu;
+import kitchenpos.domain.MenuProduct;
+
+public class Fixture {
+
+    public static final List<MenuProduct> MENU_PRODUCTS = asList(new MenuProduct(1L, 1L));
+
+    public static Menu createMenu(final int price) {
+        return new Menu("치킨은 살 안쪄요, 살은 내가 쪄요", BigDecimal.valueOf(price), 1L, emptyList());
+    }
+
+    public static Menu createMenu(final int price,
+                                  final long menuGroupId) {
+        return new Menu("치킨은 살 안쪄요, 살은 내가 쪄요", BigDecimal.valueOf(price), menuGroupId, emptyList());
+    }
+
+    public static Menu createMenu(final int price,
+                                  final List<MenuProduct> menuProducts) {
+        return new Menu("치킨은 살 안쪄요, 살은 내가 쪄요", BigDecimal.valueOf(price), 1L, menuProducts);
+    }
+
+    public static Menu createMenuWithProduct(final int price) {
+        return new Menu("치킨은 살 안쪄요, 살은 내가 쪄요", BigDecimal.valueOf(price), 1L, MENU_PRODUCTS);
+    }
+
+    public static Menu createMenuWithProduct(final int price, final long menuGroupId) {
+        return new Menu("치킨은 살 안쪄요, 살은 내가 쪄요", BigDecimal.valueOf(price), menuGroupId, MENU_PRODUCTS);
+    }
+}
