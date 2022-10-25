e5344e6f5b8a8720870af011de1323c753978566
koo <gudonghee2000@gmail.com>
2022-10-25 17:30:22 +0900

    feat: DB truncate를 위한 trucate.sql 파일 생성

--- src/test/java/kitchenpos/application/ServiceTest.java
+++ src/test/java/kitchenpos/application/ServiceTest.java
@@ -1,7 +1,9 @@
 package kitchenpos.application;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 
+@Sql("/truncate.sql")
 @SpringBootTest
 public class ServiceTest {
 }
