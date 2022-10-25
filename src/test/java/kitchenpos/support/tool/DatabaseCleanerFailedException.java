d4fb24ca830adb637590d7032921bd7c4f92eef7
dwl21 <nggus5@naver.com>
2022-10-25 13:58:32 +0900

    test: DatabaseCleanerFailed 예외 구현

---
+++ src/test/java/kitchenpos/support/tool/DatabaseCleanerFailedException.java
@@ -0,0 +1,8 @@
+package kitchenpos.support.tool;
+
+public class DatabaseCleanerFailedException extends RuntimeException {
+
+    public DatabaseCleanerFailedException(String message) {
+        super(message);
+    }
+}
