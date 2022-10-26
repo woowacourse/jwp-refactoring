c67a23d6f6dc42a648b100723d0a1b462ed0e8af
hyeonbinsa <shb1833@gmail.com>
2022-10-27 00:11:29 +0900

    refactor: MenuGroupDao Fake 객체 생성하여 테스트 수정

--- src/test/java/kitchenpos/application/MenuGroupServiceTest.java
+++ src/test/java/kitchenpos/application/MenuGroupServiceTest.java
@@ -3,23 +3,20 @@ package kitchenpos.application;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
-import kitchenpos.dao.MenuDao;
+import kitchenpos.dao.FakeMenuGroupDao;
 import kitchenpos.domain.MenuGroup;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
 
-@SpringBootTest
-@Transactional
 public class MenuGroupServiceTest {
 
-    @Autowired
     private MenuGroupService menuGroupService;
 
-    @Autowired
-    private MenuDao menuDao;
+    @BeforeEach
+    void beforeEach() {
+        menuGroupService = new MenuGroupService(new FakeMenuGroupDao());
+    }
 
     @Test
     @DisplayName("메뉴 그룹을 생성한다.")
@@ -38,10 +35,17 @@ public class MenuGroupServiceTest {
     @Test
     @DisplayName("메뉴 그룹 목록을 조회한다.")
     void list() {
+        // given
+        MenuGroup menuGroup1 = new MenuGroup();
+        menuGroup1.setName("한바리반메뉴");
+        MenuGroup menuGroup2 = new MenuGroup();
+        menuGroup2.setName("종합 세트");
+        menuGroupService.create(menuGroup1);
+        menuGroupService.create(menuGroup2);
         // when
         List<MenuGroup> menuGroups = menuGroupService.list();
 
         // then
-        assertThat(menuGroups.size()).isEqualTo(4);
+        assertThat(menuGroups.size()).isEqualTo(2);
     }
 }
