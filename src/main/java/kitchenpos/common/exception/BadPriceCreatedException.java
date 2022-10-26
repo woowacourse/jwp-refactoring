90d159e58d9a2290a99753fe56cc9978a9468176
dwl21 <nggus5@naver.com>
2022-10-26 14:52:50 +0900

    feat: 가격 생성 실패 예외 구현

---
+++ src/main/java/kitchenpos/common/exception/BadPriceCreatedException.java
@@ -0,0 +1,8 @@
+package kitchenpos.common.exception;
+
+public class BadPriceCreatedException extends BadRequestException{
+
+    public BadPriceCreatedException() {
+        super("가격이 null 이거나 최솟 값 미만입니다.");
+    }
+}
