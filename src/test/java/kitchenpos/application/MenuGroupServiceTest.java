9658d7f5533d60eb0fcd18ffe82743a21211b08e
chawani <chawani@naver.com>
2022-10-26 17:51:14 +0900

    refactor: MenuGroup, MenuGroupService 리팩토링
    - request dto 생성
    - 테스트 코드 수정

--- src/test/java/kitchenpos/application/MenuGroupServiceTest.java
+++ src/test/java/kitchenpos/application/MenuGroupServiceTest.java
@@ -4,7 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import kitchenpos.dao.MenuGroupDao;
 import kitchenpos.domain.MenuGroup;
-import org.junit.jupiter.api.BeforeEach;
+import kitchenpos.ui.dto.request.MenuGroupCreateRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,17 +18,10 @@ class MenuGroupServiceTest {
     @Autowired
     private MenuGroupDao menuGroupDao;
 
-    private MenuGroup menuGroup;
-
-    @BeforeEach
-    void setUp() {
-        menuGroup = new MenuGroup();
-        menuGroup.setName("한판 메뉴");
-    }
-
     @Test
     void 메뉴그룹을_생성한다() {
-        MenuGroup savedMenuGroup = menuGroupService.create(menuGroup);
+        MenuGroupCreateRequest menuGroupCreateRequest = new MenuGroupCreateRequest("한판 메뉴");
+        MenuGroup savedMenuGroup = menuGroupService.create(menuGroupCreateRequest);
 
         assertThat(menuGroupDao.existsById(savedMenuGroup.getId())).isTrue();
     }
@@ -36,7 +29,7 @@ class MenuGroupServiceTest {
     @Test
     void 메뉴그룹_리스트를_반환한다() {
         int beforeSize = menuGroupService.list().size();
-        menuGroupService.create(menuGroup);
+        menuGroupDao.save(new MenuGroup("한판 메뉴"));
 
         assertThat(menuGroupService.list().size()).isEqualTo(beforeSize + 1);
     }
