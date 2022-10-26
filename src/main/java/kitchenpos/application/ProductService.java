404c61949ca2d1a5ab1de8a7ce094c52ae870d94
nailseong <nnis9857@gmail.com>
2022-10-27 02:27:20 +0900

    refactor: Product DTO

--- src/main/java/kitchenpos/application/ProductService.java
+++ src/main/java/kitchenpos/application/ProductService.java
@@ -1,7 +1,10 @@
 package kitchenpos.application;
 
+import java.util.stream.Collectors;
 import kitchenpos.dao.ProductDao;
 import kitchenpos.domain.Product;
+import kitchenpos.ui.dto.request.ProductCreateRequest;
+import kitchenpos.ui.dto.response.ProductResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,7 +13,9 @@ import java.util.List;
 import java.util.Objects;
 
 @Service
+@Transactional(readOnly = true)
 public class ProductService {
+
     private final ProductDao productDao;
 
     public ProductService(final ProductDao productDao) {
@@ -18,17 +23,29 @@ public class ProductService {
     }
 
     @Transactional
-    public Product create(final Product product) {
-        final BigDecimal price = product.getPrice();
+    public ProductResponse create(final ProductCreateRequest request) {
+        final BigDecimal price = request.getPrice();
 
         if (Objects.isNull(price) || price.compareTo(BigDecimal.ZERO) < 0) {
             throw new IllegalArgumentException();
         }
 
-        return productDao.save(product);
+        final Product product = new Product();
+        product.setName(request.getName());
+        product.setPrice(price);
+
+        final Product saved = productDao.save(product);
+        return new ProductResponse(saved.getId(), saved.getName(), saved.getPrice());
     }
 
     public List<Product> list() {
         return productDao.findAll();
+    }
+
+    public List<ProductResponse> list2() {
+        return productDao.findAll()
+                .stream()
+                .map(it -> new ProductResponse(it.getId(), it.getName(), it.getPrice()))
+                .collect(Collectors.toList());
     }
 }
