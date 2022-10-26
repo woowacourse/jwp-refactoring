904aba830d2e50ecbc2d4ee34225f795fc164a42
nailseong <nnis9857@gmail.com>
2022-10-27 02:25:39 +0900

    refactor: MenuGroup DTO

--- src/main/java/kitchenpos/application/MenuGroupService.java
+++ src/main/java/kitchenpos/application/MenuGroupService.java
@@ -1,14 +1,19 @@
 package kitchenpos.application;
 
+import java.util.stream.Collectors;
 import kitchenpos.dao.MenuGroupDao;
 import kitchenpos.domain.MenuGroup;
+import kitchenpos.ui.dto.request.MenuGroupCreateRequest;
+import kitchenpos.ui.dto.response.MenuGroupResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional(readOnly = true)
 public class MenuGroupService {
+
     private final MenuGroupDao menuGroupDao;
 
     public MenuGroupService(final MenuGroupDao menuGroupDao) {
@@ -16,11 +21,17 @@ public class MenuGroupService {
     }
 
     @Transactional
-    public MenuGroup create(final MenuGroup menuGroup) {
-        return menuGroupDao.save(menuGroup);
+    public MenuGroupResponse create(final MenuGroupCreateRequest request) {
+        final MenuGroup menuGroup = new MenuGroup();
+        menuGroup.setName(request.getName());
+        final MenuGroup saved = menuGroupDao.save(menuGroup);
+        return new MenuGroupResponse(saved.getId(), saved.getName());
     }
 
-    public List<MenuGroup> list() {
-        return menuGroupDao.findAll();
+    public List<MenuGroupResponse> list() {
+        return menuGroupDao.findAll()
+                .stream()
+                .map(it -> new MenuGroupResponse(it.getId(), it.getName()))
+                .collect(Collectors.toList());
     }
 }
