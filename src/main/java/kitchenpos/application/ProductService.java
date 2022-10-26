554648bfbe2ddf34420813418f7998cae0ffc57e
DOES <ldk980130@gmail.com>
2022-10-26 10:29:11 +0900

    refactor: ProductService 리팩터링

--- src/main/java/kitchenpos/application/ProductService.java
+++ src/main/java/kitchenpos/application/ProductService.java
@@ -1,17 +1,15 @@
 package kitchenpos.application;
 
-import kitchenpos.dao.ProductDao;
-import kitchenpos.domain.Product;
-import kitchenpos.dto.ProductRequest;
-import kitchenpos.dto.ProductResponse;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.math.BigDecimal;
-import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
+import kitchenpos.dao.ProductDao;
+import kitchenpos.domain.Product;
+import kitchenpos.dto.ProductRequest;
+import kitchenpos.dto.ProductResponse;
 
 @Service
 public class ProductService {
@@ -24,12 +22,6 @@ public class ProductService {
 
     @Transactional
     public ProductResponse create(ProductRequest productRequest) {
-        BigDecimal price = productRequest.getPrice();
-
-        if (Objects.isNull(price) || price.compareTo(BigDecimal.ZERO) < 0) {
-            throw new IllegalArgumentException("가격은 0 이상이어야 합니다.");
-        }
-
         Product product = productDao.save(productRequest.toEntity());
         return ProductResponse.from(product);
     }
--- src/main/java/kitchenpos/domain/Product.java
+++ src/main/java/kitchenpos/domain/Product.java
@@ -1,8 +1,10 @@
 package kitchenpos.domain;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public class Product {
+
     private Long id;
     private final String name;
     private final BigDecimal price;
@@ -12,9 +14,16 @@ public class Product {
     }
 
     public Product(Long id, String name, BigDecimal price) {
+        validatePrice(price);
         this.id = id;
         this.name = name;
         this.price = price;
+    }
+
+    private void validatePrice(BigDecimal price) {
+        if (Objects.isNull(price) || price.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("가격은 0 이상이어야 합니다.");
+        }
     }
 
     public Long getId() {
