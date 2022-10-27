a444a62b7fd7ed2c6e93cc1d14b2aa9189a65182
syoun602 <syoun602@gmail.com>
2022-10-27 14:38:32 +0900

    refactor: 주문 도메인 캡슐화

--- src/main/java/kitchenpos/application/TableService.java
+++ src/main/java/kitchenpos/application/TableService.java
@@ -1,5 +1,6 @@
 package kitchenpos.application;
 
+import java.util.List;
 import kitchenpos.dao.OrderDao;
 import kitchenpos.dao.OrderTableDao;
 import kitchenpos.domain.OrderStatus;
@@ -7,12 +8,9 @@ import kitchenpos.domain.OrderTable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
-
 @Service
 public class TableService {
+
     private final OrderDao orderDao;
     private final OrderTableDao orderTableDao;
 
@@ -37,17 +35,8 @@ public class TableService {
     public OrderTable changeEmpty(final Long orderTableId, final OrderTable orderTable) {
         final OrderTable savedOrderTable = orderTableDao.findById(orderTableId)
                 .orElseThrow(IllegalArgumentException::new);
-
-        if (Objects.nonNull(savedOrderTable.getTableGroupId())) {
-            throw new IllegalArgumentException();
-        }
-
-        if (orderDao.existsByOrderTableIdAndOrderStatusIn(
-                orderTableId, Arrays.asList(OrderStatus.COOKING.name(), OrderStatus.MEAL.name()))) {
-            throw new IllegalArgumentException();
-        }
-
-        savedOrderTable.setEmpty(orderTable.isEmpty());
+        validateChangeable(orderTableId, savedOrderTable);
+        savedOrderTable.changeEmpty(orderTable.isEmpty());
 
         return orderTableDao.save(savedOrderTable);
     }
@@ -55,20 +44,28 @@ public class TableService {
     @Transactional
     public OrderTable changeNumberOfGuests(final Long orderTableId, final OrderTable orderTable) {
         final int numberOfGuests = orderTable.getNumberOfGuests();
+        final OrderTable savedOrderTable = orderTableDao.findById(orderTableId)
+                .orElseThrow(IllegalArgumentException::new);
+        validateNotEmpty(savedOrderTable);
+        savedOrderTable.changeNumberOfGuests(numberOfGuests);
 
-        if (numberOfGuests < 0) {
+        return orderTableDao.save(savedOrderTable);
+    }
+
+    private void validateChangeable(final Long orderTableId, final OrderTable savedOrderTable) {
+        if (savedOrderTable.isTableGroupMapped()) {
             throw new IllegalArgumentException();
         }
 
-        final OrderTable savedOrderTable = orderTableDao.findById(orderTableId)
-                .orElseThrow(IllegalArgumentException::new);
+        if (orderDao.existsByOrderTableIdAndOrderStatusIn(
+                orderTableId, List.of(OrderStatus.COOKING.name(), OrderStatus.MEAL.name()))) {
+            throw new IllegalArgumentException();
+        }
+    }
 
+    private void validateNotEmpty(final OrderTable savedOrderTable) {
         if (savedOrderTable.isEmpty()) {
             throw new IllegalArgumentException();
         }
-
-        savedOrderTable.setNumberOfGuests(numberOfGuests);
-
-        return orderTableDao.save(savedOrderTable);
     }
 }
--- src/main/java/kitchenpos/domain/OrderTable.java
+++ src/main/java/kitchenpos/domain/OrderTable.java
@@ -11,9 +11,28 @@ public class OrderTable {
     }
 
     public OrderTable(final Long tableGroupId, final int numberOfGuests, final boolean empty) {
+        validateNumberOfGuestsNotNegative(numberOfGuests);
         this.tableGroupId = tableGroupId;
         this.numberOfGuests = numberOfGuests;
         this.empty = empty;
+    }
+
+    public void changeNumberOfGuests(final int numberOfGuests) {
+        this.numberOfGuests = numberOfGuests;
+    }
+
+    public boolean isTableGroupMapped() {
+        return tableGroupId != null;
+    }
+
+    public void changeEmpty(final boolean empty) {
+        this.empty = empty;
+    }
+
+    private void validateNumberOfGuestsNotNegative(final int numberOfGuests) {
+        if (numberOfGuests < 0) {
+            throw new IllegalArgumentException();
+        }
     }
 
     public Long getId() {
