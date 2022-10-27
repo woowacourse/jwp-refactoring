6db8ef8a38184725659a8cc10560c6a55e471fdb
yeon-06 <rnjstldus2@gmail.com>
2022-10-28 00:27:37 +0900

    test: Order의 주문 상태 변경하는 단위 테스트 추가

---
+++ src/test/java/kitchenpos/domain/OrderTest.java
@@ -0,0 +1,38 @@
+package kitchenpos.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+@SuppressWarnings("NonAsciiCharacters")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class OrderTest {
+
+    @Test
+    void 상태_변경() {
+        // given
+        Order order = new Order(null, OrderStatus.COOKING, LocalDateTime.now(), new ArrayList<>());
+        OrderStatus changedOrderStatus = OrderStatus.COMPLETION;
+
+        // when
+        order.changeOrderStatus(changedOrderStatus);
+
+        // then
+        assertThat(order.getOrderStatus()).isEqualTo(changedOrderStatus);
+    }
+
+    @Test
+    void 완료된_주문_상태는_변경_불가() {
+        // given
+        Order order = new Order(null, OrderStatus.COMPLETION, LocalDateTime.now(), new ArrayList<>());
+
+        // when & then
+        assertThatThrownBy(() -> order.changeOrderStatus(OrderStatus.COOKING))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
