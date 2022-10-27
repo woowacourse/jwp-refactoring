5adc3c7b264daaf7116225e5555b5191d91ea9d9
Juhyung990122 <harukawa99@naver.com>
2022-10-27 12:29:26 +0900

    test: ProductServiceTest 리팩토링

--- src/test/java/kitchenpos/application/ProductServiceTest.java
+++ src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -1,8 +1,12 @@
 package kitchenpos.application;
 
+import static kitchenpos.support.ProductFixture.상품1;
+import static kitchenpos.support.ProductFixture.상품2;
+import static kitchenpos.support.ProductFixture.상품3;
+import static kitchenpos.support.ProductFixture.상품_생성;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -10,59 +14,60 @@ import kitchenpos.domain.Product;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class ProductServiceTest {
-
-    @Autowired
-    private ProductService productService;
+class ProductServiceTest extends ServiceTest {
 
     @Nested
     @DisplayName("create 메서드를 테스트한다")
-    class create_test{
+    class create_test {
 
         @Test
-        @DisplayName("정상적으로 Product를 생성한다.")
-        void create(){
-            final BigDecimal price = BigDecimal.valueOf(1000);
-            final Product product = new Product("test product", price);
+        @DisplayName("상품을 생성한다.")
+        void create() {
+            final Product product = 상품_생성("상품명", 10000);
 
-            final Product actual = productService.create(product);
+            final Product actual = 상품_등록(product);
 
-            assertThat(actual).isNotNull();
+            assertAll(
+                    () -> assertThat(actual.getId()).isNotNull(),
+                    () -> assertThat(actual.getName()).isEqualTo("상품명"),
+                    () -> assertThat(actual.getPrice().intValue()).isEqualTo(10000)
+            );
+
         }
 
         @Test
         @DisplayName("가격이 null이면 예외를 발생시킨다.")
         void create_nullPrice() {
-            final BigDecimal price = null;
-            final Product product = new Product("test product", price);
+            final Product product = 상품_생성("상품명", null);
 
-            assertThatThrownBy(() ->  productService.create(product))
-                            .isInstanceOf(IllegalArgumentException.class);
+            assertThatThrownBy(() -> 상품_등록(product))
+                    .isInstanceOf(IllegalArgumentException.class);
         }
 
         @Test
         @DisplayName("가격이 음수이면 예외를 발생시킨다.")
-        void create_negativePrice(){
-            final BigDecimal price = BigDecimal.valueOf(-1000);
-            final Product product = new Product("test product", price);
+        void create_negativePrice() {
+            final Product product = 상품_생성("상품명", -10000);
 
-            assertThatThrownBy(() ->  productService.create(product))
-                            .isInstanceOf(IllegalArgumentException.class);
+            assertThatThrownBy(() -> 상품_등록(product))
+                    .isInstanceOf(IllegalArgumentException.class);
         }
 
     }
 
     @Test
     @DisplayName("현재 등록된 물품들을 반환한다.")
-    void list(){
+    void list() {
+        상품_등록(상품1);
+        상품_등록(상품2);
+        상품_등록(상품3);
+
         final List<Product> actual = productService.list();
-        assertThat(actual).hasSize(6);
+
+        assertThat(actual).hasSize(3);
+
     }
-
-
 }
