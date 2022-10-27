b6ed7fd2ecf77b665b1f0a269d78625234d79e08
corinne <yoo77hyeon@gmail.com>
2022-10-27 17:11:36 +0900

    refactor: MenuGroup Jpa 적용

--- src/main/java/kitchenpos/domain/MenuGroup.java
+++ src/main/java/kitchenpos/domain/MenuGroup.java
@@ -1,14 +1,27 @@
 package kitchenpos.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class MenuGroup {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
-    public MenuGroup() {
-        this(null);
+    protected MenuGroup() {
     }
 
-    public MenuGroup(String name) {
+    public MenuGroup(final Long id, final String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public MenuGroup(final String name) {
         this.name = name;
     }
 
