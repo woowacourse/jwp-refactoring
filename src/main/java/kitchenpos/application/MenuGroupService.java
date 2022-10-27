c8fa318e9c001fb2cb73a86b64df3213aedc3882
hyeonbinsa <shb1833@gmail.com>
2022-10-27 22:36:35 +0900

    feat: MenuGroupController DTO 적용

--- src/main/java/kitchenpos/application/MenuGroupService.java
+++ src/main/java/kitchenpos/application/MenuGroupService.java
@@ -1,11 +1,11 @@
 package kitchenpos.application;
 
+import java.util.List;
 import kitchenpos.dao.MenuGroupDao;
 import kitchenpos.domain.MenuGroup;
+import kitchenpos.dto.MenuGroupRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Service
 public class MenuGroupService {
@@ -16,7 +16,8 @@ public class MenuGroupService {
     }
 
     @Transactional
-    public MenuGroup create(final MenuGroup menuGroup) {
+    public MenuGroup create(final MenuGroupRequest menuGroupRequest) {
+        MenuGroup menuGroup = MenuGroup.from(menuGroupRequest);
         return menuGroupDao.save(menuGroup);
     }
 
--- src/main/java/kitchenpos/domain/MenuGroup.java
+++ src/main/java/kitchenpos/domain/MenuGroup.java
@@ -1,8 +1,11 @@
 package kitchenpos.domain;
 
+import kitchenpos.dto.MenuGroupRequest;
+
 public class MenuGroup {
-    private Long id;
-    private String name;
+
+    private final Long id;
+    private final String name;
 
     public MenuGroup(Long id, String name) {
         this.id = id;
@@ -11,6 +14,10 @@ public class MenuGroup {
 
     public MenuGroup(String name) {
         this(null, name);
+    }
+
+    public static MenuGroup from(MenuGroupRequest menuGroupRequest) {
+        return new MenuGroup(menuGroupRequest.getName());
     }
 
     public Long getId() {
