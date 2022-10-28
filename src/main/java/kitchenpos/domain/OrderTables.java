d95bca64698a5e8d4e83cc28a5700bdef3feafba
bcc0830 <bcc0830@bithub.com>
2022-10-28 21:14:13 +0900

    feat : 주문 테이블 목록인 OrderTables 일급 컬렉션 추가

---
+++ src/main/java/kitchenpos/domain/OrderTables.java
@@ -0,0 +1,49 @@
+package kitchenpos.domain;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class OrderTables {
+
+    private final List<OrderTable> orderTables;
+
+    public OrderTables(List<OrderTable> orderTables) {
+        validateOverTwo(orderTables);
+        this.orderTables = orderTables;
+    }
+
+    private void validateOverTwo(List<OrderTable> orderTables) {
+        if (orderTables.size() < 2) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public boolean canGroup(List<Long> orderTableIds) {
+        if (!isSameSize(orderTableIds)) {
+            return false;
+        }
+        return this.orderTables.stream()
+                .noneMatch(each -> each.getTableGroupId() != null || !each.isEmpty());
+    }
+
+    private boolean isSameSize(List<Long> orderTableIds) {
+        return orderTableIds.size() == this.orderTables.size();
+    }
+
+    public OrderTables changeTableGroupId(Long tableGroupId) {
+        List<OrderTable> changedOrderTables = this.orderTables.stream()
+                .map(orderTable -> orderTable.changeTableGroupId(tableGroupId))
+                .collect(Collectors.toUnmodifiableList());
+        return new OrderTables(changedOrderTables);
+    }
+
+    public List<OrderTable> getOrderTables() {
+        return orderTables;
+    }
+
+    public List<Long> getOrderTableIds() {
+        return this.orderTables.stream()
+                .map(OrderTable::getId)
+                .collect(Collectors.toUnmodifiableList());
+    }
+}
