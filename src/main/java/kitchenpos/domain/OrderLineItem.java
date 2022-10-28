3103a61cf0cd027a48b61f03df88c2a14daddeb2
Soomin Sohn <soomin.sohn.dev@gmail.com>
2022-10-28 22:10:10 +0900

    refactor: OrderLineItem setter삭제

--- src/main/java/kitchenpos/domain/OrderLineItem.java
+++ src/main/java/kitchenpos/domain/OrderLineItem.java
@@ -6,7 +6,11 @@ public class OrderLineItem {
     private Long menuId;
     private long quantity;
 
-    public OrderLineItem() {
+    public OrderLineItem(Long seq, Long orderId, Long menuId, long quantity) {
+        this.seq = seq;
+        this.orderId = orderId;
+        this.menuId = menuId;
+        this.quantity = quantity;
     }
 
     public OrderLineItem(Long seq, Long menuId, long quantity) {
@@ -17,10 +21,6 @@ public class OrderLineItem {
 
     public Long getSeq() {
         return seq;
-    }
-
-    public void setSeq(final Long seq) {
-        this.seq = seq;
     }
 
     public Long getOrderId() {
@@ -35,15 +35,7 @@ public class OrderLineItem {
         return menuId;
     }
 
-    public void setMenuId(final Long menuId) {
-        this.menuId = menuId;
-    }
-
     public long getQuantity() {
         return quantity;
-    }
-
-    public void setQuantity(final long quantity) {
-        this.quantity = quantity;
     }
 }
