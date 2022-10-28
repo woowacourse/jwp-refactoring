58e57b3ab0fd72764dd81d1189d5dc055c17bd9d
hyeonbinsa <shb1833@gmail.com>
2022-10-28 18:10:45 +0900

    refactor: MenuGroup Response DTO 구현

--- src/main/java/kitchenpos/application/MenuGroupService.java
+++ src/main/java/kitchenpos/application/MenuGroupService.java
@@ -1,14 +1,17 @@
 package kitchenpos.application;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import kitchenpos.dao.MenuGroupDao;
 import kitchenpos.domain.MenuGroup;
 import kitchenpos.dto.request.MenuGroupRequest;
+import kitchenpos.dto.response.MenuGroupResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class MenuGroupService {
+
     private final MenuGroupDao menuGroupDao;
 
     public MenuGroupService(final MenuGroupDao menuGroupDao) {
@@ -16,12 +19,14 @@ public class MenuGroupService {
     }
 
     @Transactional
-    public MenuGroup create(final MenuGroupRequest menuGroupRequest) {
-        MenuGroup menuGroup = MenuGroup.from(menuGroupRequest);
-        return menuGroupDao.save(menuGroup);
+    public MenuGroupResponse create(final MenuGroupRequest menuGroupRequest) {
+        MenuGroup menuGroup = menuGroupDao.save(MenuGroup.from(menuGroupRequest));
+        return MenuGroupResponse.from(menuGroup);
     }
 
-    public List<MenuGroup> list() {
-        return menuGroupDao.findAll();
+    public List<MenuGroupResponse> list() {
+        return menuGroupDao.findAll().stream()
+                .map(MenuGroupResponse::from)
+                .collect(Collectors.toList());
     }
 }
--- src/test/java/kitchenpos/application/MenuGroupServiceTest.java
+++ src/test/java/kitchenpos/application/MenuGroupServiceTest.java
@@ -1,13 +1,12 @@
 package kitchenpos.application;
 
 import static kitchenpos.fixture.MenuGroupFixture.세트메뉴_REQUEST;
-import static kitchenpos.fixture.MenuGroupFixture.할인메뉴;
 import static kitchenpos.fixture.MenuGroupFixture.할인메뉴_REQUEST;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
 import kitchenpos.dao.FakeMenuGroupDao;
-import kitchenpos.domain.MenuGroup;
+import kitchenpos.dto.response.MenuGroupResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -25,11 +24,11 @@ public class MenuGroupServiceTest {
     @DisplayName("메뉴 그룹을 생성한다.")
     void create() {
         // when
-        MenuGroup newMenuGroup = menuGroupService.create(할인메뉴_REQUEST);
+        MenuGroupResponse menuGroupResponse = menuGroupService.create(할인메뉴_REQUEST);
 
         // then
-        assertThat(newMenuGroup.getId()).isEqualTo(1L);
-        assertThat(newMenuGroup.getName()).isEqualTo(할인메뉴_REQUEST.getName());
+        assertThat(menuGroupResponse.getId()).isEqualTo(1L);
+        assertThat(menuGroupResponse.getName()).isEqualTo(할인메뉴_REQUEST.getName());
     }
 
     @Test
@@ -40,9 +39,9 @@ public class MenuGroupServiceTest {
         menuGroupService.create(세트메뉴_REQUEST);
 
         // when
-        List<MenuGroup> menuGroups = menuGroupService.list();
+        List<MenuGroupResponse> menuGroupResponses = menuGroupService.list();
 
         // then
-        assertThat(menuGroups.size()).isEqualTo(2);
+        assertThat(menuGroupResponses.size()).isEqualTo(2);
     }
 }
