f597e5de00ac4fad384cbf8d1263554e7e03ac43
Ohzzi <clover7even@naver.com>
2022-10-28 16:46:18 +0900

    refactor: Product의 불필요한 서비스 로직 제거

--- src/main/java/kitchenpos/application/ProductService.java
+++ src/main/java/kitchenpos/application/ProductService.java
@@ -1,9 +1,6 @@
 package kitchenpos.application;
 
-import java.math.BigDecimal;
 import java.util.List;
-import java.util.Objects;
-import kitchenpos.domain.Price;
 import kitchenpos.domain.Product;
 import kitchenpos.domain.repository.ProductRepository;
 import org.springframework.stereotype.Service;
@@ -11,24 +8,18 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ProductService {
-    private final ProductRepository productDao;
+    private final ProductRepository productRepository;
 
-    public ProductService(final ProductRepository productDao) {
-        this.productDao = productDao;
+    public ProductService(final ProductRepository productRepository) {
+        this.productRepository = productRepository;
     }
 
     @Transactional
     public Product create(final Product product) {
-        final Price price = product.getPrice();
-
-        if (Objects.isNull(price) || price.getValue().compareTo(BigDecimal.ZERO) < 0) {
-            throw new IllegalArgumentException();
-        }
-
-        return productDao.save(product);
+        return productRepository.save(product);
     }
 
     public List<Product> list() {
-        return productDao.findAll();
+        return productRepository.findAll();
     }
 }
