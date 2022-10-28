9316795dc48fdeda78c070113cb2411d67070535
hyeonic <dev.hyeonic@gmail.com>
2022-10-28 10:02:44 +0900

    feat: OrderRestController OrderSaveRequest 변경

--- src/main/java/kitchenpos/ui/OrderRestController.java
+++ src/main/java/kitchenpos/ui/OrderRestController.java
@@ -2,6 +2,7 @@ package kitchenpos.ui;
 
 import kitchenpos.application.OrderService;
 import kitchenpos.domain.Order;
+import kitchenpos.dto.OrderSaveRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,30 +10,28 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/orders")
 public class OrderRestController {
+
     private final OrderService orderService;
 
     public OrderRestController(final OrderService orderService) {
         this.orderService = orderService;
     }
 
-    @PostMapping("/api/orders")
-    public ResponseEntity<Order> create(@RequestBody final Order order) {
-        final Order created = orderService.create(order);
+    @PostMapping
+    public ResponseEntity<Order> create(@RequestBody final OrderSaveRequest request) {
+        final Order created = orderService.create(request.toEntity());
         final URI uri = URI.create("/api/orders/" + created.getId());
-        return ResponseEntity.created(uri)
-                .body(created)
-                ;
+        return ResponseEntity.created(uri).body(created);
     }
 
-    @GetMapping("/api/orders")
+    @GetMapping
     public ResponseEntity<List<Order>> list() {
-        return ResponseEntity.ok()
-                .body(orderService.list())
-                ;
+        return ResponseEntity.ok().body(orderService.list());
     }
 
-    @PutMapping("/api/orders/{orderId}/order-status")
+    @PutMapping("/{orderId}/order-status")
     public ResponseEntity<Order> changeOrderStatus(
             @PathVariable final Long orderId,
             @RequestBody final Order order
