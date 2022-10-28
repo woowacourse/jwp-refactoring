c384c637e1f438fb9f3a046770edaa1dd17006d0
hyeonic <dev.hyeonic@gmail.com>
2022-10-28 10:27:55 +0900

    feat: TableGroupRestController TableGroupSaveRequest 의존성 변경

--- src/main/java/kitchenpos/ui/TableGroupRestController.java
+++ src/main/java/kitchenpos/ui/TableGroupRestController.java
@@ -2,33 +2,32 @@ package kitchenpos.ui;
 
 import kitchenpos.application.TableGroupService;
 import kitchenpos.domain.TableGroup;
+import kitchenpos.dto.TableGroupSaveRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 
 @RestController
+@RequestMapping("/api/table-groups")
 public class TableGroupRestController {
+
     private final TableGroupService tableGroupService;
 
     public TableGroupRestController(final TableGroupService tableGroupService) {
         this.tableGroupService = tableGroupService;
     }
 
-    @PostMapping("/api/table-groups")
-    public ResponseEntity<TableGroup> create(@RequestBody final TableGroup tableGroup) {
-        final TableGroup created = tableGroupService.create(tableGroup);
+    @PostMapping
+    public ResponseEntity<TableGroup> create(@RequestBody final TableGroupSaveRequest request) {
+        final TableGroup created = tableGroupService.create(request.toEntity());
         final URI uri = URI.create("/api/table-groups/" + created.getId());
-        return ResponseEntity.created(uri)
-                .body(created)
-                ;
+        return ResponseEntity.created(uri).body(created);
     }
 
-    @DeleteMapping("/api/table-groups/{tableGroupId}")
+    @DeleteMapping("/{tableGroupId}")
     public ResponseEntity<Void> ungroup(@PathVariable final Long tableGroupId) {
         tableGroupService.ungroup(tableGroupId);
-        return ResponseEntity.noContent()
-                .build()
-                ;
+        return ResponseEntity.noContent().build();
     }
 }
