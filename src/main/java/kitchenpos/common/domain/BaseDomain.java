26e295199d85a10cdf5f2126c71811e37285f3b3
dwl21 <nggus5@naver.com>
2022-10-28 17:12:27 +0900

    feat: 생성 시간을 있는 base 도메인 생성

--- src/main/java/kitchenpos/common/domain/BaseDomain.java
+++ src/main/java/kitchenpos/common/domain/BaseDomain.java
@@ -4,9 +4,13 @@ import java.time.LocalDateTime;
 
 public abstract class BaseDomain {
 
-    protected final LocalDateTime createdTime = LocalDateTime.now();
+    protected final LocalDateTime createdDate;
 
-    public LocalDateTime getCreatedTime() {
-        return createdTime;
+    protected BaseDomain(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
     }
 }
