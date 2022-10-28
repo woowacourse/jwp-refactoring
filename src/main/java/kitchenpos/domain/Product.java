554e4b9e97650a78b1418d552d3f42a5b0f6b22a
ihan-ul <dkfghg3@naver.com>
2022-10-28 17:42:25 +0900

    refactor: product dto 추가

--- src/main/java/kitchenpos/domain/Product.java
+++ src/main/java/kitchenpos/domain/Product.java
@@ -1,33 +1,43 @@
 package kitchenpos.domain;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public class Product {
-    private Long id;
-    private String name;
-    private BigDecimal price;
+    private final Long id;
+    private final String name;
+    private final BigDecimal price;
+
+    public Product(final String name, final long price) {
+        this(name, BigDecimal.valueOf(price));
+    }
+
+    public Product(final String name, final BigDecimal price) {
+        this(null, name, price);
+    }
+
+    public Product(final Long id, final String name, final BigDecimal price) {
+        validate(price);
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    private static void validate(final BigDecimal price) {
+        if (Objects.isNull(price) || price.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException();
+        }
+    }
 
     public Long getId() {
         return id;
-    }
-
-    public void setId(final Long id) {
-        this.id = id;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(final String name) {
-        this.name = name;
-    }
-
     public BigDecimal getPrice() {
         return price;
-    }
-
-    public void setPrice(final BigDecimal price) {
-        this.price = price;
     }
 }
--- src/test/java/kitchenpos/application/ProductServiceTest.java
+++ src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -2,12 +2,13 @@ package kitchenpos.application;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import java.math.BigDecimal;
 import java.util.List;
 import javax.transaction.Transactional;
-import kitchenpos.domain.Product;
+import kitchenpos.application.dto.ProductRequest;
+import kitchenpos.application.dto.ProductResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -25,22 +26,18 @@ class ProductServiceTest {
     @DisplayName("상품의 가격이 null인 경우 예외가 발생한다")
     @Test
     void null_price_exception() {
-        final Product product = new Product();
-        product.setName("양념");
-        product.setPrice(null);
+        final ProductRequest request = new ProductRequest("양념", null);
 
-        assertThatThrownBy(() -> productService.create(product))
+        assertThatThrownBy(() -> productService.create(request))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("상품의 가격이 0보다 작은 경우 예외가 발생한다")
     @Test
     void negative_price_exception() {
-        final Product product = new Product();
-        product.setName("양념");
-        product.setPrice(BigDecimal.valueOf(-1));
+        final ProductRequest request = new ProductRequest("양념", BigDecimal.valueOf(-1L));
 
-        assertThatThrownBy(() -> productService.create(product))
+        assertThatThrownBy(() -> productService.create(request))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -48,24 +45,22 @@ class ProductServiceTest {
     @ValueSource(ints = {0, 1})
     @ParameterizedTest
     void create(final int price) {
-        final Product product = new Product();
-        product.setName("양념");
-        product.setPrice(BigDecimal.valueOf(price));
+        final ProductRequest request = new ProductRequest("양념", BigDecimal.valueOf(price));
 
-        final Product createdProduct = productService.create(product);
+        final ProductResponse createdProduct = productService.create(request);
 
         assertAll(
                 () -> assertThat(createdProduct.getId()).isNotNull(),
-                () -> assertThat(createdProduct.getName()).isEqualTo(product.getName()),
-                () -> assertThat(createdProduct.getPrice()).isEqualByComparingTo(product.getPrice())
+                () -> assertThat(createdProduct.getName()).isEqualTo(request.getName()),
+                () -> assertThat(createdProduct.getPrice()).isEqualByComparingTo(request.getPrice())
         );
     }
 
     @DisplayName("전체 상품을 조회할 수 있다")
     @Test
     void findAll() {
-        final List<Product> products = productService.list();
+        final List<ProductResponse> productResponses = productService.list();
 
-        assertThat(products).hasSize(6);
+        assertThat(productResponses).hasSize(6);
     }
 }
