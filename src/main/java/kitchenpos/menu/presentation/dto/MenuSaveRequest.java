faa5e6c68381330ce16e95731dab120897e916d9
dwl21 <nggus5@naver.com>
2022-10-29 01:29:29 +0900

    feat: 서비스 레이어 메뉴 생성 DTO 구현

---
+++ src/main/java/kitchenpos/menu/presentation/dto/MenuSaveRequest.java
@@ -0,0 +1,34 @@
+package kitchenpos.menu.presentation.dto;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import kitchenpos.common.domain.Name;
+import kitchenpos.common.domain.Price;
+import kitchenpos.menu.application.dto.MenuProductSaveRequest;
+import kitchenpos.menu.domain.Menu;
+import kitchenpos.menu.domain.MenuProduct;
+
+public class MenuSaveRequest {
+
+    private String name;
+    private Integer price;
+    private Long menuGroupId;
+    private List<MenuProductSaveRequest> menuProducts;
+
+    public MenuSaveRequest(String name, Integer price, Long menuGroupId, List<MenuProductSaveRequest> menuProducts) {
+        this.name = name;
+        this.price = price;
+        this.menuGroupId = menuGroupId;
+        this.menuProducts = menuProducts;
+    }
+
+    public Menu toEntity() {
+        return new Menu(Name.of(name), Price.from(price), menuGroupId);
+    }
+
+    public List<MenuProduct> toMenuProductEntities(Long menuId) {
+        return menuProducts.stream()
+            .map(it -> it.toEntity(menuId))
+            .collect(Collectors.toList());
+    }
+}
