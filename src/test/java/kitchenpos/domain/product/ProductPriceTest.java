8e738bc5aaca8d85ac9304e6797167e9b345474f
김찬 <gmelddl514@gmail.com>
2022-10-28 15:23:58 +0900

    test: Product 도메인 테스트 추가

---
+++ src/test/java/kitchenpos/domain/product/ProductPriceTest.java
@@ -0,0 +1,23 @@
+package kitchenpos.domain.product;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.math.BigDecimal;
+import org.junit.jupiter.api.Test;
+
+class ProductPriceTest {
+
+    @Test
+    void 생성_시_null이면_예외가_발생한다() {
+        assertThatThrownBy(() -> new ProductPrice(null))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("가격은 null 혹은 0 미만일 수 없습니다.");
+    }
+    
+    @Test
+    void 생성_시_음수이면_예외가_발생한다() {
+        assertThatThrownBy(() -> new ProductPrice(BigDecimal.valueOf(-1)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("가격은 null 혹은 0 미만일 수 없습니다.");
+    }
+}
