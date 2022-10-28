835106e53ef20144454df853b239e0909a57c7fa
hyunrrr <whgusrms96@gmail.com>
2022-10-28 20:28:22 +0900

    fix: 잘못된 import 수정

--- src/main/java/kitchenpos/repository/MenuRepository.java
+++ src/main/java/kitchenpos/repository/MenuRepository.java
@@ -1,6 +1,6 @@
 package kitchenpos.repository;
 
-import kitchenpos.domain.Menu;
+import kitchenpos.domain.entity.Menu;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MenuRepository extends JpaRepository<Menu, Long> {
