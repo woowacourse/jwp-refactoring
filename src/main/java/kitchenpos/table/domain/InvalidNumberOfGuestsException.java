ea0f0c5c1d24eebb3a36b00d5bb087b9e6a8f02d
dwl21 <nggus5@naver.com>
2022-10-28 22:30:07 +0900

    feat: 올바르지 않은 손님 수로 변경할 때 발생하는 예외 구현

---
+++ src/main/java/kitchenpos/table/domain/InvalidNumberOfGuestsException.java
@@ -0,0 +1,8 @@
+package kitchenpos.table.domain;
+
+public class InvalidNumberOfGuestsException extends RuntimeException {
+
+    public InvalidNumberOfGuestsException() {
+        super("올바르지 않은 손님 수입니다.");
+    }
+}
