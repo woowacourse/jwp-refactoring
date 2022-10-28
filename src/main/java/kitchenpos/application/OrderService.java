850ab2ac760e2a1b5e438d7805845f3d73bb2421
Hyunta <mohai2618@gmail.com>
2022-10-28 16:31:47 +0900

    refactor: OrderService의 create 메서드 구현

--- src/main/java/kitchenpos/application/OrderService.java
+++ src/main/java/kitchenpos/application/OrderService.java
@@ -8,13 +8,13 @@ import kitchenpos.dao.OrderLineItemDao;
 import kitchenpos.dao.OrderTableDao;
 import kitchenpos.domain.Order;
 import kitchenpos.domain.OrderLineItem;
+import kitchenpos.domain.OrderLineItems;
 import kitchenpos.domain.OrderStatus;
 import kitchenpos.domain.OrderTable;
 import kitchenpos.ui.dto.OrderCreateRequest;
 import kitchenpos.ui.dto.OrderUpdateRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.CollectionUtils;
 
 @Service
 public class OrderService {
@@ -37,15 +37,8 @@ public class OrderService {
 
     @Transactional
     public Order create(final OrderCreateRequest request) {
-        final List<OrderLineItem> orderLineItems = request.getOrderLineItems();
-
-        if (CollectionUtils.isEmpty(orderLineItems)) {
-            throw new IllegalArgumentException();
-        }
-
-        final List<Long> menuIds = orderLineItems.stream()
-                .map(OrderLineItem::getMenuId)
-                .collect(Collectors.toList());
+        OrderLineItems orderLineItems = new OrderLineItems(request.getOrderLineItems());
+        final List<Long> menuIds = orderLineItems.getMenuIds();
 
         if (orderLineItems.size() != menuDao.countByIdIn(menuIds)) {
             throw new IllegalArgumentException();
@@ -53,17 +46,18 @@ public class OrderService {
 
         final OrderTable orderTable = orderTableDao.findById(request.getOrderTableId())
                 .orElseThrow(IllegalArgumentException::new);
-
         orderTable.checkEmpty();
 
         final Order savedOrder = orderDao.save(Order.createFrom(orderTable.getId()));
+        setOrderLineItems(savedOrder, orderLineItems);
+        return savedOrder;
+    }
 
-        List<OrderLineItem> savedOrderLineItems = orderLineItems.stream()
+    private void setOrderLineItems(Order savedOrder, OrderLineItems orderLineItems) {
+        List<OrderLineItem> savedOrderLineItems = orderLineItems.getValue().stream()
                 .peek(orderLineItem -> orderLineItem.setOrderId(savedOrder.getId()))
                 .collect(Collectors.toList());
-
         savedOrder.setOrderLineItems(savedOrderLineItems);
-        return savedOrder;
     }
 
     public List<Order> list() {
---
+++ src/main/java/kitchenpos/domain/OrderLineItems.java
@@ -0,0 +1,35 @@
+package kitchenpos.domain;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.util.CollectionUtils;
+
+public class OrderLineItems {
+
+    private List<OrderLineItem> value;
+
+    public OrderLineItems(List<OrderLineItem> orderLineItems) {
+        checkEmpty(orderLineItems);
+        this.value = orderLineItems;
+    }
+
+    private void checkEmpty(List<OrderLineItem> orderLineItems) {
+        if (CollectionUtils.isEmpty(orderLineItems)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public long size() {
+        return value.size();
+    }
+
+    public List<Long> getMenuIds() {
+        return value.stream()
+                .map(OrderLineItem::getMenuId)
+                .collect(Collectors.toList());
+    }
+
+    public List<OrderLineItem> getValue() {
+        return value;
+    }
+}
