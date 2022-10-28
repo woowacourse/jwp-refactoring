431de8eb4c1735ee1f7ea1b710a26a2615d49a86
hyunrrr <whgusrms96@gmail.com>
2022-10-28 14:50:24 +0900

    refactor: menuGroup의 deprecated메서드 삭제

--- src/main/java/kitchenpos/domain/MenuGroup.java
+++ src/main/java/kitchenpos/domain/MenuGroup.java
@@ -6,30 +6,21 @@ public class MenuGroup {
     private Long id;
     private String name;
 
-    @Deprecated
-    public MenuGroup() {
+    public MenuGroup(Long id, String name) {
+        this.id = id;
+        this.name = name;
     }
 
     public MenuGroup(String name) {
-        this.name = name;
+        this(null, name);
     }
 
     public Long getId() {
         return id;
     }
 
-    @Deprecated
-    public void setId(final Long id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
-    }
-
-    @Deprecated
-    public void setName(final String name) {
-        this.name = name;
     }
 
     @Override
