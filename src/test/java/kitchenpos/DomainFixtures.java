ddd9642ba16e5e5e841aaf118e0de5422e944370
jaejae-yoo <wotj102@gmail.com>
2022-10-28 17:52:57 +0900

    refactor: MenuGroupRequest 생성

--- src/test/java/kitchenpos/DomainFixtures.java
+++ src/test/java/kitchenpos/DomainFixtures.java
@@ -2,11 +2,11 @@ package kitchenpos;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import kitchenpos.application.dto.MenuGroupRequest;
 import kitchenpos.application.dto.MenuRequest;
 import kitchenpos.application.dto.OrderTableRequest;
 import kitchenpos.application.dto.ProductRequest;
 import kitchenpos.domain.MenuGroup;
-import kitchenpos.domain.OrderTable;
 
 public class DomainFixtures {
 
@@ -23,6 +23,10 @@ public class DomainFixtures {
 
     public static MenuGroup 면_메뉴_그룹() {
         return new MenuGroup("면");
+    }
+
+    public static MenuGroupRequest 면_메뉴_그룹_요청() {
+        return new MenuGroupRequest("면");
     }
 
     public static OrderTableRequest 빈_주문_테이블_3인() {
--- src/test/java/kitchenpos/acceptance/MenuGroupAcceptanceTest.java
+++ src/test/java/kitchenpos/acceptance/MenuGroupAcceptanceTest.java
@@ -1,10 +1,11 @@
 package kitchenpos.acceptance;
 
-import static kitchenpos.DomainFixtures.면_메뉴_그룹;
+import static kitchenpos.DomainFixtures.면_메뉴_그룹_요청;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Arrays;
 import java.util.List;
+import kitchenpos.application.dto.MenuGroupRequest;
 import kitchenpos.domain.MenuGroup;
 import org.junit.jupiter.api.Test;
 
@@ -13,7 +14,7 @@ public class MenuGroupAcceptanceTest extends AcceptanceTest {
     @Test
     void 메뉴_그룹을_추가한다() {
         MenuGroup menuGroup = testRestTemplate.postForObject("http://localhost:" + port + "/api/menu-groups",
-                면_메뉴_그룹(), MenuGroup.class);
+                new MenuGroupRequest("면"), MenuGroup.class);
 
         assertThat(menuGroup.getName()).isEqualTo("면");
         assertThat(menuGroup.getId()).isNotZero();
@@ -22,9 +23,9 @@ public class MenuGroupAcceptanceTest extends AcceptanceTest {
     @Test
     void 메뉴_그룹을_조회한다() {
         testRestTemplate.postForObject("http://localhost:" + port + "/api/menu-groups",
-                면_메뉴_그룹(), MenuGroup.class);
+                면_메뉴_그룹_요청(), MenuGroup.class);
         testRestTemplate.postForObject("http://localhost:" + port + "/api/menu-groups",
-                new MenuGroup("육류"), MenuGroup.class);
+                new MenuGroupRequest("육류"), MenuGroup.class);
 
         List<MenuGroup> groups = Arrays.asList(
                 testRestTemplate.getForObject("http://localhost:" + port + "/api/menu-groups", MenuGroup[].class));
