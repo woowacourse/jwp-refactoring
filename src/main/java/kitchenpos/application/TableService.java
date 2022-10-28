e9d2a06d15ac338b0c764a6f9d6d115f9634137c
sinb57 <sinb57@naver.com>
2022-10-29 01:18:11 +0900

    refactor: TableService 메서드 분리

--- src/main/java/kitchenpos/application/TableService.java
+++ src/main/java/kitchenpos/application/TableService.java
@@ -13,6 +13,7 @@ import java.util.Objects;
 
 @Service
 public class TableService {
+
     private final OrderDao orderDao;
     private final OrderTableDao orderTableDao;
 
@@ -38,37 +39,53 @@ public class TableService {
         final OrderTable savedOrderTable = orderTableDao.findById(orderTableId)
                 .orElseThrow(() -> new IllegalArgumentException("주문 테이블을 찾을 수 없습니다."));
 
-        if (Objects.nonNull(savedOrderTable.getTableGroupId())) {
-            throw new IllegalArgumentException("단체 지정된 테이블입니다.");
-        }
-
-        if (orderDao.existsByOrderTableIdAndOrderStatusIn(
-                orderTableId, Arrays.asList(OrderStatus.COOKING.name(), OrderStatus.MEAL.name()))) {
-            throw new IllegalArgumentException("계산이 완료되지 않은 테이블입니다.");
-        }
+        validateOrderTableNotGrouped(savedOrderTable);
+        validateAllOrderTablesCompleted(orderTableId);
 
         savedOrderTable.setEmpty(orderTable.isEmpty());
 
         return orderTableDao.save(savedOrderTable);
     }
 
+    private void validateOrderTableNotGrouped(final OrderTable savedOrderTable) {
+        if (Objects.nonNull(savedOrderTable.getTableGroupId())) {
+            throw new IllegalArgumentException("단체 지정된 테이블입니다.");
+        }
+    }
+
+    private void validateAllOrderTablesCompleted(final Long orderTableId) {
+        if (orderDao.existsByOrderTableIdAndOrderStatusIn(
+                orderTableId, Arrays.asList(OrderStatus.COOKING.name(), OrderStatus.MEAL.name()))) {
+            throw new IllegalArgumentException("계산이 완료되지 않은 테이블입니다.");
+        }
+    }
+
     @Transactional
     public OrderTable changeNumberOfGuests(final Long orderTableId, final OrderTable orderTable) {
         final int numberOfGuests = orderTable.getNumberOfGuests();
 
-        if (numberOfGuests < 0) {
-            throw new IllegalArgumentException("손님 수는 음수가 될 수 없습니다.");
-        }
+        validateNumberOfGuestsNotNegative(numberOfGuests);
 
         final OrderTable savedOrderTable = orderTableDao.findById(orderTableId)
                 .orElseThrow(() -> new IllegalArgumentException("주문 테이블을 찾을 수 없습니다."));
 
-        if (savedOrderTable.isEmpty()) {
-            throw new IllegalArgumentException("주문 테이블이 비어있습니다.");
-        }
+        validateOrderTableNotEmpty(savedOrderTable);
 
         savedOrderTable.setNumberOfGuests(numberOfGuests);
 
         return orderTableDao.save(savedOrderTable);
     }
+
+    private void validateNumberOfGuestsNotNegative(int numberOfGuests) {
+        if (numberOfGuests < 0) {
+            throw new IllegalArgumentException("손님 수는 음수가 될 수 없습니다.");
+        }
+    }
+
+    private void validateOrderTableNotEmpty(OrderTable savedOrderTable) {
+        if (savedOrderTable.isEmpty()) {
+            throw new IllegalArgumentException("주문 테이블이 비어있습니다.");
+        }
+    }
+
 }
