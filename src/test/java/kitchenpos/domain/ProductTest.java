d0c5d388b52bd5c66836e43a68c36c92e1e1516c
nailseong <nnis9857@gmail.com>
2022-10-28 20:51:41 +0900

    test: Product 단위 테스트

---
+++ src/test/java/kitchenpos/domain/ProductTest.java
@@ -0,0 +1,33 @@
+package kitchenpos.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.math.BigDecimal;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("상품 도메인의")
+class ProductTest {
+
+    @Nested
+    @DisplayName("정적 팩토리 메서드 of는")
+    class Of {
+
+        @Test
+        @DisplayName("가격이 null일 수 없다.")
+        void of_priceIsNull_exception() {
+            // when & then
+            assertThatThrownBy(() -> Product.of("치킨", null))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        @DisplayName("가격이 음수일 수 없다.")
+        void of_priceIsNegative_exception() {
+            // when & then
+            assertThatThrownBy(() -> Product.of("치킨", BigDecimal.valueOf(-1L)))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+    }
+}
