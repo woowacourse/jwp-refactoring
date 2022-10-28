baea20a20111d0dbc1f17b9df39f24b90d94a488
Richard JEON <ztzy1907@gmail.com>
2022-10-28 14:59:57 +0900

    fix(MenuGroup): 메뉴 그룹 name 중복 유효성 검증 제거

--- src/main/java/kitchenpos/application/concrete/JpaMenuGroupService.java
+++ src/main/java/kitchenpos/application/concrete/JpaMenuGroupService.java
@@ -3,7 +3,6 @@ package kitchenpos.application.concrete;
 import java.util.List;
 import kitchenpos.application.MenuGroupService;
 import kitchenpos.domain.MenuGroup;
-import kitchenpos.exception.badrequest.MenuGroupNameDuplicateException;
 import kitchenpos.repository.MenuGroupRepository;
 import kitchenpos.ui.dto.request.MenuGroupCreateRequest;
 import org.springframework.stereotype.Service;
@@ -21,17 +20,9 @@ public class JpaMenuGroupService implements MenuGroupService {
     @Transactional
     @Override
     public MenuGroup create(final MenuGroupCreateRequest request) {
-        final var name = request.getName();
-        validateDuplicateMenuGroupName(name);
-        final var newMenuGroup = new MenuGroup(name);
+        final var newMenuGroup = new MenuGroup(request.getName());
 
         return menuGroupRepository.save(newMenuGroup);
-    }
-
-    private void validateDuplicateMenuGroupName(final String name) {
-        if (menuGroupRepository.existsByName(name)) {
-            throw new MenuGroupNameDuplicateException(name);
-        }
     }
 
     @Override
