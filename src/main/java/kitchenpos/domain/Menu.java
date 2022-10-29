925557338ed4b1f4b6c4a73735f01d1b44c62fe4
Hyunta <mohai2618@gmail.com>
2022-10-29 19:47:05 +0900

    feat: Menu, MenuProduct jpa 변환

--- src/main/java/kitchenpos/domain/Menu.java
+++ src/main/java/kitchenpos/domain/Menu.java
@@ -3,14 +3,37 @@ package kitchenpos.domain;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Objects;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import kitchenpos.ui.dto.MenuProductCreateRequest;
 
+@Entity
 public class Menu {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "name", nullable = false)
     private String name;
+
+    @Column(name = "price", precision = 19, scale = 2, nullable = false)
     private BigDecimal price;
+
+    @Column(name = "menu_group_id", nullable = false)
     private Long menuGroupId;
+
+    @OneToMany(mappedBy = "menuId",
+            orphanRemoval = true,
+            cascade = CascadeType.ALL)
     private List<MenuProduct> menuProducts;
+
+    public Menu() {
+    }
 
     public Menu(Long id, String name, BigDecimal price, Long menuGroupId, List<MenuProduct> menuProducts) {
         checkPrice(price);
