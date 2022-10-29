8cebab9f1f76e88aaefb2aa67c5cd550be2f8c92
Wishoon <heonga2@gmail.com>
2022-10-29 22:16:16 +0900

    refactor: 테스트에서의 DisplayName 가독성 향상 환경 설정

--- src/test/java/kitchenpos/support/annotation/AcceptanceTest.java
+++ src/test/java/kitchenpos/support/annotation/AcceptanceTest.java
@@ -4,6 +4,8 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import kitchenpos.support.execution.AcceptanceTestExecutionListener;
 import kitchenpos.support.extension.DataCleanerExtension;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
@@ -13,7 +15,7 @@ import org.springframework.test.context.TestExecutionListeners;
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @Retention(RetentionPolicy.RUNTIME)
 @ExtendWith(DataCleanerExtension.class)
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 @TestExecutionListeners(value = {AcceptanceTestExecutionListener.class}, mergeMode = TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS)
 public @interface AcceptanceTest {
 }
-
