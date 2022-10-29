bbec864857b46036096c2c3b124341891b5f296e
hyeonic <dev.hyeonic@gmail.com>
2022-10-30 03:49:59 +0900

    feat: MenuProduct amount 계산 로직 추가

--- src/main/java/kitchenpos/domain/MenuProduct.java
+++ src/main/java/kitchenpos/domain/MenuProduct.java
@@ -1,10 +1,12 @@
 package kitchenpos.domain;
 
+import java.math.BigDecimal;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Transient;
 
 @Entity
 public class MenuProduct {
@@ -21,6 +23,27 @@ public class MenuProduct {
 
     @Column(name = "quantity")
     private long quantity;
+
+    @Transient
+    private BigDecimal price;
+
+    protected MenuProduct() {
+    }
+
+    public MenuProduct(final Long productId, final long quantity, final BigDecimal price) {
+        this(null, productId, quantity, price);
+    }
+
+    public MenuProduct(final Long menuId, final Long productId, final long quantity, final BigDecimal price) {
+        this.menuId = menuId;
+        this.productId = productId;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public BigDecimal getAmount() {
+        return price.multiply(BigDecimal.valueOf(quantity));
+    }
 
     public Long getSeq() {
         return seq;
@@ -52,5 +75,9 @@ public class MenuProduct {
 
     public void setQuantity(final long quantity) {
         this.quantity = quantity;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
     }
 }
