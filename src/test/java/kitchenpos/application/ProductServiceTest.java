4010b70159eafbba576e7d1b0616be0ed286c23c
Kim Seunglae <obey1342@gmail.com>
2022-10-30 00:31:28 +0900

    refactor: ProductServiceTest 구조 변경

--- src/test/java/kitchenpos/application/ProductServiceTest.java
+++ src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -7,29 +7,17 @@ import java.math.BigDecimal;
 import java.util.List;
 import kitchenpos.application.dto.request.ProductCreateRequest;
 import kitchenpos.application.dto.response.ProductResponse;
-import kitchenpos.dao.ProductDao;
-import kitchenpos.fixture.ProductFixture;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class ProductServiceTest {
-
-    private ProductDao productDao;
-    private ProductService productService;
-
-    @BeforeEach
-    void setUp() {
-        productDao = ProductFixture.setUp().getProductDao();
-        productService = new ProductService(productDao);
-    }
+class ProductServiceTest extends ServiceTest {
 
     @Test
     @DisplayName("상품을 생성한다.")
     void createProduct() {
         final String productName = "맥북m1";
         final BigDecimal productPrice = BigDecimal.valueOf(3000);
-        final ProductCreateRequest request = ProductFixture.createProductRequest(productName, productPrice);
+        final ProductCreateRequest request = new ProductCreateRequest(productName, productPrice);
 
         final ProductResponse response = productService.create(request);
 
@@ -42,14 +30,12 @@ class ProductServiceTest {
     @Test
     @DisplayName("상품 목록을 조회한다.")
     void getProducts() {
-        final List<ProductResponse> expectedProducts = ProductFixture.setUp()
-                .getFixtures();
         final List<ProductResponse> products = productService.list();
 
         assertAll(
-                () -> assertThat(products).hasSameSizeAs(expectedProducts),
-                () -> assertThat(products).usingRecursiveComparison()
-                        .isEqualTo(expectedProducts)
+                () -> assertThat(products).hasSize(2),
+                () -> assertThat(products.get(0).getName()).isEqualTo("후라이드"),
+                () -> assertThat(products.get(1).getName()).isEqualTo("양념치킨")
         );
     }
 }
