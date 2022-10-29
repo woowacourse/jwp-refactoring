49112b15dfb0e271174cc6e8133f00ae0c1aafc2
Kim Seunglae <obey1342@gmail.com>
2022-10-30 00:32:07 +0900

    refactor: TableService 테스트코드 구조 변경

--- src/test/java/kitchenpos/application/TableGroupServiceTest.java
+++ src/test/java/kitchenpos/application/TableGroupServiceTest.java
@@ -24,20 +24,25 @@ import org.junit.jupiter.api.Test;
 class TableGroupServiceTest extends ServiceTest {
 
     private Long tableGroupId;
+    private Long orderTableId1;
+    private Long orderTableId2;
 
     @BeforeEach
     void setUp() {
-        tableGroupId = tableGroupDao.save(new TableGroup(List.of(
-                new OrderTable(1L, null, 0, true),
-                new OrderTable(2L, null, 0, true)
-        ))).getId();
+        orderTableDao.save(new OrderTable(null, null, 0, true));
+        orderTableDao.save(new OrderTable(null, null, 0, true));
+        final OrderTable orderTable1 = orderTableDao.save(new OrderTable(null, null, 0, true));
+        final OrderTable orderTable2 = orderTableDao.save(new OrderTable(null, null, 0, true));
+        tableGroupId = tableGroupDao.save(new TableGroup(List.of(orderTable1, orderTable2))).getId();
+        orderTableId1 = orderTable1.getId();
+        orderTableId2 = orderTable2.getId();
     }
 
     @Test
     @DisplayName("테이블 그룹을 생성한다.")
     void createTableGroup() {
         final TableGroupCreateRequest request = new TableGroupCreateRequest(
-                List.of(new OrderTableGroupRequest(3L), new OrderTableGroupRequest(4L)));
+                List.of(new OrderTableGroupRequest(1L), new OrderTableGroupRequest(2L)));
         final TableGroupResponse actual = tableGroupService.create(request);
 
         for (OrderTableResponse orderTable : actual.getOrderTables()) {
@@ -64,7 +69,7 @@ class TableGroupServiceTest extends ServiceTest {
     @DisplayName("테이블 그룹을 해제한다.")
     void ungroup() {
         tableGroupService.ungroup(tableGroupId);
-        final List<OrderTable> actualOrderTables = orderTableDao.findAllByIdIn(List.of(1L, 2L));
+        final List<OrderTable> actualOrderTables = orderTableDao.findAllByIdIn(List.of(orderTableId1, orderTableId2));
 
         assertAll(
                 () -> assertThat(actualOrderTables.get(0).getTableGroupId()).isNull(),
@@ -77,7 +82,7 @@ class TableGroupServiceTest extends ServiceTest {
     @Test
     @DisplayName("각 주문 테이블의 주문 상태가 식사 상태면 예외 발생")
     void whenIsMeal() {
-        orderDao.save(new Order(1L, MEAL.name(), LocalDateTime.now(), List.of(new OrderLineItem(1L, 2))));
+        orderDao.save(new Order(orderTableId1, MEAL.name(), LocalDateTime.now(), List.of(new OrderLineItem(1L, 2))));
 
         assertThatThrownBy(() -> tableGroupService.ungroup(tableGroupId))
                 .isInstanceOf(IllegalArgumentException.class);
@@ -86,7 +91,7 @@ class TableGroupServiceTest extends ServiceTest {
     @Test
     @DisplayName("각 주문 테이블의 주문 상태가 조리 상태면 예외 발생")
     void whenIsCooking() {
-        orderDao.save(new Order(1L, COOKING.name(), LocalDateTime.now(), List.of(new OrderLineItem(1L, 2))));
+        orderDao.save(new Order(orderTableId2, COOKING.name(), LocalDateTime.now(), List.of(new OrderLineItem(1L, 2))));
 
         assertThatThrownBy(() -> tableGroupService.ungroup(tableGroupId))
                 .isInstanceOf(IllegalArgumentException.class);
--- src/test/java/kitchenpos/application/TableServiceTest.java
+++ src/test/java/kitchenpos/application/TableServiceTest.java
@@ -1,37 +1,22 @@
 package kitchenpos.application;
 
-import static kitchenpos.fixture.OrderTableFixture.두명인_테이블;
-import static kitchenpos.fixture.OrderTableFixture.주문가능_테이블;
-import static kitchenpos.fixture.OrderTableFixture.테이블그룹이_존재하지_않는_테이블;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import kitchenpos.application.dto.request.OrderTableCreateReqeust;
 import kitchenpos.application.dto.response.OrderTableResponse;
-import kitchenpos.dao.OrderDao;
-import kitchenpos.dao.OrderTableDao;
+import kitchenpos.domain.Order;
+import kitchenpos.domain.OrderLineItem;
 import kitchenpos.domain.OrderStatus;
 import kitchenpos.domain.OrderTable;
-import kitchenpos.fixture.OrderFixture;
 import kitchenpos.fixture.OrderTableFixture;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class TableServiceTest {
-
-    private OrderDao orderDao;
-    private OrderTableDao orderTableDao;
-    private TableService tableService;
-
-    @BeforeEach
-    void setUp() {
-        orderDao = OrderFixture.setUp().getOrderDao();
-        orderTableDao = OrderTableFixture.setUp().getOrderTableDao();
-        tableService = new TableService(orderDao, orderTableDao);
-    }
+class TableServiceTest extends ServiceTest {
 
     @Test
     @DisplayName("테이블을 생성한다.")
@@ -51,34 +36,28 @@ class TableServiceTest {
     @Test
     @DisplayName("테이블 목록들을 조회한다.")
     void getTables() {
-        final List<OrderTableResponse> expectedOrderTables = OrderTableFixture.setUp().getFixtures();
-
         final List<OrderTableResponse> orderTables = tableService.list();
 
-        assertAll(
-                () -> assertThat(orderTables).hasSameSizeAs(expectedOrderTables),
-                () -> assertThat(orderTables)
-                        .usingRecursiveComparison()
-                        .isEqualTo(expectedOrderTables)
-        );
+        assertThat(orderTables).hasSize(2);
     }
 
     @Test
     @DisplayName("테이블의 상태를 변경한다.")
     void changeEmptyTable() {
-        final OrderTable orderTable = orderTableDao.findById(테이블그룹이_존재하지_않는_테이블)
+        final OrderTable orderTable = orderTableDao.findById(1L)
                 .orElseThrow();
 
-        final OrderTableResponse changedOrderTable = tableService.changeEmpty(orderTable.getId(), true);
+        final OrderTableResponse changedOrderTable = tableService.changeEmpty(orderTable.getId(), false);
 
-        assertThat(changedOrderTable.isEmpty()).isTrue();
+        assertThat(changedOrderTable.isEmpty()).isFalse();
     }
 
     @Test
     @DisplayName("주문 테이블의 주문의 상태가 조리 상태면 예외 발생")
     void whenOrderTableWithCookingStatus() {
         final OrderTable savedOrderTableId = orderTableDao.save(new OrderTable(null, 3, false));
-        orderDao.save(OrderFixture.createOrder(savedOrderTableId.getId(), OrderStatus.COOKING.name()));
+        orderDao.save(new Order(savedOrderTableId.getId(), OrderStatus.COOKING.name(), LocalDateTime.now(),
+                List.of(new OrderLineItem(1L, 1L))));
 
         assertThatThrownBy(() -> tableService.changeEmpty(savedOrderTableId.getId(), true))
                 .isInstanceOf(IllegalArgumentException.class);
@@ -88,16 +67,19 @@ class TableServiceTest {
     @DisplayName("주문 테이블의 주문의 상태가 식사 상태면 예외 발생")
     void whenOrderTableWithMealStatus() {
         final OrderTable savedOrderTableId = orderTableDao.save(new OrderTable(null, 3, false));
-        orderDao.save(OrderFixture.createOrder(savedOrderTableId.getId(), OrderStatus.MEAL.name()));
+        orderDao.save(new Order(savedOrderTableId.getId(), OrderStatus.MEAL.name(), LocalDateTime.now(),
+                List.of(new OrderLineItem(1L, 1L))));
 
-        assertThatThrownBy(() -> tableService.changeEmpty(두명인_테이블, true))
+        assertThatThrownBy(() -> tableService.changeEmpty(savedOrderTableId.getId(), true))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("주문 테이블의 손님 수를 변경한다.")
     void changeNumberOrGuests() {
-        final OrderTableResponse changedOrderTable = tableService.changeNumberOfGuests(주문가능_테이블, 5);
+        final OrderTable savedOrderTableId = orderTableDao.save(new OrderTable(null, 3, false));
+
+        final OrderTableResponse changedOrderTable = tableService.changeNumberOfGuests(savedOrderTableId.getId(), 5);
 
         assertThat(changedOrderTable.getNumberOfGuests()).isEqualTo(5);
     }
