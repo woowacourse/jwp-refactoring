3dcd17efe5bbea3f401d0596010964e6bf1ea2ae
yukong <kyb062666@gmail.com>
2022-10-30 01:34:03 +0900

    refactor: 메서드명 변경

--- src/main/java/kitchenpos/application/TableService.java
+++ src/main/java/kitchenpos/application/TableService.java
@@ -39,7 +39,7 @@ public class TableService {
     @Transactional
     public OrderTableResponse changeEmpty(final Long orderTableId, final OrderTableRequest request) {
         final OrderTable orderTable = TableConvertor.convertToOrderTable(request);
-        final OrderTable changedOrderTable = changeMenu(orderTableId, orderTable);
+        final OrderTable changedOrderTable = changeEmptyOfTable(orderTableId, orderTable);
         return TableConvertor.convertToOrderTableResponse(changedOrderTable);
     }
 
@@ -54,10 +54,10 @@ public class TableService {
         return TableConvertor.convertToOrderTableResponse(changedOrderTable);
     }
 
-    private OrderTable changeMenu(final Long orderTableId, final OrderTable orderTable) {
+    private OrderTable changeEmptyOfTable(final Long orderTableId, final OrderTable orderTable) {
         final OrderTable savedOrderTable = findOrderTableById(orderTableId);
 
-        validateOrderTableExists(savedOrderTable);
+        validateOrderTableIsNotExists(savedOrderTable);
         validateOrderStatusIsCompletion(orderTableId);
 
         savedOrderTable.changeEmpty(orderTable.isEmpty());
@@ -69,7 +69,7 @@ public class TableService {
             .orElseThrow(() -> new IllegalArgumentException(String.format("존재하지 않는 테이블 입니다. [%s]", orderTableId)));
     }
 
-    private void validateOrderTableExists(final OrderTable savedOrderTable) {
+    private void validateOrderTableIsNotExists(final OrderTable savedOrderTable) {
         if (Objects.nonNull(savedOrderTable.getTableGroupId())) {
             throw new IllegalArgumentException(String.format("테이블 그룹이 존재합니다. [%s]", savedOrderTable.getTableGroupId()));
         }
