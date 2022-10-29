d83917eba02a0bd9f4b3cbf38468bac2d2d5bcdd
hyeonic <dev.hyeonic@gmail.com>
2022-10-30 00:02:16 +0900

    feat: Menu Request 및 Response dto 추가

--- src/test/java/kitchenpos/application/fixture/MenuFixtures.java
+++ src/test/java/kitchenpos/application/fixture/MenuFixtures.java
@@ -4,6 +4,8 @@ import java.math.BigDecimal;
 import java.util.List;
 import kitchenpos.domain.Menu;
 import kitchenpos.domain.MenuProduct;
+import kitchenpos.dto.MenuResponse;
+import kitchenpos.dto.MenuSaveRequest;
 
 public class MenuFixtures {
 
@@ -28,5 +30,12 @@ public class MenuFixtures {
                                           final Long menuGroupId,
                                           final List<MenuProduct> menuProducts) {
         return new Menu(name, price, menuGroupId, menuProducts);
+    }
+
+    public static final MenuSaveRequest generateMenuSaveRequest(final String name,
+                                                                final BigDecimal price,
+                                                                final Long menuGroupId,
+                                                                final List<MenuProduct> menuProducts) {
+        return new MenuSaveRequest(name, price, menuGroupId, menuProducts);
     }
 }
