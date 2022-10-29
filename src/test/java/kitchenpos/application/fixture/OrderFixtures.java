984bd3eb2bee9a3ce2fb0da8d0a184cfbb795f44
hyeonic <dev.hyeonic@gmail.com>
2022-10-30 00:42:26 +0900

    refactor: Order 리플렉션 활용 생성기 추가

--- src/test/java/kitchenpos/application/fixture/OrderFixtures.java
+++ src/test/java/kitchenpos/application/fixture/OrderFixtures.java
@@ -1,5 +1,7 @@
 package kitchenpos.application.fixture;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 import java.time.LocalDateTime;
 import java.util.List;
 import kitchenpos.domain.Order;
@@ -9,11 +11,8 @@ import kitchenpos.dto.OrderSaveRequest;
 
 public class OrderFixtures {
 
-    public static final Order generateOrder(final Long orderTableId, final List<OrderLineItem> orderLineItems) {
-        return generateOrder(null, orderTableId, null, LocalDateTime.now(), orderLineItems);
-    }
-
-    public static final Order generateOrder(final Long orderTableId, final OrderStatus orderStatus,
+    public static final Order generateOrder(final Long orderTableId,
+                                            final OrderStatus orderStatus,
                                             final List<OrderLineItem> orderLineItems) {
         return generateOrder(null, orderTableId, orderStatus.name(), LocalDateTime.now(), orderLineItems);
     }
@@ -33,7 +32,36 @@ public class OrderFixtures {
                                             final String orderStatus,
                                             final LocalDateTime orderedTime,
                                             final List<OrderLineItem> orderLineItems) {
-        return new Order(orderTableId, orderStatus, orderedTime, orderLineItems);
+        try {
+            Constructor<Order> constructor = Order.class.getDeclaredConstructor();
+            constructor.setAccessible(true);
+            Order order = constructor.newInstance();
+            Class<? extends Order> clazz = order.getClass();
+
+            Field idField = clazz.getDeclaredField("id");
+            idField.setAccessible(true);
+            idField.set(order, id);
+
+            Field orderTableIdField = clazz.getDeclaredField("orderTableId");
+            orderTableIdField.setAccessible(true);
+            orderTableIdField.set(order, orderTableId);
+
+            Field orderStatusField = clazz.getDeclaredField("orderStatus");
+            orderStatusField.setAccessible(true);
+            orderStatusField.set(order, orderStatus);
+
+            Field orderedTimeField = clazz.getDeclaredField("orderedTime");
+            orderedTimeField.setAccessible(true);
+            orderedTimeField.set(order, orderedTime);
+
+            Field orderLineItemsField = clazz.getDeclaredField("orderLineItems");
+            orderLineItemsField.setAccessible(true);
+            orderLineItemsField.set(order, orderLineItems);
+
+            return order;
+        } catch (final Exception e) {
+            throw new RuntimeException();
+        }
     }
 
     public static final OrderSaveRequest generateOrderSaveRequest(final Long orderTableId,
