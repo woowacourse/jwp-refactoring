130e29af7f73e00374258afd90e46d2adbf55fb1
devHudi <devhudi@gmail.com>
2022-10-29 19:09:07 +0900

    test: 상품 인수 테스트 작성

---
+++ src/test/java/kitchenpos/acceptance/ProductAcceptanceTest.java
@@ -0,0 +1,51 @@
+package kitchenpos.acceptance;
+
+import static org.junit.jupiter.api.DynamicTest.dynamicTest;
+
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import java.math.BigDecimal;
+import java.util.stream.Stream;
+import kitchenpos.domain.Product;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
+import org.springframework.http.HttpStatus;
+
+public class ProductAcceptanceTest extends AcceptanceTest {
+
+    @DisplayName("상품을 관리한다.")
+    @TestFactory
+    Stream<DynamicTest> manageProduct() {
+        return Stream.of(
+                dynamicTest("상품을 추가한다.", () -> {
+                    // when
+                    ExtractableResponse<Response> response = 상품을_생성한다("강정치킨", 17_000);
+
+                    // then
+                    상태코드를_검증한다(response, HttpStatus.CREATED);
+                    필드가_Null이_아닌지_검증한다(response, "id");
+                }),
+                dynamicTest("모든 상품을 조회한다.", () -> {
+                    // when
+                    ExtractableResponse<Response> response = 모든_상품을_조회한다();
+
+                    // then
+                    상태코드를_검증한다(response, HttpStatus.OK);
+                    리스트_길이를_검증한다(response, ".", 1);
+                })
+        );
+    }
+
+    ExtractableResponse<Response> 상품을_생성한다(final String name, final int price) {
+        Product product = new Product();
+        product.setName(name);
+        product.setPrice(BigDecimal.valueOf(price));
+
+        return post("/api/products", product);
+    }
+
+    ExtractableResponse<Response> 모든_상품을_조회한다() {
+        return get("/api/products");
+    }
+}
