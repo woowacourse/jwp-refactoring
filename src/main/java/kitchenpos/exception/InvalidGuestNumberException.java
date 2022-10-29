ae251b658a074b85491f75fe25c4f3ae7610369c
Chris <bjuuuu98@gmail.com>
2022-10-29 14:19:54 +0900

    refactor(InvalidGuestNumberException): 예외 메시지 구체화

--- src/main/java/kitchenpos/exception/InvalidGuestNumberException.java
+++ src/main/java/kitchenpos/exception/InvalidGuestNumberException.java
@@ -1,4 +1,10 @@
 package kitchenpos.exception;
 
 public class InvalidGuestNumberException extends IllegalArgumentException {
+
+    private static final String MESSAGE = "고객의 수는 1명 이상이어야 합니다.";
+
+    public InvalidGuestNumberException() {
+        super(MESSAGE);
+    }
 }
