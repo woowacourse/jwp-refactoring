6b9c470c3b110f663e3936672da120162a5b57e7
koo <gudonghee2000@gmail.com>
2022-10-30 20:43:56 +0900

    refactor: 전체 service 객체의 트랜잭션 애노테이션 추가

--- src/main/java/kitchenpos/application/OrderService.java
+++ src/main/java/kitchenpos/application/OrderService.java
@@ -20,6 +20,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
 public class OrderService {
 
     private final MenuRepository menuRepository;
