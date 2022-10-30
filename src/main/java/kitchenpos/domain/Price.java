334ec2270cd4f4623a00adb3c74947199090f073
yukong <kyb062666@gmail.com>
2022-10-31 04:01:59 +0900

    refactor: 메서드명 변경

--- src/main/java/kitchenpos/domain/Price.java
+++ src/main/java/kitchenpos/domain/Price.java
@@ -8,7 +8,7 @@ public class Price {
     private final BigDecimal price;
 
     public Price(final BigDecimal price) {
-        validatePriceIsValid(price);
+        validatePriceIsNotNullAndIsHigherMinimum(price);
         this.price = price;
     }
 
@@ -20,7 +20,7 @@ public class Price {
         return price.compareTo(other) > 0;
     }
 
-    private void validatePriceIsValid(final BigDecimal price) {
+    private void validatePriceIsNotNullAndIsHigherMinimum(final BigDecimal price) {
         if (Objects.isNull(price) || price.compareTo(BigDecimal.ZERO) < 0) {
             throw new IllegalArgumentException(String.format("상품의 가격은 null 이 아니며 0원 이상이어야 합니다. [%s]", price));
         }
