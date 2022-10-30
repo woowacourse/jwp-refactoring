868559d6b7d940626b73536a7e43948d8db216fe
hyeonic <dev.hyeonic@gmail.com>
2022-10-30 10:32:42 +0900

    feat: OrderLineItem Request dto 추가

--- src/main/java/kitchenpos/dto/OrderSaveRequest.java
+++ src/main/java/kitchenpos/dto/OrderSaveRequest.java
@@ -1,33 +1,25 @@
 package kitchenpos.dto;
 
-import java.time.LocalDateTime;
 import java.util.List;
-import kitchenpos.domain.Order;
-import kitchenpos.domain.OrderLineItem;
-import kitchenpos.domain.OrderStatus;
 
 public class OrderSaveRequest {
 
     private Long orderTableId;
-    private List<OrderLineItem> orderLineItems;
+    private List<OrderLineItemSaveRequest> orderLineItems;
 
     private OrderSaveRequest() {
     }
 
-    public OrderSaveRequest(final Long orderTableId, final List<OrderLineItem> orderLineItems) {
+    public OrderSaveRequest(final Long orderTableId, final List<OrderLineItemSaveRequest> orderLineItems) {
         this.orderTableId = orderTableId;
         this.orderLineItems = orderLineItems;
-    }
-
-    public Order toEntity() {
-        return new Order(orderTableId, OrderStatus.COOKING.name(), LocalDateTime.now(), orderLineItems);
     }
 
     public Long getOrderTableId() {
         return orderTableId;
     }
 
-    public List<OrderLineItem> getOrderLineItems() {
+    public List<OrderLineItemSaveRequest> getOrderLineItems() {
         return orderLineItems;
     }
 }
