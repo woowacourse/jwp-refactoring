bc8f4fa61a4d3ed0b4c4ae4b4bb99c95bd32542b
Seongwon97 <osw970325@naver.com>
2022-10-30 23:56:31 +0900

    refactor: Price의 내부 검증 로직의 접근제어자를 private으로 수정

--- src/main/java/kitchenpos/domain/Price.java
+++ src/main/java/kitchenpos/domain/Price.java
@@ -12,7 +12,7 @@ public class Price {
         this.value = value;
     }
 
-    public void validatePrice(BigDecimal price) {
+    private void validatePrice(BigDecimal price) {
         if (Objects.isNull(price) || price.compareTo(BigDecimal.ZERO) < 0) {
             throw new IllegalArgumentException();
         }
