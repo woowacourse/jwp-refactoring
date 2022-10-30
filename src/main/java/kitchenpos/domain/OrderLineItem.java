c585a05fa93abfc8ea74523958b27734bcb0ac17
Hongdonggeon <ghd700@naver.com>
2022-10-30 19:45:02 +0900

    feat: OrderLineItem 도메인 정리

--- src/main/java/kitchenpos/domain/OrderLineItem.java
+++ src/main/java/kitchenpos/domain/OrderLineItem.java
@@ -21,37 +21,19 @@ public class OrderLineItem {
         this(null, orderId, menuId, quantity);
     }
 
-    public OrderLineItem() {}
-
     public Long getSeq() {
         return seq;
-    }
-
-    public void setSeq(final Long seq) {
-        this.seq = seq;
     }
 
     public Long getOrderId() {
         return orderId;
     }
 
-    public void setOrderId(final Long orderId) {
-        this.orderId = orderId;
-    }
-
     public Long getMenuId() {
         return menuId;
     }
 
-    public void setMenuId(final Long menuId) {
-        this.menuId = menuId;
-    }
-
     public long getQuantity() {
         return quantity;
-    }
-
-    public void setQuantity(final long quantity) {
-        this.quantity = quantity;
     }
 }
