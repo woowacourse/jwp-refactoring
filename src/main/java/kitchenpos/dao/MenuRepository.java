fd963426e46422fc4ddc86458cb14d6bd0351313
yhh1056 <yhh1056@naver.com>
2022-10-31 14:18:18 +0900

    refactor: 쿼리메서드 제거

--- src/main/java/kitchenpos/dao/MenuRepository.java
+++ src/main/java/kitchenpos/dao/MenuRepository.java
@@ -4,7 +4,6 @@ import java.util.List;
 import kitchenpos.domain.Menu;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface MenuRepository extends JpaRepository<Menu, Long> {
