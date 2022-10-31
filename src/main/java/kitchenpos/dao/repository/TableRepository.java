33f2085af284e21c3cfb64a85a7d650ab6be31a9
BETTERFUTURE4 <rerub0831@gmail.com>
2022-10-31 20:12:10 +0900

    chore: 컨벤션에 맞게 코드 수정

--- src/main/java/kitchenpos/dao/repository/TableRepository.java
+++ src/main/java/kitchenpos/dao/repository/TableRepository.java
@@ -1,8 +1,8 @@
 package kitchenpos.dao.repository;
 
 import java.util.List;
-import kitchenpos.dao.jdbctemplate.JdbcTemplateOrderTableDao;
 import kitchenpos.dao.OrderTableDao;
+import kitchenpos.dao.jdbctemplate.JdbcTemplateOrderTableDao;
 import kitchenpos.domain.OrderTable;
 import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.stereotype.Repository;
