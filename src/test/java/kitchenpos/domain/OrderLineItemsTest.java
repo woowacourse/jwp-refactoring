449c0bcd4d20fededb1f70e820900e54e6faf1eb
kth990303 <kth990303@naver.com>
2022-10-31 21:41:06 +0900

    test: 도메인 검증 테스트 작성

---
+++ src/test/java/kitchenpos/domain/OrderLineItemsTest.java
@@ -0,0 +1,19 @@
+package kitchenpos.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class OrderLineItemsTest {
+    @Test
+    @DisplayName("주문 메뉴가 올바른지 확인한다")
+    void hasValidMenus() {
+        final OrderLineItems orderLineItems = new OrderLineItems(new ArrayList<>());
+
+        final boolean actual = orderLineItems.hasValidMenus(0);
+
+        assertThat(actual).isTrue();
+    }
+}
