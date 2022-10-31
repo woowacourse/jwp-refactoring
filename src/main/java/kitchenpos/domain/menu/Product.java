a9bacb4fc430ebaccdf1975d8af7ea6eeec5ceef
devHudi <devhudi@gmail.com>
2022-10-31 13:55:12 +0900

    refactor: Product를 JPA로 리팩토링

--- src/main/java/kitchenpos/domain/menu/Product.java
+++ src/main/java/kitchenpos/domain/menu/Product.java
@@ -2,13 +2,25 @@ package kitchenpos.domain.menu;
 
 import java.math.BigDecimal;
 import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Table(name = "product")
+@Entity
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
+
     private BigDecimal price;
 
-    public Product() {
+    protected Product() {
     }
 
     public Product(final String name, final BigDecimal price) {
