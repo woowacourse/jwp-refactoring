40ce961109d8b881a120b618086fa4c5bceef550
Chris <bjuuuu98@gmail.com>
2022-10-31 12:09:50 +0900

    refactor: 모든 Repository에 @Repository 등록

--- src/main/java/kitchenpos/dao/OrderLineItemRepository.java
+++ src/main/java/kitchenpos/dao/OrderLineItemRepository.java
@@ -2,6 +2,8 @@ package kitchenpos.dao;
 
 import kitchenpos.domain.OrderLineItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OrderLineItemRepository extends JpaRepository<OrderLineItem, Long> {
 }
