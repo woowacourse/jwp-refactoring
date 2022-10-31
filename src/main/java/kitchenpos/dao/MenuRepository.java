fa9568bf890ac4a22a2198b3f9529a7a0fd27043
Chris <bjuuuu98@gmail.com>
2022-10-31 12:08:33 +0900

    refactor: 불필요한 메서드 삭제

--- src/main/java/kitchenpos/dao/MenuRepository.java
+++ src/main/java/kitchenpos/dao/MenuRepository.java
@@ -1,12 +1,9 @@
 package kitchenpos.dao;
 
-import java.util.List;
 import kitchenpos.domain.Menu;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MenuRepository extends JpaRepository<Menu, Long> {
-
-    int countByIdIn(List<Long> menuIds);
 }
