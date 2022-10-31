20b69c35389516f807e66b8c514b62eb09a330ec
devHudi <devhudi@gmail.com>
2022-10-31 13:01:49 +0900

    test: Menu의 불필요한 setter 제거

--- src/main/java/kitchenpos/domain/menu/Menu.java
+++ src/main/java/kitchenpos/domain/menu/Menu.java
@@ -16,8 +16,14 @@ public class Menu {
 
     public Menu(final String name, final BigDecimal price, final Long menuGroupId,
                 final List<MenuProduct> menuProducts) {
+        this(null, name, price, menuGroupId, menuProducts);
+    }
+
+    public Menu(final Long id, final String name, final BigDecimal price, final Long menuGroupId,
+                final List<MenuProduct> menuProducts) {
         validatePrice(price);
 
+        this.id = id;
         this.name = name;
         this.price = price;
         this.menuGroupId = menuGroupId;
@@ -34,32 +40,16 @@ public class Menu {
         return id;
     }
 
-    public void setId(final Long id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(final String name) {
-        this.name = name;
     }
 
     public BigDecimal getPrice() {
         return price;
     }
 
-    public void setPrice(final BigDecimal price) {
-        this.price = price;
-    }
-
     public Long getMenuGroupId() {
         return menuGroupId;
-    }
-
-    public void setMenuGroupId(final Long menuGroupId) {
-        this.menuGroupId = menuGroupId;
     }
 
     public List<MenuProduct> getMenuProducts() {
