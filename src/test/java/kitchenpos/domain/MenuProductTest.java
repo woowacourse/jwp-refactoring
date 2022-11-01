c38fd59ba10c683057509b7ee1372c0994b7902d
meatsby <meatsby@gmail.com>
2022-11-01 13:32:17 +0900

    test: 도메인 테스트 추가

---
+++ src/test/java/kitchenpos/domain/MenuProductTest.java
@@ -0,0 +1,35 @@
+package kitchenpos.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.math.BigDecimal;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class MenuProductTest {
+
+    @Test
+    @DisplayName("메뉴 상품에 메뉴를 지정한다.")
+    void arrangeMenu() {
+        MenuGroup menuGroup = MenuGroup.builder()
+                .name("메뉴 그룹")
+                .build();
+        Product product = Product.builder()
+                .name("상품")
+                .price(BigDecimal.valueOf(5000))
+                .build();
+        MenuProduct menuProduct = MenuProduct.builder()
+                .product(product)
+                .quantity(1L)
+                .build();
+        Menu menu = Menu.builder()
+                .name("메뉴")
+                .price(BigDecimal.valueOf(5000))
+                .menuGroup(menuGroup)
+                .menuProducts(List.of(menuProduct))
+                .build();
+
+        assertThat(menuProduct.getMenu()).isEqualTo(menu);
+    }
+}
---
+++ src/test/java/kitchenpos/domain/OrderTablesTest.java
@@ -0,0 +1,49 @@
+package kitchenpos.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class OrderTablesTest {
+
+    @ParameterizedTest
+    @MethodSource("orderTables")
+    @DisplayName("테이블이 비어있거나 2개 미만일 경우 예외가 발생한다.")
+    void validateOrderTables(final List<OrderTable> orderTables) {
+        assertThatThrownBy(() -> new OrderTables(orderTables))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("주문 테이블은 비어있거나 2개 미만일 수 없습니다.");
+    }
+
+    @ParameterizedTest
+    @MethodSource("orderTable")
+    @DisplayName("활성화되어 있거나 단체 지정된 주문 테이블은 그룹으로 만들 경우 예외가 발생한다.")
+    void validateGroup(final OrderTable orderTable) {
+        assertThatThrownBy(() -> new OrderTables(List.of(orderTable)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("주문 테이블은 비어있거나 2개 미만일 수 없습니다.");
+    }
+
+    private static Stream<Arguments> orderTables() {
+        return Stream.of(
+                Arguments.of(List.of()),
+                Arguments.of(List.of(new OrderTable()))
+        );
+    }
+
+    private static Stream<Arguments> orderTable() {
+        return Stream.of(
+                Arguments.of(OrderTable.builder()
+                        .empty(false)
+                        .build()),
+                Arguments.of(OrderTable.builder()
+                        .tableGroup(new TableGroup())
+                        .build())
+        );
+    }
+}
