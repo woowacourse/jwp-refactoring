0d7843a72df640ae803547f8e4bd58cc9c570c8a
hyeonic <dev.hyeonic@gmail.com>
2022-11-01 16:15:10 +0900

    test: Product 검증 추가

--- src/main/java/kitchenpos/domain/Product.java
+++ src/main/java/kitchenpos/domain/Product.java
@@ -25,35 +25,26 @@ public class Product {
     }
 
     public Product(final String name, final BigDecimal price) {
+        this.name = name;
+        this.price = price;
+        validatePrice();
+    }
+
+    private void validatePrice() {
         if (Objects.isNull(price) || price.compareTo(BigDecimal.ZERO) < 0) {
             throw new IllegalArgumentException();
         }
-
-        this.name = name;
-        this.price = price;
     }
 
     public Long getId() {
         return id;
     }
 
-    public void setId(final Long id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
     }
 
-    public void setName(final String name) {
-        this.name = name;
-    }
-
     public BigDecimal getPrice() {
         return price;
-    }
-
-    public void setPrice(final BigDecimal price) {
-        this.price = price;
     }
 }
