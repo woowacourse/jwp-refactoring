0ce01d697f748841597d0b3fdf73a20612142682
koo <gudonghee2000@gmail.com>
2022-11-01 14:19:21 +0900

    test: ProductTest의 테스트 코드 수정

--- src/test/java/kitchenpos/domain/ProductTest.java
+++ src/test/java/kitchenpos/domain/ProductTest.java
@@ -1,9 +1,9 @@
 package kitchenpos.domain;
 
 import static kitchenpos.common.constants.Constants.야채곱창_이름;
-import static kitchenpos.common.constants.Constants.잘못된_가격;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.math.BigDecimal;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,6 +12,9 @@ class ProductTest {
     @DisplayName("상품 가격이 0원 보다 작으면 예외가 발생한다.")
     @Test
     void 상품_가격이_0원_보다_작으면_예외가_발생한다() {
+        // given
+        BigDecimal 잘못된_가격 = BigDecimal.valueOf(-1L);
+
         // when & then
         assertThatThrownBy(() -> new Product(야채곱창_이름, 잘못된_가격))
                 .isInstanceOf(IllegalArgumentException.class)
@@ -21,8 +24,11 @@ class ProductTest {
     @DisplayName("상품 가격이 null 이면 예외가 발생한다.")
     @Test
     void 상품_가격이_null_이면_예외가_발생한다() {
+        // given
+        BigDecimal 잘못된_가격 = null;
+
         // when & then
-        assertThatThrownBy(() -> new Product(야채곱창_이름, null))
+        assertThatThrownBy(() -> new Product(야채곱창_이름, 잘못된_가격))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("가격은 0원 이상의 정수로 입력해주세요.");
     }
