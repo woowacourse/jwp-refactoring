c5508fb413b6c3c63050db2c5355e22622d5c725
koo <gudonghee2000@gmail.com>
2022-11-01 14:21:15 +0900

    refactor: MenuGroupService의 줄바꿈 컨벤션 추가

--- src/main/java/kitchenpos/application/MenuGroupService.java
+++ src/main/java/kitchenpos/application/MenuGroupService.java
@@ -12,6 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @Transactional(readOnly = true)
 public class MenuGroupService {
+
     private final MenuGroupRepository menuGroupRepository;
 
     public MenuGroupService(final MenuGroupRepository menuGroupRepository) {
