d8e3e32726d8c80dcc966b00c71440264fe70cb6
Donggyu <a29661498@gmail.com>
2022-11-01 09:00:15 +0900

    chore: 사용하지 않는 메소드 제거

--- src/main/java/kitchenpos/domain/repository/OrderTableRepository.java
+++ src/main/java/kitchenpos/domain/repository/OrderTableRepository.java
@@ -6,7 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderTableRepository extends JpaRepository<OrderTable, Long> {
 
-    List<OrderTable> findAllByTableGroupId(Long groupId);
-
     List<OrderTable> findAllByIdIn(List<Long> ids);
 }
