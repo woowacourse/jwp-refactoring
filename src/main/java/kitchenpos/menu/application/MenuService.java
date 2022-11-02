eb6a8c4d5145a7403630d5d554473700ef6574e7
ulimy <ulim130@gmail.com>
2022-11-02 20:58:04 +0900

    refactor : MenuValidator 생성

--- src/main/java/kitchenpos/menu/application/MenuService.java
+++ src/main/java/kitchenpos/menu/application/MenuService.java
@@ -3,36 +3,30 @@ package kitchenpos.menu.application;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import kitchenpos.product.event.VerifiedMenuProductsEvent;
 import kitchenpos.menu.domain.Menu;
+import kitchenpos.menu.domain.MenuValidator;
 import kitchenpos.menu.dto.application.MenuProductDto;
 import kitchenpos.menu.dto.request.CreateMenuRequest;
 import kitchenpos.menu.dto.response.MenuResponse;
-import kitchenpos.menu.repository.MenuGroupRepository;
 import kitchenpos.menu.repository.MenuRepository;
 
 @Service
 @Transactional(readOnly = true)
 public class MenuService {
     private final MenuRepository menuRepository;
-    private final MenuGroupRepository menuGroupRepository;
-    private final ApplicationEventPublisher publisher;
+    private final MenuValidator menuValidator;
 
-    public MenuService(MenuRepository menuRepository, MenuGroupRepository menuGroupRepository,
-        ApplicationEventPublisher publisher) {
+    public MenuService(MenuRepository menuRepository, MenuValidator menuValidator) {
         this.menuRepository = menuRepository;
-        this.menuGroupRepository = menuGroupRepository;
-        this.publisher = publisher;
+        this.menuValidator = menuValidator;
     }
 
     @Transactional
     public MenuResponse create(final CreateMenuRequest request) {
-        validateMenuGroupById(request.getMenuGroupId());
-        publisher.publishEvent(new VerifiedMenuProductsEvent(request.getPrice(), request.getMenuProducts()));
+        menuValidator.validateCreateMenu(request);
 
         List<MenuProductDto> menuProducts = request.getMenuProducts().stream()
             .map(it -> new MenuProductDto(it.getProductId(), it.getQuantity()))
@@ -52,11 +46,6 @@ public class MenuService {
         return menuRepository.findAll().stream()
             .map(MenuResponse::new)
             .collect(Collectors.toList());
-    }
-
-    private void validateMenuGroupById(Long menuGroupId) {
-        menuGroupRepository.findById(menuGroupId)
-            .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 메뉴그룹입니다."));
     }
 
 }
---
+++ src/main/java/kitchenpos/menu/domain/MenuValidator.java
@@ -0,0 +1,66 @@
+package kitchenpos.menu.domain;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Component;
+
+import kitchenpos.menu.dto.request.CreateMenuProductRequest;
+import kitchenpos.menu.dto.request.CreateMenuRequest;
+import kitchenpos.menu.repository.MenuGroupRepository;
+import kitchenpos.product.domain.Product;
+import kitchenpos.product.repository.ProductRepository;
+
+@Component
+public class MenuValidator {
+    private MenuGroupRepository menuGroupRepository;
+    private ProductRepository productRepository;
+
+    public MenuValidator(MenuGroupRepository menuGroupRepository, ProductRepository productRepository) {
+        this.menuGroupRepository = menuGroupRepository;
+        this.productRepository = productRepository;
+    }
+
+    public void validateCreateMenu(final CreateMenuRequest request) {
+        final Long menuGroupId = request.getMenuGroupId();
+        validateMenuGroupById(menuGroupId);
+
+        final List<Product> products = getProducts(request.getMenuProducts());
+        validateProperPrice(products, request);
+    }
+
+    private void validateMenuGroupById(final Long menuGroupId) {
+        menuGroupRepository.findById(menuGroupId)
+            .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 메뉴그룹입니다."));
+    }
+
+    private List<Product> getProducts(final List<CreateMenuProductRequest> menuProducts) {
+        final List<Long> productIds = menuProducts.stream()
+            .map(CreateMenuProductRequest::getProductId)
+            .collect(Collectors.toList());
+
+        final List<Product> products = productRepository.findAllByIdIn(productIds);
+
+        if (productIds.size() != products.size()) {
+            throw new IllegalArgumentException("존재하지 않는 제품입니다.");
+        }
+
+        return products;
+    }
+
+    private void validateProperPrice(final List<Product> products, final CreateMenuRequest request) {
+        Map<Long, BigDecimal> productsPriceMap = products.stream()
+            .collect(Collectors.toMap(it -> it.getId(), it -> it.getPrice()));
+
+        BigDecimal totalPrice = request.getMenuProducts().stream()
+            .map(it -> productsPriceMap.get(it.getProductId()).multiply(BigDecimal.valueOf(it.getQuantity())))
+            .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+        if (request.getPrice().compareTo(totalPrice) > 0) {
+            throw new IllegalArgumentException("각 상품 가격의 합보다 큰 가격을 적용할 수 없습니다.");
+        }
+    }
+
+}
