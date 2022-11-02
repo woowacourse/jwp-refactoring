ec454a34d9754ec6d3f0852f306c2a4c6ee965a8
skullKim <kyk990328@gmail.com>
2022-11-02 21:20:44 +0900

    test: 주문 예외에 대한 인수 테스트

--- src/test/java/kitchenpos/acceptance/OrderAcceptanceTest.java
+++ src/test/java/kitchenpos/acceptance/OrderAcceptanceTest.java
@@ -1,6 +1,7 @@
 package kitchenpos.acceptance;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
@@ -12,6 +13,7 @@ import kitchenpos.acceptance.common.httpcommunication.MenuHttpCommunication;
 import kitchenpos.acceptance.common.httpcommunication.OrderHttpCommunication;
 import kitchenpos.acceptance.common.httpcommunication.OrderTableHttpCommunication;
 import kitchenpos.acceptance.common.httpcommunication.ProductHttpCommunication;
+import kitchenpos.common.annotation.SpringTestWithData;
 import kitchenpos.common.fixture.RequestBody;
 import kitchenpos.ui.dto.response.MenuGroupResponse;
 import kitchenpos.ui.dto.response.MenuResponse;
@@ -19,19 +21,42 @@ import kitchenpos.ui.dto.response.OrderResponse;
 import kitchenpos.ui.dto.response.OrderTableResponse;
 import kitchenpos.ui.dto.response.ProductResponse;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 
 @DisplayName("OrderAcceptance 는 ")
 public class OrderAcceptanceTest extends AcceptanceTest {
 
-    @DisplayName("주문을 생성한다.")
-    @Test
-    void createOrder() {
-        final ExtractableResponse<Response> response = order().getResponse();
+    @DisplayName("주문을 생성할 때 ")
+    @SpringTestWithData
+    @Nested
+    class OrderCreationTest {
 
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
-        assertThat(response.header("Location")).isNotBlank();
+        @DisplayName("주문을 생성한다.")
+        @Test
+        void createOrderSuccess() {
+            final ExtractableResponse<Response> response = order().getResponse();
+
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+            assertThat(response.header("Location")).isNotBlank();
+        }
+
+        @DisplayName("상품이 존재하지 않으면 에러응답이 온다.")
+        @Test
+        void createOrderFail() {
+            final OrderTableResponse orderTable = OrderTableHttpCommunication.create(RequestBody.ORDER_TABLE_1)
+                    .getResponseBodyAsObject(OrderTableResponse.class);
+            final OrderTableResponse nonEmptyOrderTable = OrderTableHttpCommunication.changeEmpty(orderTable.getId(),
+                            RequestBody.NON_EMPTY_TABLE)
+                    .getResponseBodyAsObject(OrderTableResponse.class);
+
+            final ExtractableResponse<Response> response = OrderHttpCommunication.create(
+                            RequestBody.getOrder(2L, nonEmptyOrderTable.getId()))
+                    .getResponse();
+
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        }
     }
 
     @DisplayName("주문 항목을 가져온다.")
@@ -44,15 +69,35 @@ public class OrderAcceptanceTest extends AcceptanceTest {
         assertThat(orders.size()).isEqualTo(1);
     }
 
-    @DisplayName("주문 상태를 변경한다")
-    @Test
-    void changeOrderStatus() {
-        final OrderResponse order = order().getResponseBodyAsObject(OrderResponse.class);
-        final OrderResponse result = OrderHttpCommunication.changeOrderStatus(order.getId(),
-                        Map.of("orderStatus", "MEAL"))
-                .getResponseBodyAsObject(OrderResponse.class);
+    @DisplayName("주문 상태를 변경할 때 ")
+    @SpringTestWithData
+    class OrderStatusAlternationTest {
 
-        assertThat(result.getOrderStatus().name()).isEqualTo("MEAL");
+        @DisplayName("주문 상태를 변경한다")
+        @Test
+        void changeOrderStatusSuccess() {
+            final OrderResponse order = order().getResponseBodyAsObject(OrderResponse.class);
+            final OrderResponse result = OrderHttpCommunication.changeOrderStatus(order.getId(),
+                            Map.of("orderStatus", "MEAL"))
+                    .getResponseBodyAsObject(OrderResponse.class);
+
+            assertThat(result.getOrderStatus().name()).isEqualTo("MEAL");
+        }
+
+        @DisplayName("주문 상태가 COMPLETION이면 에러를 던진다.")
+        @Test
+        void changeOrderStatusFail() {
+            final OrderResponse order = order().getResponseBodyAsObject(OrderResponse.class);
+            final OrderResponse result = OrderHttpCommunication.changeOrderStatus(order.getId(),
+                            Map.of("orderStatus", "COMPLETION"))
+                    .getResponseBodyAsObject(OrderResponse.class);
+
+            final ExtractableResponse<Response> response = OrderHttpCommunication.changeOrderStatus(order.getId(),
+                            Map.of("orderStatus", "MEAL"))
+                    .getResponse();
+
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     private HttpCommunication order() {
