5d20e7c3a047938bf6ade8a05af28988ff8d4af2
정진혁 <wilgur513@naver.com>
2022-11-04 20:14:05 +0900

    refactor: OrderLineItemCreator 추출

--- src/main/java/kitchenpos/order/application/OrderService.java
+++ src/main/java/kitchenpos/order/application/OrderService.java
@@ -2,67 +2,43 @@ package kitchenpos.order.application;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 import kitchenpos.menu.domain.repository.MenuRepository;
 import kitchenpos.order.domain.repository.OrderRepository;
 import kitchenpos.order.domain.repository.OrderTableRepository;
-import kitchenpos.menu.domain.Menu;
 import kitchenpos.order.domain.Order;
 import kitchenpos.order.domain.OrderLineItem;
 import kitchenpos.order.domain.OrderStatus;
 import kitchenpos.order.domain.OrderTable;
-import kitchenpos.order.ui.request.OrderLineItemRequest;
 import kitchenpos.order.ui.request.OrderRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class OrderService {
-    private final MenuRepository menuRepository;
+
     private final OrderRepository orderRepository;
     private final OrderTableRepository orderTableRepository;
+    private final OrderLineItemCreator orderLineItemCreator;
 
     public OrderService(
-            final MenuRepository menuRepository,
             final OrderRepository orderRepository,
-            final OrderTableRepository orderTableRepository
+            final OrderTableRepository orderTableRepository,
+            final OrderLineItemCreator orderLineItemCreator
     ) {
-        this.menuRepository = menuRepository;
         this.orderRepository = orderRepository;
         this.orderTableRepository = orderTableRepository;
+        this.orderLineItemCreator = orderLineItemCreator;
     }
 
     @Transactional
     public Order create(final OrderRequest request) {
-        final List<OrderLineItem> orderLineItems = createOrderLineItems(request);
+        final List<OrderLineItem> orderLineItems = orderLineItemCreator.create(request);
         final OrderTable orderTable = orderTableRepository.findById(request.getOrderTableId())
                 .orElseThrow(IllegalArgumentException::new);
         if (orderTable.isEmpty()) {
             throw new IllegalArgumentException();
         }
         return orderRepository.save(new Order(orderTable.getId(), LocalDateTime.now(), orderLineItems));
-    }
-
-    private List<OrderLineItem> createOrderLineItems(final OrderRequest request) {
-        final List<Long> menuIds = request.getOrderLineItems().stream()
-                .map(OrderLineItemRequest::getMenuId)
-                .collect(Collectors.toList());
-
-        Map<Long, Menu> menus = menuRepository.findAllByIdIn(menuIds).stream().collect(
-                Collectors.toMap(Menu::getId, menu -> menu)
-        );
-
-        if (menus.size() != menuIds.size()) {
-            throw new IllegalArgumentException();
-        }
-
-        return request.getOrderLineItems().stream()
-                .map(i -> {
-                    Menu menu = menus.get(i.getMenuId());
-                    return new OrderLineItem(menu.getId(), menu.getName(),menu.getPrice(), i.getQuantity());
-                })
-                .collect(Collectors.toList());
     }
 
     public List<Order> list() {
