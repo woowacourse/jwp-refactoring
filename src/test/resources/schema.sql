dd72d0f16949a99bc85142f0aab99fd8ef6194ec
jojogreen91 <iawbg13@gmail.com>
2022-11-04 23:41:32 +0900

    refactor: 테이블 스키마 변경

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -92,3 +92,7 @@ ALTER TABLE menu_product
 ALTER TABLE order_table
     ADD CONSTRAINT fk_order_table_table_group
         FOREIGN KEY (table_group_id) REFERENCES table_group (id);
+
+ALTER TABLE order_line_item DROP COLUMN menu_id;
+ALTER TABLE order_line_item ADD COLUMN name VARCHAR(255) not null;
+ALTER TABLE order_line_item ADD COLUMN price DECIMAL(19, 2) not null;
