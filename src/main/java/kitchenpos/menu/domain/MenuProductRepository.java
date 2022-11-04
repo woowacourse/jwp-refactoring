68e23b8d17f81cb1c28183b137417619a07ed5b7
Hongdonggeon <ghd700@naver.com>
2022-11-04 13:50:09 +0900

    refactor: 불필요한 메서드 제거

--- src/main/java/kitchenpos/menu/domain/MenuProductRepository.java
+++ src/main/java/kitchenpos/menu/domain/MenuProductRepository.java
@@ -3,6 +3,4 @@ package kitchenpos.menu.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MenuProductRepository extends JpaRepository<MenuProduct, Long> {
-
-//    List<MenuProduct> findAllByMenuId(Long menuId);
 }
--- src/main/java/kitchenpos/order/domain/OrderLineItemRepository.java
+++ src/main/java/kitchenpos/order/domain/OrderLineItemRepository.java
@@ -3,5 +3,4 @@ package kitchenpos.order.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderLineItemRepository extends JpaRepository<OrderLineItem, Long> {
-//    List<OrderLineItem> findByOrderId(Long id);
 }
