0fdc810ce6596c47db8a677e48a73ffea00650e4
jayjaehunchoi <wogns0108@naver.com>
2022-11-05 10:15:14 +0900

    refactor: 멀티 모듈 적용

---
+++ kitchenpos-core/src/test/java/kitchenpos/CoreTestApplication.java
@@ -0,0 +1,12 @@
+package kitchenpos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootApplication
+class CoreTestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CoreTestApplication.class, args);
+    }
+}
