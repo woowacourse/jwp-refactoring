613b9cf10a50eb2fb3e9c69b6ca485db1cd4a3d8
Jeong Jinwoo <jwjeong96@gmail.com>
2022-11-07 15:24:09 +0900

    fix: add TestApplications for fixing broken tests

---
+++ application/src/test/java/kitchenpos/TestApplication.java
@@ -0,0 +1,12 @@
+package kitchenpos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+    }
+}
