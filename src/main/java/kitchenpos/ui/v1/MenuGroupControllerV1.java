f5211cfa3f54db89a7ad89e84a4f56eaff2ad9ed
seokjin8678 <seokjin8678@gmail.com>
2023-10-11 01:53:50 +0900

    refactor: MenuGroupController 리팩터링, 테스트 코드 추가

---
+++ src/main/java/kitchenpos/ui/v1/MenuGroupControllerV1.java
@@ -0,0 +1,37 @@
+package kitchenpos.ui.v1;
+
+import java.net.URI;
+import java.util.List;
+import kitchenpos.application.MenuGroupService;
+import kitchenpos.domain.MenuGroup;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/menu-groups")
+public class MenuGroupControllerV1 {
+
+    private final MenuGroupService menuGroupService;
+
+    public MenuGroupControllerV1(MenuGroupService menuGroupService) {
+        this.menuGroupService = menuGroupService;
+    }
+
+    @PostMapping
+    public ResponseEntity<MenuGroup> create(@RequestBody MenuGroup menuGroup) {
+        MenuGroup created = menuGroupService.create(menuGroup);
+        URI uri = URI.create("/api/v1/menu-groups/" + created.getId());
+        return ResponseEntity.created(uri)
+            .body(created);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<MenuGroup>> findAll() {
+        return ResponseEntity.ok()
+            .body(menuGroupService.list());
+    }
+}
