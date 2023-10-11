032841b0fd8a92ba164678067bc7e7599b99df04
Rosie <dev.kayoung@gmail.com>
2023-10-11 20:17:15 +0900

    test: 메뉴그룹 리스트 조회

--- src/test/java/kitchenpos/application/MenuGroupServiceTest.java
+++ src/test/java/kitchenpos/application/MenuGroupServiceTest.java
@@ -1,5 +1,8 @@
 package kitchenpos.application;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import kitchenpos.dao.MenuGroupDao;
 import kitchenpos.domain.MenuGroup;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +18,9 @@ class MenuGroupServiceTest {
     @Autowired
     private MenuGroupService menuGroupService;
 
+    @Autowired
+    private MenuGroupDao menuGroupDao;
+
     @Test
     void 메뉴_그룹을_생성할_수_있다() {
         //given
@@ -29,5 +35,20 @@ class MenuGroupServiceTest {
                 () -> assertThat(생성된_메뉴그룹.getId()).isNotNull(),
                 () -> assertThat(생성된_메뉴그룹.getName()).isEqualTo(메뉴_그룹.getName())
         );
+    }
+
+    @Test
+    void 메뉴_그룹_리스트를_조회할_수있다() {
+        //given
+        List<Long> 모든_그룹_아이디 = menuGroupDao.findAll().stream()
+                .map(MenuGroup::getId)
+                .collect(Collectors.toList());
+
+        //when
+        List<MenuGroup> 메뉴_그룹_리스트 = menuGroupService.list();
+
+        //then
+        assertThat(메뉴_그룹_리스트).extracting(MenuGroup::getId)
+                .containsAll(모든_그룹_아이디);
     }
 }
