bad19416fac0d267ce2d3daed9fdeb11fcf2284e
Combi153 <jcm04@naver.com>
2023-10-11 22:16:27 +0900

    [test] OrderLineItemDao 테스트 코드 추가

---
+++ src/test/java/kitchenpos/common/fixture/OrderLineItemFixture.java
@@ -0,0 +1,14 @@
+package kitchenpos.common.fixture;
+
+import kitchenpos.domain.OrderLineItem;
+
+public class OrderLineItemFixture {
+
+    public static OrderLineItem 주문_항목(Long menuId, Long orderId) {
+        return new OrderLineItem(menuId, orderId, 1L);
+    }
+
+    public static OrderLineItem 주문_항목(Long orderLineItemId, Long menuId, Long orderId) {
+        return new OrderLineItem(orderLineItemId, menuId, orderId, 1L);
+    }
+}
---
+++ src/test/java/kitchenpos/dao/JdbcTemplateOrderLineItemDaoTest.java
@@ -0,0 +1,112 @@
+package kitchenpos.dao;
+
+import static kitchenpos.common.fixture.MenuFixture.메뉴;
+import static kitchenpos.common.fixture.MenuGroupFixture.메뉴_그룹;
+import static kitchenpos.common.fixture.OrderFixture.주문;
+import static kitchenpos.common.fixture.OrderLineItemFixture.주문_항목;
+import static kitchenpos.common.fixture.OrderTableFixture.주문_테이블;
+import static kitchenpos.common.fixture.TableGroupFixture.단체_지정;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
+
+import java.util.List;
+import javax.sql.DataSource;
+import kitchenpos.domain.OrderLineItem;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.test.context.jdbc.Sql;
+
+@SuppressWarnings("NonAsciiCharacters")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@Sql(value = "classpath:test_truncate_table.sql", executionPhase = BEFORE_TEST_METHOD)
+@JdbcTest
+class JdbcTemplateOrderLineItemDaoTest {
+
+    @Autowired
+    private DataSource dataSource;
+
+    private JdbcTemplateOrderLineItemDao jdbcTemplateOrderLineItemDao;
+    private Long menuId;
+    private Long orderId;
+
+    @BeforeEach
+    void setUp() {
+        jdbcTemplateOrderLineItemDao = new JdbcTemplateOrderLineItemDao(dataSource);
+
+        JdbcTemplateMenuGroupDao jdbcTemplateMenuGroupDao = new JdbcTemplateMenuGroupDao(dataSource);
+        Long menuGroupId = jdbcTemplateMenuGroupDao.save(메뉴_그룹()).getId();
+
+        JdbcTemplateMenuDao jdbcTemplateMenuDao = new JdbcTemplateMenuDao(dataSource);
+        menuId = jdbcTemplateMenuDao.save(메뉴(menuGroupId)).getId();
+
+        JdbcTemplateTableGroupDao jdbcTemplateTableGroupDao = new JdbcTemplateTableGroupDao(dataSource);
+        Long tableGroupId = jdbcTemplateTableGroupDao.save(단체_지정()).getId();
+
+        JdbcTemplateOrderTableDao jdbcTemplateOrderTableDao = new JdbcTemplateOrderTableDao(dataSource);
+        Long orderTableId = jdbcTemplateOrderTableDao.save(주문_테이블(tableGroupId)).getId();
+
+        JdbcTemplateOrderDao jdbcTemplateOrderDao = new JdbcTemplateOrderDao(dataSource);
+        orderId = jdbcTemplateOrderDao.save(주문(orderTableId)).getId();
+    }
+
+    @Test
+    void 주문_항목을_저장한다() {
+        // given
+        OrderLineItem orderLineItem = 주문_항목(menuId, orderId);
+
+        // when
+        OrderLineItem savedOrderLineItem = jdbcTemplateOrderLineItemDao.save(orderLineItem);
+
+        // then
+        assertThat(savedOrderLineItem).usingRecursiveComparison()
+                .ignoringFields("seq")
+                .isEqualTo(주문_항목(menuId, orderId));
+    }
+
+    @Test
+    void ID로_주문_항목을_조회한다() {
+        // given
+        Long orderLineItemId = jdbcTemplateOrderLineItemDao.save(주문_항목(menuId, orderId)).getSeq();
+
+        // when
+        OrderLineItem orderLineItem = jdbcTemplateOrderLineItemDao.findById(orderLineItemId).get();
+
+        // then
+        assertThat(orderLineItem).usingRecursiveComparison()
+                .isEqualTo(주문_항목(orderLineItemId, menuId, orderId));
+    }
+
+    @Test
+    void 전체_주문_항목을_조회한다() {
+        // given
+        Long orderLineItemId_A = jdbcTemplateOrderLineItemDao.save(주문_항목(menuId, orderId)).getSeq();
+        Long orderLineItemId_B = jdbcTemplateOrderLineItemDao.save(주문_항목(menuId, orderId)).getSeq();
+
+        // when
+        List<OrderLineItem> orderLineItems = jdbcTemplateOrderLineItemDao.findAll();
+
+        // then
+        assertThat(orderLineItems).usingRecursiveComparison()
+                .isEqualTo(List.of(
+                        주문_항목(orderLineItemId_A, menuId, orderId),
+                        주문_항목(orderLineItemId_B, menuId, orderId))
+                );
+    }
+
+    @Test
+    void 주문_ID로_주문_항목을_조회한다() {
+        // given
+        Long orderLineItemId = jdbcTemplateOrderLineItemDao.save(주문_항목(menuId, orderId)).getSeq();
+
+        // when
+        List<OrderLineItem> orderLineItems = jdbcTemplateOrderLineItemDao.findAllByOrderId(orderId);
+
+        // then
+        assertThat(orderLineItems).usingRecursiveComparison()
+                .isEqualTo(List.of(주문_항목(orderLineItemId, menuId, orderId)));
+    }
+}
