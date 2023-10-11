11111d5f561bdc700a0600e461e718c0eec2852a
drunkenhw <drunkenhw@gmail.com>
2023-10-11 12:34:15 +0900

    test: menu group service test 작성

---
+++ src/test/java/kitchenpos/application/MenuGroupServiceTest.java
@@ -0,0 +1,58 @@
+package kitchenpos.application;
+
+import kitchenpos.dao.MenuGroupDao;
+import kitchenpos.domain.MenuGroup;
+import kitchenpos.fake.InMemoryMenuGroupService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@SuppressWarnings("NonAsciiCharacters")
+class MenuGroupServiceTest {
+
+    private MenuGroupService menuGroupService;
+    private MenuGroupDao menuGroupDao;
+
+    @BeforeEach
+    void before() {
+        menuGroupDao = new InMemoryMenuGroupService();
+        menuGroupService = new MenuGroupService(menuGroupDao);
+    }
+
+    @Test
+    void 주문_그룹을_생성한다() {
+        // given
+        MenuGroup menuGroup = new MenuGroup("korean");
+
+        // when
+        MenuGroup savedMenuGroup = menuGroupService.create(menuGroup);
+
+        // then
+        assertSoftly(softly -> {
+            softly.assertThat(savedMenuGroup.getName()).isEqualTo(menuGroup.getName());
+            softly.assertThat(savedMenuGroup.getId()).isNotNull();
+        });
+    }
+
+    @Test
+    void 주문을_전체_조회한다() {
+        // given
+        menuGroupDao.save(new MenuGroup("korean"));
+        menuGroupDao.save(new MenuGroup("french"));
+
+        // when
+        List<MenuGroup> savedMenuGroups = menuGroupService.list();
+
+        // then
+        assertThat(savedMenuGroups).map(MenuGroup::getName)
+                .usingRecursiveComparison()
+                .isEqualTo(List.of("korean", "french"));
+    }
+}
