aa9d95e254a8d288306ebd60d2cdc2ee04f009a7
iamjooon2 <iamjooon2@gmail.com>
2023-10-12 01:06:17 +0900

    test: 테이블 단체 지정 해제 테스트 추가

--- src/test/java/kitchenpos/application/TableGroupServiceTest.java
+++ src/test/java/kitchenpos/application/TableGroupServiceTest.java
@@ -3,8 +3,11 @@ package kitchenpos.application;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import kitchenpos.dao.OrderDao;
 import kitchenpos.dao.OrderTableDao;
 import kitchenpos.dao.TableGroupDao;
+import kitchenpos.domain.Order;
+import kitchenpos.domain.OrderStatus;
 import kitchenpos.domain.OrderTable;
 import kitchenpos.domain.TableGroup;
 import org.junit.jupiter.api.DisplayNameGeneration;
@@ -16,10 +19,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import static kitchenpos.fixture.FixtureFactory.단체_지정_생성;
+import static kitchenpos.fixture.FixtureFactory.주문_생성;
 import static kitchenpos.fixture.FixtureFactory.주문_테이블_생성;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @Transactional
 @SpringBootTest
@@ -28,7 +33,13 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 class TableGroupServiceTest {
 
     @Autowired
+    private TableGroupDao tableGroupDao;
+
+    @Autowired
     private OrderTableDao orderTableDao;
+
+    @Autowired
+    private OrderDao orderDao;
 
     @Autowired
     private TableGroupService tableGroupService;
@@ -116,14 +127,11 @@ class TableGroupServiceTest {
         @Test
         void 주문_테이블이_빈_상태가_아니라면_예외가_발생한다() {
             // given
-            final OrderTable firstOrderTable = 주문_테이블_생성(null, 3, false);
-            final OrderTable secondOrderTable = 주문_테이블_생성(null, 3, false);
-
-            final OrderTable firstSavedOrderTable = orderTableDao.save(firstOrderTable);
-            final OrderTable secondSavedOrderTable = orderTableDao.save(secondOrderTable);
+            final OrderTable firstOrderTable = orderTableDao.save(주문_테이블_생성(null, 3, false));
+            final OrderTable secondOrderTable = orderTableDao.save(주문_테이블_생성(null, 3, false));
 
             final TableGroup expected = 단체_지정_생성(LocalDateTime.now(), List.of(
-                    firstSavedOrderTable, secondSavedOrderTable
+                    firstOrderTable, secondOrderTable
             ));
 
             // expect
@@ -131,6 +139,38 @@ class TableGroupServiceTest {
                     .isInstanceOf(IllegalArgumentException.class);
         }
 
+    }
+
+    @Test
+    void 단체_지정을_해제할_수_있다() {
+        // given
+        final OrderTable firstOrderTable = orderTableDao.save(주문_테이블_생성(null, 3, false));
+        final OrderTable secondOrderTable = orderTableDao.save(주문_테이블_생성(null, 3, false));
+
+        final TableGroup tableGroup = 단체_지정_생성(LocalDateTime.now(), List.of(
+                firstOrderTable, secondOrderTable
+        ));
+        final TableGroup expected = tableGroupDao.save(tableGroup);
+
+        // expect
+        assertDoesNotThrow(() -> tableGroupService.ungroup(expected.getId()));
+    }
+
+    @Test
+    void 단체_지정_해제시_식사_중인_주문이_있을_경우_예외가_발생한다() {
+        // given
+        final OrderTable firstOrderTable = orderTableDao.save(주문_테이블_생성(null, 1, true));
+        final OrderTable secondOrderTable = orderTableDao.save(주문_테이블_생성(null, 2, true));
+        final Order order = 주문_생성(firstOrderTable.getId(), OrderStatus.COOKING.name(), LocalDateTime.now(), null);
+        orderDao.save(order);
+
+        final TableGroup expected = tableGroupService.create(단체_지정_생성(LocalDateTime.now(), List.of(
+                firstOrderTable, secondOrderTable
+        )));
+
+        // expect
+        assertThatThrownBy(() -> tableGroupService.ungroup(expected.getId()))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
 }
