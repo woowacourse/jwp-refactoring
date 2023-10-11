49e73a9e4f5737c836145c37766fd726517c2d40
greeng00se <thegreengoose7@gmail.com>
2023-10-12 00:51:27 +0900

    test: 주문 테스트 추가

--- src/test/java/kitchenpos/fixture/OrderFixture.java
+++ src/test/java/kitchenpos/fixture/OrderFixture.java
@@ -1,7 +1,9 @@
 package kitchenpos.fixture;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import kitchenpos.domain.Order;
+import kitchenpos.domain.OrderLineItem;
 import kitchenpos.domain.OrderStatus;
 
 public class OrderFixture {
@@ -11,6 +13,14 @@ public class OrderFixture {
         order.setOrderTableId(orderTableId);
         order.setOrderedTime(LocalDateTime.now());
         order.setOrderStatus(orderStatus.name());
+        return order;
+    }
+
+    public static Order 주문(Long orderTableId, List<OrderLineItem> orderLineItems) {
+        Order order = new Order();
+        order.setOrderTableId(orderTableId);
+        order.setOrderedTime(LocalDateTime.now());
+        order.setOrderLineItems(orderLineItems);
         return order;
     }
 }
