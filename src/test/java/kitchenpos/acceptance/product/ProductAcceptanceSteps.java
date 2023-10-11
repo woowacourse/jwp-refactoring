ae18c2788c73ef2dc8509c13201ab0c88bdae341
shin-mallang <huipulci1@naver.com>
2023-10-12 05:59:56 +0900

    상품 인수테스트 작성

---
+++ src/test/java/kitchenpos/acceptance/product/ProductAcceptanceSteps.java
@@ -0,0 +1,55 @@
+package kitchenpos.acceptance.product;
+
+import static kitchenpos.acceptance.AcceptanceSteps.given;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.restassured.common.mapper.TypeRef;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import java.math.BigDecimal;
+import java.util.List;
+import kitchenpos.domain.Product;
+import org.assertj.core.data.Percentage;
+
+@SuppressWarnings("NonAsciiCharacters")
+public class ProductAcceptanceSteps {
+
+    public static ExtractableResponse<Response> 상품_등록_요청을_보낸다(String 상품_이름, double 가격) {
+        Product product = new Product();
+        product.setName(상품_이름);
+        product.setPrice(new BigDecimal(가격));
+        return given()
+                .body(product)
+                .post("/api/products")
+                .then().log().all()
+                .extract();
+    }
+
+    public static List<Product> 상품_목록_조회_요청을_보낸다() {
+        ExtractableResponse<Response> extract = given()
+                .get("/api/products")
+                .then().log().all()
+                .extract();
+        return extract.as(new TypeRef<>() {
+        });
+    }
+
+    public static Product 상품_정보(String 이름, double 가격) {
+        Product product = new Product();
+        product.setName(이름);
+        product.setPrice(new BigDecimal(가격));
+        return product;
+    }
+
+    public static void 상품_목록_조회_결과를_검증한다(List<Product> 응답, List<Product> 예상_결과) {
+        assertThat(응답)
+                .usingRecursiveComparison()
+                .ignoringExpectedNullFields()
+                .ignoringFields("price")
+                .isEqualTo(예상_결과);
+        for (int i = 0; i < 응답.size(); i++) {
+            assertThat(응답.get(i).getPrice())
+                    .isCloseTo(예상_결과.get(i).getPrice(), Percentage.withPercentage(100));
+        }
+    }
+}
---
+++ src/test/java/kitchenpos/acceptance/product/ProductAcceptanceTest.java
@@ -0,0 +1,59 @@
+package kitchenpos.acceptance.product;
+
+import static kitchenpos.acceptance.AcceptanceSteps.응답_상태를_검증한다;
+import static kitchenpos.acceptance.product.ProductAcceptanceSteps.상품_목록_조회_결과를_검증한다;
+import static kitchenpos.acceptance.product.ProductAcceptanceSteps.상품_목록_조회_요청을_보낸다;
+import static kitchenpos.acceptance.product.ProductAcceptanceSteps.상품_정보;
+import static kitchenpos.acceptance.product.ProductAcceptanceSteps.상품_등록_요청을_보낸다;
+
+import java.util.List;
+import kitchenpos.acceptance.AcceptanceTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+@SuppressWarnings("NonAsciiCharacters")
+@DisplayName("상품 인수 테스트")
+public class ProductAcceptanceTest {
+
+    @Nested
+    class 상품_등록_API extends AcceptanceTest {
+
+        @Test
+        void 상품을_등록할_수_있다() {
+            // when
+            var 응답 = 상품_등록_요청을_보낸다("강정치킨", 19000);
+
+            // then
+            응답_상태를_검증한다(응답, 201);
+        }
+
+        @Test
+        void 상품_등록_시_가격이_0보다_작으면_오류() {
+            // when
+            var 응답 = 상품_등록_요청을_보낸다("강정치킨", -1);
+
+            // then
+            응답_상태를_검증한다(응답, 500);
+        }
+    }
+
+    @Nested
+    class 상품_조회_API extends AcceptanceTest {
+
+        @Test
+        void 상품들을_조회할_수_있다() {
+            // given
+            상품_등록_요청을_보낸다("말랑치킨", 100_000);
+            상품_등록_요청을_보낸다("강정치킨", 20_000);
+
+            // when
+            var 응답 = 상품_목록_조회_요청을_보낸다();
+
+            // then
+            상품_목록_조회_결과를_검증한다(응답,
+                    List.of(상품_정보("말랑치킨", 100_000.00), 상품_정보("강정치킨", 20_000.00))
+            );
+        }
+    }
+}
