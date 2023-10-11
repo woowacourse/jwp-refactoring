2f5dbd27b4831dafd641419c417b2dbe03ebed99
Rosie <dev.kayoung@gmail.com>
2023-10-11 20:06:14 +0900

    test: 상품 리스트 조회

--- src/test/java/kitchenpos/application/ProductServiceTest.java
+++ src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -1,6 +1,9 @@
 package kitchenpos.application;
 
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.stream.Collectors;
+import kitchenpos.dao.ProductDao;
 import kitchenpos.domain.Product;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Nested;
@@ -10,6 +13,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static java.util.stream.Collectors.toList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
@@ -19,6 +23,9 @@ class ProductServiceTest {
 
     @Autowired
     private ProductService productService;
+
+    @Autowired
+    private ProductDao productDao;
 
     @Nested
     class 상품_생성 {
@@ -65,4 +72,20 @@ class ProductServiceTest {
                     .isInstanceOf(IllegalArgumentException.class);
         }
     }
+
+    @Test
+    void 상품_리스트를_조회할_수_있다() {
+        //given
+        List<Long> allProductIds = productDao.findAll().stream()
+                .map(Product::getId)
+                .collect(toList());
+
+        //when
+        List<Product> products = productService.list();
+
+        //then
+        assertThat(products).extracting(Product::getId)
+                .containsAll(allProductIds);
+    }
+
 }
