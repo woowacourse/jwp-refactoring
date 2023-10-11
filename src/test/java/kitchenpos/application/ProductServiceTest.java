0033b06b2229c73b983847861a2ee5fd6b8addbb
bjk1649 <qhwns1649@naver.com>
2023-10-11 17:04:10 +0900

    test: 상품 등록 서비스 테스트 추가

---
+++ src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -0,0 +1,108 @@
+package kitchenpos.application;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.BDDMockito.any;
+import static org.mockito.BDDMockito.mock;
+import static org.mockito.BDDMockito.when;
+import static org.mockito.Mockito.only;
+import static org.mockito.Mockito.verify;
+
+import java.math.BigDecimal;
+import kitchenpos.dao.ProductDao;
+import kitchenpos.domain.Product;
+import org.assertj.core.api.ThrowableAssert.ThrowingCallable;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.ThrowingSupplier;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class ProductServiceTest {
+
+    @Nested
+    class 상품_등록 {
+
+        @ParameterizedTest
+        @NullAndEmptySource
+        void 이름이_공백이나_비어있다면_예외가_발생한다(String invalidName) {
+            // given
+            final var productDao = mock(ProductDao.class);
+            final var productService = new ProductService(productDao);
+            final var productWithInvalidName = createProduct(invalidName, BigDecimal.valueOf(1000));
+            when(productDao.save(any(Product.class))).thenReturn(productWithInvalidName);
+
+            // when
+            final ThrowingCallable throwingCallable = () -> productService.create(productWithInvalidName);
+
+            // then
+            assertThatThrownBy(throwingCallable)
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        void 이름이_255자_이내가_아니라면_예외가_발생한다() {
+            // given
+            final var productDao = mock(ProductDao.class);
+            final var productService = new ProductService(productDao);
+            final var invalidName = "름".repeat(256);
+            final var productWithInvalidName = createProduct(invalidName, BigDecimal.valueOf(1000));
+            when(productDao.save(any(Product.class))).thenReturn(productWithInvalidName);
+
+            // when
+            final ThrowingCallable throwingCallable = () -> productService.create(productWithInvalidName);
+
+            // then
+            assertThatThrownBy(throwingCallable)
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @ParameterizedTest
+        @NullSource
+        @ValueSource(strings = {"-1", "0", "12345123451234512345"})
+        void 가격이_null이거나_1에서_19자리의_양수가_아니라면_예외가_발생한다(BigDecimal price) {
+            // given
+            final var productDao = mock(ProductDao.class);
+            final var productService = new ProductService(productDao);
+            final var productWithInvalidPrice = createProduct("validName", price);
+            when(productDao.save(any(Product.class))).thenReturn(productWithInvalidPrice);
+
+            // when
+            final ThrowingCallable throwingCallable = () -> productService.create(productWithInvalidPrice);
+
+            // then
+            assertThatThrownBy(throwingCallable)
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        void 정상적인_이름과_가격을_가진다면_상품이_등록된다() {
+            // given
+            final var productDao = mock(ProductDao.class);
+            final var productService = new ProductService(productDao);
+            final var validName = "validName";
+            final var validPrice = BigDecimal.valueOf(1000);
+            final var productWithValidNameAndPrice = createProduct(validName, validPrice);
+            when(productDao.save(any(Product.class))).thenReturn(productWithValidNameAndPrice);
+
+            // when
+            final ThrowingSupplier<Product> throwingSupplier = () -> productService.create(productWithValidNameAndPrice);
+
+            // then
+            assertAll(
+                    () -> Assertions.assertDoesNotThrow(throwingSupplier),
+                    () -> verify(productDao, only()).save(any(Product.class))
+            );
+        }
+
+        private Product createProduct(final String name, final BigDecimal price) {
+            final var productWithInvalidName = new Product();
+            productWithInvalidName.setName(name);
+            productWithInvalidName.setPrice(price);
+            return productWithInvalidName;
+        }
+    }
+}
