6a365f820e2bb82500822ea3c2a765db96d188bc
최준호 <junho5336@gmail.com>
2023-10-11 21:52:16 +0900

    test: 주문 컨트롤러 테스트 작성

---
+++ src/test/java/kitchenpos/fixture/ProductFixture.java
@@ -0,0 +1,21 @@
+package kitchenpos.fixture;
+
+import java.math.BigDecimal;
+import kitchenpos.domain.Product;
+
+@SuppressWarnings("NonAsciiCharacters")
+public class ProductFixture {
+
+        private ProductFixture() {
+        }
+
+        public static class PRODUCT {
+            public static Product 후라이드_치킨() {
+                Product product = new Product();
+                product.setId(1L);
+                product.setName("후라이드치킨");
+                product.setPrice(BigDecimal.valueOf(16000L));
+                return product;
+            }
+        }
+}
