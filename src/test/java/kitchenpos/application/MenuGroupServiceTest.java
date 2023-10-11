9fc88f46baef3f01ef4b770e551d04fcf4b110ca
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2023-10-11 14:13:25 +0900

    test: 메뉴 그룹 조회 테스트 코드를 작성한다.

--- src/test/java/kitchenpos/application/MenuGroupServiceTest.java
+++ src/test/java/kitchenpos/application/MenuGroupServiceTest.java
@@ -1,31 +1,64 @@
 package kitchenpos.application;
 
 import kitchenpos.application.test.IntegrateServiceTest;
+import kitchenpos.dao.MenuGroupDao;
 import kitchenpos.domain.MenuGroup;
 import kitchenpos.domain.MenuGroupFixture;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.List;
+
 import static kitchenpos.domain.MenuGroupFixture.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class MenuGroupServiceTest extends IntegrateServiceTest {
 
     @Autowired
+    private MenuGroupDao menuGroupDao;
+
+    @Autowired
     private MenuGroupService menuGroupService;
 
     @Nested
-    class 메뉴_생성 {
+    class 메뉴_그룹_생성 {
         @Test
-        void 메뉴를_생성한다() {
+        void 모든_메뉴_그룹을_생성한다() {
             // given
             MenuGroup 인기_메뉴 = 인기_메뉴_생성();
 
             // when, then
             assertDoesNotThrow(() -> menuGroupService.create(인기_메뉴));
         }
+    }
+
+    @Nested
+    class 메뉴_그룹_조회 {
+
+        @BeforeEach
+        void setUp() {
+            MenuGroup 인기_메뉴 = 인기_메뉴_생성();
+            menuGroupDao.save(인기_메뉴);
+        }
+
+        @Test
+        void 모든_메뉴_그룹을_조회한다() {
+            // when
+            List<MenuGroup> menuGroups = menuGroupService.list();
+
+            // then
+            assertAll(
+                    () -> assertThat(menuGroups).hasSize(1),
+                    () -> assertThat(menuGroups).extracting(MenuGroup::getName)
+                            .contains("인기 메뉴")
+            );
+        }
+
     }
 
 }
