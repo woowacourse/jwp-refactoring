fbc5fe3bf3802a0c1af84e1b4802dc15fb93d5f4
Kong-hana01 <gksqlsl11@khu.ac.kr>
2023-10-11 22:06:22 +0900

    test: 메뉴 그룹 관련 서비스 테스트 코드 작성

---
+++ src/test/java/kitchenpos/application/MenuGroupServiceTest.java
@@ -0,0 +1,47 @@
+package kitchenpos.application;
+
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+import kitchenpos.dao.MenuGroupDao;
+import kitchenpos.domain.MenuGroup;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class MenuGroupServiceTest {
+
+    private MenuGroupService menuGroupService;
+    @Mock
+    private MenuGroupDao menuGroupDao;
+
+    @BeforeEach
+    void init() {
+        menuGroupService = new MenuGroupService(menuGroupDao);
+    }
+
+    @Test
+    @DisplayName("메뉴 그룹을 생성할 수 있다.")
+    void create_success() {
+        MenuGroup menuGroup = new MenuGroup();
+        MenuGroup savedMenuGroup = new MenuGroup();
+        when(menuGroupDao.save(menuGroup)).thenReturn(savedMenuGroup);
+
+        Assertions.assertThat(menuGroupService.create(menuGroup)).isEqualTo(savedMenuGroup);
+    }
+
+    @Test
+    @DisplayName("현재 저장된 메뉴 그룹 목록을 확인할 수 있다.")
+    void list_success() {
+        MenuGroup menuGroup1 = new MenuGroup();
+        MenuGroup menuGroup2 = new MenuGroup();
+        when(menuGroupDao.findAll()).thenReturn(List.of(menuGroup1, menuGroup2));
+
+        Assertions.assertThat(menuGroupService.list()).containsExactlyInAnyOrder(menuGroup1, menuGroup2);
+    }
+}
