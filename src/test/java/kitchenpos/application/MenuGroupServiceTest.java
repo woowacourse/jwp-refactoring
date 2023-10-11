c58761c32e7c0d3e2d530b86b9aea848cdce3608
LJW25 <dbjwlee@gmail.com>
2023-10-11 23:51:49 +0900

    test: MenuGroupService 테스트 코드 작성

---
+++ src/test/java/kitchenpos/application/MenuGroupServiceTest.java
@@ -0,0 +1,47 @@
+package kitchenpos.application;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import kitchenpos.dao.MenuGroupDao;
+import kitchenpos.domain.MenuGroup;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+
+@SpringBootTest
+@Sql(value = "classpath:data/truncate.sql")
+class MenuGroupServiceTest {
+
+    @Autowired
+    private MenuGroupDao menuGroupDao;
+
+    @Autowired
+    private MenuGroupService menuGroupService;
+
+    @Test
+    void create() {
+        // given
+        final MenuGroup menuGroup = new MenuGroup("추천메뉴");
+
+        // when
+        final MenuGroup result = menuGroupService.create(menuGroup);
+
+        // then
+        assertThat(result.getId()).isNotNull();
+    }
+
+    @Test
+    void list() {
+        // given
+        menuGroupService.create(new MenuGroup("Group1"));
+        menuGroupService.create(new MenuGroup("Group2"));
+
+        // when
+        final List<MenuGroup> result = menuGroupService.list();
+
+        // then
+        assertThat(result).hasSize(2);
+    }
+}
