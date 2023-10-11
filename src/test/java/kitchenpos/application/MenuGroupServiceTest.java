d4bd33b10b9b6e1d3afeb8a8b56e4d0ba3f75f95
최준호 <junho5336@gmail.com>
2023-10-12 01:27:00 +0900

    test: 메뉴 그룹 테스트 작성

---
+++ src/test/java/kitchenpos/application/MenuGroupServiceTest.java
@@ -0,0 +1,55 @@
+package kitchenpos.application;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+import java.util.List;
+import kitchenpos.dao.MenuGroupDao;
+import kitchenpos.domain.MenuGroup;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@SuppressWarnings("NonAsciiCharacters")
+@ExtendWith(MockitoExtension.class)
+class MenuGroupServiceTest {
+
+    @Mock
+    private MenuGroupDao menuGroupDao;
+
+    @InjectMocks
+    private MenuGroupService menuGroupService;
+
+    @Test
+    void 메뉴_그룹을_생성한다() {
+        // given
+        MenuGroup menuGroup = new MenuGroup();
+        given(menuGroupDao.save(any()))
+                .willReturn(menuGroup);
+
+        // when & then
+        MenuGroup result = menuGroupService.create(menuGroup);
+        Assertions.assertThat(result)
+                .usingRecursiveComparison()
+                .isEqualTo(menuGroup);
+    }
+
+    @Test
+    void 메뉴_그룹_목록을_조회한다() {
+        // given
+        MenuGroup menuGroup = new MenuGroup();
+        given(menuGroupDao.findAll())
+                .willReturn(List.of(menuGroup));
+
+        // when & then
+        Assertions.assertThat(menuGroupService.list())
+                .usingRecursiveComparison()
+                .isEqualTo(List.of(menuGroup));
+    }
+}
