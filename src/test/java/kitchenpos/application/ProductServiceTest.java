40afc165b5cf8d3a09c6d6e0b2844fb12c4f9168
yoondgu <doyoungwork@gmail.com>
2023-10-12 06:56:55 +0900

    test: 상품 통합 테스트 작성
    - 리팩터링 시 의존성이 존재하는 테스트를 분리하는 것이 목 테스트 수정보다 편할 것이라 판단함

---
+++ src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -0,0 +1,73 @@
+package kitchenpos.application;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+import kitchenpos.IntegrationTest;
+import kitchenpos.dao.ProductDao;
+import kitchenpos.domain.Product;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+class ProductServiceTest extends IntegrationTest {
+
+    @Autowired
+    private ProductDao productDao;
+    private ProductService productService;
+
+    @Override
+    @BeforeEach
+    public void setUp() {
+        super.setUp();
+        productService = new ProductService(productDao);
+    }
+
+    @Test
+    @DisplayName("상품 등록 시 전달받은 정보를 새 id로 저장한다.")
+    void 상품_등록_성공_저장() {
+        // given
+        final Product product = new Product();
+        product.setName("상품");
+        product.setPrice(new BigDecimal("1000"));
+
+        // when
+        final Product saved = productService.create(product);
+
+        // then
+        assertThat(productService.list())
+                .map(Product::getId)
+                .filteredOn(id -> Objects.equals(id, saved.getId()))
+                .hasSize(1);
+    }
+
+    @Test
+    @DisplayName("상품 등록 시 금액 정보가 있어야 한다.")
+    void 상품_등록_실패_금액_없음() {
+        // given
+        final Product product = new Product();
+        product.setName("상품");
+
+        // when
+        // then
+        assertThatThrownBy(() -> productService.create(product))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("상품 등록 시 금액은 0 이상의 수여야 한다.")
+    void 상품_등록_실패_금액_음수() {
+        // given
+        final Product product = new Product();
+        product.setName("상품");
+        product.setPrice(new BigDecimal("-1000"));
+
+        // when
+        // then
+        assertThatThrownBy(() -> productService.create(product))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
