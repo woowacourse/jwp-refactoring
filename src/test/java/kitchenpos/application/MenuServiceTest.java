7de55fe2759a6766ca2a8321db44e879e1a97aa0
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2023-10-11 15:01:34 +0900

    test: 메뉴 목록 조회 테스트 코드를 작성한다.

--- src/test/java/kitchenpos/application/MenuServiceTest.java
+++ src/test/java/kitchenpos/application/MenuServiceTest.java
@@ -20,6 +20,7 @@ import java.util.List;
 import static kitchenpos.domain.fixture.MenuFixture.치킨_피자_세트_메뉴_생성;
 import static kitchenpos.domain.fixture.MenuGroupFixture.*;
 import static kitchenpos.domain.fixture.ProductFixture.*;
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -37,36 +38,43 @@ class MenuServiceTest extends IntegrateServiceTest {
     @Autowired
     private MenuDao menuDao;
 
+    Menu menu;
+    MenuProduct 치킨_메뉴_상품;
+    MenuProduct 피자_메뉴_상품;
+    Product 치킨;
+    Product 피자;
+
+    @BeforeEach
+    void setUp() {
+        MenuGroup 인기_메뉴 = menuGroupDao.save(인기_메뉴_생성());
+        Menu 치킨_피자_세트_메뉴 = 치킨_피자_세트_메뉴_생성(인기_메뉴.getId());
+
+        Product 치킨 =  productDao.save(치킨_생성());
+        Product 피자 =  productDao.save(피자_생성());
+
+        MenuProduct 치킨_메뉴_상품 = getMenuProduct(치킨.getId(), 1L);
+        MenuProduct 피자_메뉴_상품 = getMenuProduct(피자.getId(),1L);
+
+        List<MenuProduct> menuProducts = List.of(치킨_메뉴_상품, 피자_메뉴_상품);
+
+        치킨_피자_세트_메뉴.setMenuProducts(menuProducts);
+
+        this.menu = 치킨_피자_세트_메뉴;
+        this.치킨_메뉴_상품 = 치킨_메뉴_상품;
+        this.피자_메뉴_상품 = 피자_메뉴_상품;
+        this.치킨 = 치킨;
+        this.피자 = 피자;
+    }
+
+    private MenuProduct getMenuProduct(final Long productId, final Long quantity) {
+        MenuProduct menuProduct = new MenuProduct();
+        menuProduct.setProductId(productId);
+        menuProduct.setQuantity(quantity);
+        return menuProduct;
+    }
+
     @Nested
     class 메뉴를_생성한다 {
-
-        Menu menu;
-        MenuProduct 치킨_메뉴_상품;
-        MenuProduct 피자_메뉴_상품;
-        Product 치킨;
-        Product 피자;
-
-        @BeforeEach
-        void setUp() {
-            MenuGroup 인기_메뉴 = menuGroupDao.save(인기_메뉴_생성());
-            Menu 치킨_피자_세트_메뉴 = 치킨_피자_세트_메뉴_생성(인기_메뉴.getId());
-
-            Product 치킨 =  productDao.save(치킨_생성());
-            Product 피자 =  productDao.save(피자_생성());
-
-            MenuProduct 치킨_메뉴_상품 = getMenuProduct(치킨.getId(), 1L);
-            MenuProduct 피자_메뉴_상품 = getMenuProduct(피자.getId(),1L);
-
-            List<MenuProduct> menuProducts = List.of(치킨_메뉴_상품, 피자_메뉴_상품);
-
-            치킨_피자_세트_메뉴.setMenuProducts(menuProducts);
-
-            this.menu = 치킨_피자_세트_메뉴;
-            this.치킨_메뉴_상품 = 치킨_메뉴_상품;
-            this.피자_메뉴_상품 = 피자_메뉴_상품;
-            this.치킨 = 치킨;
-            this.피자 = 피자;
-        }
 
         @Test
         void 메뉴를_생성한다() {
@@ -109,7 +117,7 @@ class MenuServiceTest extends IntegrateServiceTest {
             menu.setMenuProducts(List.of(menuProduct));
 
             // when, then
-            assertThrows(IllegalArgumentException.class, () -> menuService.create(this.menu));
+            assertThrows(IllegalArgumentException.class, () -> menuService.create(menu));
         }
 
         @Test
@@ -118,14 +126,30 @@ class MenuServiceTest extends IntegrateServiceTest {
             menu.setPrice(BigDecimal.valueOf(100000));
 
             // when, then
-            assertThrows(IllegalArgumentException.class, () -> menuService.create(this.menu));
+            assertThrows(IllegalArgumentException.class, () -> menuService.create(menu));
         }
 
-        private MenuProduct getMenuProduct(final Long productId, final Long quantity) {
-            MenuProduct menuProduct = new MenuProduct();
-            menuProduct.setProductId(productId);
-            menuProduct.setQuantity(quantity);
-            return menuProduct;
+    }
+
+    @Nested
+    class 메뉴_목록을_조회한다 {
+
+        @BeforeEach
+        void setUp() {
+            menuDao.save(menu);
+        }
+
+        @Test
+        void 메뉴_목록을_조회한다() {
+            // when
+            List<Menu> menus = menuService.list();
+
+            // then
+            Assertions.assertAll(
+                    () -> assertThat(menus).hasSize(1),
+                    () -> assertThat(menus).extracting(Menu::getName)
+                            .contains("치킨 피자 세트")
+            );
         }
 
     }
