9f071b0cd59c5664bfc89689da501378aca3c0d8
yujamint <woawoa0513@gmail.com>
2023-10-12 21:34:39 +0900

    test: ProductService 테스트 작성

---
+++ src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -0,0 +1,59 @@
+package kitchenpos.application;
+
+import static kitchenpos.fixture.ProductFixtures.CHICKEN_PRICE_18000;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.BDDMockito.given;
+
+import java.math.BigDecimal;
+import kitchenpos.dao.ProductDao;
+import kitchenpos.domain.Product;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class ProductServiceTest {
+
+    @Mock
+    private ProductDao productDao;
+
+    @InjectMocks
+    private ProductService productService;
+
+    @DisplayName("상품을 생성한다")
+    @Test
+    void create() {
+        // given
+        Product product = CHICKEN_PRICE_18000;
+
+        Product expected = new Product();
+        CHICKEN_PRICE_18000.setId(1L);
+
+        given(productDao.save(product)).willReturn(expected);
+
+        // when
+        Product actual = productService.create(product);
+
+        // then
+        assertThat(actual.getId()).isEqualTo(expected.getId());
+    }
+
+    @DisplayName("가격이 0원보다 낮은 상품을 생성하면 예외가 발생한다")
+    @ValueSource(ints = {-1, -100, -35_000, -100_000})
+    @ParameterizedTest
+    void create_PriceLowerThanZero_ExceptionThrown(int invalidPrice) {
+        // given
+        Product product = new Product();
+        product.setPrice(BigDecimal.valueOf(invalidPrice));
+
+        // when, then
+        assertThatThrownBy(() -> productService.create(product))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
