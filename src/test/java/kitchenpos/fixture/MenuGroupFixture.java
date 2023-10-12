3a21e149c24ffec272376dd7b031efeb784a3d33
Go-Jaecheol <gojaech@naver.com>
2023-10-12 15:00:53 +0900

    test: MenuService 테스트 추가

--- src/test/java/kitchenpos/fixture/MenuGroupFixture.java
+++ src/test/java/kitchenpos/fixture/MenuGroupFixture.java
@@ -7,6 +7,7 @@ public class MenuGroupFixture {
 
     public static MenuGroup 메뉴그룹_신메뉴() {
         final var menuGroup = new MenuGroup();
+        menuGroup.setId(1L);
         menuGroup.setName("신메뉴");
         return menuGroup;
     }
--- src/test/java/kitchenpos/fixture/ProductFixture.java
+++ src/test/java/kitchenpos/fixture/ProductFixture.java
@@ -9,6 +9,7 @@ public class ProductFixture {
 
     public static Product 상품_망고_1000원() {
         final var product = new Product();
+        product.setId(1L);
         product.setName("망고");
         product.setPrice(BigDecimal.valueOf(1000));
         return product;
@@ -16,6 +17,7 @@ public class ProductFixture {
 
     public static Product 상품_치킨_15000원() {
         final var product = new Product();
+        product.setId(2L);
         product.setName("치킨");
         product.setPrice(BigDecimal.valueOf(15000));
         return product;
