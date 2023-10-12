1d26d3fa188a11cd5889e5e87db10ac9e8111dd7
amaran-th <songsy405@naver.com>
2023-10-12 12:18:19 +0900

    test: 상품 서비스 테스트 작성

---
+++ src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -0,0 +1,70 @@
+package kitchenpos.application;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.math.BigDecimal;
+import java.util.List;
+import javax.transaction.Transactional;
+import kitchenpos.dao.ProductDao;
+import kitchenpos.domain.Product;
+import org.assertj.core.api.ThrowableAssert.ThrowingCallable;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Transactional
+class ProductServiceTest {
+
+  @Autowired
+  private ProductDao productDao;
+
+  @Autowired
+  private ProductService productService;
+
+  @Test
+  @DisplayName("상품을 등록할 수 있다.")
+  void create_success() {
+    //given
+    final Product product = new Product();
+    product.setName("깐풍 치킨");
+    product.setPrice(BigDecimal.valueOf(1000));
+
+    //when
+    final Product actual = productService.create(product);
+
+    //then
+    assertThat(actual).isNotNull();
+
+  }
+
+  @ParameterizedTest
+  @ValueSource(longs = {-1L, -1000L})
+  @DisplayName("상품을 등록할 때 상품의 가격이 0보다 작을 경우 예외를 반환한다.")
+  void create_fail_negative_price(final Long wrongPrice) {
+    final Product product = new Product();
+    product.setPrice(BigDecimal.valueOf(wrongPrice));
+
+    //when
+    final ThrowingCallable actual = () -> productService.create(product);
+
+    //then
+    assertThatThrownBy(actual).isInstanceOf(IllegalArgumentException.class);
+
+  }
+
+  @Test
+  @DisplayName("등록된 상품 목록을 조회할 수 있다.")
+  void list_success() {
+    // TODO: 2023/10/12 Fixture로 만들기
+    // given, when
+    final List<Product> actual = productService.list();
+
+    //then
+    assertThat(actual).hasSize(6);
+  }
+}
