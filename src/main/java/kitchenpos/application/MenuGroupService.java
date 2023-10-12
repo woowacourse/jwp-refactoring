260f0ae2371abce9f85723c258ca99b3cd5a4bf9
woo-chang <clllickme@naver.com>
2023-10-12 14:09:39 +0900

    test: 메뉴 그룹 로직 테스트

--- src/main/java/kitchenpos/application/MenuGroupService.java
+++ src/main/java/kitchenpos/application/MenuGroupService.java
@@ -1,22 +1,22 @@
 package kitchenpos.application;
 
+import java.util.List;
 import kitchenpos.dao.MenuGroupDao;
 import kitchenpos.domain.MenuGroup;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
 @Service
 public class MenuGroupService {
+
     private final MenuGroupDao menuGroupDao;
 
-    public MenuGroupService(final MenuGroupDao menuGroupDao) {
+    public MenuGroupService(MenuGroupDao menuGroupDao) {
         this.menuGroupDao = menuGroupDao;
     }
 
     @Transactional
-    public MenuGroup create(final MenuGroup menuGroup) {
+    public MenuGroup create(MenuGroup menuGroup) {
         return menuGroupDao.save(menuGroup);
     }
 
