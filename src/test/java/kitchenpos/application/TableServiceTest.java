26187878197b990b0b53523d7416222ed000025b
hong-sile <gurwns9325@naver.com>
2023-10-12 21:12:31 +0900

    test: tableService test 작성

--- src/test/java/kitchenpos/application/TableServiceTest.java
+++ src/test/java/kitchenpos/application/TableServiceTest.java
@@ -1,11 +1,26 @@
 package kitchenpos.application;
 
+import static kitchenpos.fixture.MenuFixture.후라이드치킨;
+import static kitchenpos.fixture.OrderFixture.createOrderLineItem;
+import static kitchenpos.fixture.ProductFixture.후라이드;
+import static kitchenpos.fixture.TableFixture.주문_테이블;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
+import kitchenpos.domain.Menu;
+import kitchenpos.domain.MenuGroup;
+import kitchenpos.domain.MenuProduct;
+import kitchenpos.domain.Order;
+import kitchenpos.domain.OrderLineItem;
 import kitchenpos.domain.OrderTable;
+import kitchenpos.domain.Product;
+import kitchenpos.domain.TableGroup;
+import kitchenpos.fixture.MenuFixture;
 import kitchenpos.fixture.TableFixture;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -13,11 +28,22 @@ class TableServiceTest extends ServiceIntegrationTest {
 
     @Autowired
     private TableService tableService;
+    @Autowired
+    private TableGroupService tableGroupService;
+    @Autowired
+    private OrderService orderService;
+    @Autowired
+    private ProductService productService;
+    @Autowired
+    private MenuService menuService;
+    @Autowired
+    private MenuGroupService menuGroupService;
+
 
     @Test
     @DisplayName("table을 생성한다.")
     void create() {
-        final OrderTable orderTable = TableFixture.주문_테이블();
+        final OrderTable orderTable = 주문_테이블();
 
         final OrderTable savedOrderTable = tableService.create(orderTable);
 
@@ -36,7 +62,134 @@ class TableServiceTest extends ServiceIntegrationTest {
         final List<OrderTable> foundedOrderTables = tableService.list();
 
         assertThat(foundedOrderTables)
-            .usingRecursiveFieldByFieldElementComparatorIgnoringFields("id", "tableGroupId")
+            .usingRecursiveFieldByFieldElementComparatorIgnoringFields("id")
             .isEqualTo(orderTables);
+    }
+
+    @Nested
+    @DisplayName("table의 상태를 empty로 바꾼다.")
+    class ChangeEmpty {
+
+        @Test
+        @DisplayName("성공적으로 바꾼다.")
+        void success() {
+            final OrderTable orderTable = tableService.create(주문_테이블());
+
+            final OrderTable changedOrderTable = tableService.changeEmpty(orderTable.getId(),
+                orderTable);
+
+            assertTrue(changedOrderTable.isEmpty());
+        }
+
+        @Test
+        @DisplayName("tableGroupId가 null이 아닌 경우 예외처리한다.")
+        void throwExceptionTableGroupIdIsNonNull() {
+            final OrderTable orderTable = 주문_테이블();
+            final OrderTable savedOrderTable = tableService.create(orderTable);
+            saveTableGroup(savedOrderTable);
+
+            final Long orderTableId = savedOrderTable.getId();
+            assertThatThrownBy(
+                () -> tableService.changeEmpty(orderTableId, savedOrderTable)
+            ).isInstanceOf(IllegalArgumentException.class);
+        }
+
+        private void saveTableGroup(final OrderTable savedOrderTable) {
+            final TableGroup tableGroup = new TableGroup();
+            final OrderTable orderTable = tableService.create(주문_테이블());
+            tableGroup.setOrderTables(List.of(savedOrderTable, orderTable));
+            tableGroupService.create(tableGroup);
+        }
+
+        @Test
+        @DisplayName("저장된 table에 속한 order가 cooking이나 meal 상태인 경우 예외처리")
+        void throwExceptionTableIsEmpty() {
+            //given
+            final OrderTable orderTable = 주문_테이블();
+            orderTable.setEmpty(false);
+            final OrderTable savedOrderTable = tableService.create(orderTable);
+            createOrderSuccessfully(savedOrderTable);
+
+            //when
+            final Long orderTableId = savedOrderTable.getId();
+            assertThatThrownBy(
+                () -> tableService.changeEmpty(orderTableId, savedOrderTable)
+            ).isInstanceOf(IllegalArgumentException.class);
+        }
+
+        private void createOrderSuccessfully(final OrderTable orderTable) {
+            final Menu menu = createMenu();
+            final OrderLineItem orderLineItem = createOrderLineItem(menu.getId(), 1L);
+
+            final Order order = new Order();
+            order.setOrderLineItems(List.of(orderLineItem));
+            order.setOrderTableId(orderTable.getId());
+            orderService.create(order);
+        }
+
+        private Menu createMenu() {
+            final Product savedProduct = productService.create(후라이드());
+            final MenuProduct menuProduct = MenuFixture.createMenuProduct(savedProduct, 1L);
+            final MenuGroup savedMenuGroup = menuGroupService.create(MenuFixture.한마리메뉴());
+            final Menu menu = 후라이드치킨(savedMenuGroup, List.of(menuProduct));
+
+            return menuService.create(menu);
+        }
+    }
+
+    @Nested
+    @DisplayName("게스트의 수를 변경할 수 있다.")
+    class ChangeNumberOfGuests {
+
+        @Test
+        @DisplayName("정상적으로 변경한다.")
+        void success() {
+            //given
+            final OrderTable orderTable = 주문_테이블();
+            orderTable.setEmpty(false);
+
+            final OrderTable savedOrderTable = tableService.create(orderTable);
+            savedOrderTable.setNumberOfGuests(10);
+
+            //when
+            final OrderTable changedOrderTable =
+                tableService.changeNumberOfGuests(savedOrderTable.getId(), savedOrderTable);
+
+            //then
+            assertThat(changedOrderTable.getNumberOfGuests())
+                .isEqualTo(savedOrderTable.getNumberOfGuests());
+        }
+
+        @Test
+        @DisplayName("numberOfGuest가 0미만인 경우 예외처리")
+        void throwExceptionNumberOfGuestLowerThan0() {
+            //given
+            final OrderTable orderTable = 주문_테이블();
+            orderTable.setEmpty(false);
+
+            final OrderTable savedOrderTable = tableService.create(orderTable);
+            savedOrderTable.setNumberOfGuests(-1);
+
+            //when
+            assertThatThrownBy(() ->
+                tableService.changeNumberOfGuests(savedOrderTable.getId(), savedOrderTable)
+            ).isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        @DisplayName("orderTable이 비어있는 경우 예외처리")
+        void throwExceptionOrderTableIsEmpty() {
+            //given
+            final OrderTable orderTable = 주문_테이블();
+            orderTable.setEmpty(true);
+
+            final OrderTable savedOrderTable = tableService.create(orderTable);
+            savedOrderTable.setNumberOfGuests(10);
+
+            //when
+            assertThatThrownBy(() ->
+                tableService.changeNumberOfGuests(savedOrderTable.getId(), savedOrderTable)
+            ).isInstanceOf(IllegalArgumentException.class);
+        }
     }
 }
