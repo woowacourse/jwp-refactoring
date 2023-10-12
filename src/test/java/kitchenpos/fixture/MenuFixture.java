658e098cb2c0da89c8ed47101b180960e0b22f69
최준호 <junho5336@gmail.com>
2023-10-13 02:10:59 +0900

    refactor: 미사용 Fixture 삭제

--- src/test/java/kitchenpos/fixture/MenuFixture.java
+++ src/test/java/kitchenpos/fixture/MenuFixture.java
@@ -24,28 +24,11 @@ public class MenuFixture {
             menu.setMenuProducts(List.of(후라이드_치킨()));
             return menu;
         }
-
-        public static Menu 메뉴_등록_요청(Long price) {
-            Menu menu = new Menu();
-            menu.setId(1L);
-            menu.setName("후라이드치킨");
-            menu.setPrice(BigDecimal.valueOf(price));
-            menu.setMenuGroupId(1L);
-            menu.setMenuProducts(List.of(후라이드_치킨()));
-            return menu;
-        }
     }
 
     public static class MENU_PRODUCT {
 
         public static MenuProduct 후라이드_치킨() {
-            MenuProduct menuProduct = new MenuProduct();
-            menuProduct.setProductId(1L);
-            menuProduct.setQuantity(1);
-            return menuProduct;
-        }
-
-        public static MenuProduct 후라이드_치킨(Long price) {
             MenuProduct menuProduct = new MenuProduct();
             menuProduct.setProductId(1L);
             menuProduct.setQuantity(1);
