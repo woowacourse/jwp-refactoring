c03f6e12c6fa2a033a33c7d0da27703094f1fa89
hanueleee <ezzeezz@naver.com>
2023-10-12 14:47:07 +0900

    feat: 서비스 테스트를 위한 ServiceTest 구현

---
+++ src/test/java/kitchenpos/application/ServiceTest.java
@@ -0,0 +1,15 @@
+package kitchenpos.application;
+
+import javax.transaction.Transactional;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+
+@Sql("/truncate.sql")
+@Transactional
+@SpringBootTest
+@SuppressWarnings("NonAsciiCharacters")
+@DisplayNameGeneration(ReplaceUnderscores.class)
+public class ServiceTest {
+}
