b030cfbe96f1064b678de2f1c68bf1ecda624680
Kong-hana01 <gksqlsl11@khu.ac.kr>
2023-10-12 11:18:21 +0900

    feat: MenuService 관련 커스텀 예외 구현

---
+++ src/main/java/kitchenpos/application/exception/KitchenPosException.java
@@ -0,0 +1,8 @@
+package kitchenpos.application.exception;
+
+public class KitchenPosException extends RuntimeException {
+
+    public KitchenPosException(String message) {
+        super(message);
+    }
+}
