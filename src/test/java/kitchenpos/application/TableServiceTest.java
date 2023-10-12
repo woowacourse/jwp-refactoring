697f16c3c4e1fd9a357600e981577ec61ba79934
odo27 <odo27@naver.com>
2023-10-12 16:33:27 +0900

    test: 주문 테이블 상태 변경 테스트 작성

--- src/test/java/kitchenpos/application/TableServiceTest.java
+++ src/test/java/kitchenpos/application/TableServiceTest.java
@@ -1,11 +1,15 @@
 package kitchenpos.application;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
+import kitchenpos.domain.OrderStatus;
 import kitchenpos.domain.OrderTable;
+import kitchenpos.domain.TableGroup;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class TableServiceTest extends IntegrationTest {
@@ -43,5 +47,61 @@ class TableServiceTest extends IntegrationTest {
                 () -> assertThat(result.get(0).getId()).isEqualTo(savedOrderTable1.getId()),
                 () -> assertThat(result.get(1).getId()).isEqualTo(savedOrderTable2.getId())
         );
+    }
+
+    @Nested
+    class 테이블_상태_변경 {
+
+        @Test
+        void 존재하지_않는_주문_테이블을_변경하면_예외가_발생한다() {
+            // when & then
+            assertThatThrownBy(() -> tableService.changeEmpty(1L, orderTable1))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        void 지정된_단체가_있는_테이블을_변경하면_예외가_발생한다() {
+            // given
+            orderTable1.setEmpty(true);
+            orderTable2.setEmpty(true);
+            OrderTable savedOrderTable1 = orderTableDao.save(orderTable1);
+            OrderTable savedOrderTable2 = orderTableDao.save(orderTable2);
+
+            TableGroup tableGroup = new TableGroup();
+            tableGroup.setOrderTables(List.of(savedOrderTable1, savedOrderTable2));
+            TableGroup savedTableGroup = tableGroupService.create(tableGroup);
+
+            orderTable1.setTableGroupId(savedTableGroup.getId());
+            Long id = orderTableDao.save(orderTable1).getId();
+
+            // when & then
+            assertThatThrownBy(() -> tableService.changeEmpty(id, orderTable2))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        void 조리중이거나_식사중인_테이블을_변경하면_예외가_발생한다() {
+            // given
+            orderTable1.setEmpty(true);
+            OrderTable savedOrderTable1 = orderTableDao.save(orderTable1);
+            주문(savedOrderTable1, OrderStatus.COOKING, 맛있는_메뉴());
+
+            // when & then
+            assertThatThrownBy(() -> tableService.changeEmpty(savedOrderTable1.getId(), orderTable2))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        void 주문_테이블_상태를_변경한다() {
+            // given
+            orderTable1.setEmpty(true);
+            OrderTable savedOrderTable1 = orderTableDao.save(orderTable1);
+
+            // when
+            OrderTable result = tableService.changeEmpty(savedOrderTable1.getId(), orderTable2);
+
+            // then
+            assertThat(result.isEmpty()).isFalse();
+        }
     }
 }
