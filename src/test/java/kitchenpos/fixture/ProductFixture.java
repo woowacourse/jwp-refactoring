2e775df8f9af87ae0cd59b25b1ffc2e9eef0302d
Go-Jaecheol <gojaech@naver.com>
2023-10-12 13:39:53 +0900

    test: ProductService 테스트 추가

---
+++ src/test/java/kitchenpos/fixture/ProductFixture.java
@@ -0,0 +1,23 @@
+package kitchenpos.fixture;
+
+import kitchenpos.domain.Product;
+
+import java.math.BigDecimal;
+
+@SuppressWarnings("NonAsciiCharacters")
+public class ProductFixture {
+
+    public static Product 상품_망고_1000원() {
+        final var product = new Product();
+        product.setName("망고");
+        product.setPrice(BigDecimal.valueOf(1000));
+        return product;
+    }
+
+    public static Product 상품_치킨_15000원() {
+        final var product = new Product();
+        product.setName("치킨");
+        product.setPrice(BigDecimal.valueOf(15000));
+        return product;
+    }
+}
