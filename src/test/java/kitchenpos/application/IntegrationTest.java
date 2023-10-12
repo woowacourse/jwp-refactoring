562485dbb0c30e02c3a9661f34a4bcba945022d2
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-10-12 14:12:34 +0900

    test: 메뉴 저장시 발생하는 예외 경우 테스트 추가

--- src/test/java/kitchenpos/application/IntegrationTest.java
+++ src/test/java/kitchenpos/application/IntegrationTest.java
@@ -1,5 +1,6 @@
 package kitchenpos.application;
 
+import java.math.BigDecimal;
 import kitchenpos.dao.MenuDao;
 import kitchenpos.dao.MenuGroupDao;
 import kitchenpos.dao.MenuProductDao;
@@ -8,6 +9,10 @@ import kitchenpos.dao.OrderLineItemDao;
 import kitchenpos.dao.OrderTableDao;
 import kitchenpos.dao.ProductDao;
 import kitchenpos.dao.TableGroupDao;
+import kitchenpos.domain.Menu;
+import kitchenpos.domain.MenuGroup;
+import kitchenpos.domain.MenuProduct;
+import kitchenpos.domain.Product;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,4 +42,39 @@ public abstract class IntegrationTest {
     protected ProductDao productDao;
     @Autowired
     protected TableGroupDao tableGroupDao;
+
+    protected MenuGroup generateMenuGroup(final String name) {
+        final MenuGroup menuGroup = new MenuGroup();
+        menuGroup.setName(name);
+        return menuGroupDao.save(menuGroup);
+    }
+
+    protected Menu generateMenu(final String name) {
+        final Menu menu = new Menu();
+        menu.setName(name);
+        menu.setPrice(BigDecimal.valueOf(220000));
+        menu.setMenuGroupId(generateMenuGroup(name + "-group").getId());
+        return menuDao.save(menu);
+    }
+
+    protected MenuProduct generateMenuProduct(final String menuName, final String productName) {
+        final MenuProduct menuProduct = new MenuProduct();
+        menuProduct.setMenuId(generateMenu(menuName).getId());
+        menuProduct.setProductId(generateProduct(productName).getId());
+        return menuProductDao.save(menuProduct);
+    }
+
+    protected Product generateProduct(final String name) {
+        final Product product = new Product();
+        product.setName(name);
+        product.setPrice(BigDecimal.valueOf(16000));
+        return productDao.save(product);
+    }
+
+    protected Product generateProduct(final String name, final Long price) {
+        final Product product = new Product();
+        product.setName(name);
+        product.setPrice(BigDecimal.valueOf(price));
+        return productDao.save(product);
+    }
 }
