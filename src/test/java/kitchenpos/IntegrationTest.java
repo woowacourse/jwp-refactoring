97362c24525c58bf8410cb5e9c79d24b30ccc800
yoondgu <doyoungwork@gmail.com>
2023-10-12 16:38:34 +0900

    test: 통합 테스트 트랜잭션 적용

--- src/test/java/kitchenpos/IntegrationTest.java
+++ src/test/java/kitchenpos/IntegrationTest.java
@@ -4,7 +4,9 @@ import io.restassured.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class IntegrationTest {
 
