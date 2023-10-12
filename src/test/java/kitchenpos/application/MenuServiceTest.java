aba1ff0ea2ec4dab94352bf62237312c6585a01f
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-10-12 14:24:26 +0900

    test: 메뉴 전제 조회 로직 테스트 추가

--- src/test/java/kitchenpos/application/MenuServiceTest.java
+++ src/test/java/kitchenpos/application/MenuServiceTest.java
@@ -135,4 +135,36 @@ class MenuServiceTest extends IntegrationTest {
                     .isInstanceOf(IllegalArgumentException.class);
         }
     }
+
+    @Test
+    void list() {
+        // given
+        final Menu savedChicken = generateMenu("chicken");
+        final Product chickenProduct = generateProduct("chicken", 10000L);
+        final MenuProduct menuProductA = new MenuProduct();
+        menuProductA.setMenuId(savedChicken.getMenuGroupId());
+        menuProductA.setProductId(chickenProduct.getId());
+        menuProductA.setQuantity(2);
+        menuProductDao.save(menuProductA);
+
+        final Product sauceProduct = generateProduct("sauce", 3000L);
+        final MenuProduct menuProductB = new MenuProduct();
+        menuProductB.setMenuId(savedChicken.getMenuGroupId());
+        menuProductB.setProductId(sauceProduct.getId());
+        menuProductB.setQuantity(5);
+        menuProductDao.save(menuProductB);
+
+        final Menu requestMenu = new Menu();
+        requestMenu.setName("seasoningChicken");
+        requestMenu.setMenuProducts(List.of(menuProductA, menuProductB));
+        requestMenu.setPrice(BigDecimal.valueOf(35000));
+        requestMenu.setMenuGroupId(savedChicken.getMenuGroupId());
+
+        // when
+        final Menu createdMenu = menuService.create(requestMenu);
+
+        // then
+        assertThat(createdMenu.getId()).isNotNull();
+        assertThat(createdMenu.getMenuProducts()).hasSize(2);
+    }
 }
