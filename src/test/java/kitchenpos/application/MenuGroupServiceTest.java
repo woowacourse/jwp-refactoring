36aafb7fa1937f3d845eaf507e6fe2b52dfb4a2b
donghae-kim <donghae1999@gmail.com>
2023-10-12 17:46:21 +0900

    test: MenuGroup 서비스 테스트 추가

---
+++ src/test/java/kitchenpos/application/MenuGroupServiceTest.java
@@ -0,0 +1,60 @@
+package kitchenpos.application;
+
+import kitchenpos.domain.MenuGroup;
+import kitchenpos.fixture.Fixture;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@Sql("/data.sql")
+class MenuGroupServiceTest {
+
+    @Autowired
+    private MenuGroupService menuGroupService;
+
+    @Test
+    @DisplayName("메뉴 그룹을 생성할 수 있다.")
+    void create() {
+        //given
+        final MenuGroup menuGroup = Fixture.menuGroup("오션 메뉴 그룹");
+
+        //when
+        final MenuGroup createdMenuGroup = menuGroupService.create(menuGroup);
+
+        //then
+        assertAll(
+                () -> assertThat(createdMenuGroup.getId()).isNotNull(),
+                () -> assertThat(createdMenuGroup.getName()).isEqualTo(menuGroup.getName())
+        );
+    }
+
+    @Test
+    @DisplayName("메뉴 그룹들을 조회할 수 있다.")
+    void list() {
+        //given
+        final MenuGroup menuGroup = Fixture.menuGroup("오션 메뉴 그룹");
+        final MenuGroup menuGroup2 = Fixture.menuGroup("동해 메뉴 그룹");
+        menuGroupService.create(menuGroup);
+        menuGroupService.create(menuGroup2);
+
+        //when
+        final List<MenuGroup> menuGroups = menuGroupService.list();
+
+        //then
+        assertAll(
+                () -> assertThat(menuGroups).hasSize(2),
+                () -> assertThat(menuGroups.get(0).getName()).isEqualTo(menuGroup.getName()),
+                () -> assertThat(menuGroups.get(1).getName()).isEqualTo(menuGroup2.getName())
+        );
+    }
+}
