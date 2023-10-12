4cd4e99bb2591b6ef93359270882a041f18e012c
swonny <wonny921@gmail.com>
2023-10-12 12:17:58 +0900

    test: 주문 서비스 테스트 추가

---
+++ src/test/java/kitchenpos/application/OrderServiceTest.java
@@ -0,0 +1,118 @@
+package kitchenpos.application;
+
+import kitchenpos.application.fixture.OrderServiceFixture;
+import kitchenpos.dao.MenuDao;
+import kitchenpos.dao.OrderDao;
+import kitchenpos.dao.OrderLineItemDao;
+import kitchenpos.dao.OrderTableDao;
+import kitchenpos.domain.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+class OrderServiceTest extends OrderServiceFixture {
+
+    @Mock
+    private MenuDao menuDao;
+
+    @Mock
+    private OrderDao orderDao;
+
+    @Mock
+    private OrderLineItemDao orderLineItemDao;
+
+    @Mock
+    private OrderTableDao orderTableDao;
+
+    @InjectMocks
+    private OrderService orderService;
+
+    @Test
+    void 메뉴를_주문한다() {
+        given(menuDao.countByIdIn(any())).willReturn(2L);
+        given(orderTableDao.findById(any())).willReturn(Optional.of(주문_생성시_사용하는_주문_테이블));
+        given(orderDao.save(any())).willReturn(저장한_주문);
+        given(orderLineItemDao.save(any())).willReturn(저장한_첫번째_주문항목).willReturn(저장한_두번째_주문항목);
+
+        final Order actual = orderService.create(요청된_주문);
+
+        assertThat(actual).isEqualTo(저장한_주문);
+    }
+
+    @Test
+    void 주문_항목이_1개_미만이면_예외가_발생한다() {
+        assertThatThrownBy(() -> orderService.create(주문항목이_1개_미만인_주문))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 주문_항목에서_입력받은_메뉴가_올바른_메뉴가_아니라면_예외가_발생한다() {
+        final long invalidOrderLineItemsSize = 0;
+        given(menuDao.countByIdIn(any())).willReturn(invalidOrderLineItemsSize);
+
+        assertThatThrownBy(() -> orderService.create(주문항목이_2개인_주문));
+    }
+
+    @Test
+    void 유효하지_않은_주문_테이블_아이디라면_예외가_발생한다() {
+        given(menuDao.countByIdIn(any())).willReturn(2L);
+        given(orderTableDao.findById(any())).willThrow(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> orderService.create(유효하지_않은_주문테이블_아이디를_갖는_주문));
+    }
+
+    @Test
+    void 주문_테이블_아이디에_해당하는_주문_테이블이_empty_table이라면_예외가_발생한다() {
+        given(menuDao.countByIdIn(any())).willReturn(2L);
+        given(orderTableDao.findById(any())).willReturn(Optional.of(empty가_true인_주문테이블));
+
+        assertThatThrownBy(() -> orderService.create(유효하지_않은_주문테이블_아이디를_갖는_주문));
+    }
+
+    @Test
+    void 모든_주문내역을_조회한다() {
+        given(orderDao.findAll()).willReturn(저장된_주문_리스트);
+
+        final List<Order> actual = orderService.list();
+
+        assertThat(actual).hasSize(저장된_주문_리스트.size());
+    }
+
+    @Test
+    void 주문_상태를_변경한다() {
+        given(orderDao.findById(any())).willReturn(Optional.of(상태를_변경할_식사중인_주문));
+        given(orderDao.save(any())).willReturn(식사중에서_완료로_상태변경된_주문);
+        given(orderLineItemDao.findAllByOrderId(any())).willReturn(식사중에서_완료로_상태변경된_주문.getOrderLineItems());
+
+        final Order actual = orderService.changeOrderStatus(상태를_변경할_식사중인_주문.getId(), 식사중에서_완료로_상태변경된_주문);
+
+        assertThat(actual).isEqualTo(식사중에서_완료로_상태변경된_주문);
+    }
+
+    @Test
+    void 유효하지_않은_주문_번호를_입력한_경우_예외가_발생한다() {
+        given(orderDao.findById(any())).willReturn(Optional.of(완료_상태인_주문));
+
+        assertThatThrownBy(() -> orderService.changeOrderStatus(완료_상태인_주문.getId(), 완료_상태인_주문))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void order_status가_잘못_입력된_경우_예외가_발생한다() {
+        given(orderDao.findById(any())).willReturn(Optional.of(잘못된_상태로_수정하고자_하는_주문));
+
+        assertThatThrownBy(() -> orderService.changeOrderStatus(잘못된_상태로_수정하고자_하는_주문.getId(), 잘못된_상태로_수정하고자_하는_주문))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
---
+++ src/test/java/kitchenpos/application/fixture/OrderServiceFixture.java
@@ -0,0 +1,111 @@
+package kitchenpos.application.fixture;
+
+import kitchenpos.domain.Menu;
+import kitchenpos.domain.Order;
+import kitchenpos.domain.OrderLineItem;
+import kitchenpos.domain.OrderTable;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.List;
+
+public class OrderServiceFixture {
+
+    protected Order 요청된_주문;
+    protected Order 저장한_주문;
+    protected OrderTable 주문_생성시_사용하는_주문_테이블;
+    protected OrderLineItem 저장한_첫번째_주문항목;
+    protected OrderLineItem 저장한_두번째_주문항목;
+    protected Order 주문항목이_1개_미만인_주문;
+    protected Order 주문항목이_2개인_주문;
+    protected Order 유효하지_않은_주문테이블_아이디를_갖는_주문;
+    protected OrderTable empty가_true인_주문테이블;
+    protected List<Order> 저장된_주문_리스트;
+    protected Order 상태를_변경할_식사중인_주문;
+    protected Order 식사중에서_완료로_상태변경된_주문;
+    protected Order 완료_상태인_주문;
+    protected Order 잘못된_상태로_수정하고자_하는_주문;
+
+    @BeforeEach
+    void setUp() {
+        // create() 메서드 사용 픽스처
+        주문_생성시_사용하는_주문_테이블 = new OrderTable();
+        주문_생성시_사용하는_주문_테이블.setId(1L);
+        주문_생성시_사용하는_주문_테이블.setEmpty(false);
+
+        final Menu 메뉴 = new Menu();
+        메뉴.setId(1L);
+
+        저장한_첫번째_주문항목 = new OrderLineItem();
+        저장한_두번째_주문항목 = new OrderLineItem();
+        저장한_첫번째_주문항목.setMenuId(메뉴.getId());
+        저장한_두번째_주문항목.setMenuId(메뉴.getId());
+
+        요청된_주문 = new Order();
+        요청된_주문.setOrderTableId(주문_생성시_사용하는_주문_테이블.getId());
+        요청된_주문.setOrderedTime(LocalDateTime.now());
+        요청된_주문.setOrderLineItems(List.of(저장한_첫번째_주문항목, 저장한_두번째_주문항목));
+
+        저장한_주문 = new Order();
+        저장한_주문.setOrderTableId(요청된_주문.getOrderTableId());
+        저장한_주문.setOrderedTime(LocalDateTime.now());
+        저장한_주문.setOrderLineItems(요청된_주문.getOrderLineItems());
+
+        // 주문항목 1개 미만 예외 케이스 테스트 픽스처
+        주문항목이_1개_미만인_주문 = new Order();
+        주문항목이_1개_미만인_주문.setOrderLineItems(Collections.EMPTY_LIST);
+
+        // 주문항목 개수 예외 케이스 테스트 픽스처
+        주문항목이_2개인_주문 = new Order();
+        주문항목이_2개인_주문.setOrderLineItems(요청된_주문.getOrderLineItems());
+
+        // 유효하지_않은_주문_테이블_아이디라면_예외가_발생한다
+        final long invalidTableId = -999L;
+        유효하지_않은_주문테이블_아이디를_갖는_주문 = new Order();
+        유효하지_않은_주문테이블_아이디를_갖는_주문.setOrderTableId(invalidTableId);
+        유효하지_않은_주문테이블_아이디를_갖는_주문.setOrderLineItems(요청된_주문.getOrderLineItems());
+
+        // 주문_테이블_아이디에_해당하는_주문_테이블이_empty_table이라면_예외가_발생한다
+        empty가_true인_주문테이블 = new OrderTable();
+        empty가_true인_주문테이블.setId(1L);
+        empty가_true인_주문테이블.setEmpty(true);
+        유효하지_않은_주문테이블_아이디를_갖는_주문 = new Order();
+        유효하지_않은_주문테이블_아이디를_갖는_주문.setOrderTableId(empty가_true인_주문테이블.getId());
+        유효하지_않은_주문테이블_아이디를_갖는_주문.setOrderLineItems(요청된_주문.getOrderLineItems());
+
+        // 모든 주문내역 조회
+        저장된_주문_리스트 = List.of(저장한_주문);
+
+        // 주문 상태를 변경한다
+        상태를_변경할_식사중인_주문 = new Order();
+        상태를_변경할_식사중인_주문.setId(1L);
+        상태를_변경할_식사중인_주문.setOrderStatus("MEAL");
+        상태를_변경할_식사중인_주문.setOrderTableId(주문_생성시_사용하는_주문_테이블.getId());
+        상태를_변경할_식사중인_주문.setOrderedTime(LocalDateTime.now());
+        상태를_변경할_식사중인_주문.setOrderLineItems(요청된_주문.getOrderLineItems());
+
+        식사중에서_완료로_상태변경된_주문 = new Order();
+        식사중에서_완료로_상태변경된_주문.setOrderStatus("COMPLETION");
+        식사중에서_완료로_상태변경된_주문.setId(상태를_변경할_식사중인_주문.getId());
+        식사중에서_완료로_상태변경된_주문.setOrderTableId(상태를_변경할_식사중인_주문.getOrderTableId());
+        식사중에서_완료로_상태변경된_주문.setOrderedTime(상태를_변경할_식사중인_주문.getOrderedTime());
+        식사중에서_완료로_상태변경된_주문.setOrderLineItems(상태를_변경할_식사중인_주문.getOrderLineItems());
+
+        // 유효하지_않은_주문_번호를_입력한_경우_예외가_발생한다
+        완료_상태인_주문 = new Order();
+        완료_상태인_주문.setId(1L);
+        완료_상태인_주문.setOrderStatus("COMPLETION");
+        완료_상태인_주문.setOrderTableId(주문_생성시_사용하는_주문_테이블.getId());
+        완료_상태인_주문.setOrderedTime(LocalDateTime.now());
+        완료_상태인_주문.setOrderLineItems(요청된_주문.getOrderLineItems());
+
+        // order_status가_잘못_입력된_경우_예외가_발생한다
+        잘못된_상태로_수정하고자_하는_주문 = new Order();
+        잘못된_상태로_수정하고자_하는_주문.setId(1L);
+        잘못된_상태로_수정하고자_하는_주문.setOrderStatus("INVALID STATUS");
+        잘못된_상태로_수정하고자_하는_주문.setOrderTableId(주문_생성시_사용하는_주문_테이블.getId());
+        잘못된_상태로_수정하고자_하는_주문.setOrderedTime(LocalDateTime.now());
+        잘못된_상태로_수정하고자_하는_주문.setOrderLineItems(요청된_주문.getOrderLineItems());
+    }
+}
