70a721ad241bdddefeea0aee95e56ad776a3c13d
wugawuga <dnr3655@naver.com>
2023-10-12 15:29:50 +0900

    test: MenuServiceTest 추가

---
+++ src/test/java/kitchenpos/application/MenuServiceTest.java
@@ -0,0 +1,149 @@
+package kitchenpos.application;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import java.math.BigDecimal;
+import java.util.List;
+import kitchenpos.dao.MenuGroupDao;
+import kitchenpos.dao.ProductDao;
+import kitchenpos.domain.Menu;
+import kitchenpos.domain.MenuGroup;
+import kitchenpos.domain.MenuProduct;
+import kitchenpos.domain.Product;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql(scripts = {"classpath:truncate.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+class MenuServiceTest {
+
+    @Autowired
+    private MenuService menuService;
+
+    @Autowired
+    private ProductDao productDao;
+
+    @Autowired
+    private MenuGroupDao menuGroupDao;
+
+    @Test
+    void 메뉴를_등록할_수_있다() {
+        // given
+        final Product savedProduct = createProduct(2000);
+        final MenuProduct menuProduct = makeMenuProduct(savedProduct);
+        final MenuGroup savedMenuGroup = createMenuGroup("테스트 메뉴 그룹");
+
+        final Menu menu = new Menu();
+        menu.setName("테스트 메뉴");
+        menu.setPrice(new BigDecimal(2000));
+        menu.setMenuGroupId(savedMenuGroup.getId());
+        menu.setMenuProducts(List.of(menuProduct));
+
+        // when
+        final Menu result = menuService.create(menu);
+
+        // then
+        assertThat(result.getName()).isEqualTo(menu.getName());
+        assertThat(result.getPrice()).isEqualByComparingTo(menu.getPrice());
+    }
+
+    @Test
+    void 메뉴가격이_상품가격보다_클_경우_예외가_발생한다() {
+        // given
+        final Product savedProduct = createProduct(2000);
+        final MenuProduct menuProduct = makeMenuProduct(savedProduct);
+        final MenuGroup savedMenuGroup = createMenuGroup("테스트 메뉴 그룹");
+
+        // when
+        final Menu menu = new Menu();
+        menu.setName("테스트 메뉴");
+        menu.setPrice(new BigDecimal(3000));
+        menu.setMenuGroupId(savedMenuGroup.getId());
+        menu.setMenuProducts(List.of(menuProduct));
+
+        // then
+        assertThatThrownBy(() -> menuService.create(menu))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 메뉴가격이_0보다_작은경우_예외가_발생한다() {
+        // given
+        final Product savedProduct = createProduct(2000);
+        final MenuProduct menuProduct = makeMenuProduct(savedProduct);
+        final MenuGroup savedMenuGroup = createMenuGroup("테스트 메뉴 그룹");
+
+        // when
+        final Menu menu = new Menu();
+        menu.setName("테스트 메뉴");
+        menu.setPrice(new BigDecimal(-1));
+        menu.setMenuGroupId(savedMenuGroup.getId());
+        menu.setMenuProducts(List.of(menuProduct));
+
+        // then
+        assertThatThrownBy(() -> menuService.create(menu))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 메뉴등록시_메뉴그룹이_존재하지_않으면_예외가_발생한다() {
+        // given
+        final Product savedProduct = createProduct(2000);
+        final MenuProduct menuProduct = makeMenuProduct(savedProduct);
+
+        // when
+        final Menu menu = new Menu();
+        menu.setName("테스트 메뉴");
+        menu.setPrice(new BigDecimal(3000));
+        menu.setMenuGroupId(null);
+        menu.setMenuProducts(List.of(menuProduct));
+
+        // then
+        assertThatThrownBy(() -> menuService.create(menu))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 메뉴목록을_조회할_수_있다() {
+        // given
+        final Product savedProduct = createProduct(2000);
+        final MenuProduct menuProduct = makeMenuProduct(savedProduct);
+        final MenuGroup savedMenuGroup = createMenuGroup("테스트 메뉴 그룹");
+
+        final Menu menu = new Menu();
+        menu.setName("테스트 메뉴");
+        menu.setPrice(new BigDecimal(2000));
+        menu.setMenuGroupId(savedMenuGroup.getId());
+        menu.setMenuProducts(List.of(menuProduct));
+        menuService.create(menu);
+
+        // when
+        final List<Menu> results = menuService.list();
+
+        // then
+        assertThat(results).hasSize(1);
+    }
+
+    private Product createProduct(final int price) {
+        final Product product = new Product();
+        product.setName("테스트 상품");
+        product.setPrice(new BigDecimal(price));
+        return productDao.save(product);
+    }
+
+    private MenuProduct makeMenuProduct(final Product savedProduct) {
+        final MenuProduct menuProduct = new MenuProduct();
+        menuProduct.setProductId(savedProduct.getId());
+        menuProduct.setQuantity(1);
+        return menuProduct;
+    }
+
+    private MenuGroup createMenuGroup(final String groupName) {
+        final MenuGroup menuGroup = new MenuGroup();
+        menuGroup.setName(groupName);
+        return menuGroupDao.save(menuGroup);
+    }
+}
