e7f84a77fa2ec438a5b98850a5455c97ded40890
gitchan <eunkeeeee@gmail.com>
2023-10-12 11:30:06 +0900

    test: Product 통합테스트 작성

--- src/main/java/kitchenpos/domain/Product.java
+++ src/main/java/kitchenpos/domain/Product.java
@@ -7,6 +7,14 @@ public class Product {
     private String name;
     private BigDecimal price;
 
+    public Product() {
+    }
+
+    public Product(final String name, final BigDecimal price) {
+        this.name = name;
+        this.price = price;
+    }
+
     public Long getId() {
         return id;
     }
---
+++ src/test/java/kitchenpos/step/ProductStep.java
@@ -0,0 +1,43 @@
+package kitchenpos.step;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import kitchenpos.domain.Product;
+
+import java.math.BigDecimal;
+
+import static io.restassured.http.ContentType.JSON;
+
+public class ProductStep {
+
+    public static final Product 스키야키 = new Product("스키야키", BigDecimal.valueOf(11_900));
+    public static final Product 우동 = new Product("우동", BigDecimal.valueOf(8_900));
+
+    public static ExtractableResponse<Response> 상품_생성_요청(final Product product) {
+        return RestAssured.given()
+                .log().all()
+                .contentType(JSON)
+                .body(product)
+
+                .when()
+                .post("/api/products")
+
+                .then()
+                .log().all()
+                .extract();
+    }
+
+    public static ExtractableResponse<Response> 상품_조회_요청() {
+        return RestAssured.given()
+                .log().all()
+                .contentType(JSON)
+
+                .when()
+                .get("/api/products")
+
+                .then()
+                .log().all()
+                .extract();
+    }
+}
