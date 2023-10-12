a264b8a37c6b27a8bf7ac03e0e630c179aeb1cda
swonny <wonny921@gmail.com>
2023-10-12 11:37:50 +0900

    test: 메뉴 서비스 테스트 추가

---
+++ src/test/java/kitchenpos/application/MenuServiceTest.java
@@ -0,0 +1,101 @@
+package kitchenpos.application;
+
+import kitchenpos.application.fixture.MenuServiceFixture;
+import kitchenpos.dao.JdbcTemplateMenuDao;
+import kitchenpos.dao.JdbcTemplateMenuGroupDao;
+import kitchenpos.dao.JdbcTemplateMenuProductDao;
+import kitchenpos.dao.JdbcTemplateProductDao;
+import kitchenpos.domain.Menu;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+class MenuServiceTest extends MenuServiceFixture {
+
+    @Mock
+    private JdbcTemplateMenuDao menuDao;
+
+    @Mock
+    private JdbcTemplateProductDao productDao;
+
+    @Mock
+    private JdbcTemplateMenuGroupDao menuGroupDao;
+
+    @Mock
+    private JdbcTemplateMenuProductDao menuProductDao;
+
+    @InjectMocks
+    private MenuService menuService;
+
+    @Test
+    void 메뉴를_생성한다() {
+        given(menuGroupDao.existsById(anyLong())).willReturn(true);
+        given(menuProductDao.save(any())).willReturn(첫번째_메뉴_상품);
+        given(menuDao.save(any(Menu.class))).willReturn(저장된_메뉴);
+        given(productDao.findById(any())).willReturn(Optional.of(첫번째_메뉴_상품의_상품));
+
+        final Menu actual = menuService.create(저장된_메뉴);
+
+        assertThat(actual).isEqualTo(저장된_메뉴);
+    }
+
+    @Test
+    void 전달_받은_메뉴의_가격이_입력되지_않았다면_예외가_발생한다() {
+        assertThatThrownBy(() -> menuService.create(가격이_입력되지_않은_메뉴))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+
+    @Test
+    void 전달_받은_메뉴의_가격이_0보다_작은_경우_예외가_발생한다() {
+        assertThatThrownBy(() -> menuService.create(가격이_0보다_작은_메뉴))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 유효하지_않은_메뉴_그룹_아이디를_전달_받으면_예외가_발생한다() {
+        given(menuGroupDao.existsById(anyLong())).willThrow(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> menuService.create(유효하지_않은_메뉴_그룹_아이디를_갖는_메뉴))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 유효하지_않은_메뉴_상품_아이디를_전달_받으면_예외가_발생한다() {
+        given(menuGroupDao.existsById(anyLong())).willReturn(true);
+        given(productDao.findById(anyLong())).willThrow(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> menuService.create(유효하지_않은_메뉴_상품_아이디를_갖는_메뉴))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 메뉴의_가격이_메뉴에_포함된_상품_가격을_합친_것보다_작은_경우_예외가_발생한다() {
+        given(menuGroupDao.existsById(anyLong())).willReturn(true);
+        given(productDao.findById(anyLong())).willThrow(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> menuService.create(유효하지_않은_가격을_갖는_메뉴))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 모든_메뉴를_조회한다() {
+        given(menuDao.findAll()).willReturn(저장된_메뉴_리스트);
+
+        final List<Menu> actual = menuService.list();
+
+        assertThat(actual).isEqualTo(저장된_메뉴_리스트);
+    }
+}
---
+++ src/test/java/kitchenpos/application/fixture/MenuServiceFixture.java
@@ -0,0 +1,83 @@
+package kitchenpos.application.fixture;
+
+import kitchenpos.domain.Menu;
+import kitchenpos.domain.MenuProduct;
+import kitchenpos.domain.Product;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class MenuServiceFixture {
+
+    protected Menu 저장된_메뉴 = new Menu();
+    protected Menu 가격이_0보다_작은_메뉴 = new Menu();
+    protected Menu 가격이_입력되지_않은_메뉴 = new Menu();
+    protected Menu 유효하지_않은_메뉴_그룹_아이디를_갖는_메뉴 = new Menu();
+    protected Menu 유효하지_않은_메뉴_상품_아이디를_갖는_메뉴 = new Menu();
+    protected Menu 유효하지_않은_가격을_갖는_메뉴 = new Menu();
+    protected MenuProduct 저장할_메뉴_상품 = new MenuProduct();
+    protected MenuProduct 메뉴_리스트에_저장된_메뉴_상품 = new MenuProduct();
+    protected MenuProduct 첫번째_메뉴_상품 = new MenuProduct();
+    protected Product 첫번째_메뉴_상품의_상품 = new Product();
+    protected List<Menu> 저장된_메뉴_리스트;
+
+    @BeforeEach
+    void setUp() {
+        첫번째_메뉴_상품의_상품 = new Product();
+        첫번째_메뉴_상품의_상품.setId(1L);
+        첫번째_메뉴_상품의_상품.setPrice(BigDecimal.valueOf(20_000));
+
+        첫번째_메뉴_상품.setSeq(1L);
+        첫번째_메뉴_상품.setMenuId(1L);
+        첫번째_메뉴_상품.setProductId(첫번째_메뉴_상품의_상품.getId());
+        첫번째_메뉴_상품.setQuantity(1L);
+
+        저장된_메뉴.setId(1L);
+        저장된_메뉴.setName("후라이드");
+        저장된_메뉴.setPrice(BigDecimal.valueOf(19_000));
+        저장된_메뉴.setMenuGroupId(1L);
+        저장된_메뉴.setMenuProducts(List.of(첫번째_메뉴_상품));
+
+        저장된_메뉴_리스트 = List.of(저장된_메뉴);
+
+        가격이_입력되지_않은_메뉴.setPrice(null);
+        가격이_0보다_작은_메뉴.setPrice(BigDecimal.valueOf(-1L));
+
+        final long 유효하지_않은_그룹_메뉴_아이디 = -999L;
+        유효하지_않은_메뉴_그룹_아이디를_갖는_메뉴.setName("후라이드");
+        유효하지_않은_메뉴_그룹_아이디를_갖는_메뉴.setPrice(BigDecimal.valueOf(19_000));
+        유효하지_않은_메뉴_그룹_아이디를_갖는_메뉴.setMenuGroupId(유효하지_않은_그룹_메뉴_아이디);
+        유효하지_않은_메뉴_그룹_아이디를_갖는_메뉴.setMenuProducts(List.of(첫번째_메뉴_상품));
+
+        final MenuProduct 유효하지_않은_상품 = new MenuProduct();
+        final long 유효하지_않은_상품_아이디 = -999L;
+        유효하지_않은_상품.setProductId(유효하지_않은_상품_아이디);
+        유효하지_않은_메뉴_상품_아이디를_갖는_메뉴.setName("후라이드");
+        유효하지_않은_메뉴_상품_아이디를_갖는_메뉴.setPrice(BigDecimal.valueOf(19_000));
+        유효하지_않은_메뉴_상품_아이디를_갖는_메뉴.setMenuGroupId(1L);
+        유효하지_않은_메뉴_상품_아이디를_갖는_메뉴.setMenuProducts(List.of(유효하지_않은_상품));
+
+        final Product 첫_번째_상품 = new Product();
+        final Product 두_번째_상품 = new Product();
+        첫_번째_상품.setId(2L);
+        두_번째_상품.setId(3L);
+        첫_번째_상품.setPrice(BigDecimal.valueOf(10_000));
+        두_번째_상품.setPrice(BigDecimal.valueOf(20_000));
+
+        final MenuProduct 첫_번째_메뉴_상품 = new MenuProduct();
+        final MenuProduct 두_번째_메뉴_상품 = new MenuProduct();
+        첫_번째_메뉴_상품.setProductId(첫_번째_상품.getId());
+        첫_번째_메뉴_상품.setQuantity(1L);
+        두_번째_메뉴_상품.setQuantity(2L);
+
+        final BigDecimal 유효하지_않은_메뉴_가격 = BigDecimal.ZERO;
+
+        유효하지_않은_가격을_갖는_메뉴.setId(1L);
+        유효하지_않은_가격을_갖는_메뉴.setName("후라이드");
+        유효하지_않은_가격을_갖는_메뉴.setPrice(유효하지_않은_메뉴_가격);
+        유효하지_않은_가격을_갖는_메뉴.setMenuGroupId(1L);
+        유효하지_않은_가격을_갖는_메뉴.setMenuProducts(List.of(첫_번째_메뉴_상품, 두_번째_메뉴_상품));
+
+    }
+}
