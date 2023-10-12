d8f229aafece18f13df9761a5a89bf823d1ebdf7
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2023-10-12 11:30:00 +0900

    test: 주문 테이블을 empty로 변경하는 테스트 코드를 작성한다.

--- src/test/java/kitchenpos/application/TableServiceTest.java
+++ src/test/java/kitchenpos/application/TableServiceTest.java
@@ -3,6 +3,7 @@ package kitchenpos.application;
 import kitchenpos.application.test.ServiceUnitTest;
 import kitchenpos.dao.OrderDao;
 import kitchenpos.dao.OrderTableDao;
+import kitchenpos.domain.OrderStatus;
 import kitchenpos.domain.OrderTable;
 import kitchenpos.domain.fixture.OrderTableFixture;
 import org.junit.jupiter.api.Assertions;
@@ -13,11 +14,14 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static kitchenpos.domain.fixture.OrderTableFixture.*;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 class TableServiceTest extends ServiceUnitTest {
 
@@ -57,7 +61,7 @@ class TableServiceTest extends ServiceUnitTest {
         @Test
         void 주문_목록을_반환한다() {
             // given
-            Mockito.when(orderTableDao.findAll()).thenReturn(List.of(orderTable));
+            when(orderTableDao.findAll()).thenReturn(List.of(orderTable));
 
             // when
             List<OrderTable> orderTables = tableService.list();
@@ -67,6 +71,60 @@ class TableServiceTest extends ServiceUnitTest {
                     () -> assertThat(orderTables).hasSize(1),
                     () -> assertThat(orderTables.get(0)).isEqualTo(orderTable)
             );
+        }
+
+    }
+
+    @Nested
+    class 주문_목록을_empty로_변경한다 {
+
+        @Test
+        void 주문_목록을_empty로_변경한다() {
+            // given
+            when(orderTableDao.findById(orderTable.getId())).thenReturn(Optional.ofNullable(orderTable));
+            when(orderDao.existsByOrderTableIdAndOrderStatusIn(orderTable.getId(), Arrays.asList(OrderStatus.COOKING.name(), OrderStatus.MEAL.name())))
+                    .thenReturn(false);
+            when(orderTableDao.save(orderTable)).thenReturn(orderTable);
+
+            // when
+            OrderTable savedOrderTable = tableService.changeEmpty(orderTable.getId(), orderTable);
+
+            // then
+            assertAll(
+                    () -> assertThat(savedOrderTable).isEqualTo(savedOrderTable),
+                    () -> assertThat(savedOrderTable.isEmpty()).isEqualTo(orderTable.isEmpty())
+            );
+        }
+
+        @Test
+        void 주문_테이블이_존재하지_않는다면_예외가_발생한다() {
+            // given
+            when(orderTableDao.findById(orderTable.getId())).thenReturn(Optional.ofNullable(null));
+
+            // when
+            assertThrows(IllegalArgumentException.class, () -> tableService.changeEmpty(orderTable.getId(), orderTable));
+        }
+
+        @Test
+        void 주문_테이블의_tableGroupId가_존재한다면_예외가_발생한다() {
+            // given
+            orderTable.setTableGroupId(1L);
+            when(orderTableDao.findById(orderTable.getId())).thenReturn(Optional.ofNullable(orderTable));
+
+            // when
+            assertThrows(IllegalArgumentException.class, () -> tableService.changeEmpty(orderTable.getId(), orderTable));
+        }
+
+        @Test
+        void 주문_상태가_COOKING_또는_MEAL이라면_예외가_발생한다() {
+            // given
+            when(orderTableDao.findById(orderTable.getId())).thenReturn(Optional.ofNullable(orderTable));
+            when(orderTableDao.findById(orderTable.getId())).thenReturn(Optional.ofNullable(orderTable));
+            when(orderDao.existsByOrderTableIdAndOrderStatusIn(orderTable.getId(), Arrays.asList(OrderStatus.COOKING.name(), OrderStatus.MEAL.name())))
+                    .thenReturn(true);
+
+            // when
+            assertThrows(IllegalArgumentException.class, () -> tableService.changeEmpty(orderTable.getId(), orderTable));
         }
 
     }
