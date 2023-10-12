0dfc1563a5a53434f3903a856a8e91833d0899bf
sosow0212 <sosow0212@naver.com>
2023-10-12 16:30:13 +0900

    refactor: 기존 코드로 이전

--- src/main/java/kitchenpos/application/menu/MenuService.java
+++ src/main/java/kitchenpos/application/menu/MenuService.java
@@ -14,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class MenuService {
@@ -38,7 +39,10 @@ public class MenuService {
     @Transactional
     public Menu create(final MenuCreateRequest req) {
         Menu menu = req.toDomain();
-        menu.validateInvalidPrice();
+
+        if (Objects.isNull(menu.getPrice()) || menu.getPrice().compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException();
+        }
 
         if (!menuGroupDao.existsById(menu.getMenuGroupId())) {
             throw new IllegalArgumentException();
@@ -51,7 +55,9 @@ public class MenuService {
             sum = sum.add(product.getPrice().multiply(BigDecimal.valueOf(menuProduct.getQuantity())));
         }
 
-        menu.validatePriceBiggerThanMenuProductsSum(sum);
+        if (menu.getPrice().compareTo(sum) > 0) {
+            throw new IllegalArgumentException();
+        }
 
         final Menu savedMenu = menuDao.save(menu);
 
