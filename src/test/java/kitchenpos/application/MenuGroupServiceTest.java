ae6314b471b34e66a37ac77fe89d39195a35235e
Ethan <kim76744dev@gmail.com>
2023-10-12 11:12:47 +0900

    test: 메뉴 그룹 관련 테스트 추가

---
+++ src/test/java/kitchenpos/application/MenuGroupServiceTest.java
@@ -0,0 +1,53 @@
+package kitchenpos.application;
+
+import kitchenpos.dao.MenuGroupDao;
+import kitchenpos.domain.MenuGroup;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+@SuppressWarnings("NonAsciiCharacters")
+@ExtendWith(MockitoExtension.class)
+class MenuGroupServiceTest {
+
+    @InjectMocks
+    private MenuGroupService menuGroupService;
+
+    @Mock
+    private MenuGroupDao menuGroupDao;
+
+    @Test
+    void 메뉴_그룹을_생성할_수_있다() {
+        // given
+        final MenuGroup expected = new MenuGroup("식사");
+        given(menuGroupDao.save(any(MenuGroup.class))).willReturn(expected);
+
+        // when
+        final MenuGroup actual = menuGroupService.create(new MenuGroup());
+
+        // then
+        assertThat(actual.getName()).isEqualTo(expected.getName());
+    }
+
+    @Test
+    void 메뉴_그룹을_전체_조회할_수_있다() {
+        // given
+        final MenuGroup siksa = new MenuGroup("식사");
+        final MenuGroup noodles = new MenuGroup("면류");
+        given(menuGroupDao.findAll()).willReturn(List.of(siksa, noodles));
+
+        // when
+        final List<MenuGroup> actual = menuGroupService.list();
+
+        // then
+        assertThat(actual).containsExactly(siksa, noodles);
+    }
+}
