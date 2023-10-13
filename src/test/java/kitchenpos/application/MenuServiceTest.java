403aaee8d46bcb73fe67e7403a4bfd5dd5157028
ezzanzzan <cjsdmswjd821@naver.com>
2023-10-13 17:48:21 +0900

    test: MenuServiceTest 리팩토링
    - Fixture 제거

--- src/test/java/kitchenpos/application/MenuServiceTest.java
+++ src/test/java/kitchenpos/application/MenuServiceTest.java
@@ -19,11 +19,6 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
-import static kitchenpos.fixture.Fixture.MAX_PRICE;
-import static kitchenpos.fixture.Fixture.메뉴;
-import static kitchenpos.fixture.Fixture.메뉴_그룹;
-import static kitchenpos.fixture.Fixture.메뉴_상품;
-import static kitchenpos.fixture.Fixture.상품;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
@@ -40,78 +35,68 @@ class MenuServiceTest {
     @Autowired
     private ProductService productService;
 
-    private MenuGroup menuGroup;
+    private Long savedMenuGroupId;
     private List<MenuProduct> menuProducts = new ArrayList<>();
 
     @BeforeEach
     void setUp() {
-        final Product product = productService.create(상품("상품", BigDecimal.ONE));
-        menuGroup = menuGroupService.create(메뉴_그룹("메뉴 그룹"));
-        menuProducts.add(메뉴_상품(product, 2));
+        final Product product = new Product(null, "상품", BigDecimal.ONE);
+        final Product savedProduct = productService.create(product);
+        final MenuGroup menuGroup = new MenuGroup(null, "메뉴 그룹");
+        final MenuGroup savedMenuGroup = menuGroupService.create(menuGroup);
+        savedMenuGroupId = savedMenuGroup.getId();
+        menuProducts.add(new MenuProduct(null, null, savedProduct.getId(), 2));
     }
 
     @Nested
     class 메뉴를_등록한다 {
         @Test
         void 메뉴가_정상적으로_등록된다() {
-            final Menu menu = 메뉴("메뉴", BigDecimal.ONE, menuGroup.getId(), menuProducts);
+            final Menu menu = new Menu(null, "메뉴", BigDecimal.ONE, savedMenuGroupId, menuProducts);
             final Menu savedMenu = menuService.create(menu);
 
-            assertSoftly(softAssertions -> {
-                assertThat(savedMenu.getId()).isNotNull();
-                assertThat(savedMenu.getName()).isEqualTo(menu.getName());
-                assertThat(savedMenu.getPrice()).isEqualByComparingTo(menu.getPrice());
-                assertThat(savedMenu.getMenuGroupId()).isEqualTo(menu.getMenuGroupId());
-                assertThat(savedMenu.getMenuProducts())
-                        .usingRecursiveComparison()
-                        .ignoringFields("seq", "menuId")
-                        .isEqualTo(menuProducts);
+            assertSoftly(softly -> {
+                softly.assertThat(savedMenu.getId()).isNotNull();
+                softly.assertThat(savedMenu.getName()).isEqualTo(menu.getName());
+                softly.assertThat(savedMenu.getPrice()).isEqualByComparingTo(menu.getPrice());
+                softly.assertThat(savedMenu.getMenuGroupId()).isEqualTo(menu.getMenuGroupId());
             });
         }
 
         @ParameterizedTest
         @ValueSource(ints = {0, 1, 255})
         void 메뉴_이름은_255자_이하이다(int length) {
-            final Menu menu = 메뉴("메".repeat(length), BigDecimal.ONE, menuGroup.getId(), menuProducts);
+            final Menu menu = new Menu(null, "메".repeat(length), BigDecimal.ONE, savedMenuGroupId, menuProducts);
             final Menu savedMenu = menuService.create(menu);
 
-            assertSoftly(softAssertions -> {
-                assertThat(savedMenu.getId()).isNotNull();
-                assertThat(savedMenu.getName()).isEqualTo(menu.getName());
-                assertThat(savedMenu.getPrice()).isEqualByComparingTo(menu.getPrice());
-                assertThat(savedMenu.getMenuGroupId()).isEqualTo(menu.getMenuGroupId());
-                assertThat(savedMenu.getMenuProducts())
-                        .usingRecursiveComparison()
-                        .ignoringFields("seq", "menuId")
-                        .isEqualTo(menuProducts);
+            assertSoftly(softly -> {
+                softly.assertThat(savedMenu.getId()).isNotNull();
+                softly.assertThat(savedMenu.getName()).isEqualTo(menu.getName());
+                softly.assertThat(savedMenu.getPrice()).isEqualByComparingTo(menu.getPrice());
+                softly.assertThat(savedMenu.getMenuGroupId()).isEqualTo(menu.getMenuGroupId());
             });
         }
 
         @ParameterizedTest
         @ValueSource(doubles = {0, 1, 999_999_999_999_999.99})
         void 메뉴_가격은_0원_보다_크고_1000조_보다_작다(double price) {
-            final Product product = productService.create(상품("상품1", BigDecimal.valueOf(MAX_PRICE)));
-            MenuProduct 메뉴_상품 = 메뉴_상품(product, 1);
-            menuProducts.add(메뉴_상품);
+            final Product product = productService.create(new Product(null, "상품", BigDecimal.valueOf(999_999_999_999_999.99)));
+            menuProducts.add(new MenuProduct(null, null, product.getId(), 1));
 
-            final Menu menu = 메뉴("메뉴", BigDecimal.valueOf(price), menuGroup.getId(), menuProducts);
+            final Menu menu = new Menu(null, "메뉴", BigDecimal.valueOf(price), savedMenuGroupId, menuProducts);
             final Menu savedMenu = menuService.create(menu);
 
-            assertSoftly(softAssertions -> {
-                assertThat(savedMenu.getId()).isNotNull();
-                assertThat(savedMenu.getName()).isEqualTo(menu.getName());
-                assertThat(savedMenu.getPrice()).isEqualByComparingTo(menu.getPrice());
-                assertThat(savedMenu.getMenuGroupId()).isEqualTo(menu.getMenuGroupId());
-                assertThat(savedMenu.getMenuProducts())
-                        .usingRecursiveComparison()
-                        .ignoringFields("seq", "menuId")
-                        .isEqualTo(menuProducts);
+            assertSoftly(softly -> {
+                softly.assertThat(savedMenu.getId()).isNotNull();
+                softly.assertThat(savedMenu.getName()).isEqualTo(menu.getName());
+                softly.assertThat(savedMenu.getPrice()).isEqualByComparingTo(menu.getPrice());
+                softly.assertThat(savedMenu.getMenuGroupId()).isEqualTo(menu.getMenuGroupId());
             });
         }
 
         @Test
         void 메뉴_이름이_없으면_예외가_발생한다() {
-            final Menu menu = 메뉴(null, BigDecimal.ONE, menuGroup.getId(), menuProducts);
+            final Menu menu = new Menu(null, null, BigDecimal.ONE, savedMenuGroupId, menuProducts);
 
             assertThatThrownBy(() -> menuService.create(menu))
                     .isInstanceOf(IllegalArgumentException.class);
@@ -119,7 +104,7 @@ class MenuServiceTest {
 
         @Test
         void 메뉴_가격이_없으면_예외가_발생한다() {
-            final Menu menu = 메뉴("메뉴", null, menuGroup.getId(), menuProducts);
+            final Menu menu = new Menu(null, "메뉴", null, savedMenuGroupId, menuProducts);
 
             assertThatThrownBy(() -> menuService.create(menu))
                     .isInstanceOf(IllegalArgumentException.class);
@@ -127,7 +112,7 @@ class MenuServiceTest {
 
         @Test
         void 메뉴_이름이_256자_이상이면_예외가_발생한다() {
-            final Menu menu = 메뉴("메".repeat(256), BigDecimal.ONE, menuGroup.getId(), menuProducts);
+            final Menu menu = new Menu(null, "메".repeat(256), BigDecimal.ONE, savedMenuGroupId, menuProducts);
 
             assertThatThrownBy(() -> menuService.create(menu))
                     .isInstanceOf(IllegalArgumentException.class);
@@ -135,7 +120,7 @@ class MenuServiceTest {
 
         @Test
         void 메뉴_가격이_0원_보다_작으면_예외가_발생한다() {
-            final Menu menu = 메뉴("메뉴", BigDecimal.valueOf(-1), menuGroup.getId(), menuProducts);
+            final Menu menu = new Menu(null, "메뉴", BigDecimal.valueOf(-1), savedMenuGroupId, menuProducts);
 
             assertThatThrownBy(() -> menuService.create(menu))
                     .isInstanceOf(IllegalArgumentException.class);
@@ -143,10 +128,10 @@ class MenuServiceTest {
 
         @Test
         void 메뉴_가격이_1000조_이상이면_예외가_발생한다() {
-            final Product product = productService.create(상품("상품1", BigDecimal.valueOf(MAX_PRICE)));
-            menuProducts.add(메뉴_상품(product, 1));
+            final Product product = productService.create(new Product(null, "상품", BigDecimal.valueOf(999_999_999_999_999.99)));
+            menuProducts.add(new MenuProduct(null, null, product.getId(), 1));
 
-            final Menu menu = 메뉴("메뉴", BigDecimal.valueOf(Math.pow(10, 16)), menuGroup.getId(), menuProducts);
+            final Menu menu = new Menu(null, "메뉴", BigDecimal.valueOf(Math.pow(10, 16)), savedMenuGroupId, menuProducts);
 
             assertThatThrownBy(() -> menuService.create(menu))
                     .isInstanceOf(IllegalArgumentException.class);
@@ -154,7 +139,7 @@ class MenuServiceTest {
 
         @Test
         void 메뉴_그룹이_없으면_예외가_발생한다() {
-            final Menu menu = 메뉴("메뉴", BigDecimal.ONE, null, menuProducts);
+            final Menu menu = new Menu(null, "메뉴", BigDecimal.ONE, null, menuProducts);
 
             assertThatThrownBy(() -> menuService.create(menu))
                     .isInstanceOf(IllegalArgumentException.class);
@@ -162,7 +147,7 @@ class MenuServiceTest {
 
         @Test
         void 메뉴_그룹이_존재하지_않으면_예외가_발생한다() {
-            final Menu menu = 메뉴("메뉴", BigDecimal.ONE, menuGroup.getId() + 1, menuProducts);
+            final Menu menu = new Menu(null, "메뉴", BigDecimal.ONE, savedMenuGroupId + 1, menuProducts);
 
             assertThatThrownBy(() -> menuService.create(menu))
                     .isInstanceOf(IllegalArgumentException.class);
@@ -170,11 +155,11 @@ class MenuServiceTest {
 
         @Test
         void 메뉴_상품이_존재하지_않으면_예외가_발생한다() {
-            final Product product = productService.create(상품("상품1", BigDecimal.ONE));
+            final Product product = productService.create(new Product(null, "상품", BigDecimal.ONE));
             product.setId(product.getId() + 1);
-            menuProducts.add(메뉴_상품(product, 1));
+            menuProducts.add(new MenuProduct(null, null, product.getId(), 1));
 
-            final Menu menu = 메뉴("메뉴", BigDecimal.ZERO, menuGroup.getId(), menuProducts);
+            final Menu menu = new Menu(null, "메뉴", BigDecimal.ZERO, savedMenuGroupId, menuProducts);
 
             assertThatThrownBy(() -> menuService.create(menu))
                     .isInstanceOf(IllegalArgumentException.class);
@@ -185,18 +170,12 @@ class MenuServiceTest {
     void 메뉴의_목록을_조회한다() {
         final List<Menu> expected = menuService.list();
         for (int i = 0; i < 3; i++) {
-            final Menu menu = 메뉴("메뉴" + i, BigDecimal.ONE, menuGroup.getId(), menuProducts);
+            final Menu menu = new Menu(null, "메뉴" + i, BigDecimal.ONE, savedMenuGroupId, menuProducts);
             expected.add(menuService.create(menu));
         }
 
         final List<Menu> result = menuService.list();
 
-        assertSoftly(softAssertions -> {
-            assertThat(result).hasSize(expected.size());
-            assertThat(result)
-                    .usingRecursiveComparison()
-                    .ignoringFields("id")
-                    .isEqualTo(expected);
-        });
+        assertThat(result).hasSize(expected.size());
     }
 }
