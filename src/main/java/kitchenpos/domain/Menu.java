590c3dad0ee86a36ae125b60410d5f4818db7134
sosow0212 <sosow0212@naver.com>
2023-10-13 13:58:37 +0900

    refactor: setter를 사용하지 않도록 menu 개선

--- src/main/java/kitchenpos/domain/Menu.java
+++ src/main/java/kitchenpos/domain/Menu.java
@@ -12,7 +12,14 @@ public class Menu {
     private Long menuGroupId;
     private List<MenuProduct> menuProducts;
 
-    public Menu() {
+    protected Menu() {
+    }
+
+    public Menu(final Long id, final String name, final BigDecimal price, final Long menuGroupId) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.menuGroupId = menuGroupId;
     }
 
     public Menu(final String name, final BigDecimal price, final Long menuGroupId, final List<MenuProduct> menuProducts) {
@@ -22,44 +29,28 @@ public class Menu {
         this.menuProducts = menuProducts;
     }
 
-    public Long getId() {
-        return id;
+    public void addMenuProducts(final List<MenuProduct> menuProducts) {
+        this.menuProducts = menuProducts;
     }
 
-    public void setId(final Long id) {
-        this.id = id;
+    public Long getId() {
+        return id;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(final String name) {
-        this.name = name;
-    }
-
     public BigDecimal getPrice() {
         return price;
-    }
-
-    public void setPrice(final BigDecimal price) {
-        this.price = price;
     }
 
     public Long getMenuGroupId() {
         return menuGroupId;
     }
 
-    public void setMenuGroupId(final Long menuGroupId) {
-        this.menuGroupId = menuGroupId;
-    }
-
     public List<MenuProduct> getMenuProducts() {
         return menuProducts;
-    }
-
-    public void setMenuProducts(final List<MenuProduct> menuProducts) {
-        this.menuProducts = menuProducts;
     }
 
     @Override
