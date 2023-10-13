af99a6acf404f43fa5d955671e9f4fc538ed9d57
kiarakim <kiarakim413@gmail.com>
2023-10-13 18:56:54 +0900

    test: OrderService 테스트 작성

--- src/test/java/kitchenpos/fixture/OrderFixture.java
+++ src/test/java/kitchenpos/fixture/OrderFixture.java
@@ -8,7 +8,12 @@ import java.util.List;
 
 public class OrderFixture {
 
-    public static Order 주문_상품_없이_생성(Long orderId, String orderStatus, LocalDateTime orderedTime, List<OrderLineItem> orderLineItems) {
-        return new Order(orderId, orderStatus, orderedTime, orderLineItems);
+    public static Order 주문_상품_없이_생성(Long orderTableId, String orderStatus, LocalDateTime orderedTime, List<OrderLineItem> orderLineItems) {
+        return new Order(orderTableId, orderStatus, orderedTime, orderLineItems);
+    }
+
+    public static Order 주문_상태_변경(Order order, String orderStatus) {
+        order.setOrderStatus(orderStatus);
+        return order;
     }
 }
