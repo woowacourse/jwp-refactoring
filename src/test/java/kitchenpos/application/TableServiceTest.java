dec60e93df196cf83670e1ec72378eaa73988e2d
jaeyeon kim <jakind@naver.com>
2023-10-13 10:58:00 +0900

    test : Order Table 조회 테스트 추가

--- src/test/java/kitchenpos/application/TableServiceTest.java
+++ src/test/java/kitchenpos/application/TableServiceTest.java
@@ -2,9 +2,13 @@ package kitchenpos.application;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 import kitchenpos.domain.OrderTable;
 import kitchenpos.fixture.OrderTableFixture;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -28,6 +32,37 @@ class TableServiceTest extends ServiceIntegrationTest {
         assertThat(savedOrderTableId).usingRecursiveComparison()
                 .ignoringFields("id")
                 .isEqualTo(orderTable);
+    }
+
+    @Test
+    void Order_Table_목록을_반환한다() {
+        // given
+        List<OrderTable> orderTables = List.of(
+                OrderTableFixture.테이블_그룹이_없는_주문_테이블_생성(1, false),
+                OrderTableFixture.테이블_그룹이_없는_주문_테이블_생성(1, false),
+                OrderTableFixture.테이블_그룹이_없는_주문_테이블_생성(1, false)
+        );
+        List<OrderTable> savedOrderTables = new ArrayList<>();
+        for (OrderTable orderTable : orderTables) {
+            savedOrderTables.add(tableService.create(orderTable));
+        }
+
+        // when
+        List<OrderTable> orderTablesExcludeExistingData = tableService.list()
+                .stream()
+                .filter(orderTable ->
+                        containsObjects(
+                                savedOrderTables,
+                                orderTableInSavedOrderTables -> orderTableInSavedOrderTables.getId()
+                                        .equals(orderTable.getId())
+                        )
+                )
+                .collect(Collectors.toList());
+
+        // then
+        assertThat(orderTablesExcludeExistingData).usingRecursiveComparison()
+                .ignoringFields("id")
+                .isEqualTo(orderTables);
     }
 
 }
