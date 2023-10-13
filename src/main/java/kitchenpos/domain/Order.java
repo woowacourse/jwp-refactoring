f71ed1a612d0cd62e997ef83580cb8e37c8ef85e
kevstevie <109793396+kevstevie@users.noreply.github.com>
2023-10-13 22:41:45 +0900

    레거시리팩토링 step1 주드 미션 제출합니다 (#463)
    * docs: 기능 목록 작성
    
    * test: fake dao 작성
    
    * test: menuGroupService test 작성
    
    * test: MenuServiceTest 작성
    
    * feat: 생성자 추가
    
    * test: orderServiceTest 작성
    
    * test: productServiceTest 작성
    
    * test: exists 메서드 로직 수정
    
    * test: tableService test 작성
    
    * test: tableGroupService test 작성
    
    * test: 컬렉션 static 제거
    
    * refactor: 기능 요구 사항 및 test 추가

--- src/main/java/kitchenpos/domain/Order.java
+++ src/main/java/kitchenpos/domain/Order.java
@@ -10,6 +10,17 @@ public class Order {
     private LocalDateTime orderedTime;
     private List<OrderLineItem> orderLineItems;
 
+    public Order() {
+    }
+
+    public Order(Long id, Long orderTableId, String orderStatus, LocalDateTime orderedTime, List<OrderLineItem> orderLineItems) {
+        this.id = id;
+        this.orderTableId = orderTableId;
+        this.orderStatus = orderStatus;
+        this.orderedTime = orderedTime;
+        this.orderLineItems = orderLineItems;
+    }
+
     public Long getId() {
         return id;
     }
