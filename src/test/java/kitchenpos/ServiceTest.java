e88843d313bccc415672c16c02954cf5987cf1ca
yeonkkk <tjddus0302@gmail.com>
2023-10-14 17:04:03 +0900

    refactor: ServiceTest에 애너테이션 추가

--- src/test/java/kitchenpos/ServiceTest.java
+++ src/test/java/kitchenpos/ServiceTest.java
@@ -6,12 +6,16 @@ import kitchenpos.dao.OrderDao;
 import kitchenpos.dao.OrderTableDao;
 import kitchenpos.dao.ProductDao;
 import kitchenpos.dao.TableGroupDao;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
-
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 @SpringBootTest
-public class ServiceTest {
+@Transactional
+public abstract class ServiceTest {
 
     @Autowired
     protected ProductDao productDao;
