c74122c98edce840eca9f67c672d5fc8b6ff3f7d
greeng00se <thegreengoose7@gmail.com>
2023-10-14 23:44:16 +0900

    feat: Auditing을 위한 설정 추가

---
+++ src/main/java/kitchenpos/common/JpaConfig.java
@@ -0,0 +1,9 @@
+package kitchenpos.common;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+}
