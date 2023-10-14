16eefdd91b26d6bf2a1245261d30685182313f90
우가 <dnr3655@naver.com>
2023-10-14 19:40:26 +0900

    [레거시 코드 리팩토링 미션 - 1단계] 우가(윤정욱) 미션 제출합니다. (#495)
    * docs: 요구사항 명세 정리
    
    * chore: test 격리를 위해 설정 추가
    
    * test: ProductServiceTest 추가
    
    * test: MenuServiceTest 추가
    
    * test: MenuGroupServiceTest 추가
    
    * test: TableServiceTest 추가
    
    * test: TableGroupServiceTest 추가
    
    * test: TableGroupServiceTest 케이스 추가
    
    * test: OrderServiceTest 테스트 추가
    
    * refactor: 리뷰 반영
    
    * refactor: 통합테스트 환경 NONE 으로 수정

---
+++ src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -0,0 +1,69 @@
+package kitchenpos.application;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.math.BigDecimal;
+import java.util.List;
+import kitchenpos.domain.Product;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@Sql(scripts = {"classpath:truncate.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+class ProductServiceTest {
+
+    @Autowired
+    private ProductService productService;
+
+    @Test
+    void 상품을_등록할_수_있다() {
+        // given
+        final Product product = new Product();
+        product.setName("테스트 상품");
+        product.setPrice(new BigDecimal(2000));
+
+        // when
+        final Product result = productService.create(product);
+
+        // then
+        assertThat(result.getName()).isEqualTo(product.getName());
+        assertThat(result.getPrice()).isEqualByComparingTo(product.getPrice());
+    }
+
+    @NullSource
+    @ValueSource(strings = {"-1"})
+    @ParameterizedTest
+    void 상품가격이_없거나_0보다_작을경우_예외가_발생한다(final BigDecimal price) {
+        // given
+        final Product product = new Product();
+        product.setName("테스트 상품");
+        product.setPrice(price);
+
+        // when then
+        assertThatThrownBy(() -> productService.create(product))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 상품조회를_할_수_있다() {
+        // given
+        final Product product = new Product();
+        product.setName("테스트 상품");
+        product.setPrice(new BigDecimal(2000));
+        productService.create(product);
+
+        // when
+        final List<Product> results = productService.list();
+
+        // then
+        assertThat(results).hasSize(1);
+        assertThat(results.get(0).getName()).isEqualTo(product.getName());
+        assertThat(results.get(0).getPrice()).isEqualByComparingTo(product.getPrice());
+    }
+}
---
+++ src/test/java/kitchenpos/application/TableGroupServiceTest.java
@@ -0,0 +1,155 @@
+package kitchenpos.application;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import kitchenpos.dao.OrderDao;
+import kitchenpos.dao.OrderTableDao;
+import kitchenpos.domain.Order;
+import kitchenpos.domain.OrderStatus;
+import kitchenpos.domain.OrderTable;
+import kitchenpos.domain.TableGroup;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@Sql(scripts = {"classpath:truncate.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+class TableGroupServiceTest {
+
+    @Autowired
+    private TableGroupService tableGroupService;
+
+    @Autowired
+    private OrderTableDao orderTableDao;
+
+    @Autowired
+    private OrderDao orderDao;
+
+    private OrderTable firstTable;
+    private OrderTable secondTable;
+
+    @BeforeEach
+    void init() {
+        final OrderTable firstOrderTable = new OrderTable();
+        firstOrderTable.setEmpty(true);
+        firstOrderTable.setNumberOfGuests(0);
+        firstTable = orderTableDao.save(firstOrderTable);
+
+        final OrderTable secondOrderTable = new OrderTable();
+        secondOrderTable.setEmpty(true);
+        secondOrderTable.setNumberOfGuests(0);
+        secondTable = orderTableDao.save(secondOrderTable);
+    }
+
+    @Test
+    void 테이블_그룹을_생성한다() {
+        // given
+        final TableGroup tableGroup = new TableGroup();
+        tableGroup.setOrderTables(List.of(firstTable, secondTable));
+
+        // when
+        final TableGroup savedTableGroup = tableGroupService.create(tableGroup);
+
+        // then
+        final List<OrderTable> updatedOrderTables = savedTableGroup.getOrderTables();
+        final List<Long> updatedOrderTableIds = updatedOrderTables.stream().map(OrderTable::getId)
+                .collect(Collectors.toList());
+
+        assertThat(updatedOrderTables).hasSize(2);
+        assertThat(updatedOrderTableIds).containsExactly(firstTable.getId(), secondTable.getId());
+    }
+
+    @Test
+    void 테이블_그룹_생성_시_테이블_리스트가_비어있으면_예외가_발생한다() {
+        // given
+        final TableGroup tableGroup = new TableGroup();
+
+        // when
+        tableGroup.setOrderTables(Collections.emptyList());
+
+        // then
+        assertThatThrownBy(() -> tableGroupService.create(tableGroup))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 테이블_그룹_생성_시_테이블_리스트가_1개만_있으면_예외가_발생한다() {
+        // given
+        final TableGroup tableGroup = new TableGroup();
+
+        // then
+        tableGroup.setOrderTables(List.of(firstTable));
+
+        // then
+        assertThatThrownBy(() -> tableGroupService.create(tableGroup))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 테이블_그룹_생성_시_이미_다른_그룹에_속한경우_예외가_발생한다() {
+        // given
+        final TableGroup firstTableGroup = new TableGroup();
+        firstTableGroup.setOrderTables(List.of(firstTable, secondTable));
+
+        final TableGroup savedFirstTableGroup = tableGroupService.create(firstTableGroup);
+
+        firstTable.setTableGroupId(savedFirstTableGroup.getId());
+        secondTable.setTableGroupId(savedFirstTableGroup.getId());
+
+        // when
+        final TableGroup newTableGroup = new TableGroup();
+        newTableGroup.setOrderTables(List.of(firstTable, secondTable));
+
+        // then
+        assertThatThrownBy(() -> tableGroupService.create(newTableGroup))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 테이블_그룹을_해제한다() {
+        // given
+        final TableGroup tableGroup = new TableGroup();
+        tableGroup.setOrderTables(List.of(firstTable, secondTable));
+        final TableGroup savedTableGroup = tableGroupService.create(tableGroup);
+
+        // when
+        tableGroupService.ungroup(savedTableGroup.getId());
+
+        // then
+        final OrderTable ungroupedOrderTable = orderTableDao.findAll().stream()
+                .filter(orderTable -> orderTable.getId().equals(firstTable.getId()))
+                .findFirst()
+                .get();
+
+        assertThat(ungroupedOrderTable.getTableGroupId()).isNull();
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = OrderStatus.class, names = {"COOKING", "MEAL"})
+    void 조리_또는_식사_중인_테이블_그룹을_해제할_때_예외가_발생한다(final OrderStatus orderStatus) {
+        // given
+        final TableGroup tableGroup = new TableGroup();
+        tableGroup.setOrderTables(List.of(firstTable, secondTable));
+        final TableGroup createdGroup = tableGroupService.create(tableGroup);
+
+        // when
+        final Order order = new Order();
+        order.setOrderStatus(orderStatus.name());
+        order.setOrderTableId(firstTable.getId());
+        order.setOrderedTime(LocalDateTime.now());
+        orderDao.save(order);
+
+        // then
+        assertThatThrownBy(() -> tableGroupService.ungroup(createdGroup.getId()))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
