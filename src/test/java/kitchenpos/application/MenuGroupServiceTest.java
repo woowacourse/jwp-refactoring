2563706a13643cb1ea6c50f920d4e7063015e360
zillionme <beatus96@naver.com>
2023-10-14 21:58:30 +0900

    test: MenuGroupServiceTest 테스트 수정

--- src/test/java/kitchenpos/application/MenuGroupServiceTest.java
+++ src/test/java/kitchenpos/application/MenuGroupServiceTest.java
@@ -5,12 +5,11 @@ import kitchenpos.domain.MenuGroup;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+import static kitchenpos.fixture.MenuGroupFixture.MENU_GROUP;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SuppressWarnings("NonAsciiCharacters")
@@ -27,34 +26,31 @@ class MenuGroupServiceTest {
     @Test
     void 메뉴그룹을_생성한다() {
         //given
-        MenuGroup menuGroup = new MenuGroup();
-        menuGroup.setName("중국식 메뉴 그룹");
+        MenuGroup expected = MENU_GROUP("중국식 메뉴 그룹");
 
         //when
-        MenuGroup savedMenuGroup = menuGroupService.create(menuGroup);
+        MenuGroup actual = menuGroupService.create(expected);
 
         //then
-        assertThat(savedMenuGroup.getName())
-                .isEqualTo(menuGroup.getName());
+        assertThat(actual.getName())
+                .isEqualTo(expected.getName());
     }
 
     @Test
     void 메뉴_그룹_목록을_조회한다() {
         //given
-        MenuGroup menuGroup1 = new MenuGroup();
-        menuGroup1.setName("중국식 메뉴 그룹");
+        MenuGroup menuGroup1 = MENU_GROUP("중국식 메뉴 그룹");
         MenuGroup savedMenuGroup1 = menuGroupDao.save(menuGroup1);
-        MenuGroup menuGroup2 = new MenuGroup();
-        menuGroup2.setName("한식 메뉴 그룹");
+        MenuGroup menuGroup2 = MENU_GROUP("한식 메뉴 그룹");
         MenuGroup savedMenuGroup2 = menuGroupDao.save(menuGroup2);
 
         //when
         List<MenuGroup> queriedMenuGroups = menuGroupService.list();
 
         //then
-        List<MenuGroup> savedMenuGroup = List.of(savedMenuGroup1, savedMenuGroup2);
+        List<MenuGroup> expected = List.of(savedMenuGroup1, savedMenuGroup2);
         assertThat(queriedMenuGroups)
                 .usingRecursiveComparison()
-                .isEqualTo(savedMenuGroup);
+                .isEqualTo(expected);
     }
 }
