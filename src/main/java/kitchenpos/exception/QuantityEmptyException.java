c4ba443a99c86a2c0c23d2d4c0124036636d4f2f
sosow0212 <sosow0212@naver.com>
2023-10-15 15:18:35 +0900

    feat: 수량 원시값 포장

---
+++ src/main/java/kitchenpos/exception/QuantityEmptyException.java
@@ -0,0 +1,8 @@
+package kitchenpos.exception;
+
+public class QuantityEmptyException extends RuntimeException {
+
+    public QuantityEmptyException() {
+        super("수량은 0개 이상이어야 합니다.");
+    }
+}
---
+++ src/test/java/kitchenpos/domain/QuantityTest.java
@@ -0,0 +1,23 @@
+package kitchenpos.domain;
+
+import kitchenpos.exception.QuantityEmptyException;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@SuppressWarnings("NonAsciiCharacters")
+class QuantityTest {
+
+    @Test
+    void 수량은_0개_미만이면_예외를_발생한다() {
+        // given
+        Long quantity = -1L;
+
+        // when & then
+        assertThatThrownBy(() -> Quantity.from(quantity))
+                .isInstanceOf(QuantityEmptyException.class);
+    }
+}
