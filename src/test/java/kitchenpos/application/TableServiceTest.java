9f1b7af607a36d69203066367b5a02484b15504b
SproutMj <mjk54565@gmail.com>
2023-10-15 20:22:53 +0900

    test: TableServiceTest를 SpringBootTest로 변

--- src/test/java/kitchenpos/application/TableServiceTest.java
+++ src/test/java/kitchenpos/application/TableServiceTest.java
@@ -1,183 +1,249 @@
 package kitchenpos.application;
 
-import kitchenpos.dao.OrderDao;
-import kitchenpos.dao.OrderTableDao;
-import kitchenpos.domain.OrderStatus;
+import kitchenpos.ServiceTest;
+import kitchenpos.domain.Order;
 import kitchenpos.domain.OrderTable;
-import org.junit.jupiter.api.BeforeEach;
+import kitchenpos.domain.TableGroup;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.Arrays;
+import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
-import static org.mockito.BDDMockito.given;
 
-@ExtendWith(MockitoExtension.class)
-class TableServiceTest {
+class TableServiceTest extends ServiceTest {
 
-    @Mock
-    private OrderDao orderDao;
-    @Mock
-    private OrderTableDao orderTableDao;
-
-    @InjectMocks
+    @Autowired
     TableService tableService;
 
-    private OrderTable orderTable;
+    @DisplayName("주문 테이블 생성 테스트")
+    @Nested
+    class TableCreateTest {
 
-    @BeforeEach
-    void beforeEach() {
-        orderTable = new OrderTable();
-        orderTable.setId(1L);
-        orderTable.setNumberOfGuests(3);
-        orderTable.setEmpty(false);
-        orderTable.setTableGroupId(null);
+        @DisplayName("주문 테이블을 생성한다.")
+        @Test
+        void orderTableCreate() {
+            //given
+            final OrderTable orderTable = new OrderTable();
+            orderTable.setNumberOfGuests(0);
+            orderTable.setEmpty(true);
+
+            //when
+            final OrderTable actual = tableService.create(orderTable);
+
+            //then
+            assertSoftly(softly -> {
+                softly.assertThat(actual.getId()).isNotNull();
+                softly.assertThat(actual.getTableGroupId()).isNull();
+                softly.assertThat(actual.getNumberOfGuests()).isEqualTo(orderTable.getNumberOfGuests());
+                softly.assertThat(actual.isEmpty()).isEqualTo(orderTable.isEmpty());
+            });
+        }
     }
 
-    @DisplayName("order table을 생성한다.")
-    @Test
-    void orderTable을_생성() {
-        //given
-        final OrderTable expected = orderTable;
-        given(orderTableDao.save(expected))
-                .willReturn(expected);
+    @DisplayName("주문 테이블 조회 테스트")
+    @Nested
+    class OrderTableFindTest {
 
-        //when
-        final OrderTable actual = tableService.create(expected);
+        @DisplayName("전체 주문 테이블을 조회한다.")
+        @Test
+        void orderTableFindAll() {
+            //given
+            OrderTable orderTable = new OrderTable();
+            orderTable.setEmpty(true);
+            orderTable.setNumberOfGuests(0);
+            orderTable = testFixtureBuilder.buildOrderTable(orderTable);
 
-        //then
-        assertSoftly(softly -> {
-            softly.assertThat(actual).isEqualTo(expected);
-            softly.assertThat(actual.getTableGroupId()).isNull();
-        });
+            //when
+            final List<OrderTable> actual = tableService.list();
+
+            //then
+            final Long orderTableId = orderTable.getId();
+            assertSoftly(softly -> {
+                softly.assertThat(actual.size()).isEqualTo(1);
+                softly.assertThat(actual.get(0).getId()).isEqualTo(orderTableId);
+            });
+        }
     }
 
-    @DisplayName("전체 orderTable을 조회한다.")
-    @Test
-    void 전체_orderTable_조회() {
-        //given
-        final List<OrderTable> orderTables = List.of(orderTable);
-        given(orderTableDao.findAll())
-                .willReturn(orderTables);
 
-        //when
-        final List<OrderTable> actual = tableService.list();
+    @DisplayName("주문 테이블 비어있는지 변경 테스트")
+    @Nested
+    class OrderTableChangeEmptyTest {
 
-        assertSoftly(softly -> {
-            softly.assertThat(actual.size()).isEqualTo(orderTables.size());
-        });
+        @DisplayName("주문 테이블이 비어있는지를 변경한다.")
+        @ParameterizedTest
+        @ValueSource(booleans = {true, false})
+        void orderTableChangeEmpty(final boolean isEmpty) {
+            //given
+            OrderTable orderTable = new OrderTable();
+            orderTable.setEmpty(isEmpty);
+            orderTable.setTableGroupId(null);
+            orderTable.setNumberOfGuests(0);
+            orderTable = testFixtureBuilder.buildOrderTable(orderTable);
+
+            final OrderTable changingStatus = new OrderTable();
+            changingStatus.setEmpty(!isEmpty);
+
+            //when
+            final OrderTable actual = tableService.changeEmpty(orderTable.getId(), changingStatus);
+
+            //then
+            assertSoftly(softly -> {
+                softly.assertThat(actual.isEmpty()).isEqualTo(changingStatus.isEmpty());
+            });
+        }
+
+        @DisplayName("주문 테이블이 비어있는지를 변경할 때 주문 테이블이 없으면 실패한다.")
+        @Test
+        void orderTableChangeEmptyFailWhenNotExistTableGroup() {
+            //given
+            final long notExistOrderTableId = -1L;
+            final OrderTable changingStatus = new OrderTable();
+            changingStatus.setEmpty(true);
+
+            // when & then
+            assertThatThrownBy(() -> tableService.changeEmpty(notExistOrderTableId, changingStatus))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @DisplayName("주문 테이블이 비어있는지를 변경할 때 단체 지정 id가 null이 아니면 실패한다.")
+        @Test
+        void orderTableChangeEmptyFailWhenTableGroupIdIsNotNull() {
+            //given
+            TableGroup tableGroup = new TableGroup();
+            tableGroup.setOrderTables(Collections.emptyList());
+            tableGroup.setCreatedDate(LocalDateTime.now());
+            tableGroup = testFixtureBuilder.buildTableGroup(tableGroup);
+
+            OrderTable orderTable = new OrderTable();
+            orderTable.setEmpty(true);
+            orderTable.setTableGroupId(tableGroup.getId());
+            orderTable.setNumberOfGuests(0);
+            orderTable = testFixtureBuilder.buildOrderTable(orderTable);
+
+            final OrderTable changingStatus = new OrderTable();
+            changingStatus.setEmpty(!orderTable.isEmpty());
+
+            // when & then
+            final Long orderTableId = orderTable.getId();
+            assertThatThrownBy(() -> tableService.changeEmpty(orderTableId, changingStatus))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @DisplayName("주문 테이블이 비어있는지를 변경할 때 주문 상태가 조리나 식사면 실패한다.")
+        @ParameterizedTest
+        @ValueSource(strings = {"COOKING", "MEAL"})
+        void orderTableChangeEmptyFailWhenOrderStatusNotCompletion(final String orderStatus) {
+            //given
+            OrderTable notCompletionOrdertable = new OrderTable();
+            notCompletionOrdertable.setEmpty(false);
+            notCompletionOrdertable.setTableGroupId(null);
+            notCompletionOrdertable.setNumberOfGuests(3);
+            notCompletionOrdertable = testFixtureBuilder.buildOrderTable(notCompletionOrdertable);
+
+            final Order notCompletionOrder = new Order();
+            notCompletionOrder.setOrderStatus(orderStatus);
+            notCompletionOrder.setOrderedTime(LocalDateTime.now());
+            notCompletionOrder.setOrderTableId(notCompletionOrdertable.getId());
+            testFixtureBuilder.buildOrder(notCompletionOrder);
+
+            final OrderTable changingStatus = new OrderTable();
+            changingStatus.setEmpty(!notCompletionOrdertable.isEmpty());
+
+            // when & then
+            final Long orderTableId = notCompletionOrdertable.getId();
+            assertThatThrownBy(() -> tableService.changeEmpty(orderTableId, changingStatus))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
     }
 
-    @DisplayName("orderTable의 empty를 변경한다.")
-    @Test
-    void orderTable_empty_변경() {
-        //given
-        given(orderTableDao.findById(orderTable.getId()))
-                .willReturn(Optional.of(orderTable));
-        given(orderDao.existsByOrderTableIdAndOrderStatusIn(orderTable.getId(), Arrays.asList(OrderStatus.COOKING.name(), OrderStatus.MEAL.name())))
-                .willReturn(false);
-        given(orderTableDao.save(orderTable))
-                .willReturn(orderTable);
+    @DisplayName("주문 테이블 고객 수 변경")
+    @Nested
+    class orderTableChangeNumberOfGuestsTest {
 
-        final boolean expected = !orderTable.isEmpty();
-        final OrderTable parameter = new OrderTable();
-        parameter.setEmpty(expected);
+        @DisplayName("주문 테이블의 고객 수를 변경한다.")
+        @Test
+        void orderTableChangeNumberOfGuests() {
+            //given
+            OrderTable orderTable = new OrderTable();
+            orderTable.setEmpty(false);
+            final int beforeGuests = 10;
+            orderTable.setNumberOfGuests(beforeGuests);
+            orderTable = testFixtureBuilder.buildOrderTable(orderTable);
 
-        //when
-        final OrderTable actual = tableService.changeEmpty(orderTable.getId(), parameter);
+            final OrderTable afterGuests = new OrderTable();
+            afterGuests.setNumberOfGuests(beforeGuests + 100);
 
-        //then
-        assertSoftly(softly -> {
-            softly.assertThat(actual.isEmpty()).isEqualTo(expected);
-        });
-    }
+            //when
+            final OrderTable actual = tableService.changeNumberOfGuests(orderTable.getId(), afterGuests);
 
-    @DisplayName("orderTable의 empty를 변경시 tableGroupId가 null이 아니면 실패한다.")
-    @Test
-    void tableGroupId가_null이_아니면_예외() {
-        //given
-        given(orderTableDao.findById(orderTable.getId()))
-                .willReturn(Optional.of(orderTable));
+            //then
+            assertSoftly(softly -> {
+                softly.assertThat(actual.getNumberOfGuests()).isEqualTo(afterGuests.getNumberOfGuests());
+            });
+        }
 
-        final boolean expected = !orderTable.isEmpty();
-        final OrderTable parameter = new OrderTable();
-        parameter.setEmpty(expected);
-        orderTable.setTableGroupId(1L);
+        @DisplayName("변경할 고객 수가 음수면 실패한다.")
+        @Test
+        void orderTableChangeNumberOfGuestsFailWhenChangingGuestsLessThenZero() {
+            //given
+            OrderTable orderTable = new OrderTable();
+            orderTable.setEmpty(false);
+            orderTable.setNumberOfGuests(3);
+            orderTable = testFixtureBuilder.buildOrderTable(orderTable);
 
-        // when & then
-        assertThatThrownBy(() -> tableService.changeEmpty(orderTable.getId(), parameter))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
+            final OrderTable guestsLessThenZero = new OrderTable();
+            guestsLessThenZero.setNumberOfGuests(-1);
 
-    @DisplayName("orderTable의 empty를 변경시 order 상태가 cooking이거나 meal이면 예외를 던진다.")
-    @Test
-    void order상태가_cooking이거나_meal이면_예외() {
-        //given
-        given(orderTableDao.findById(orderTable.getId()))
-                .willReturn(Optional.of(orderTable));
-        given(orderDao.existsByOrderTableIdAndOrderStatusIn(orderTable.getId(), Arrays.asList(OrderStatus.COOKING.name(), OrderStatus.MEAL.name())))
-                .willReturn(true);
+            // when & then
+            final Long orderTableId = orderTable.getId();
+            assertThatThrownBy(() -> tableService.changeNumberOfGuests(orderTableId, guestsLessThenZero))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
 
-        final boolean expected = !orderTable.isEmpty();
-        final OrderTable parameter = new OrderTable();
-        parameter.setEmpty(expected);
+        @DisplayName("변경할 주문 테이블이 존재하지 않으면 실패한다.")
+        @Test
+        void orderTableChangeNumberOfGuestsFailWhenNotExistOrderTable() {
+            //given
+            OrderTable orderTable = new OrderTable();
+            orderTable.setEmpty(true);
+            orderTable.setNumberOfGuests(3);
+            orderTable.setId(-1L);
 
-        // when & then
-        assertThatThrownBy(() -> tableService.changeEmpty(orderTable.getId(), parameter))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
+            final OrderTable changingGuests = new OrderTable();
+            changingGuests.setNumberOfGuests(5);
 
-    @DisplayName("orderTable의 guest 수를 변경한다.")
-    @Test
-    void orderTable의_guest_수_변경() {
-        //given
-        given(orderTableDao.findById(orderTable.getId()))
-                .willReturn(Optional.of(orderTable));
-        given(orderTableDao.save(orderTable))
-                .willReturn(orderTable);
-        final OrderTable parameter = orderTable;
-        final int expected = 3;
-        parameter.setNumberOfGuests(expected);
+            // when & then
+            final Long orderTableId = orderTable.getId();
+            assertThatThrownBy(() -> tableService.changeNumberOfGuests(orderTableId, changingGuests))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
 
-        //when
-        final OrderTable actual = tableService.changeNumberOfGuests(parameter.getId(), parameter);
+        @DisplayName("주문 테이블이 비어있으면 실패한다.")
+        @Test
+        void orderTableChangeNumberOfGuestsFailWhenOrderTableIsEmpty() {
+            //given
+            OrderTable orderTable = new OrderTable();
+            orderTable.setEmpty(true);
+            orderTable.setNumberOfGuests(3);
+            orderTable = testFixtureBuilder.buildOrderTable(orderTable);
 
-        //then
-        assertSoftly(softly -> {
-            softly.assertThat(actual.getNumberOfGuests()).isEqualTo(expected);
-        });
-    }
+            final OrderTable changingGuests = new OrderTable();
+            changingGuests.setNumberOfGuests(5);
 
-    @DisplayName("orderTable의 guest 수를 변경시 음수면 예외를 던진다.")
-    @Test
-    void orderTable의_guest_수가_음수면_예외() {
-        //given
-        final OrderTable parameter = orderTable;
-        final int expected = -10;
-        parameter.setNumberOfGuests(expected);
-
-        // when & then
-        assertThatThrownBy(() -> tableService.changeNumberOfGuests(parameter.getId(), parameter))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("orderTable의 guest 수를 변경시 isEmpty가 true면 예외를 던진다.")
-    @Test
-    void orderTable의_isEmpty가_true면_예외() {
-        //given
-        final OrderTable expected = orderTable;
-        expected.setEmpty(true);
-
-        // when & then
-        assertThatThrownBy(() -> tableService.changeNumberOfGuests(expected.getId(), expected))
-                .isInstanceOf(IllegalArgumentException.class);
+            // when & then
+            final Long orderTableId = orderTable.getId();
+            assertThatThrownBy(() -> tableService.changeNumberOfGuests(orderTableId, changingGuests))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
     }
 }
