e66ddb0019c8ecc3431f6f170a21cb23f5296f88
HyeonjaeKwon <kwonhyeonjae01@gmail.com>
2023-10-15 13:39:54 +0900

    refactor: ProductServiceTest 중복 제거

--- src/test/java/kitchenpos/application/ProductServiceTest.java
+++ src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -4,12 +4,13 @@ import kitchenpos.dao.ProductDao;
 import kitchenpos.domain.Product;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.NullSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import support.fixture.ProductBuilder;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -28,13 +29,9 @@ class ProductServiceTest {
     private ProductService productService;
 
     private static Stream<List<Product>> should_return_product_list_when_request_list() {
-        final Product product1 = new Product();
-        product1.setName("상품1");
-        product1.setPrice(BigDecimal.valueOf(1000));
+        final Product product1 = new ProductBuilder().build();
 
-        final Product product2 = new Product();
-        product2.setName("상품2");
-        product2.setPrice(BigDecimal.valueOf(2000));
+        final Product product2 = new ProductBuilder().build();
 
         return Stream.of(
                 List.of(),
@@ -92,26 +89,13 @@ class ProductServiceTest {
                     .isEqualTo(expect);
         }
 
-        @Test
-        @DisplayName("상품 가격이 null일 경우 IllegalArgumentException이 발생한다.")
-        void should_throw_IllegalArgumentException_when_price_is_null() {
-            // given
-            final Product product = new Product();
-            product.setName("상품");
-            product.setPrice(null);
-
-            // when & then
-            assertThrowsExactly(IllegalArgumentException.class, () -> productService.create(product));
-        }
-
         @ParameterizedTest
+        @NullSource
         @CsvSource(value = {"-1", "-2", "-100000000000"})
-        @DisplayName("상품 가격이 0 미만일 경우 IllegalArgumentException이 발생한다.")
+        @DisplayName("상품 null이거나 가격이 0 미만일 경우 IllegalArgumentException이 발생한다.")
         void should_throw_IllegalArgumentException_when_price_is_smaller_then_zero(final BigDecimal price) {
             // given
-            final Product product = new Product();
-            product.setName("상품");
-            product.setPrice(price);
+            final Product product = new ProductBuilder().setPrice(price).build();
 
             // when & then
             assertThrowsExactly(IllegalArgumentException.class, () -> productService.create(product));
