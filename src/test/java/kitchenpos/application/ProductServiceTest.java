e16f0e7671ebe8d447f31dba2595867c775f734a
SproutMj <mjk54565@gmail.com>
2023-10-15 16:25:22 +0900

    test: productServiceTest를 SpringBootTest로 변경

--- src/test/java/kitchenpos/application/ProductServiceTest.java
+++ src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -1,95 +1,94 @@
 package kitchenpos.application;
 
-import kitchenpos.dao.ProductDao;
+import kitchenpos.ServiceTest;
 import kitchenpos.domain.Product;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.math.BigDecimal;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
-import static org.mockito.BDDMockito.given;
 
-@ExtendWith(MockitoExtension.class)
-class ProductServiceTest {
+class ProductServiceTest extends ServiceTest {
 
-    @Mock
-    private ProductDao productDao;
-
-    @InjectMocks
+    @Autowired
     private ProductService productService;
 
-    private Product product;
+    @DisplayName("상품 생성")
+    @Nested
+    class ProductCreateTest {
 
-    @BeforeEach
-    void beforeEach() {
-        product = new Product();
-        product.setPrice(new BigDecimal(1000));
-        product.setId(1L);
-        product.setName("product");
+        @DisplayName("상품을 생성한다.")
+        @Test
+        void productCreate() {
+            //given
+            final Product product = new Product();
+            product.setPrice(new BigDecimal(1000));
+            product.setName("product");
+
+            //when
+            final Product actual = productService.create(product);
+
+            //then
+            assertSoftly(softly -> {
+                softly.assertThat(actual.getId()).isNotNull();
+                softly.assertThat(actual.getPrice()).isEqualTo(product.getPrice());
+                softly.assertThat(actual.getName()).isEqualTo(product.getName());
+            });
+        }
+
+        @DisplayName("가격이 null이면 상품 생성을 실패한다.")
+        @Test
+        void productCreateFailWhenPriceIsNull() {
+            //given
+            final Product product = new Product();
+            product.setPrice(null);
+
+            // when & then
+            assertThatThrownBy(() -> productService.create(product))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @DisplayName("가격이 음수면 상품 생성을 실패한다.")
+        @Test
+        void productCreateFailWhenPriceLessThenZero() {
+            //given
+            final Product product = new Product();
+            product.setPrice(new BigDecimal(-1));
+
+            // when & then
+            assertThatThrownBy(() -> productService.create(product))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
     }
 
-    @DisplayName("product를 생성한다.")
-    @Test
-    void product_생성() {
-        //given
-        final Product expected = product;
-        given(productDao.save(expected))
-                .willReturn(expected);
+    @DisplayName("상품 조회 테스트")
+    @Nested
+    class ProductFindTest {
 
-        //when
-        final Product actual = productService.create(expected);
+        @DisplayName("상품을 전체 조회한다.")
+        @Test
+        void productFindAll() {
+            //given
+            Product product = new Product();
+            product.setPrice(new BigDecimal(1000));
+            product.setName("product");
+            final Product expected = testFixtureBuilder.buildProduct(product);
 
-        //then
-        assertSoftly(softly -> {
-            softly.assertThat(actual).isEqualTo(expected);
-        });
-    }
+            //when
+            final List<Product> actual = productService.list();
 
-    @DisplayName("price가 null이면 생성을 실패한다.")
-    @Test
-    void price가_null이면_예외() {
-        //given
-        final Product expected = product;
-        product.setPrice(null);
-
-        // when & then
-        assertThatThrownBy(() -> productService.create(expected))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("price가 음수면 생성을 실패한다.")
-    @Test
-    void price가_음수면_예외() {
-        //given
-        final Product expected = product;
-        product.setPrice(new BigDecimal(-100));
-
-        // when & then
-        assertThatThrownBy(() -> productService.create(expected))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("product를 전체 조회한다.")
-    @Test
-    void product_전체_조회() {
-        //given
-        given(productDao.findAll())
-                .willReturn(List.of(product));
-
-        //when
-        final List<Product> actual = productService.list();
-
-        assertSoftly(softly -> {
-            softly.assertThat(actual.size()).isEqualTo(1);
-            softly.assertThat(actual.get(0)).isEqualTo(product);
-        });
+            //then
+            assertSoftly(softly -> {
+                softly.assertThat(actual.size()).isEqualTo(1);
+                softly.assertThat(actual.get(0).getId()).isEqualTo(expected.getId());
+                softly.assertThat(actual.get(0).getPrice()).isEqualTo(expected.getPrice());
+                softly.assertThat(actual.get(0).getName()).isEqualTo(expected.getName());
+            });
+        }
     }
 }
