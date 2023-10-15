6f337359be58e935c4e2b056977d94c401d90828
sosow0212 <sosow0212@naver.com>
2023-10-15 15:15:23 +0900

    feat: price 원시값 포장

---
+++ src/main/java/kitchenpos/domain/Price.java
@@ -0,0 +1,39 @@
+package kitchenpos.domain;
+
+import kitchenpos.exception.PriceEmptyException;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+@Embeddable
+public class Price {
+
+    private static final int ZERO_PRICE = 0;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    protected Price() {
+    }
+
+    private Price(final BigDecimal price) {
+        this.price = price;
+    }
+
+    public static Price from(final Long price) {
+        validateEmptyPrice(price);
+        return new Price(BigDecimal.valueOf(price));
+    }
+
+    private static void validateEmptyPrice(final Long price) {
+        if (Objects.isNull(price) || price < ZERO_PRICE) {
+            throw new PriceEmptyException();
+        }
+    }
+
+    public Long getPrice() {
+        return price.longValue();
+    }
+}
