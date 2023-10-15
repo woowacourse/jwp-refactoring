643a0cec6d2df7ba0aaa90db036c654a6bc9c179
woo-chang <clllickme@naver.com>
2023-10-15 15:59:14 +0900

    test: 중복된 시나리오 분리

--- src/test/java/kitchenpos/application/OrderServiceTest.java
+++ src/test/java/kitchenpos/application/OrderServiceTest.java
@@ -58,16 +58,21 @@ class OrderServiceTest extends ServiceTest {
     @Nested
     class 주문_추가_시 {
 
-        @Test
-        void 정상적인_주문이라면_주문을_추가한다() {
-            //given
+        private Menu menu;
+
+        @BeforeEach
+        void setUp() {
             Product product = productDao.save(상품("텐동", BigDecimal.valueOf(11000)));
             MenuProduct menuProduct = 메뉴_상품(null, product.getId(), 1);
             MenuGroup menuGroup = menuGroupDao.save(메뉴_그룹("일식"));
-            Menu menu = menuDao.save(메뉴("텐동", BigDecimal.valueOf(11000), menuGroup.getId(), List.of(menuProduct)));
+            menu = menuDao.save(메뉴("텐동", BigDecimal.valueOf(11000), menuGroup.getId(), List.of(menuProduct)));
             menuProduct.setMenuId(menu.getId());
             menuProductDao.save(menuProduct);
+        }
 
+        @Test
+        void 정상적인_주문이라면_주문을_추가한다() {
+            //given
             OrderTable orderTable = orderTableDao.save(테이블(null, 10, false));
             OrderLineItem orderLineItem = 주문_메뉴_목록(orderTable.getId(), menu.getId(), 1);
             LocalDateTime orderedTime = LocalDateTime.now();
@@ -91,11 +96,6 @@ class OrderServiceTest extends ServiceTest {
         @Test
         void 주문_메뉴_목록이_비어있으면_예외를_던진다() {
             //given
-            Product product = 상품("텐동", BigDecimal.valueOf(11000));
-            MenuProduct menuProduct = 메뉴_상품(null, product.getId(), 1);
-            MenuGroup menuGroup = menuGroupDao.save(메뉴_그룹("일식"));
-            Menu menu = menuDao.save(메뉴("텐동", BigDecimal.valueOf(11000), menuGroup.getId(), List.of(menuProduct)));
-
             OrderTable orderTable = orderTableDao.save(테이블(null, 10, false));
             Order order = 주문(orderTable.getId(), null, LocalDateTime.now(), Collections.emptyList());
 
@@ -107,15 +107,10 @@ class OrderServiceTest extends ServiceTest {
         @Test
         void 주문_메뉴_목록에_메뉴가_존재하지_않으면_예외를_던진다() {
             //given
-            Product product = 상품("텐동", BigDecimal.valueOf(11000));
-            MenuProduct menuProduct = 메뉴_상품(null, product.getId(), 1);
-            MenuGroup menuGroup = menuGroupDao.save(메뉴_그룹("일식"));
-            Menu menu = menuDao.save(메뉴("텐동", BigDecimal.valueOf(11000), menuGroup.getId(), List.of(menuProduct)));
-
             OrderTable orderTable = orderTableDao.save(테이블(null, 10, false));
             OrderLineItem orderLineItem = 주문_메뉴_목록(orderTable.getId(), null, 1);
-            LocalDateTime cookingTime = LocalDateTime.now();
-            Order order = 주문(orderTable.getId(), null, cookingTime, List.of(orderLineItem));
+            LocalDateTime orderedTime = LocalDateTime.now();
+            Order order = 주문(orderTable.getId(), null, orderedTime, List.of(orderLineItem));
 
             //when, then
             assertThatThrownBy(() -> orderService.create(order))
@@ -125,15 +120,10 @@ class OrderServiceTest extends ServiceTest {
         @Test
         void 존재하지_않는_테이블이라면_예외를_던진다() {
             //given
-            Product product = 상품("텐동", BigDecimal.valueOf(11000));
-            MenuProduct menuProduct = 메뉴_상품(null, product.getId(), 1);
-            MenuGroup menuGroup = menuGroupDao.save(메뉴_그룹("일식"));
-            Menu menu = menuDao.save(메뉴("텐동", BigDecimal.valueOf(11000), menuGroup.getId(), List.of(menuProduct)));
-
             OrderTable orderTable = orderTableDao.save(테이블(null, 10, false));
             OrderLineItem orderLineItem = 주문_메뉴_목록(orderTable.getId(), menu.getId(), 1);
-            LocalDateTime cookingTime = LocalDateTime.now();
-            Order order = 주문(-1L, null, cookingTime, List.of(orderLineItem));
+            LocalDateTime orderedTime = LocalDateTime.now();
+            Order order = 주문(-1L, null, orderedTime, List.of(orderLineItem));
 
             //when, then
             assertThatThrownBy(() -> orderService.create(order))
@@ -143,15 +133,10 @@ class OrderServiceTest extends ServiceTest {
         @Test
         void 테이블이_비어있으면_예외를_던진다() {
             //given
-            Product product = 상품("텐동", BigDecimal.valueOf(11000));
-            MenuProduct menuProduct = 메뉴_상품(null, product.getId(), 1);
-            MenuGroup menuGroup = menuGroupDao.save(메뉴_그룹("일식"));
-            Menu menu = menuDao.save(메뉴("텐동", BigDecimal.valueOf(11000), menuGroup.getId(), List.of(menuProduct)));
-
             OrderTable orderTable = orderTableDao.save(테이블(null, 10, true));
             OrderLineItem orderLineItem = 주문_메뉴_목록(orderTable.getId(), menu.getId(), 1);
-            LocalDateTime cookingTime = LocalDateTime.now();
-            Order order = 주문(orderTable.getId(), null, cookingTime, List.of(orderLineItem));
+            LocalDateTime orderedTime = LocalDateTime.now();
+            Order order = 주문(orderTable.getId(), null, orderedTime, List.of(orderLineItem));
 
             //when, then
             assertThatThrownBy(() -> orderService.create(order))
