60b8d012c210e7b383b0b5be7b2e56dc5f0a0089
gitchan <eunkeeeee@gmail.com>
2023-10-15 18:33:08 +0900

    refactor: 테스트 내 Nested 클래스 추가

--- src/test/java/kitchenpos/TableAcceptanceTest.java
+++ src/test/java/kitchenpos/TableAcceptanceTest.java
@@ -3,6 +3,7 @@ package kitchenpos;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import kitchenpos.domain.OrderTable;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static kitchenpos.fixture.OrderTableFixture.NOT_EMPTY_테이블;
@@ -18,65 +19,77 @@ import static org.springframework.http.HttpStatus.OK;
 
 class TableAcceptanceTest extends AcceptanceTest {
 
-    @Test
-    void 테이블을_생성한다() {
-        final ExtractableResponse<Response> response = 테이블_생성_요청(NOT_EMPTY_테이블());
-        final OrderTable result = response.jsonPath().getObject("", OrderTable.class);
+    @Nested
+    class TableCreateTest {
 
-        assertAll(
-                () -> assertThat(response.statusCode()).isEqualTo(CREATED.value()),
-                () -> assertThat(result)
-                        .usingRecursiveComparison()
-                        .ignoringFields("id")
-                        .isEqualTo(NOT_EMPTY_테이블())
-        );
+        @Test
+        void 테이블을_생성한다() {
+            final ExtractableResponse<Response> response = 테이블_생성_요청(NOT_EMPTY_테이블());
+            final OrderTable result = response.jsonPath().getObject("", OrderTable.class);
+
+            assertAll(
+                    () -> assertThat(response.statusCode()).isEqualTo(CREATED.value()),
+                    () -> assertThat(result)
+                            .usingRecursiveComparison()
+                            .ignoringFields("id")
+                            .isEqualTo(NOT_EMPTY_테이블())
+            );
+        }
     }
 
-    @Test
-    void 테이블을_조회한다() {
-        final OrderTable savedOrderTable = 테이블_생성_요청하고_테이블_반환(NOT_EMPTY_테이블());
+    @Nested
+    class TableQueryTest {
 
-        final ExtractableResponse<Response> response = 테이블_조회_요청();
+        @Test
+        void 테이블을_조회한다() {
+            final OrderTable savedOrderTable = 테이블_생성_요청하고_테이블_반환(NOT_EMPTY_테이블());
 
-        assertAll(
-                () -> assertThat(response.statusCode()).isEqualTo(OK.value()),
-                () -> assertThat(response.jsonPath().getList("", OrderTable.class).get(0))
-                        .usingRecursiveComparison()
-                        .isEqualTo(savedOrderTable)
-        );
+            final ExtractableResponse<Response> response = 테이블_조회_요청();
+
+            assertAll(
+                    () -> assertThat(response.statusCode()).isEqualTo(OK.value()),
+                    () -> assertThat(response.jsonPath().getList("", OrderTable.class).get(0))
+                            .usingRecursiveComparison()
+                            .isEqualTo(savedOrderTable)
+            );
+        }
     }
 
-    @Test
-    void 테이블을_Empty_상태로_변경한다() {
-        final OrderTable savedOrderTable = 테이블_생성_요청하고_테이블_반환(NOT_EMPTY_테이블());
-        savedOrderTable.setEmpty(true);
+    @Nested
+    class TableUpdateTest {
 
-        final ExtractableResponse<Response> response = 테이블_상태_Empty로_변경_요청(savedOrderTable);
+        @Test
+        void 테이블을_Empty_상태로_변경한다() {
+            final OrderTable savedOrderTable = 테이블_생성_요청하고_테이블_반환(NOT_EMPTY_테이블());
+            savedOrderTable.setEmpty(true);
 
-        assertAll(
-                () -> assertThat(response.statusCode()).isEqualTo(OK.value()),
-                () -> assertThat(response.jsonPath().getBoolean("empty")).isTrue(),
-                () -> assertThat(response.jsonPath().getObject("", OrderTable.class))
-                        .usingRecursiveComparison()
-                        .isEqualTo(savedOrderTable)
-        );
-    }
+            final ExtractableResponse<Response> response = 테이블_상태_Empty로_변경_요청(savedOrderTable);
 
-    @Test
-    void 테이블의_사람_수를_변경한다() {
-        final OrderTable savedOrderTable = 테이블_생성_요청하고_테이블_반환(NOT_EMPTY_테이블());
+            assertAll(
+                    () -> assertThat(response.statusCode()).isEqualTo(OK.value()),
+                    () -> assertThat(response.jsonPath().getBoolean("empty")).isTrue(),
+                    () -> assertThat(response.jsonPath().getObject("", OrderTable.class))
+                            .usingRecursiveComparison()
+                            .isEqualTo(savedOrderTable)
+            );
+        }
 
-        final int newNumberOfGuests = 5;
-        savedOrderTable.setNumberOfGuests(newNumberOfGuests);
+        @Test
+        void 테이블의_사람_수를_변경한다() {
+            final OrderTable savedOrderTable = 테이블_생성_요청하고_테이블_반환(NOT_EMPTY_테이블());
 
-        final ExtractableResponse<Response> response = 테이블에_앉은_사람_수_변경(savedOrderTable);
+            final int newNumberOfGuests = 5;
+            savedOrderTable.setNumberOfGuests(newNumberOfGuests);
 
-        assertAll(
-                () -> assertThat(response.statusCode()).isEqualTo(OK.value()),
-                () -> assertThat(response.jsonPath().getInt("numberOfGuests")).isEqualTo(newNumberOfGuests),
-                () -> assertThat(response.jsonPath().getObject("", OrderTable.class))
-                        .usingRecursiveComparison()
-                        .isEqualTo(savedOrderTable)
-        );
+            final ExtractableResponse<Response> response = 테이블에_앉은_사람_수_변경(savedOrderTable);
+
+            assertAll(
+                    () -> assertThat(response.statusCode()).isEqualTo(OK.value()),
+                    () -> assertThat(response.jsonPath().getInt("numberOfGuests")).isEqualTo(newNumberOfGuests),
+                    () -> assertThat(response.jsonPath().getObject("", OrderTable.class))
+                            .usingRecursiveComparison()
+                            .isEqualTo(savedOrderTable)
+            );
+        }
     }
 }
