b8d71e2fa5e5cb5149f5bee5a81ac34c2f9e7a22
SproutMj <mjk54565@gmail.com>
2023-10-15 19:40:37 +0900

    test: TableGroupServiceTest를 SpringBootTest로 변

--- src/test/java/kitchenpos/application/TableGroupServiceTest.java
+++ src/test/java/kitchenpos/application/TableGroupServiceTest.java
@@ -1,197 +1,202 @@
 package kitchenpos.application;
 
-import kitchenpos.dao.OrderDao;
-import kitchenpos.dao.OrderTableDao;
-import kitchenpos.dao.TableGroupDao;
-import kitchenpos.domain.OrderStatus;
+import kitchenpos.ServiceTest;
+import kitchenpos.domain.Order;
 import kitchenpos.domain.OrderTable;
 import kitchenpos.domain.TableGroup;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.LocalDateTime;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.mockito.BDDMockito.given;
 
-@ExtendWith(MockitoExtension.class)
-class TableGroupServiceTest {
+class TableGroupServiceTest extends ServiceTest {
 
-    @Mock
-    private OrderDao orderDao;
-    @Mock
-    private OrderTableDao orderTableDao;
-    @Mock
-    private TableGroupDao tableGroupDao;
-
-    @InjectMocks
+    @Autowired
     private TableGroupService tableGroupService;
 
-    TableGroup tableGroup;
     List<OrderTable> orderTables;
 
     @BeforeEach
     void beforeEach() {
-        tableGroup = new TableGroup();
-        tableGroup.setId(1L);
-        tableGroup.setCreatedDate(LocalDateTime.now());
-
-        final OrderTable orderTable1 = new OrderTable();
-        orderTable1.setId(1L);
+        OrderTable orderTable1 = new OrderTable();
         orderTable1.setEmpty(true);
         orderTable1.setNumberOfGuests(3);
         orderTable1.setTableGroupId(null);
+        orderTable1 = testFixtureBuilder.buildOrderTable(orderTable1);
 
-        final OrderTable orderTable2 = new OrderTable();
-        orderTable2.setId(2L);
+        OrderTable orderTable2 = new OrderTable();
         orderTable2.setEmpty(true);
         orderTable2.setNumberOfGuests(5);
         orderTable2.setTableGroupId(null);
+        orderTable2 = testFixtureBuilder.buildOrderTable(orderTable2);
 
-        orderTables = List.of(orderTable1, orderTable2);
+        orderTables = new ArrayList<>();
+        orderTables.add(orderTable1);
+        orderTables.add(orderTable2);
     }
 
-    @DisplayName("tableGroup을 생성한다.")
-    @Test
-    void tableGroup_생성() {
-        //given
-        final TableGroup expected = tableGroup;
-        expected.setOrderTables(orderTables);
+    @DisplayName("단체 지정 생성 테스트")
+    @Nested
+    class TableGroupCreateTest {
 
-        final List<Long> orderTableIds = orderTables.stream()
-                .map(OrderTable::getId)
-                .collect(Collectors.toList());
-        given(orderTableDao.findAllByIdIn(orderTableIds))
-                .willReturn(orderTables);
-        given(tableGroupDao.save(expected))
-                .willReturn(expected);
-        for (final OrderTable orderTable : orderTables) {
-            orderTable.setEmpty(true);
-            given(orderTableDao.save(orderTable))
-                    .willReturn(orderTable);
+        @DisplayName("단체 지정을 생성한다.")
+        @Test
+        void tableGroupCreate() {
+            //given
+            final TableGroup tableGroup = new TableGroup();
+            tableGroup.setOrderTables(orderTables);
+
+            //when
+            final TableGroup actual = tableGroupService.create(tableGroup);
+
+            //then
+            assertSoftly(softly -> {
+                softly.assertThat(actual.getId()).isNotNull();
+            });
         }
 
-        //when
-        final TableGroup actual = tableGroupService.create(expected);
+        @DisplayName("주문 테이블들이 비어있으면 실패한다.")
+        @Test
+        void tableGroupCreateFailWhenOrderTablesIsEmpty() {
+            //given
+            final TableGroup tableGroup = new TableGroup();
+            tableGroup.setOrderTables(Collections.emptyList());
 
-        //then
-        assertSoftly(softly -> {
-            softly.assertThat(actual.getId()).isEqualTo(expected.getId());
-        });
-    }
-
-    @DisplayName("orderTables가 비어있으면 예외를 던진다.")
-    @Test
-    void orderTables가_비어있으면_예외() {
-        //given
-        final TableGroup expected = tableGroup;
-        expected.setOrderTables(Collections.emptyList());
-
-        // when & then
-        assertThatThrownBy(() -> tableGroupService.create(expected))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("orderTables의 크기가 2개 미만이면 예외를 던진다.")
-    @Test
-    void orderTables의_크기가_2개미만이면_예외() {
-        //given
-        final TableGroup expected = tableGroup;
-        expected.setOrderTables(List.of(orderTables.get(0)));
-
-        // when & then
-        assertThatThrownBy(() -> tableGroupService.create(expected))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("orderTable이 empty가 true가 아니면 예외를 던진다.")
-    @Test
-    void orderTable이_empty가_false면_예외() {
-        //given
-        final TableGroup expected = tableGroup;
-        expected.setOrderTables(orderTables);
-
-        final List<Long> orderTableIds = orderTables.stream()
-                .map(OrderTable::getId)
-                .collect(Collectors.toList());
-        given(orderTableDao.findAllByIdIn(orderTableIds))
-                .willReturn(orderTables);
-        for (final OrderTable orderTable : orderTables) {
-            orderTable.setEmpty(false);
+            // when & then
+            assertThatThrownBy(() -> tableGroupService.create(tableGroup))
+                    .isInstanceOf(IllegalArgumentException.class);
         }
 
-        // when & then
-        assertThatThrownBy(() -> tableGroupService.create(expected))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
+        @DisplayName("주문 테이블들의 크기가 2개 미만이면 실패한다.")
+        @Test
+        void tableGroupCreateFailWhenOrderTablesSizeLessThenTwo() {
+            //given
+            final TableGroup tableGroup = new TableGroup();
+            tableGroup.setOrderTables(List.of(orderTables.get(0)));
 
-    @DisplayName("orderTable의 tableGroupId가 null이 아니면 예외를 던진다.")
-    @Test
-    void orderTable의_tableGroupId가_null이_아니면_예외() {
-        //given
-        final TableGroup expected = tableGroup;
-        expected.setOrderTables(orderTables);
-
-        final List<Long> orderTableIds = orderTables.stream()
-                .map(OrderTable::getId)
-                .collect(Collectors.toList());
-        given(orderTableDao.findAllByIdIn(orderTableIds))
-                .willReturn(orderTables);
-        for (final OrderTable orderTable : orderTables) {
-            orderTable.setEmpty(true);
-            orderTable.setTableGroupId(1L);
+            // when & then
+            assertThatThrownBy(() -> tableGroupService.create(tableGroup))
+                    .isInstanceOf(IllegalArgumentException.class);
         }
 
-        // when & then
-        assertThatThrownBy(() -> tableGroupService.create(expected))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
+        @DisplayName("주문 테이블이 존재하지 않으면 실패한다.")
+        @Test
+        void tableGroupCreateFailWhenNotExistOrderTable() {
+            //given
+            final OrderTable notExistOrderTable = new OrderTable();
+            notExistOrderTable.setId(-1L);
+            notExistOrderTable.setEmpty(true);
+            notExistOrderTable.setTableGroupId(null);
+            notExistOrderTable.setNumberOfGuests(3);
+            orderTables.add(notExistOrderTable);
 
-    @DisplayName("tableGroup을 unGroup한다.")
-    @Test
-    void tableGroup을_unGroup() {
-        final Long tableGroupId = tableGroup.getId();
-        given(orderTableDao.findAllByTableGroupId(tableGroupId))
-                .willReturn(orderTables);
-        final List<Long> orderTableIds = orderTables.stream()
-                .map(OrderTable::getId)
-                .collect(Collectors.toList());
-        given(orderDao.existsByOrderTableIdInAndOrderStatusIn(orderTableIds, Arrays.asList(OrderStatus.COOKING.name(), OrderStatus.MEAL.name())))
-                .willReturn(false);
-        for (final OrderTable orderTable : orderTables) {
-            given(orderTableDao.save(orderTable))
-                    .willReturn(orderTable);
+            final TableGroup tableGroup = new TableGroup();
+            tableGroup.setOrderTables(orderTables);
+
+            // when & then
+            assertThatThrownBy(() -> tableGroupService.create(tableGroup))
+                    .isInstanceOf(IllegalArgumentException.class);
         }
 
-        //when
-        assertDoesNotThrow(() -> tableGroupService.ungroup(tableGroupId));
+        @DisplayName("주문 테이블이 비어있지 않으면 실패한다.")
+        @Test
+        void tableGroupCreateFailWhenOrderTableIsNotEmpty() {
+            //given
+            OrderTable notEmptyOrderTable = new OrderTable();
+            notEmptyOrderTable.setEmpty(false);
+            notEmptyOrderTable.setTableGroupId(null);
+            notEmptyOrderTable.setNumberOfGuests(3);
+            notEmptyOrderTable = testFixtureBuilder.buildOrderTable(notEmptyOrderTable);
+            orderTables.add(notEmptyOrderTable);
+
+            final TableGroup tableGroup = new TableGroup();
+            tableGroup.setOrderTables(orderTables);
+
+            // when & then
+            assertThatThrownBy(() -> tableGroupService.create(tableGroup))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @DisplayName("주문 테이블의 단체 지정 id가 null이 아니면 실패한다.")
+        @Test
+        void tableGroupCreateFailWhenOrderTableIsNotNullTableGroupId() {
+            //given
+            TableGroup beforeTableGroup = new TableGroup();
+            beforeTableGroup.setCreatedDate(LocalDateTime.now());
+            beforeTableGroup.setOrderTables(orderTables);
+            beforeTableGroup = testFixtureBuilder.buildTableGroup(beforeTableGroup);
+
+            OrderTable tableGroupIdNotNullOrderTable = new OrderTable();
+            tableGroupIdNotNullOrderTable.setEmpty(true);
+            tableGroupIdNotNullOrderTable.setTableGroupId(beforeTableGroup.getId());
+            tableGroupIdNotNullOrderTable.setNumberOfGuests(3);
+            tableGroupIdNotNullOrderTable = testFixtureBuilder.buildOrderTable(tableGroupIdNotNullOrderTable);
+
+            final TableGroup tableGroup = new TableGroup();
+            orderTables.add(tableGroupIdNotNullOrderTable);
+            tableGroup.setOrderTables(orderTables);
+            // when & then
+            assertThatThrownBy(() -> tableGroupService.create(tableGroup))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
     }
 
-    @DisplayName("tableGroup의 orderTable 중 cooking이나 meal상태인 것이 있으면 unGroup할 때 예외를 던진다.")
-    @Test
-    void unGroup할_때_cooking이나_meal인_orderTable이_있으면_예외() {
-        final Long tableGroupId = tableGroup.getId();
-        given(orderTableDao.findAllByTableGroupId(tableGroupId))
-                .willReturn(orderTables);
-        final List<Long> orderTableIds = orderTables.stream()
-                .map(OrderTable::getId)
-                .collect(Collectors.toList());
-        given(orderDao.existsByOrderTableIdInAndOrderStatusIn(orderTableIds, Arrays.asList(OrderStatus.COOKING.name(), OrderStatus.MEAL.name())))
-                .willReturn(true);
+    @DisplayName("단체 지정 해제 테스트")
+    @Nested
+    class TableGroupUpGroupTest {
 
-        // when & then
-        assertThatThrownBy(() -> tableGroupService.ungroup(tableGroupId))
-                .isInstanceOf(IllegalArgumentException.class);
+        @DisplayName("단체 지정을 해제한다.")
+        @Test
+        void tableGroupUpGroup() {
+            //given
+            TableGroup tableGroup = new TableGroup();
+            tableGroup.setCreatedDate(LocalDateTime.now());
+            tableGroup.setOrderTables(orderTables);
+            tableGroup = testFixtureBuilder.buildTableGroup(tableGroup);
+
+            //when
+            final Long tableGroupId = tableGroup.getId();
+            assertDoesNotThrow(() -> tableGroupService.ungroup(tableGroupId));
+        }
+
+        @DisplayName("단체 지정된 주문 테이블 중 조리나 식사 상태인 것이 있으면 실패한다.")
+        @ParameterizedTest
+        @ValueSource(strings = {"COOKING", "MEAL"})
+        void tableGroupUpGroupFailWhenOrderStatusInCookingOrMeal(final String orderStatus) {
+            //given
+            TableGroup tableGroup = new TableGroup();
+            tableGroup.setOrderTables(orderTables);
+            tableGroup.setCreatedDate(LocalDateTime.now());
+            tableGroup = testFixtureBuilder.buildTableGroup(tableGroup);
+
+            OrderTable notCompletionOrdertable = new OrderTable();
+            notCompletionOrdertable.setEmpty(false);
+            notCompletionOrdertable.setTableGroupId(tableGroup.getId());
+            notCompletionOrdertable.setNumberOfGuests(3);
+            notCompletionOrdertable = testFixtureBuilder.buildOrderTable(notCompletionOrdertable);
+
+            final Order notCompletionOrder = new Order();
+            notCompletionOrder.setOrderStatus(orderStatus);
+            notCompletionOrder.setOrderedTime(LocalDateTime.now());
+            notCompletionOrder.setOrderTableId(notCompletionOrdertable.getId());
+            testFixtureBuilder.buildOrder(notCompletionOrder);
+
+            // when & then
+            final Long tableGroupId = tableGroup.getId();
+            assertThatThrownBy(() -> tableGroupService.ungroup(tableGroupId))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
     }
 }
