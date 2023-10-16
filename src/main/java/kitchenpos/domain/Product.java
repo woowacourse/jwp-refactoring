da296a128016f38b1d305cbe957b5bef387b4fac
Ethan <kim76744dev@gmail.com>
2023-10-16 15:06:47 +0900

    refactor: 상품에서 setter 삭제

--- src/main/java/kitchenpos/domain/Product.java
+++ src/main/java/kitchenpos/domain/Product.java
@@ -7,11 +7,16 @@ public class Product {
     private String name;
     private BigDecimal price;
 
-    public Product() {
+    private Product() {
 
     }
 
     public Product(final String name, final BigDecimal price) {
+        this(null, name, price);
+    }
+
+    public Product(final Long id, final String name, final BigDecimal price) {
+        this.id = id;
         this.name = name;
         this.price = price;
     }
@@ -20,23 +25,11 @@ public class Product {
         return id;
     }
 
-    public void setId(final Long id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
     }
 
-    public void setName(final String name) {
-        this.name = name;
-    }
-
     public BigDecimal getPrice() {
         return price;
-    }
-
-    public void setPrice(final BigDecimal price) {
-        this.price = price;
     }
 }
