05f20b7ca434c4ad5406b9b928df35d504d6ad0b
drunkenhw <drunkenhw@gmail.com>
2023-10-16 11:57:46 +0900

    refactor: refactor product service logic to domain

--- src/main/java/kitchenpos/application/ProductService.java
+++ src/main/java/kitchenpos/application/ProductService.java
@@ -6,9 +6,7 @@ import kitchenpos.domain.ProductRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.math.BigDecimal;
 import java.util.List;
-import java.util.Objects;
 
 @Service
 public class ProductService {
@@ -21,12 +19,6 @@ public class ProductService {
     @Transactional
     public Product create(final ProductRequest request) {
         Product product = request.toProduct();
-        final BigDecimal price = product.getPrice();
-
-        if (Objects.isNull(price) || price.compareTo(BigDecimal.ZERO) < 0) {
-            throw new IllegalArgumentException("상품 가격은 0보다 커야합니다");
-        }
-
         return productRepository.save(product);
     }
 
--- src/main/java/kitchenpos/domain/Product.java
+++ src/main/java/kitchenpos/domain/Product.java
@@ -5,6 +5,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.math.BigDecimal;
+import java.util.Objects;
 
 @Entity
 public class Product {
@@ -18,6 +19,7 @@ public class Product {
     }
 
     public Product(Long id, String name, BigDecimal price) {
+        validatePrice(price);
         this.id = id;
         this.name = name;
         this.price = price;
@@ -25,6 +27,12 @@ public class Product {
 
     public Product(String name, BigDecimal price) {
         this(null, name, price);
+    }
+
+    private void validatePrice(BigDecimal price) {
+        if (Objects.isNull(price) || price.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("상품 가격은 0보다 커야합니다");
+        }
     }
 
     public Long getId() {
