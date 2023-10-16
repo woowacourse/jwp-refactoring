98cdfe84e85282c05a5d98823c43e1f48ca8519e
donghae-kim <donghae1999@gmail.com>
2023-10-16 20:26:25 +0900

    test: TableServiceTest 수정

--- src/test/java/kitchenpos/application/TableServiceTest.java
+++ src/test/java/kitchenpos/application/TableServiceTest.java
@@ -1,20 +1,15 @@
 package kitchenpos.application;
 
-import kitchenpos.dao.OrderDao;
 import kitchenpos.domain.Order;
 import kitchenpos.domain.OrderStatus;
 import kitchenpos.domain.OrderTable;
+import kitchenpos.domain.TableGroup;
 import kitchenpos.fixture.Fixture;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.jdbc.Sql;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -89,17 +84,36 @@ class TableServiceTest extends ServiceBaseTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @Test
+    @DisplayName("빈상태로 변경시 orderTable의 테이블 그룹 아이디가 null이 아니면 안된다.")
+    void changeEmptyValidTableGroupId() {
+        //given
+        final OrderTable orderTable1 = Fixture.orderTable(null, 10, true);
+        final OrderTable orderTable2 = Fixture.orderTable(null, 5, true);
+        final OrderTable savedOrderTable1 = orderTableDao.save(orderTable1);
+        final OrderTable savedOrderTable2 = orderTableDao.save(orderTable2);
+        final TableGroup tableGroup = Fixture.orderTableGroup(LocalDateTime.now(), List.of(savedOrderTable1, savedOrderTable2));
+        final TableGroup savedTableGroup = tableGroupService.create(tableGroup);
+
+        final OrderTable savedOrderTable = orderTableDao.save(Fixture.orderTable(savedTableGroup.getId(), 10, true));
+
+        //when&then
+        assertThatThrownBy(() -> tableService.changeEmpty(savedOrderTable.getId(), savedOrderTable))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
     @ParameterizedTest(name = "주문 테이블이 요리, 식사 상태이면 안된다.")
     @EnumSource(value = OrderStatus.class, names = {"COOKING", "MEAL"})
     void changeEmptyValidStatus(final OrderStatus orderStatus) {
         //given
-        final OrderTable savedOrderTable = tableService.create(orderTable);
+        final OrderTable orderTable = tableService.create(Fixture.orderTable(null, 10, true));
+        final OrderTable savedOrderTable = orderTableDao.save(orderTable);
         final Order order = Fixture.order(null, savedOrderTable.getId(), LocalDateTime.now(), null);
         order.setOrderStatus(orderStatus.name());
         orderDao.save(order);
 
         //when&then
-        assertThatThrownBy(() -> tableService.changeEmpty(orderTable.getId(), orderTable))
+        assertThatThrownBy(() -> tableService.changeEmpty(savedOrderTable.getId(), savedOrderTable))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -136,6 +150,19 @@ class TableServiceTest extends ServiceBaseTest {
 
         //when&then
         assertThatThrownBy(() -> tableService.changeNumberOfGuests(orderTable.getId(), orderTable))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("게스트 숫자 변경 시 OrderTable은 비어있으면 안된다.")
+    void changeNumberOfGuestValidEmptySavedOrderTable() {
+        //given
+        final OrderTable orderTable = tableService.create(Fixture.orderTable(null, 10, true));
+        final OrderTable savedOrderTable = orderTableDao.save(orderTable);
+        this.orderTable.setNumberOfGuests(0);
+
+        //when&then
+        assertThatThrownBy(() -> tableService.changeNumberOfGuests(savedOrderTable.getId(), savedOrderTable))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
