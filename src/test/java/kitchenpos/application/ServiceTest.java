cd036b8db5b34c265ad71814fe5bd99be370d217
Rosie <dev.kayoung@gmail.com>
2023-10-16 14:42:09 +0900

    test: ServiceTest에 `@Transactional` 추가

--- src/test/java/kitchenpos/application/ServiceTest.java
+++ src/test/java/kitchenpos/application/ServiceTest.java
@@ -10,7 +10,9 @@ import kitchenpos.dao.ProductDao;
 import kitchenpos.dao.TableGroupDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 @SpringBootTest
 public abstract class ServiceTest {
 
