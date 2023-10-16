4b8b561031926efa592d79fa586577dd63886290
hyena <goatsfish@gmail.com>
2023-10-16 14:15:17 +0900

    refactor: 상품 가격 예외 검증을 비즈니스에서 도메인의 역할로 수정

--- src/main/java/kitchenpos/domain/Product.java
+++ src/main/java/kitchenpos/domain/Product.java
@@ -15,9 +15,20 @@ public class Product {
     }
 
     public Product(final Long id, final String name, final BigDecimal price) {
+        validate(price);
         this.id = id;
         this.name = name;
         this.price = price;
+    }
+
+    private void validate(final BigDecimal price) {
+        if (isPriceNullOrNegative(price)) {
+            throw new IllegalArgumentException("상품의 가격은 null 이거나 음수일 수 없습니다.");
+        }
+    }
+
+    private boolean isPriceNullOrNegative(final BigDecimal price) {
+        return price == null || price.compareTo(BigDecimal.ZERO) < 0;
     }
 
     public Long getId() {
--- src/test/java/kitchenpos/application/product/ProductServiceTest.java
+++ src/test/java/kitchenpos/application/product/ProductServiceTest.java
@@ -6,12 +6,9 @@ import kitchenpos.domain.Product;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.math.BigDecimal;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
 
@@ -39,30 +36,5 @@ class ProductServiceTest extends ApplicationTestConfig {
             softly.assertThat(actual.getName()).isEqualTo(expected.getName());
             softly.assertThat(actual.getPrice()).isEqualByComparingTo(expected.getPrice());
         });
-    }
-
-    @DisplayName("[EXCEPTION] 상품의 가격이 음수일 경우 예외가 발생한다.")
-    @ParameterizedTest
-    @ValueSource(strings = {"-1", "-10", "-100000"})
-    void throwException_when_create_price_isNegative(final String negativeValue) {
-        // given
-        final BigDecimal negativeBigDecimal = new BigDecimal(negativeValue);
-        final Product expected = new Product("테스트용 상품 이름", negativeBigDecimal);
-
-        // expect
-        assertThatThrownBy(() -> productService.create(expected))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("[EXCEPTION] 상품의 가격이 null 일 경우 예외가 발생한다.")
-    @Test
-    void throwException_when_create_price_isNull() {
-        // given
-        final BigDecimal nullBigDecimal = null;
-        final Product expected = new Product("테스트용 상품 이름", nullBigDecimal);
-
-        // expect
-        assertThatThrownBy(() -> productService.create(expected))
-                .isInstanceOf(IllegalArgumentException.class);
     }
 }
