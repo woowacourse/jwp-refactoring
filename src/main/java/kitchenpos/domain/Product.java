88080cd21bc368b743d7b846fe8d31d36bcd7f38
shin-mallang <huipulci1@naver.com>
2023-10-17 20:38:58 +0900

    상품 가격 검증 로직을 Product 모델 내로 이동

--- src/main/java/kitchenpos/domain/Product.java
+++ src/main/java/kitchenpos/domain/Product.java
@@ -1,11 +1,28 @@
 package kitchenpos.domain;
 
 import java.math.BigDecimal;
+import java.util.Objects;
+import kitchenpos.domain.exception.InvalidProductPriceException;
 
 public class Product {
     private Long id;
     private String name;
     private BigDecimal price;
+
+    public Product() {
+    }
+
+    public Product(String name, BigDecimal price) {
+        validatePrice(price);
+        this.name = name;
+        this.price = price;
+    }
+
+    private void validatePrice(BigDecimal price) {
+        if (Objects.isNull(price) || price.compareTo(BigDecimal.ZERO) < 0) {
+            throw new InvalidProductPriceException();
+        }
+    }
 
     public Long getId() {
         return id;
