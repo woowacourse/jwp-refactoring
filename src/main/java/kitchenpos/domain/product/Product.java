a6740de5b2fee127df7fef8d193a2ee49952f245
JunDonghyuk <ehdgur4814@naver.com>
2023-10-17 09:41:16 +0900

    refactor: Product 동등성 비교 추가

--- src/main/java/kitchenpos/domain/product/Product.java
+++ src/main/java/kitchenpos/domain/product/Product.java
@@ -1,6 +1,7 @@
 package kitchenpos.domain.product;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public class Product {
 
@@ -28,5 +29,18 @@ public class Product {
 
     public BigDecimal getPrice() {
         return price.getPrice();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(getId(), product.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
     }
 }
