3893325c609fad87a5854dcc7408dafca7ff41d4
java-saeng <gusdn85554@naver.com>
2023-10-18 16:50:34 +0900

    refactor : MenuGroupService에서 Dao -> Repository 변경

--- src/test/java/kitchenpos/application/MenuGroupServiceTest.java
+++ src/test/java/kitchenpos/application/MenuGroupServiceTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
 import kitchenpos.domain.MenuGroup;
+import kitchenpos.domain.MenuGroupRepository;
+import kitchenpos.fixture.MenuGroupFixture;
 import kitchenpos.support.ServiceIntegrationTest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -17,11 +19,14 @@ class MenuGroupServiceTest extends ServiceIntegrationTest {
   @Autowired
   private MenuGroupService menuGroupService;
 
+  @Autowired
+  private MenuGroupRepository menuGroupRepository;
+
   @Test
   @DisplayName("create() : 메뉴 그룹을 생성할 수 있다.")
   void test_create() throws Exception {
     //given
-    final MenuGroup menuGroup = new MenuGroup("menugroup");
+    final MenuGroup menuGroup = MenuGroupFixture.createMenuGroup();
 
     //when
     final MenuGroup savedMenuGroup = menuGroupService.create(menuGroup);
@@ -36,10 +41,15 @@ class MenuGroupServiceTest extends ServiceIntegrationTest {
   @Test
   @DisplayName("list() : 모든 메뉴 그룹들을 조회할 수 있다.")
   void test_list() throws Exception {
+    //given
+    menuGroupRepository.save(MenuGroupFixture.createMenuGroup());
+    menuGroupRepository.save(MenuGroupFixture.createMenuGroup());
+    menuGroupRepository.save(MenuGroupFixture.createMenuGroup());
+
     //when
-    final List<MenuGroup> list = menuGroupService.list();
+    final List<MenuGroup> menuGroups = menuGroupService.list();
 
     //then
-    assertEquals(4, list.size());
+    assertEquals(3, menuGroups.size());
   }
 }
