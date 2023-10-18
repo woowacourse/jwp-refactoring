b3d8614ed74033f4de49f6b76622a6e1a027cbb6
JunDonghyuk <ehdgur4814@naver.com>
2023-10-18 15:12:50 +0900

    refactor: MenuService JPA로 이전 및 테스트 수정 및 DTO 설계

--- src/test/java/kitchenpos/application/MenuServiceTest.java
+++ src/test/java/kitchenpos/application/MenuServiceTest.java
@@ -1,16 +1,21 @@
 package kitchenpos.application;
 
-import kitchenpos.dao.MenuDao;
-import kitchenpos.dao.MenuGroupDao;
-import kitchenpos.dao.MenuProductDao;
-import kitchenpos.dao.ProductDao;
-import kitchenpos.domain.menu.Menu;
-import kitchenpos.domain.menu.MenuProduct;
+import kitchenpos.application.dto.CreateMenuDto;
+import kitchenpos.application.dto.CreateMenuProductDto;
+import kitchenpos.application.dto.MenuDto;
+import kitchenpos.repository.MenuGroupRepository;
+import kitchenpos.repository.MenuRepository;
+import kitchenpos.repository.ProductRepository;
+import kitchenpos.domain.menu.*;
 import kitchenpos.domain.product.Product;
 import kitchenpos.domain.product.ProductName;
 import kitchenpos.domain.product.ProductPrice;
+import kitchenpos.exception.MenuException;
+import kitchenpos.exception.MenuGroupException;
+import kitchenpos.exception.MenuPriceException;
+import kitchenpos.exception.ProductException;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
@@ -26,239 +31,210 @@ class MenuServiceTest extends MockServiceTest {
     private MenuService menuService;
 
     @Mock
-    private MenuDao menuDao;
+    private MenuRepository menuRepository;
 
     @Mock
-    private MenuGroupDao menuGroupDao;
+    private MenuGroupRepository menuGroupRepository;
 
     @Mock
-    private MenuProductDao menuProductDao;
-
-    @Mock
-    private ProductDao productDao;
-
-    private Menu firstMenu;
-    private Product firstProduct;
-    private Product secondProduct;
-    private MenuProduct firstMenuProduct;
-    private MenuProduct secondMenuProduct;
-
-    @BeforeEach
-    void setUp() {
-        firstMenu = new Menu();
-        firstMenu.setId(1L);
-        firstMenu.setName("불고기 한 상 세트");
-        firstMenu.setPrice(BigDecimal.valueOf(45000L));
-        firstMenu.setMenuGroupId(1L);
-
-        firstProduct = new Product(
-                1L,
-                new ProductName("pizza"),
-                new ProductPrice(BigDecimal.valueOf(10000L)));
-
-        secondProduct = new Product(
-                2L,
-                new ProductName("chicken"),
-                new ProductPrice(BigDecimal.valueOf(25000L)));
-
-        firstMenuProduct = new MenuProduct();
-        firstMenuProduct.setSeq(1L);
-        firstMenuProduct.setMenuId(firstMenu.getMenuGroupId());
-        firstMenuProduct.setProductId(firstProduct.getId());
-        firstMenuProduct.setQuantity(2L);
-
-        secondMenuProduct = new MenuProduct();
-        secondMenuProduct.setSeq(2L);
-        secondMenuProduct.setMenuId(firstMenu.getMenuGroupId());
-        secondMenuProduct.setProductId(secondProduct.getId());
-        secondMenuProduct.setQuantity(1L);
-    }
+    private ProductRepository productRepository;
 
     @Test
     void 메뉴_목록을_조회한다() {
         // given
-        List<Menu> expected = List.of(firstMenu);
-        MenuProduct expectedFirstMenuProduct = firstMenuProduct;
-        MenuProduct expectedSecondMenuProduct = secondMenuProduct;
-        firstMenu.setMenuProducts(
-                List.of(expectedFirstMenuProduct, expectedSecondMenuProduct)
-        );
+        MenuGroup menuGroup = new MenuGroup(1L, new MenuGroupName("2+1"));
+        Menu menu = new Menu(1L, new MenuName("chicken + chicken + pizza"), new MenuPrice(BigDecimal.valueOf(29000L)), menuGroup);
 
-        Menu mockReturnMenu = new Menu();
-        mockReturnMenu.setId(firstMenu.getId());
-        mockReturnMenu.setName(firstMenu.getName());
-        mockReturnMenu.setPrice(firstMenu.getPrice());
-        mockReturnMenu.setMenuGroupId(firstMenu.getMenuGroupId());
+        Product chicken = new Product(1L, new ProductName("chicken"), new ProductPrice(BigDecimal.valueOf(10000L)));
+        Product pizza = new Product(10L, new ProductName("pizza"), new ProductPrice(BigDecimal.valueOf(9500L)));
+        MenuProduct twoChicken = new MenuProduct(1L, menu, chicken, new MenuProductQuantity(2L));
+        MenuProduct onePizza = new MenuProduct(5L, menu, pizza, new MenuProductQuantity(1L));
+        menu.addMenuProducts(List.of(twoChicken, onePizza));
 
-        BDDMockito.given(menuDao.findAll())
-                .willReturn(List.of(mockReturnMenu));
-
-        MenuProduct mockFirstReturnMenuProduct = new MenuProduct();
-        mockFirstReturnMenuProduct.setSeq(expectedFirstMenuProduct.getSeq());
-        mockFirstReturnMenuProduct.setMenuId(expectedFirstMenuProduct.getMenuId());
-        mockFirstReturnMenuProduct.setProductId(expectedFirstMenuProduct.getProductId());
-        mockFirstReturnMenuProduct.setQuantity(expectedFirstMenuProduct.getQuantity());
-
-        MenuProduct mockSecondReturnMenuProduct = new MenuProduct();
-        mockSecondReturnMenuProduct.setSeq(expectedSecondMenuProduct.getSeq());
-        mockSecondReturnMenuProduct.setMenuId(expectedSecondMenuProduct.getMenuId());
-        mockSecondReturnMenuProduct.setProductId(expectedSecondMenuProduct.getProductId());
-        mockSecondReturnMenuProduct.setQuantity(expectedSecondMenuProduct.getQuantity());
-
-        BDDMockito.given(menuProductDao.findAllByMenuId(mockReturnMenu.getId()))
-                .willReturn(List.of(
-                                mockFirstReturnMenuProduct,
-                                mockSecondReturnMenuProduct
-                        )
-                );
+        BDDMockito.given(menuRepository.findAllUsingFetchJoin())
+                .willReturn(List.of(menu));
 
         // when
-        List<Menu> actual = menuService.list();
+        List<MenuDto> actual = menuService.list();
 
         // then
-        Assertions.assertThat(actual).usingRecursiveComparison().isEqualTo(expected);
+        SoftAssertions softAssertions = new SoftAssertions();
+        softAssertions.assertThat(actual.size()).isEqualTo(1);
+        softAssertions.assertThat(actual.get(0).getId()).isEqualTo(menu.getId());
+        softAssertions.assertThat(actual.get(0).getName()).isEqualTo(menu.getName());
+        softAssertions.assertThat(actual.get(0).getMenuGroupId()).isEqualTo(menu.getMenuGroup().getId());
+        softAssertions.assertThat(actual.get(0).getPrice()).isEqualTo(menu.getPrice());
+        softAssertions.assertThat(actual.get(0).getMenuProducts().size()).isEqualTo(2);
+        softAssertions.assertAll();
     }
 
     @Test
     void 메뉴를_추가한다() {
         // given
-        Menu expected = firstMenu;
-        expected.setMenuProducts(List.of(firstMenuProduct, secondMenuProduct));
+        MenuGroup menuGroup = new MenuGroup(1L, new MenuGroupName("2+1"));
+        Menu menu = new Menu(1L, new MenuName("chicken + chicken + pizza"), new MenuPrice(BigDecimal.valueOf(29000L)), menuGroup);
 
-        Menu menu = new Menu();
-        menu.setName(firstMenu.getName());
-        menu.setPrice(firstMenu.getPrice());
-        menu.setMenuGroupId(firstMenu.getMenuGroupId());
+        Product chicken = new Product(1L, new ProductName("chicken"), new ProductPrice(BigDecimal.valueOf(10000L)));
+        Product pizza = new Product(10L, new ProductName("pizza"), new ProductPrice(BigDecimal.valueOf(9500L)));
+        MenuProduct twoChicken = new MenuProduct(1L, menu, chicken, new MenuProductQuantity(2L));
+        MenuProduct onePizza = new MenuProduct(5L, menu, pizza, new MenuProductQuantity(1L));
+        menu.addMenuProducts(List.of(twoChicken, onePizza));
 
-        MenuProduct firstMenuProduct = new MenuProduct();
-        firstMenuProduct.setProductId(this.firstMenuProduct.getProductId());
-        firstMenuProduct.setQuantity(this.firstMenuProduct.getQuantity());
+        BDDMockito.given(menuGroupRepository.findById(BDDMockito.anyLong()))
+                .willReturn(Optional.of(menuGroup));
+        BDDMockito.given(productRepository.findById(pizza.getId()))
+                .willReturn(Optional.of(pizza));
+        BDDMockito.given(productRepository.findById(chicken.getId()))
+                .willReturn(Optional.of(chicken));
+        BDDMockito.given(menuRepository.save(BDDMockito.any(Menu.class)))
+                .willReturn(menu);
 
-        MenuProduct secondMenuProduct = new MenuProduct();
-        secondMenuProduct.setProductId(this.secondMenuProduct.getProductId());
-        secondMenuProduct.setQuantity(this.secondMenuProduct.getQuantity());
-
-        menu.setMenuProducts(List.of(firstMenuProduct, secondMenuProduct));
-
-        BDDMockito.given(menuGroupDao.existsById(menu.getMenuGroupId()))
-                .willReturn(true);
-
-        BDDMockito.given(productDao.findById(firstMenuProduct.getProductId()))
-                .willReturn(Optional.of(firstProduct));
-        BDDMockito.given(productDao.findById(secondMenuProduct.getProductId()))
-                .willReturn(Optional.of(secondProduct));
-
-        Menu savedMenu = new Menu();
-        savedMenu.setId(1L);
-        savedMenu.setName(menu.getName());
-        savedMenu.setPrice(menu.getPrice());
-        savedMenu.setMenuGroupId(menu.getMenuGroupId());
-
-        BDDMockito.given(menuDao.save(menu))
-                .willReturn(savedMenu);
-
-        BDDMockito.given(menuProductDao.save(firstMenuProduct))
-                .willReturn(this.firstMenuProduct);
-        BDDMockito.given(menuProductDao.save(secondMenuProduct))
-                .willReturn(this.secondMenuProduct);
+        CreateMenuDto createMenuDto = new CreateMenuDto(
+                menu.getName(),
+                menu.getPrice(),
+                menu.getMenuGroup().getId(),
+                List.of(
+                        new CreateMenuProductDto(chicken.getId(), twoChicken.getQuantity()),
+                        new CreateMenuProductDto(pizza.getId(), onePizza.getQuantity())
+                ));
 
         // when
-        Menu actual = menuService.create(menu);
+        MenuDto actual = menuService.create(createMenuDto);
 
         // then
-        Assertions.assertThat(actual).usingRecursiveComparison().isEqualTo(expected);
+        SoftAssertions softAssertions = new SoftAssertions();
+        softAssertions.assertThat(actual.getId()).isEqualTo(menu.getId());
+        softAssertions.assertThat(actual.getName()).isEqualTo(menu.getName());
+        softAssertions.assertThat(actual.getPrice()).isEqualTo(menu.getPrice());
+        softAssertions.assertThat(actual.getMenuGroupId()).isEqualTo(menu.getMenuGroup().getId());
+        softAssertions.assertThat(actual.getMenuProducts().size()).isEqualTo(2);
+        softAssertions.assertAll();
     }
 
     @Test
     void 메뉴를_추가할_때_메뉴가격이_null_이면_예외를_던진다() {
         // given
-        Menu menu = new Menu();
-        menu.setPrice(null);
+        MenuGroup menuGroup = new MenuGroup(1L, new MenuGroupName("2+1"));
+
+        BDDMockito.given(menuGroupRepository.findById(BDDMockito.anyLong()))
+                .willReturn(Optional.of(menuGroup));
+
+        BigDecimal price = null;
+        CreateMenuDto createMenuDto = new CreateMenuDto(
+                "chicken + chicken + pizza",
+                price,
+                menuGroup.getId(),
+                List.of(
+                        new CreateMenuProductDto(1L, 2L),
+                        new CreateMenuProductDto(2L, 1L)
+                ));
 
         // when, then
-        Assertions.assertThatThrownBy(() -> menuService.create(menu))
-                .isInstanceOf(IllegalArgumentException.class);
-        BDDMockito.then(menuGroupDao)
-                .should(BDDMockito.times(0))
-                .existsById(BDDMockito.anyLong());
+        Assertions.assertThatThrownBy(() -> menuService.create(createMenuDto))
+                .isInstanceOf(MenuPriceException.class);
     }
 
     @Test
     void 메뉴를_추가할_때_메뉴가격이_음수이면_예외를_던진다() {
         // given
-        Menu menu = new Menu();
-        menu.setPrice(BigDecimal.valueOf(-1000L));
+        MenuGroup menuGroup = new MenuGroup(1L, new MenuGroupName("2+1"));
+
+        BDDMockito.given(menuGroupRepository.findById(BDDMockito.anyLong()))
+                .willReturn(Optional.of(menuGroup));
+
+        BigDecimal price = BigDecimal.valueOf(-1000L);
+        CreateMenuDto createMenuDto = new CreateMenuDto(
+                "chicken + chicken + pizza",
+                price,
+                menuGroup.getId(),
+                List.of(
+                        new CreateMenuProductDto(1L, 2L),
+                        new CreateMenuProductDto(2L, 1L)
+                ));
 
         // when, then
-        Assertions.assertThatThrownBy(() -> menuService.create(menu))
-                .isInstanceOf(IllegalArgumentException.class);
-        BDDMockito.then(menuGroupDao)
-                .should(BDDMockito.times(0))
-                .existsById(BDDMockito.anyLong());
+        Assertions.assertThatThrownBy(() -> menuService.create(createMenuDto))
+                .isInstanceOf(MenuPriceException.class);
     }
 
     @Test
     void 메뉴를_추가할_때_메뉴가격이_상품가격_곱하기_상품수량의_합보다_크면_예외를_던진다() {
         // given
-        Menu menu = firstMenu;
-        menu.setMenuProducts(List.of(firstMenuProduct, secondMenuProduct));
-        BigDecimal firstMenuPrice = firstProduct.getPrice().multiply(BigDecimal.valueOf(firstMenuProduct.getQuantity()));
-        BigDecimal secondMenuPrice = secondProduct.getPrice().multiply(BigDecimal.valueOf(secondMenuProduct.getQuantity()));
+        MenuGroup menuGroup = new MenuGroup(1L, new MenuGroupName("2+1"));
 
-        BigDecimal moreMenuPrice = firstMenuPrice.add(secondMenuPrice).add(BigDecimal.valueOf(1000L));
-        menu.setPrice(moreMenuPrice);
+        Product chicken = new Product(1L, new ProductName("chicken"), new ProductPrice(BigDecimal.valueOf(10000L)));
+        Product pizza = new Product(10L, new ProductName("pizza"), new ProductPrice(BigDecimal.valueOf(9500L)));
+
+        BDDMockito.given(menuGroupRepository.findById(BDDMockito.anyLong()))
+                .willReturn(Optional.of(menuGroup));
+        BDDMockito.given(productRepository.findById(chicken.getId()))
+                .willReturn(Optional.of(chicken));
+        BDDMockito.given(productRepository.findById(pizza.getId()))
+                .willReturn(Optional.of(pizza));
+
+        BigDecimal correctPrice = chicken.getPrice().multiply(BigDecimal.valueOf(2L))
+                .add(pizza.getPrice().multiply(BigDecimal.ONE));
+        BigDecimal greaterThanCorrectPrice = correctPrice.add(BigDecimal.TEN);
+
+        CreateMenuDto createMenuDto = new CreateMenuDto(
+                "two chicken + one pizza",
+                greaterThanCorrectPrice,
+                menuGroup.getId(),
+                List.of(
+                        new CreateMenuProductDto(chicken.getId(), 2L),
+                        new CreateMenuProductDto(pizza.getId(), 1L)
+                ));
 
         // when, then
-        Assertions.assertThatThrownBy(() -> menuService.create(menu))
-                .isInstanceOf(IllegalArgumentException.class);
-        BDDMockito.then(menuDao)
-                .should(BDDMockito.times(0))
-                .save(menu);
+        Assertions.assertThatThrownBy(() -> menuService.create(createMenuDto))
+                .isInstanceOf(MenuException.class);
     }
 
     @Test
     void 메뉴를_추가할_때_메뉴_안의_상품이_존재하지_않으면_예외를_던진다() {
         // given
-        Menu menu = firstMenu;
-        menu.setMenuProducts(List.of(firstMenuProduct, secondMenuProduct));
-        BigDecimal firstMenuPrice = firstProduct.getPrice().multiply(BigDecimal.valueOf(firstMenuProduct.getQuantity()));
-        BigDecimal secondMenuPrice = secondProduct.getPrice().multiply(BigDecimal.valueOf(secondMenuProduct.getQuantity()));
+        MenuGroup menuGroup = new MenuGroup(1L, new MenuGroupName("2+1"));
 
-        BigDecimal moreMenuPrice = firstMenuPrice.add(secondMenuPrice).add(BigDecimal.valueOf(1000L));
-        menu.setPrice(moreMenuPrice);
-
-        BDDMockito.given(menuGroupDao.existsById(menu.getMenuGroupId()))
-                .willReturn(true);
-        BDDMockito.given(productDao.findById(firstMenuProduct.getProductId()))
+        BDDMockito.given(menuGroupRepository.findById(BDDMockito.anyLong()))
+                .willReturn(Optional.of(menuGroup));
+        BDDMockito.given(productRepository.findById(BDDMockito.anyLong()))
                 .willReturn(Optional.empty());
 
+        BigDecimal price = BigDecimal.valueOf(1000L);
+        CreateMenuDto createMenuDto = new CreateMenuDto(
+                "chicken + chicken + pizza",
+                price,
+                menuGroup.getId(),
+                List.of(
+                        new CreateMenuProductDto(1L, 2L),
+                        new CreateMenuProductDto(10L, 1L)
+                ));
+
         // when, then
-        Assertions.assertThatThrownBy(() -> menuService.create(menu))
-                .isInstanceOf(IllegalArgumentException.class);
-        BDDMockito.then(productDao)
-                .should(BDDMockito.times(1))
-                .findById(BDDMockito.anyLong());
+        Assertions.assertThatThrownBy(() -> menuService.create(createMenuDto))
+                .isInstanceOf(ProductException.class);
     }
 
     @Test
     void 메뉴를_추가할_때_메뉴그룹이_존재하지_않으면_예외를_던진다() {
         // given
-        Menu menu = firstMenu;
-        menu.setMenuProducts(List.of(firstMenuProduct, secondMenuProduct));
-        BigDecimal firstMenuPrice = firstProduct.getPrice().multiply(BigDecimal.valueOf(firstMenuProduct.getQuantity()));
-        BigDecimal secondMenuPrice = secondProduct.getPrice().multiply(BigDecimal.valueOf(secondMenuProduct.getQuantity()));
+        MenuGroup menuGroup = new MenuGroup(1L, new MenuGroupName("2+1"));
 
-        BigDecimal moreMenuPrice = firstMenuPrice.add(secondMenuPrice).add(BigDecimal.valueOf(1000L));
-        menu.setPrice(moreMenuPrice);
+        BDDMockito.given(menuGroupRepository.findById(BDDMockito.anyLong()))
+                .willReturn(Optional.empty());
 
-        BDDMockito.given(menuGroupDao.existsById(menu.getMenuGroupId()))
-                .willReturn(false);
+        BigDecimal price = BigDecimal.valueOf(1000L);
+        CreateMenuDto createMenuDto = new CreateMenuDto(
+                "chicken + chicken + pizza",
+                price,
+                menuGroup.getId(),
+                List.of(
+                        new CreateMenuProductDto(1L, 2L),
+                        new CreateMenuProductDto(10L, 1L)
+                ));
 
         // when, then
-        Assertions.assertThatThrownBy(() -> menuService.create(menu))
-                .isInstanceOf(IllegalArgumentException.class);
-        BDDMockito.then(productDao)
-                .should(BDDMockito.times(0))
-                .findById(BDDMockito.anyLong());
+        Assertions.assertThatThrownBy(() -> menuService.create(createMenuDto))
+                .isInstanceOf(MenuGroupException.class);
     }
 }
