2d39ee93cfae74c4507c1c0528f2f0c1ea1de5cd
hyunseo <>
2023-10-19 00:21:26 +0900

    reafactor: JpaRepository에 @Repository 제거

--- src/main/java/kitchenpos/persistence/OrderRepository.java
+++ src/main/java/kitchenpos/persistence/OrderRepository.java
@@ -5,9 +5,7 @@ import kitchenpos.domain.Order;
 import kitchenpos.domain.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
     boolean existsByOrderTableIdAndOrderStatusIn(Long orderTableId, List<OrderStatus> orderStatuses);
--- src/main/java/kitchenpos/persistence/OrderTableRepository.java
+++ src/main/java/kitchenpos/persistence/OrderTableRepository.java
@@ -4,9 +4,7 @@ package kitchenpos.persistence;
 import java.util.List;
 import kitchenpos.domain.OrderTable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface OrderTableRepository extends JpaRepository<OrderTable, Long> {
 
     List<OrderTable> findAllByTableGroupId(Long tableGroupId);
