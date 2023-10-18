abc17b05ac0f3e52df0f04d112f4372151ffb89e
parkmuhyeun <pjhg410@gmail.com>
2023-10-18 16:43:28 +0900

    refactor: products 변수명 변경

--- src/main/java/kitchenpos/domain/Products.java
+++ src/main/java/kitchenpos/domain/Products.java
@@ -5,16 +5,16 @@ import java.util.List;
 
 public class Products {
 
-    private final List<Product> products;
+    private final List<Product> values;
 
-    public Products(final List<Product> products) {
-        this.products = products;
+    public Products(final List<Product> values) {
+        this.values = values;
     }
 
     public BigDecimal calculateSum(final List<Integer> counts) {
         BigDecimal sum = BigDecimal.ZERO;
-        for (int index = 0; index < products.size(); index++) {
-            sum = sum.add(products.get(index).getPrice()
+        for (int index = 0; index < values.size(); index++) {
+            sum = sum.add(values.get(index).getPrice()
                     .multiply(BigDecimal.valueOf(counts.get(index))));
         }
         return sum;
