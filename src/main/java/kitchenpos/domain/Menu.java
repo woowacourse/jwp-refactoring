766bd9b8b994a9049e821e01d82b8b0cd8536197
hyena <goatsfish@gmail.com>
2023-10-18 22:38:25 +0900

    refactor: 메뉴 엔티티 매핑

--- src/main/java/kitchenpos/domain/Menu.java
+++ src/main/java/kitchenpos/domain/Menu.java
@@ -1,56 +1,87 @@
 package kitchenpos.domain;
 
-import java.math.BigDecimal;
-import java.util.List;
+import kitchenpos.domain.vo.Price;
+import org.hibernate.annotations.BatchSize;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Entity
 public class Menu {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "name")
     private String name;
-    private BigDecimal price;
-    private Long menuGroupId;
-    private List<MenuProduct> menuProducts;
+
+    @Embedded
+    private Price price;
+
+    @JoinColumn(name = "menu_group_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private MenuGroup menuGroup;
+
+    @BatchSize(size = 10)
+    @OneToMany(cascade = CascadeType.PERSIST, orphanRemoval = true, mappedBy = "menu")
+    private List<MenuProduct> menuProducts = new ArrayList<>();
 
     public Menu() {
     }
 
     public Menu(final String name,
-                final BigDecimal price,
-                final Long menuGroupId,
+                final Price price,
+                final MenuGroup menuGroup,
                 final List<MenuProduct> menuProducts
     ) {
-        this(null, name, price, menuGroupId, menuProducts);
+        this(null, name, price, menuGroup, menuProducts);
     }
 
     public Menu(final Long id,
                 final String name,
-                final BigDecimal price,
-                final Long menuGroupId,
+                final Price price,
+                final MenuGroup menuGroup,
                 final List<MenuProduct> menuProducts
     ) {
         validate(price);
         this.id = id;
         this.name = name;
         this.price = price;
-        this.menuGroupId = menuGroupId;
+        this.menuGroup = menuGroup;
         this.menuProducts = menuProducts;
     }
 
-    private void validate(final BigDecimal price) {
+    private void validate(final Price price) {
         if (isPriceNullOrNegative(price)) {
             throw new IllegalArgumentException("상품의 가격은 null 이거나 음수일 수 없습니다.");
         }
     }
 
-    private boolean isPriceNullOrNegative(final BigDecimal price) {
-        return price == null || price.compareTo(BigDecimal.ZERO) < 0;
+    private boolean isPriceNullOrNegative(final Price price) {
+        return price == null || price.getValue().compareTo(BigDecimal.ZERO) < 0;
     }
 
-    public void addMenuProduct(final List<MenuProduct> menuProducts) {
-        this.menuProducts = menuProducts;
+    public void addMenuProducts(final List<MenuProduct> menuProducts) {
+        this.menuProducts = menuProducts.stream()
+                .map(menuProduct -> new MenuProduct(this, menuProduct.getProduct(), menuProduct.getQuantity()))
+                .collect(Collectors.toList());
     }
 
     public boolean isGreaterThan(final BigDecimal otherPrice) {
-        return price.compareTo(otherPrice) > 0;
+        return price.getValue().compareTo(otherPrice) > 0;
     }
 
     public Long getId() {
@@ -61,12 +92,12 @@ public class Menu {
         return name;
     }
 
-    public BigDecimal getPrice() {
+    public Price getPrice() {
         return price;
     }
 
-    public Long getMenuGroupId() {
-        return menuGroupId;
+    public MenuGroup getMenuGroup() {
+        return menuGroup;
     }
 
     public List<MenuProduct> getMenuProducts() {
--- src/test/java/kitchenpos/domain/MenuTest.java
+++ src/test/java/kitchenpos/domain/MenuTest.java
@@ -1,16 +1,19 @@
 package kitchenpos.domain;
 
+import kitchenpos.domain.vo.Price;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
 class MenuTest {
 
@@ -27,7 +30,7 @@ class MenuTest {
     @ValueSource(strings = {"-1", "-10", "-100", "-1000000"})
     void throwException_price_isNegative(final String negativePriceValue) {
         // expect
-        assertThatThrownBy(() -> new Menu("테스트용 메뉴명", new BigDecimal(negativePriceValue), null, Collections.emptyList()))
+        assertThatThrownBy(() -> new Menu("테스트용 메뉴명", new Price(negativePriceValue), null, Collections.emptyList()))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -36,12 +39,35 @@ class MenuTest {
     @CsvSource(value = {"10000:9999:true", "10000:1:true", "10000:-10:true", "10000:10001:false", "10000:99999999:false"}, delimiter = ':')
     void isGreaterThan(final String menuPrice, final String otherPrice, final boolean expected) {
         // given
-        final Menu menu = new Menu("테스트용 메뉴명", new BigDecimal(menuPrice), null, Collections.emptyList());
+        final Menu menu = new Menu("테스트용 메뉴명", new Price(menuPrice), null, Collections.emptyList());
 
         // when
-        final boolean actual = menu.isGreaterThan(new BigDecimal(otherPrice));
+        final boolean actual = menu.isGreaterThan(new Price(otherPrice).getValue());
 
         // then
         assertThat(actual).isEqualTo(expected);
+    }
+
+    @DisplayName("[SUCCESS] 메뉴 상품을 추가할 경우 메뉴 상품에 자신을 주입한다.")
+    @Test
+    void success_addMenuProducts() {
+        // given
+        final Product product = new Product("테스트용 상품명", new Price("10000"));
+        final MenuGroup menuGroup = new MenuGroup("테스트용 메뉴 그룹명");
+        final Menu menu = new Menu("테스트용 메뉴명", new Price("10000"), menuGroup, new ArrayList<>());
+
+        // when
+        menu.addMenuProducts(List.of(new MenuProduct(null, product, 10)));
+
+        // then
+        final List<MenuProduct> actual = menu.getMenuProducts();
+        assertSoftly(softly -> {
+            softly.assertThat(actual).hasSize(1);
+            final MenuProduct actualMenuProduct = actual.get(0);
+
+            softly.assertThat(actualMenuProduct)
+                    .usingRecursiveComparison()
+                    .isEqualTo(new MenuProduct(menu, product, 10));
+        });
     }
 }
