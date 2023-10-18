623ff8a391aa03598b2f49a0b0da28580f595b12
Ethan <kim76744dev@gmail.com>
2023-10-18 22:27:48 +0900

    test: MenuRepositoryTest 추가

---
+++ src/test/java/kitchenpos/repository/MenuRepositoryTest.java
@@ -0,0 +1,36 @@
+package kitchenpos.repository;
+
+import kitchenpos.domain.Menu;
+import kitchenpos.domain.MenuGroup;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MenuRepositoryTest extends RepositoryTestConfig {
+
+    @Autowired
+    private MenuRepository menuRepository;
+
+    @Test
+    void countByIdIn() {
+        // given
+        final MenuGroup japanese = createMenuGroup("일식");
+        final Menu sushi = createMenu("초밥", BigDecimal.valueOf(15000), japanese);
+        final Menu ramen = createMenu("라면", BigDecimal.valueOf(9000), japanese);
+        createMenu("텐동", BigDecimal.valueOf(10000), japanese);
+
+        em.flush();
+        em.close();
+
+        // when
+        final long notSavedMenuId = -1L;
+        final long actual = menuRepository.countByIdIn(List.of(sushi.getId(), ramen.getId(), notSavedMenuId));
+
+        // then
+        assertThat(actual).isEqualTo(2);
+    }
+}
