87dad2b355a8a5a344694f1e7f79a5eb673c6d05
junpakPark <112045553+junpakPark@users.noreply.github.com>
2023-10-18 19:11:45 +0900

    feat: Price VO 구현

---
+++ src/main/java/kitchenpos/refactoring/domain/Price.java
@@ -0,0 +1,24 @@
+package kitchenpos.refactoring.domain;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class Price {
+
+    private final BigDecimal price;
+
+    public Price(final BigDecimal price) {
+        validate(price);
+        this.price = price;
+    }
+
+    private void validate(final BigDecimal price) {
+        if (Objects.isNull(price) || price.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+}
