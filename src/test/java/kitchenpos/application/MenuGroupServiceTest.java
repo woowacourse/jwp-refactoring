77b7505e68359d263018f46c3ddde508cb0fa24d
yoondgu <doyoungwork@gmail.com>
2023-10-19 02:51:52 +0900

    refactor: MenuGroup setter 제거 및 단위테스트 작성

--- src/test/java/kitchenpos/application/MenuGroupServiceTest.java
+++ src/test/java/kitchenpos/application/MenuGroupServiceTest.java
@@ -1,7 +1,6 @@
 package kitchenpos.application;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.Objects;
 import kitchenpos.IntegrationTest;
@@ -10,6 +9,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+@SuppressWarnings("NonAsciiCharacters")
 class MenuGroupServiceTest extends IntegrationTest {
 
     @Autowired
@@ -20,8 +20,7 @@ class MenuGroupServiceTest extends IntegrationTest {
     @DisplayName("메뉴 그룹 등록 시 전달받은 정보를 새 id로 저장한다.")
     void 메뉴_그룹_등록_성공_저장() {
         // given
-        final MenuGroup menuGroup = new MenuGroup();
-        menuGroup.setName("중식");
+        final MenuGroup menuGroup = new MenuGroup("중식");
 
         // when
         final MenuGroup saved = menuGroupService.create(menuGroup);
@@ -31,18 +30,5 @@ class MenuGroupServiceTest extends IntegrationTest {
                 .map(MenuGroup::getId)
                 .filteredOn(id -> Objects.equals(id, saved.getId()))
                 .hasSize(1);
-    }
-
-    @Test
-    @DisplayName("메뉴 그룹 등록 시 이름이 있어야 한다.")
-    void 메뉴_그룹_등록_실패_이름_없음() {
-        // given
-        // when
-        final MenuGroup menuGroup = new MenuGroup();
-
-        // then
-        /// TODO: 2023/10/12 DB 가기 전에 예외처리하기
-        assertThatThrownBy(() -> menuGroupService.create(menuGroup))
-                .isInstanceOf(Exception.class);
     }
 }
