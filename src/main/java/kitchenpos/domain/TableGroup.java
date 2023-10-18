f01617d58067ed647c671324bb471ee1506d32d7
hyena <goatsfish@gmail.com>
2023-10-18 22:37:30 +0900

    refactor: 단체 지정 엔티티 매핑

--- src/main/java/kitchenpos/domain/TableGroup.java
+++ src/main/java/kitchenpos/domain/TableGroup.java
@@ -1,11 +1,28 @@
 package kitchenpos.domain;
 
+import org.hibernate.annotations.BatchSize;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Entity
 public class TableGroup {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "created_date")
     private LocalDateTime createdDate;
+
+    @BatchSize(size = 10)
+    @OneToMany(cascade = CascadeType.PERSIST, mappedBy = "tableGroup")
     private List<OrderTable> orderTables;
 
     public TableGroup() {
@@ -21,7 +38,11 @@ public class TableGroup {
         this.orderTables = orderTables;
     }
 
-    public void addOrderTables(final List<OrderTable> orderTables) {
+    public void addOrderTablesAndChangeEmptyFull(final List<OrderTable> orderTables) {
+        orderTables.forEach(orderTable -> {
+            orderTable.setTableGroup(this);
+            orderTable.changeOrderTableFull();
+        });
         this.orderTables = orderTables;
     }
 
