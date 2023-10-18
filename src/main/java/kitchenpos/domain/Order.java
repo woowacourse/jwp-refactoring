d97553deb15aef58830cad0b8b759fd6fb1a2ca9
hyena <goatsfish@gmail.com>
2023-10-18 22:39:05 +0900

    refactor: 주문 엔티티 매핑

--- src/main/java/kitchenpos/domain/Order.java
+++ src/main/java/kitchenpos/domain/Order.java
@@ -1,49 +1,76 @@
 package kitchenpos.domain;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
+@Table(name = "orders")
+@Entity
 public class Order {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Long orderTableId;
+
+    @JoinColumn(name = "order_table_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private OrderTable orderTable;
+
+    @Column(name = "order_status")
     private String orderStatus;
+
+    @Column(name = "ordered_time")
     private LocalDateTime orderedTime;
+
+    @OneToMany(cascade = CascadeType.PERSIST, orphanRemoval = true, mappedBy = "order")
     private List<OrderLineItem> orderLineItems;
 
     public Order() {
     }
 
-    public Order(final Long orderTableId,
+    public Order(final OrderTable orderTable,
                  final String orderStatus,
                  final LocalDateTime orderedTime,
                  final List<OrderLineItem> orderLineItems
     ) {
-        this(null, orderTableId, orderStatus, orderedTime, orderLineItems);
+        this(null, orderTable, orderStatus, orderedTime, orderLineItems);
     }
 
     public Order(final Long id,
-                 final Long orderTableId,
+                 final OrderTable orderTable,
                  final String orderStatus,
                  final LocalDateTime orderedTime,
                  final List<OrderLineItem> orderLineItems
     ) {
         this.id = id;
-        this.orderTableId = orderTableId;
+        this.orderTable = orderTable;
         this.orderStatus = orderStatus;
         this.orderedTime = orderedTime;
         this.orderLineItems = orderLineItems;
     }
 
     public void addOrderLineItems(final List<OrderLineItem> orderLineItems) {
-        this.orderLineItems = orderLineItems;
+        this.orderLineItems = orderLineItems.stream()
+                .map(orderLineItem -> new OrderLineItem(this, orderLineItem.getMenu(), orderLineItem.getQuantity()))
+                .collect(Collectors.toList());
     }
 
     public Long getId() {
         return id;
     }
 
-    public Long getOrderTableId() {
-        return orderTableId;
+    public OrderTable getOrderTable() {
+        return orderTable;
     }
 
     public String getOrderStatus() {
@@ -62,8 +89,8 @@ public class Order {
         this.id = id;
     }
 
-    public void setOrderTableId(final Long orderTableId) {
-        this.orderTableId = orderTableId;
+    public void setOrderTable(final OrderTable orderTable) {
+        this.orderTable = orderTable;
     }
 
     public void setOrderStatus(final String orderStatus) {
---
+++ src/test/java/kitchenpos/domain/OrderTest.java
@@ -0,0 +1,55 @@
+package kitchenpos.domain;
+
+import kitchenpos.domain.vo.Price;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderTest {
+
+    @DisplayName("[SUCCESS] 생성한다.")
+    @Test
+    void success_create() {
+        // given
+        final OrderTable orderTable = new OrderTable(null, 10, true);
+
+        // expect
+        assertThatCode(() -> new Order(orderTable, OrderStatus.MEAL.name(), LocalDateTime.now(), Collections.emptyList()))
+                .doesNotThrowAnyException();
+    }
+
+    @DisplayName("[SUCCESS] 주문 항목을 추가할 경우 자신을 주문 항목에 주입한다.")
+    @Test
+    void success_addOrderLineItems() {
+        // given
+        final MenuGroup menuGroup = new MenuGroup("테스트용 메뉴 그룹명");
+        final Menu menu = new Menu("테스트용 메뉴명", new Price("10000"), menuGroup, new ArrayList<>());
+
+        final OrderTable orderTable = new OrderTable(null, 10, true);
+        final Order order = new Order(orderTable, OrderStatus.COOKING.name(), LocalDateTime.now(), new ArrayList<>());
+
+        // then
+        order.addOrderLineItems(List.of(
+                new OrderLineItem(null, menu, 10)
+        ));
+
+        // when
+        final List<OrderLineItem> actual = order.getOrderLineItems();
+        assertSoftly(softly -> {
+            softly.assertThat(actual).hasSize(1);
+            final OrderLineItem actualOrderLineItem = actual.get(0);
+
+            softly.assertThat(actualOrderLineItem)
+                    .usingRecursiveComparison()
+                    .isEqualTo(new OrderLineItem(order, menu, 10));
+        });
+    }
+}
