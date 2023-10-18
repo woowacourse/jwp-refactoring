af1b1965b73cde629b72fa31ca75b0a4e648fed8
JunDonghyuk <ehdgur4814@naver.com>
2023-10-18 14:35:08 +0900

    refactor: MenuProductQuantity 테스트 및 구현

---
+++ src/main/java/kitchenpos/domain/menu/MenuProductQuantity.java
@@ -0,0 +1,51 @@
+package kitchenpos.domain.menu;
+
+import kitchenpos.exception.MenuProductQuantityException;
+
+import javax.persistence.Embeddable;
+import java.util.Objects;
+
+@Embeddable
+public class MenuProductQuantity {
+
+    private Long quantity;
+
+    protected MenuProductQuantity() {
+    }
+
+    public MenuProductQuantity(Long quantity) {
+        validateQuantity(quantity);
+        this.quantity = quantity;
+    }
+
+    private void validateQuantity(Long quantity) {
+        if (isNull(quantity) || isNegative(quantity)) {
+            throw new MenuProductQuantityException("메뉴 수량은 0 이상이어야 합니다.");
+        }
+    }
+
+    private boolean isNull(Long quantity) {
+        return Objects.isNull(quantity);
+    }
+
+    private boolean isNegative(Long quantity) {
+        return quantity < 0;
+    }
+
+    public Long getQuantity() {
+        return quantity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MenuProductQuantity that = (MenuProductQuantity) o;
+        return Objects.equals(getQuantity(), that.getQuantity());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getQuantity());
+    }
+}
---
+++ src/main/java/kitchenpos/exception/MenuProductQuantityException.java
@@ -0,0 +1,8 @@
+package kitchenpos.exception;
+
+public class MenuProductQuantityException extends RuntimeException {
+
+    public MenuProductQuantityException(String message) {
+        super(message);
+    }
+}
