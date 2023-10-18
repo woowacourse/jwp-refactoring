96ba556ca7f95245cdb7850c3594c06c63ff1e9d
JunDonghyuk <ehdgur4814@naver.com>
2023-10-18 15:09:19 +0900

    feat: MenuRepository 테스트 및 구현

---
+++ src/main/java/kitchenpos/repository/MenuRepository.java
@@ -0,0 +1,13 @@
+package kitchenpos.repository;
+
+import kitchenpos.domain.menu.Menu;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface MenuRepository extends JpaRepository<Menu, Long> {
+
+    @Query("select m from Menu m join fetch m.menuProducts")
+    List<Menu> findAllUsingFetchJoin();
+}
---
+++ src/test/java/kitchenpos/repository/MenuRepositoryTest.java
@@ -0,0 +1,56 @@
+package kitchenpos.repository;
+
+import kitchenpos.BaseTest;
+import kitchenpos.domain.menu.*;
+import kitchenpos.domain.product.Product;
+import kitchenpos.domain.product.ProductName;
+import kitchenpos.domain.product.ProductPrice;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import javax.persistence.EntityManager;
+import java.math.BigDecimal;
+import java.util.List;
+
+@DataJpaTest
+class MenuRepositoryTest extends BaseTest {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private MenuRepository menuRepository;
+
+    @Autowired
+    private MenuGroupRepository menuGroupRepository;
+
+    @Autowired
+    private EntityManager em;
+
+    @Test
+    void 메뉴를_가져올_때_메뉴_상품을_같이_조회한다() {
+        // given
+        MenuGroup menuGroup = menuGroupRepository.save(new MenuGroup(new MenuGroupName("one plus one")));
+        Product product = productRepository.save(new Product(new ProductName("pizza"), new ProductPrice(BigDecimal.valueOf(14000L))));
+        Menu menu = new Menu(new MenuName("pizza one plus one"), new MenuPrice(BigDecimal.valueOf(27000L)), menuGroup);
+        MenuProduct menuProduct = new MenuProduct(product, new MenuProductQuantity(2L));
+        menu.addMenuProducts(List.of(menuProduct));
+        menuRepository.save(menu);
+        em.flush();
+        em.clear();
+
+        // when
+        List<Menu> menus = menuRepository.findAllUsingFetchJoin();
+        for (Menu findMenu : menus) {
+            em.detach(findMenu);
+        }
+
+        // then
+        for (Menu findMenu : menus) {
+            Assertions.assertThatNoException()
+                    .isThrownBy(() -> findMenu.getMenuProducts().get(0).getQuantity());
+        }
+    }
+}
