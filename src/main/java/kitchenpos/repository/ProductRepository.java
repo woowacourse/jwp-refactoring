baf7cc7bc85b2e8d19de05b14f6f15a28fe360c4
hyena <goatsfish@gmail.com>
2023-10-19 15:59:38 +0900

    feat: 상품 조회 레포지터리 기본 메서드 구현

--- src/main/java/kitchenpos/repository/ProductRepository.java
+++ src/main/java/kitchenpos/repository/ProductRepository.java
@@ -4,4 +4,8 @@ import kitchenpos.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    default Product findProductById(final Long productId) {
+        return findById(productId).orElseThrow(() -> new IllegalArgumentException("상품 식별자값으로 상품을 조회할 수 없습니다."));
+    }
 }
---
+++ src/test/java/kitchenpos/repository/ProductRepositoryTest.java
@@ -0,0 +1,21 @@
+package kitchenpos.repository;
+
+import kitchenpos.config.RepositoryTestConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class ProductRepositoryTest extends RepositoryTestConfig {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @DisplayName("[EXCEPTION] 상품 식별자값으로 상품을 조회할 수 없는 경우 예외가 발생한다.")
+    @Test
+    void throwException_when_notFound() {
+        assertThatThrownBy(() -> productRepository.findProductById(-1L))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
