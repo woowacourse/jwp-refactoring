ee9a67e4674ac5bf0fe4a1c7e9c85ef53bb70325
hyena <goatsfish@gmail.com>
2023-10-19 17:40:15 +0900

    feat: 주문 레포지터리 조회 기본 기능 추가

--- src/main/java/kitchenpos/repository/OrderRepository.java
+++ src/main/java/kitchenpos/repository/OrderRepository.java
@@ -2,6 +2,7 @@ package kitchenpos.repository;
 
 import kitchenpos.domain.Order;
 import kitchenpos.domain.OrderStatus;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,6 +10,10 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    default Order findOrderById(final Long orderId) {
+        return findById(orderId).orElseThrow(() -> new EmptyResultDataAccessException("주문 식별자값으로 주문을 조회할 수 없습니다.", 1));
+    }
 
     @Query("select case when coalesce(count(1), 0) > 0 then true else false end " +
            " from Order o " +
--- src/test/java/kitchenpos/repository/OrderRepositoryTest.java
+++ src/test/java/kitchenpos/repository/OrderRepositoryTest.java
@@ -6,7 +6,6 @@ import kitchenpos.domain.MenuGroup;
 import kitchenpos.domain.MenuProduct;
 import kitchenpos.domain.Order;
 import kitchenpos.domain.OrderLineItem;
-import kitchenpos.domain.OrderLineItems;
 import kitchenpos.domain.OrderStatus;
 import kitchenpos.domain.OrderTable;
 import kitchenpos.domain.Product;
@@ -16,17 +15,24 @@ import kitchenpos.domain.vo.Quantity;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class OrderRepositoryTest extends RepositoryTestConfig {
 
     @Autowired
     private OrderRepository orderRepository;
+
+    @DisplayName("[EXCEPTION] 주문 식별자값으로 주문 조회를 실패할 경우 예외가 발생한다.")
+    @Test
+    void throwException_when_notFount() {
+        assertThatThrownBy(() -> orderRepository.findOrderById(0L))
+                .isInstanceOf(EmptyResultDataAccessException.class);
+    }
 
     @DisplayName("[SUCCESS] 주문 테이블 식별자값과 주문 상태 목록 조건에 해당하는 주문이 존재하는지 확인한다.")
     @Test
@@ -37,9 +43,10 @@ class OrderRepositoryTest extends RepositoryTestConfig {
         final Menu savedMenu = persistMenu(Menu.ofEmptyMenuProducts(new Name("테스트용 메뉴명"), Price.ZERO, savedMenuGroup));
         persistMenuProduct(new MenuProduct(savedMenu, savedProduct, new Quantity(1)));
         final OrderTable savedOrderTable = persistOrderTable(new OrderTable(null, 10, true));
-        final Order savedOrder = persistOrder(new Order(savedOrderTable, OrderStatus.COOKING, LocalDateTime.now(), new OrderLineItems(new ArrayList<>())));
-        persistOrderLineItem(new OrderLineItem(savedOrder, savedMenu, new Quantity(1)));
-        persistOrderLineItem(new OrderLineItem(savedOrder, savedMenu, new Quantity(1)));
+        final Order savedOrder = persistOrder(Order.ofEmptyOrderLineItems(savedOrderTable));
+        final OrderLineItem orderLineItem = OrderLineItem.ofWithoutOrder(savedMenu, new Quantity(1));
+        orderLineItem.assignOrder(savedOrder);
+        persistOrderLineItem(orderLineItem);
 
         em.flush();
         em.close();
