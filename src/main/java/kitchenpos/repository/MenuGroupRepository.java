bf1997c74a99b19d64c682eeddad89a04e2a2d6d
hyena <goatsfish@gmail.com>
2023-10-19 16:02:11 +0900

    feat: 메뉴 그룹 조회 레포지터리 기본 메서드 구현

--- src/main/java/kitchenpos/repository/MenuGroupRepository.java
+++ src/main/java/kitchenpos/repository/MenuGroupRepository.java
@@ -4,4 +4,8 @@ import kitchenpos.domain.MenuGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MenuGroupRepository extends JpaRepository<MenuGroup, Long> {
+
+    default MenuGroup findMenuGroupById(final Long menuGroupId) {
+        return findById(menuGroupId).orElseThrow(() -> new IllegalArgumentException("메뉴 그룹을 식별자 값으로 조회할 수 없습니다."));
+    }
 }
