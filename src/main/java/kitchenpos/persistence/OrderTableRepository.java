5b7928f136841c210922799cc144f03d044ebb52
hyunseo <>
2023-10-19 17:35:48 +0900

    refactor: 사용하지 않게된 클래스 및 메소드 제거

--- src/main/java/kitchenpos/persistence/OrderTableRepository.java
+++ src/main/java/kitchenpos/persistence/OrderTableRepository.java
@@ -7,7 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderTableRepository extends JpaRepository<OrderTable, Long> {
 
-    List<OrderTable> findAllByTableGroupId(Long tableGroupId);
-
     List<OrderTable> findAllByIdIn(List<Long> ids);
 }
