d2f066226722dc43aeb2edd6ed7c3953a5f3fb5d
hyunseo <>
2023-10-19 13:12:06 +0900

    refactor: TableGroup의 orderTable 일급 컬렉션 추가

--- src/main/java/kitchenpos/domain/TableGroup.java
+++ src/main/java/kitchenpos/domain/TableGroup.java
@@ -1,28 +1,23 @@
 package kitchenpos.domain;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
 
 @Entity
 public class TableGroup {
-
-    private static final int MIN_ORDER_TABLE_SIZE = 2;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private LocalDateTime createdDate;
 
-    @OneToMany
-    @JoinColumn(name = "table_group_id")
-    private final List<OrderTable> orderTables = new ArrayList<>();
+    @Embedded
+    private GroupedOrderTables groupedOrderTables;
 
     protected TableGroup() {
     }
@@ -37,20 +32,14 @@ public class TableGroup {
     }
 
     public void group(List<OrderTable> orderTables) {
-        validate(orderTables);
-        orderTables.forEach(it -> it.group(this));
-        this.orderTables.addAll(orderTables);
+        this.groupedOrderTables = new GroupedOrderTables(orderTables);
+        groupedOrderTables.group(this);
     }
 
     public void ungroup() {
-        orderTables.forEach(OrderTable::unGroup);
+        groupedOrderTables.ungroup();
     }
 
-    private void validate(List<OrderTable> orderTables) {
-        if (orderTables.size() < MIN_ORDER_TABLE_SIZE) {
-            throw new IllegalArgumentException(String.format("테이블 그룹은 최소 %s개 이상의 테이블이 필요합니다.", MIN_ORDER_TABLE_SIZE));
-        }
-    }
 
     public Long getId() {
         return id;
@@ -61,6 +50,6 @@ public class TableGroup {
     }
 
     public List<OrderTable> getOrderTables() {
-        return orderTables;
+        return groupedOrderTables.getValues();
     }
 }
