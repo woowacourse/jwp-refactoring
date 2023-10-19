5823e80e1af3fd4e8e81c886c77c28d33b205640
java-saeng <gusdn85554@naver.com>
2023-10-19 16:40:42 +0900

    refactor : MenuRepositoryImpl 에서 Entity -> Domain 변환을 mapper에서 진행

--- src/main/java/kitchenpos/dao/MenuRepositoryImpl.java
+++ src/main/java/kitchenpos/dao/MenuRepositoryImpl.java
@@ -6,7 +6,9 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 import kitchenpos.dao.entity.MenuEntity;
 import kitchenpos.dao.entity.MenuProductEntity;
-import kitchenpos.dao.entity.ProductEntity;
+import kitchenpos.dao.mapper.MenuGroupMapper;
+import kitchenpos.dao.mapper.MenuMapper;
+import kitchenpos.dao.mapper.ProductMapper;
 import kitchenpos.domain.Menu;
 import kitchenpos.domain.MenuGroup;
 import kitchenpos.domain.MenuProduct;
@@ -22,8 +24,12 @@ public class MenuRepositoryImpl implements MenuRepository {
   private final MenuProductDao menuProductDao;
   private final ProductDao productDao;
 
-  public MenuRepositoryImpl(final MenuDao menuDao, final MenuGroupDao menuGroupDao,
-      final MenuProductDao menuProductDao, final ProductDao productDao) {
+  public MenuRepositoryImpl(
+      final MenuDao menuDao,
+      final MenuGroupDao menuGroupDao,
+      final MenuProductDao menuProductDao,
+      final ProductDao productDao
+  ) {
     this.menuDao = menuDao;
     this.menuGroupDao = menuGroupDao;
     this.menuProductDao = menuProductDao;
@@ -40,86 +46,78 @@ public class MenuRepositoryImpl implements MenuRepository {
         )
     );
 
-    List<MenuProduct> savedMenuProducts = new ArrayList<>();
+    List<MenuProduct> savedMenuProducts = savedMenuProducts(menu, savedMenuEntity);
 
-    for (final MenuProduct menuProduct : menu.getMenuProducts()) {
-      final MenuProductEntity savedEntity = menuProductDao.save(
-          new MenuProductEntity(
-              savedMenuEntity.getId(),
-              menuProduct.getProduct().getId(),
-              menuProduct.getQuantity()
-          ));
-
-      savedMenuProducts.add(
-          new MenuProduct(savedEntity.getSeq(), menuProduct.getProduct(), savedEntity.getQuantity())
-      );
-    }
-
-    return new Menu(
-        savedMenuEntity.getId(),
-        savedMenuEntity.getName(),
-        savedMenuEntity.getPrice(),
+    return MenuMapper.mapToMenu(
+        savedMenuEntity,
         menu.getMenuGroup(),
         savedMenuProducts
     );
   }
 
-  private Menu mapToMenu(final MenuEntity menuEntity) {
-    return new Menu(
-        menuEntity.getId(),
-        menuEntity.getName(),
-        menuEntity.getPrice(),
-        findMenuGroup(menuEntity.getMenuGroupId()),
-        findMenuProducts(menuEntity.getId())
-    );
+  private List<MenuProduct> savedMenuProducts(final Menu menu, final MenuEntity savedMenuEntity) {
+    List<MenuProduct> savedMenuProducts = new ArrayList<>();
+
+    for (final MenuProduct menuProduct : menu.getMenuProducts()) {
+      final MenuProductEntity savedEntity = saveMenuProduct(savedMenuEntity, menuProduct);
+
+      savedMenuProducts.add(
+          MenuMapper.mapToMenuProduct(savedEntity, menuProduct.getProduct())
+      );
+    }
+
+    return savedMenuProducts;
+  }
+
+  private MenuProductEntity saveMenuProduct(
+      final MenuEntity savedMenuEntity,
+      final MenuProduct menuProduct
+  ) {
+    return menuProductDao.save(
+        new MenuProductEntity(
+            savedMenuEntity.getId(),
+            menuProduct.getProduct().getId(),
+            menuProduct.getQuantity()
+        ));
+  }
+
+  @Override
+  public Optional<Menu> findById(final Long id) {
+    return menuDao.findById(id)
+        .map(entity -> MenuMapper.mapToMenu(
+            entity,
+            findMenuGroup(entity.getMenuGroupId()),
+            findMenuProducts(entity.getId())
+        ));
   }
 
   private MenuGroup findMenuGroup(final Long menuGroupId) {
     return menuGroupDao.findById(menuGroupId)
-        .map(menuGroupEntity -> new MenuGroup(menuGroupEntity.getId(), menuGroupEntity.getName()))
+        .map(MenuGroupMapper::mapToMenuGroup)
         .orElseThrow(IllegalArgumentException::new);
   }
 
   private List<MenuProduct> findMenuProducts(final Long menuId) {
     return menuProductDao.findAllByMenuId(menuId)
         .stream()
-        .map(this::mapToMenuProduct)
+        .map(entity -> MenuMapper.mapToMenuProduct(entity, findProduct(entity)))
         .collect(Collectors.toList());
-  }
-
-  private MenuProduct mapToMenuProduct(final MenuProductEntity menuProductEntity) {
-    return new MenuProduct(
-        menuProductEntity.getSeq(),
-        findProduct(menuProductEntity),
-        menuProductEntity.getQuantity()
-    );
   }
 
   private Product findProduct(final MenuProductEntity menuProductEntity) {
     return productDao.findById(menuProductEntity.getProductId())
-        .map(this::mapToProduct)
+        .map(ProductMapper::mapToProduct)
         .orElseThrow(IllegalArgumentException::new);
-  }
-
-  private Product mapToProduct(final ProductEntity productEntity) {
-    return new Product(
-        productEntity.getId(),
-        productEntity.getName(),
-        productEntity.getPrice()
-    );
-  }
-
-  @Override
-  public Optional<Menu> findById(final Long id) {
-    return menuDao.findById(id)
-        .map(this::mapToMenu);
   }
 
   @Override
   public List<Menu> findAll() {
     return menuDao.findAll()
         .stream()
-        .map(this::mapToMenu)
+        .map(entity -> MenuMapper.mapToMenu(
+            entity,
+            findMenuGroup(entity.getMenuGroupId()),
+            findMenuProducts(entity.getId())))
         .collect(Collectors.toList());
   }
 
---
+++ src/main/java/kitchenpos/dao/mapper/MenuMapper.java
@@ -0,0 +1,37 @@
+package kitchenpos.dao.mapper;
+
+import java.util.List;
+import kitchenpos.dao.entity.MenuEntity;
+import kitchenpos.dao.entity.MenuProductEntity;
+import kitchenpos.domain.Menu;
+import kitchenpos.domain.MenuGroup;
+import kitchenpos.domain.MenuProduct;
+import kitchenpos.domain.Product;
+
+public class MenuMapper {
+
+  public static MenuProduct mapToMenuProduct(
+      final MenuProductEntity entity,
+      final Product product
+  ) {
+    return new MenuProduct(
+        entity.getSeq(),
+        product,
+        entity.getQuantity()
+    );
+  }
+
+  public static Menu mapToMenu(
+      final MenuEntity entity,
+      final MenuGroup menuGroup,
+      final List<MenuProduct> menuProducts
+  ) {
+    return new Menu(
+        entity.getId(),
+        entity.getName(),
+        entity.getPrice(),
+        menuGroup,
+        menuProducts
+    );
+  }
+}
