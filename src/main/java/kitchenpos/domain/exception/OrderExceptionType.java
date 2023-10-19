96fb3abcf95bbb4462d42dcb4df84c8aad899d9c
hong-sile <gurwns9325@naver.com>
2023-10-19 17:18:47 +0900

    refactor: OrderLineItem의 setter 제거

--- src/main/java/kitchenpos/domain/exception/OrderExceptionType.java
+++ src/main/java/kitchenpos/domain/exception/OrderExceptionType.java
@@ -4,7 +4,8 @@ import kitchenpos.basic.BasicExceptionType;
 
 public enum OrderExceptionType implements BasicExceptionType {
 
-    ORDER_IS_ALREADY_COMPLETION("이미 완료 상태의 주문은 변경할 수 없습니다.");
+    ORDER_IS_ALREADY_COMPLETION("이미 완료 상태의 주문입니다."),
+    ORDER_IS_NOT_COMPLETION("완료되지 않은 상태의 주문입니다.");
 
     private final String message;
 
