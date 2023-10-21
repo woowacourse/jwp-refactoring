72c00a5fc29b870bc8570f60bfc6ec9d040bcdf1
hgo641 <hgo641@gmail.com>
2023-10-21 16:57:08 +0900

    refactor: Menu에 검증 로직 추가

--- src/main/java/kitchenpos/domain/Menu.java
+++ src/main/java/kitchenpos/domain/Menu.java
@@ -3,6 +3,7 @@ package kitchenpos.domain;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Objects;
+import kitchenpos.exception.InvalidMenuException;
 
 public class Menu {
     private Long id;
@@ -21,11 +22,25 @@ public class Menu {
 
     public Menu(final Long id, final String name, final BigDecimal price, final Long menuGroupId,
                 final List<MenuProduct> menuProducts) {
+        validateMenuGroupId(menuGroupId);
+        validateMenuProducts(menuProducts);
         this.id = id;
         this.name = name;
         this.price = new Price(price);
         this.menuGroupId = menuGroupId;
         this.menuProducts = menuProducts;
+    }
+
+    private void validateMenuGroupId(final Long menuGroupId) {
+        if (menuGroupId == null) {
+            throw new InvalidMenuException("메뉴 그룹은 null일 수 없습니다.");
+        }
+    }
+
+    private void validateMenuProducts(final List<MenuProduct> menuProducts) {
+        if (menuProducts.isEmpty()) {
+            throw new InvalidMenuException("최소 한 개의 메뉴 상품이 등록되어야 합니다.");
+        }
     }
 
     public Long getId() {
