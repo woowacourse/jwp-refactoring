774878cd7117e61be7e9369734e22fdefb97fe28
amaran-th <songsy405@naver.com>
2023-10-21 22:36:02 +0900

    refactor: ProductRepository 인터페이스 추가 및 패키지 위치 변경

--- src/main/java/kitchenpos/product/domain/repository/ProductRepository.java
+++ src/main/java/kitchenpos/product/domain/repository/ProductRepository.java
@@ -2,36 +2,13 @@ package kitchenpos.product.domain.repository;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
-import kitchenpos.product.application.entity.ProductEntity;
-import kitchenpos.product.domain.Price;
 import kitchenpos.product.domain.Product;
-import kitchenpos.product.persistence.ProductDao;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public class ProductRepository {
+public interface ProductRepository {
 
-  private final ProductDao productDao;
+  Product save(final Product entity);
 
-  public ProductRepository(final ProductDao productDao) {
-    this.productDao = productDao;
-  }
+  Optional<Product> findById(final Long id);
 
-  public Product save(final Product entity) {
-    return productDao.save(ProductEntity.from(entity)).toProduct();
-  }
-
-  public Optional<Product> findById(final Long id) {
-    return productDao.findById(id).map(ProductEntity::toProduct);
-  }
-
-  public List<Product> findAll() {
-    return productDao.findAll()
-        .stream()
-        .map(product -> new Product(product.getId(), product.getName(),
-            new Price(product.getPrice())))
-        .collect(Collectors.toList());
-  }
-
+  List<Product> findAll();
 }
