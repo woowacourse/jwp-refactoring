22c01f18600d6833e649891c8d919484832eca76
parkmuhyeun <pjhg410@gmail.com>
2023-10-22 16:04:55 +0900

    refactor: 공백 추가

--- src/main/java/kitchenpos/domain/repository/MenuGroupRepository.java
+++ src/main/java/kitchenpos/domain/repository/MenuGroupRepository.java
@@ -2,5 +2,6 @@ package kitchenpos.domain.repository;
 
 import kitchenpos.domain.MenuGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 public interface MenuGroupRepository extends JpaRepository<MenuGroup, Long> {
 }
