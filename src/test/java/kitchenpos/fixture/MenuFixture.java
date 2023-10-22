35142c3f581292df1af425a41a06d8f2f79b2346
yoondgu <doyoungwork@gmail.com>
2023-10-22 11:57:48 +0900

    refactor: 사용하지 않는 픽스쳐 삭제

--- src/test/java/kitchenpos/fixture/MenuFixture.java
+++ src/test/java/kitchenpos/fixture/MenuFixture.java
@@ -1,8 +1,5 @@
 package kitchenpos.fixture;
 
-import static kitchenpos.fixture.ProductFixture.치킨_8000원;
-import static kitchenpos.fixture.ProductFixture.피자_8000원;
-
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -12,12 +9,6 @@ import kitchenpos.domain.MenuProduct;
 import kitchenpos.domain.Product;
 
 public class MenuFixture {
-
-    public static Menu 치킨_피자_세트_치킨_8000_1개_피자_8000_1개(final MenuGroup menuGroup) {
-        return new Menu("치킨 피자 세트", BigDecimal.valueOf(14000), menuGroup, List.of(
-                new MenuProduct(치킨_8000원(), 1), new MenuProduct(피자_8000원(), 1)
-        ));
-    }
 
     public static Menu 세트_메뉴_1개씩(
             final String name,
