6514c0de163b56abd6cdbff47a89c012fbc9fff8
yujamint <woawoa0513@gmail.com>
2023-10-22 14:19:06 +0900

    feat(Price): 금액 VO 구현

---
+++ src/main/java/kitchenpos/domain/Price.java
@@ -0,0 +1,45 @@
+package kitchenpos.domain;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class Price {
+
+    private final BigDecimal value;
+
+    public Price(BigDecimal value) {
+        validate(value);
+        this.value = value;
+    }
+
+    private void validate(BigDecimal value) {
+        if (Objects.isNull(value) || value.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public boolean isBiggerThan(BigDecimal other) {
+        return value.compareTo(other) > 0;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Price price = (Price) o;
+        return Objects.equals(value, price.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+}
