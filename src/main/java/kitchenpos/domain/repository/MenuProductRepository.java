6b5778fe608a3de51a16ad995f2fb638f1bfa4e8
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2023-10-22 21:42:59 +0900

    chore: 사용하지 않는 메서드를 제거한다.

--- src/main/java/kitchenpos/domain/repository/MenuProductRepository.java
+++ src/main/java/kitchenpos/domain/repository/MenuProductRepository.java
@@ -7,5 +7,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface MenuProductRepository extends JpaRepository<MenuProduct, Long> {
-    List<MenuProduct> findAllByMenu(Menu menu);
 }
