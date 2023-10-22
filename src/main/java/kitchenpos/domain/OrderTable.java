e27536d34c181d8f6899c01209d1138174d264a4
seokhwan-an <asok369258@naver.com>
2023-10-23 08:31:10 +0900

    refactor: 불필요한 @table 제거

--- src/main/java/kitchenpos/domain/OrderTable.java
+++ src/main/java/kitchenpos/domain/OrderTable.java
@@ -8,9 +8,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.persistence.Table;
 
-@Table(name = "order_table")
 @Entity
 public class OrderTable {
 
--- src/main/java/kitchenpos/domain/TableGroup.java
+++ src/main/java/kitchenpos/domain/TableGroup.java
@@ -9,9 +9,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-import javax.persistence.Table;
 
-@Table(name = "table_group")
 @Entity
 public class TableGroup {
 
