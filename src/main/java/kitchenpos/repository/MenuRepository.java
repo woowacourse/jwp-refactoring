716d859f1cfbafde53edbc5aea6575585e685dde
BGuga <tmdwns9913@gmail.com>
2023-10-23 01:14:15 +0900

    feat: 사용하지 않는 repository method 삭제

--- src/main/java/kitchenpos/repository/MenuRepository.java
+++ src/main/java/kitchenpos/repository/MenuRepository.java
@@ -8,7 +8,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface MenuRepository extends JpaRepository<Menu, Long> {
 
-    long countByIdIn(List<Long> menuIds);
-
     List<Menu> findAllByIdIn(List<Long> ids);
 }
