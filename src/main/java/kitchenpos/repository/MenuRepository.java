4b1c59235a8c315ca9ccea90f48ce06c89b73b3e
jaehee329 <jjhs9803@gmail.com>
2023-10-23 13:34:13 +0900

    refactor: 불필요한 어노테이션 제거

--- src/main/java/kitchenpos/repository/MenuRepository.java
+++ src/main/java/kitchenpos/repository/MenuRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface MenuRepository extends JpaRepository<Menu, Long> {
 
     @Query("SELECT m FROM Menu m JOIN FETCH m.menuGroup")
