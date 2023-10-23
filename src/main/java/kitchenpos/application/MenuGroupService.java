1fbf6474625a565b87790ac7540a0fd8deac8031
zillionme <beatus96@naver.com>
2023-10-23 13:40:53 +0900

    refactor: MenuGroup 생성 기능 수정

--- src/main/java/kitchenpos/application/MenuGroupService.java
+++ src/main/java/kitchenpos/application/MenuGroupService.java
@@ -1,7 +1,8 @@
 package kitchenpos.application;
 
-import kitchenpos.dao.MenuGroupDao;
+import kitchenpos.application.dto.MenuGroupCreateRequest;
 import kitchenpos.domain.MenuGroup;
+import kitchenpos.repository.MenuGroupRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -9,18 +10,22 @@ import java.util.List;
 
 @Service
 public class MenuGroupService {
-    private final MenuGroupDao menuGroupDao;
-
-    public MenuGroupService(final MenuGroupDao menuGroupDao) {
-        this.menuGroupDao = menuGroupDao;
+    
+    private final MenuGroupRepository menuGroupRepository;
+    
+    public MenuGroupService(final MenuGroupRepository menuGroupRepository) {
+        this.menuGroupRepository = menuGroupRepository;
     }
-
+    
     @Transactional
-    public MenuGroup create(final MenuGroup menuGroup) {
-        return menuGroupDao.save(menuGroup);
+    public MenuGroup create(final MenuGroupCreateRequest menuGroupCreateRequest) {
+        MenuGroup menuGroup = new MenuGroup(menuGroupCreateRequest.getName());
+        return menuGroupRepository.save(menuGroup);
     }
-
+    
+    
+    @Transactional(readOnly = true)
     public List<MenuGroup> list() {
-        return menuGroupDao.findAll();
+        return menuGroupRepository.findAll();
     }
 }
