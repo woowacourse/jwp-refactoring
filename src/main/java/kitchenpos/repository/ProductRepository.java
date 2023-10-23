cb885e59436f900fc6758d27ac66d9324dbe958c
yoondgu <doyoungwork@gmail.com>
2023-10-24 08:51:24 +0900

    refactor: 사용하지 않는 메서드 삭제

--- src/main/java/kitchenpos/repository/ProductRepository.java
+++ src/main/java/kitchenpos/repository/ProductRepository.java
@@ -1,12 +1,9 @@
 package kitchenpos.repository;
 
-import java.util.List;
 import kitchenpos.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     boolean existsById(Long productId);
-
-    List<Product> findAllByIdIn(List<Long> productIds);
 }
