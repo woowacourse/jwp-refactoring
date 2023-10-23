e3310a95a9429abb52960f0b4f66096b59e999a7
JunDonghyuk <ehdgur4814@naver.com>
2023-10-23 13:40:49 +0900

    refactor: OrderStatus 파싱 책임 이전

--- src/main/java/kitchenpos/application/OrderService.java
+++ src/main/java/kitchenpos/application/OrderService.java
@@ -105,18 +105,10 @@ public class OrderService {
     @Transactional
     public OrderDto changeOrderStatus(UpdateOrderStatusDto updateOrderStatusDto) {
         Long orderId = updateOrderStatusDto.getOrderId();
-        OrderStatus orderStatus = parseOrderStatus(updateOrderStatusDto.getOrderStatus());
+        OrderStatus orderStatus = OrderStatus.parseOrderStatus(updateOrderStatusDto.getOrderStatus());
         Order order = findOrder(orderId);
         order.changeOrderStatus(orderStatus);
         return OrderDto.from(order);
-    }
-
-    private OrderStatus parseOrderStatus(String orderStatus) {
-        try {
-            return OrderStatus.valueOf(orderStatus);
-        } catch (IllegalArgumentException e) {
-            throw new OrderException("유효하지 않은 주문상태라 주문상태를 변경할 수 없습니다.");
-        }
     }
 
     private Order findOrder(Long orderId) {
--- src/main/java/kitchenpos/domain/order/OrderStatus.java
+++ src/main/java/kitchenpos/domain/order/OrderStatus.java
@@ -1,5 +1,16 @@
 package kitchenpos.domain.order;
 
+import kitchenpos.exception.OrderException;
+
 public enum OrderStatus {
-    COOKING, MEAL, COMPLETION
+
+    COOKING, MEAL, COMPLETION;
+
+    public static OrderStatus parseOrderStatus(String orderStatus) {
+        try {
+            return OrderStatus.valueOf(orderStatus);
+        } catch (IllegalArgumentException e) {
+            throw new OrderException("유효하지 않은 주문상태라 주문상태를 변경할 수 없습니다.");
+        }
+    }
 }
