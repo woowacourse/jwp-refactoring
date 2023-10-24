fecc9cfc98282a69c8d4de1c31a0c0b3e640cc8a
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-10-24 17:57:41 +0900

    refactor: Menu 동등성 재정의 및 toString 추가, MenuServiceTest 코드 리팩터링

--- src/main/java/kitchenpos/domain/Menu.java
+++ src/main/java/kitchenpos/domain/Menu.java
@@ -13,6 +13,7 @@ import javax.persistence.OneToMany;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Entity
 public class Menu {
@@ -93,5 +94,27 @@ public class Menu {
 
     public void updateMenuProducts(final List<MenuProduct> menuProducts) {
         this.menuProducts = menuProducts;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Menu menu = (Menu) o;
+        return Objects.equals(id, menu.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Menu{" +
+               "id=" + id +
+               ", name='" + name + '\'' +
+               ", price=" + price +
+               '}';
     }
 }
--- src/test/java/kitchenpos/application/MenuServiceTest.java
+++ src/test/java/kitchenpos/application/MenuServiceTest.java
@@ -13,11 +13,13 @@ import kitchenpos.repository.MenuGroupRepository;
 import kitchenpos.repository.MenuProductRepository;
 import kitchenpos.repository.MenuRepository;
 import kitchenpos.repository.ProductRepository;
-import org.assertj.core.api.SoftAssertions;
+import org.assertj.core.api.*;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullSource;
@@ -26,6 +28,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import java.math.BigDecimal;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
@@ -48,110 +51,121 @@ class MenuServiceTest extends ServiceTestConfig {
     @Autowired
     ProductRepository productRepository;
 
-    private MenuGroup menuGroup;
-    private List<Product> products;
-    private Menu menu;
-    private List<MenuProduct> menuProducts;
+    @Nested
+    class 메뉴_등록 {
 
-    @BeforeEach
-    void setUp() {
-        menuGroup = menuGroupRepository.save(MenuGroupFixture.메뉴_그룹_생성());
-        products = productRepository.saveAll(ProductFixture.상품들_생성(2));
-        menu = menuRepository.save(MenuFixture.메뉴_생성(menuGroup, products));
-        final MenuProduct menuProduct1 = menuProductRepository.save(MenuProductFixture.메뉴_상품_생성(products.get(0), menu));
-        final MenuProduct menuProduct2 = menuProductRepository.save(MenuProductFixture.메뉴_상품_생성(products.get(1), menu));
-        menuProducts = List.of(menuProduct1, menuProduct2);
+        private MenuGroup menuGroup;
+        private List<Product> products;
+        private Menu menu;
+        private List<MenuProduct> menuProducts;
+
+        @BeforeEach
+        void setUp() {
+            menuGroup = menuGroupRepository.save(MenuGroupFixture.메뉴_그룹_생성());
+            products = productRepository.saveAll(ProductFixture.상품들_생성(2));
+            menu = menuRepository.save(MenuFixture.메뉴_생성(menuGroup, products));
+            final MenuProduct menuProduct1 = menuProductRepository.save(MenuProductFixture.메뉴_상품_생성(products.get(0), menu));
+            final MenuProduct menuProduct2 = menuProductRepository.save(MenuProductFixture.메뉴_상품_생성(products.get(1), menu));
+            menuProducts = List.of(menuProduct1, menuProduct2);
+        }
+
+        @Test
+        void 메뉴를_등록한다() {
+            // given
+            final Menu menu = MenuFixture.메뉴_생성(menuGroup, products);
+
+            // when
+            final Menu actual = menuService.create(menu);
+
+            // then
+            assertThat(actual.getId()).isPositive();
+        }
+
+        @ParameterizedTest
+        @NullSource
+        void 가격이_null이라면_예외를_반환한다(BigDecimal price) {
+            // given
+            final Menu menu = new Menu(MenuFixture.메뉴명, price, menuGroup, menuProducts);
+
+            // when & then
+            assertThatThrownBy(() -> menuService.create(menu))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        void 가격이_음수라면_예외를_반환한다() {
+            // given
+            final BigDecimal negative_price = BigDecimal.valueOf(-10_000);
+            final Menu menu = new Menu(MenuFixture.메뉴명, negative_price, menuGroup, menuProducts);
+
+            // when & then
+            assertThatThrownBy(() -> menuService.create(menu))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        @Disabled
+        void 존재하지_않는_메뉴_그룹이라면_예외를_반환한다() {
+            // given
+            final MenuGroup unsavedMenuGroup = MenuGroupFixture.메뉴_그룹_생성();
+            final Menu menu = MenuFixture.메뉴_생성(unsavedMenuGroup, products);
+
+            // when & then
+            assertThatThrownBy(() -> menuService.create(menu))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        @Disabled
+        void 존재하지_않는_상품이라면_예외를_반환한다() {
+            // given
+            final List<Product> unsavedProducts = ProductFixture.상품들_생성(2);
+            final Menu menu = MenuFixture.메뉴_생성(menuGroup, unsavedProducts);
+
+            // when & then
+            assertThatThrownBy(() -> menuService.create(menu))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        void 상품들의_값보다_메뉴의_가격이_더_크다면_예외를_반환한다() {
+            // given
+            final BigDecimal priceMoreThanProductsPrice = BigDecimal.valueOf(Integer.MAX_VALUE);
+            final Menu menu = new Menu(MenuFixture.메뉴명, priceMoreThanProductsPrice, menuGroup, menuProducts);
+
+            // when & then
+            assertThatThrownBy(() -> menuService.create(menu))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
     }
 
-    @Test
-    void 메뉴를_등록한다() {
-        // given
-        final Menu menu = MenuFixture.메뉴_생성(menuGroup, products);
+    @Nested
+    class 메뉴_목록_조회 {
 
-        // when
-        final Menu actual = menuService.create(menu);
+        private MenuGroup menuGroup;
+        private List<Product> products;
 
-        // then
-        SoftAssertions.assertSoftly(softAssertions -> {
-            softAssertions.assertThat(actual.getId()).isPositive();
-            softAssertions.assertThat(actual).usingRecursiveComparison()
-                          .ignoringFields("id")
-                          .isEqualTo(menu);
-        });
-    }
+        @BeforeEach
+        void setUp() {
+            menuGroup = menuGroupRepository.save(MenuGroupFixture.메뉴_그룹_생성());
+            products = productRepository.saveAll(ProductFixture.상품들_생성(2));
+        }
 
-    @ParameterizedTest
-    @NullSource
-    void 가격이_null이라면_예외를_반환한다(BigDecimal price) {
-        // given
-        final Menu menu = new Menu(MenuFixture.메뉴명, price, menuGroup, menuProducts);
+        @Test
+        void 메뉴_목록을_조회한다() {
+            // given
+            final List<Menu> menus = menuRepository.saveAll(MenuFixture.메뉴들_생성(3, menuGroup, products));
 
-        // when & then
-        assertThatThrownBy(() -> menuService.create(menu))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
+            // when
+            final List<Menu> actual = menuService.list();
 
-    @Test
-    void 가격이_음수라면_예외를_반환한다() {
-        // given
-        final BigDecimal negative_price = BigDecimal.valueOf(-10_000);
-        final Menu menu = new Menu(MenuFixture.메뉴명, negative_price, menuGroup, menuProducts);
-
-        // when & then
-        assertThatThrownBy(() -> menuService.create(menu))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @Test
-    void 존재하지_않는_메뉴_그룹이라면_예외를_반환한다() {
-        // given
-        final MenuGroup unsavedMenuGroup = MenuGroupFixture.메뉴_그룹_생성();
-        final Menu menu = new Menu(MenuFixture.메뉴명, BigDecimal.valueOf(10_000), unsavedMenuGroup, menuProducts);
-
-        // when & then
-        assertThatThrownBy(() -> menuService.create(menu))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @Test
-    void 존재하지_않는_상품이라면_예외를_반환한다() {
-        // given
-        final List<MenuProduct> unsavedMenuProducts = MenuProductFixture.메뉴_상품들_생성(products, menu);
-        final Menu menu = new Menu(MenuFixture.메뉴명, BigDecimal.valueOf(10_000), menuGroup, unsavedMenuProducts);
-
-        // when & then
-        assertThatThrownBy(() -> menuService.create(menu))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @Test
-    void 상품들의_값보다_메뉴의_가격이_더_크다면_예외를_반환한다() {
-        // given
-        final BigDecimal priceMoreThanProductsPrice = BigDecimal.valueOf(Integer.MAX_VALUE);
-        final Menu menu = new Menu(MenuFixture.메뉴명, priceMoreThanProductsPrice, menuGroup, menuProducts);
-
-        // when & then
-        assertThatThrownBy(() -> menuService.create(menu))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @Test
-    void 메뉴_목록을_조회한다() {
-        // given
-        final List<Menu> menus = MenuFixture.메뉴들_생성(3, menuGroup, products);
-
-        // when
-        final List<Menu> actual = menuService.list();
-
-        // then
-        SoftAssertions.assertSoftly(softAssertions -> {
-            softAssertions.assertThat(actual).hasSize(2);
-            softAssertions.assertThat(actual.get(0)).usingRecursiveComparison()
-                          .isEqualTo(menus.get(0));
-            softAssertions.assertThat(actual.get(1)).usingRecursiveComparison()
-                          .isEqualTo(menus.get(1));
-            softAssertions.assertThat(actual.get(2)).usingRecursiveComparison()
-                          .isEqualTo(menus.get(2));
-        });
+            // then
+            SoftAssertions.assertSoftly(softAssertions -> {
+                softAssertions.assertThat(actual).hasSize(3);
+                softAssertions.assertThat(actual.get(0)).isEqualTo(menus.get(0));
+                softAssertions.assertThat(actual.get(1)).isEqualTo(menus.get(1));
+                softAssertions.assertThat(actual.get(2)).isEqualTo(menus.get(2));
+            });
+        }
     }
 }
