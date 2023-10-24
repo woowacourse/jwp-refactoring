f2bc1e87881d61c3593a284eb1e727e3f04e9217
swonny <wonny921@gmail.com>
2023-10-24 21:15:04 +0900

    refactor: menu 컨트롤러, 서비스 전달 시 dto 사용하도록 수정

---
+++ src/main/java/kitchenpos/ui/dto/CreateMenuRequest.java
@@ -0,0 +1,45 @@
+package kitchenpos.ui.dto;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class CreateMenuRequest {
+
+    private Long id;
+
+    private Long menuGroupId;
+
+    private List<MenuProductDto> menuProductDtos;
+
+    private String name;
+
+    private BigDecimal price;
+
+    public CreateMenuRequest(final Long id, final Long menuGroupId, final List<MenuProductDto> menuProductDtos, final String name, final BigDecimal price) {
+        this.id = id;
+        this.menuGroupId = menuGroupId;
+        this.menuProductDtos = menuProductDtos;
+        this.name = name;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getMenuGroupId() {
+        return menuGroupId;
+    }
+
+    public List<MenuProductDto> getMenuProductDtos() {
+        return menuProductDtos;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+}
