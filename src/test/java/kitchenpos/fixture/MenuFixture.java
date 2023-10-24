bfc8859ec36a11acaf57c55e20093a96eea2c461
hgo641 <hgo641@gmail.com>
2023-10-24 14:40:30 +0900

    refactor: 픽스처 연관관계 설정 방법 수정

--- src/test/java/kitchenpos/fixture/MenuFixture.java
+++ src/test/java/kitchenpos/fixture/MenuFixture.java
@@ -1,39 +1,12 @@
 package kitchenpos.fixture;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.List;
 import kitchenpos.domain.Menu;
 import kitchenpos.domain.MenuProduct;
+import kitchenpos.dto.MenuRequest;
 
 public class MenuFixture {
-
-    public static Menu createChickenSetMenuById(final Long id) {
-        final Menu chickenSetMenu = new Menu(
-                "치킨+콜라 세트",
-                BigDecimal.valueOf(10000),
-                1L,
-                List.of(CHICKEN_MENU_PRODUCT)
-        );
-        chickenSetMenu.setId(id);
-        return chickenSetMenu;
-    }
-
-    public static Menu CHICKEN_SET_MENU_NON_ID = new Menu(
-            null,
-            "치킨+콜라 세트",
-            BigDecimal.valueOf(11000),
-            1L,
-            new ArrayList<>()
-    );
-
-    public static Menu CHICKEN_SET_MENU = new Menu(
-            1L,
-            "치킨+콜라 세트",
-            BigDecimal.valueOf(11000),
-            1L,
-            new ArrayList<>()
-    );
 
     public static MenuProduct CHICKEN_MENU_PRODUCT = new MenuProduct(
             1L,
@@ -49,7 +22,37 @@ public class MenuFixture {
             1
     );
 
-    static {
-        CHICKEN_SET_MENU.setMenuProducts(List.of(CHICKEN_MENU_PRODUCT, COKE_MENU_PRODUCT));
+    public static Menu CHICKEN_SET_MENU_NON_ID = new Menu(
+            null,
+            "치킨+콜라 세트",
+            BigDecimal.valueOf(11000),
+            1L,
+            List.of(CHICKEN_MENU_PRODUCT, COKE_MENU_PRODUCT)
+    );
+
+    public static Menu CHICKEN_SET_MENU = new Menu(
+            1L,
+            "치킨+콜라 세트",
+            BigDecimal.valueOf(11000),
+            1L,
+            List.of(CHICKEN_MENU_PRODUCT, COKE_MENU_PRODUCT)
+    );
+
+    public static Menu createChickenSetMenuById(final Long id) {
+        final Menu chickenSetMenu = new Menu(
+                "치킨 세트",
+                BigDecimal.valueOf(10000),
+                1L,
+                List.of(CHICKEN_MENU_PRODUCT)
+        );
+        chickenSetMenu.setId(id);
+        return chickenSetMenu;
     }
+
+    public static MenuRequest CHICKEN_SET_MENU_REQUEST = new MenuRequest(
+            "치킨 세트",
+            BigDecimal.valueOf(10000),
+            1L,
+            List.of(CHICKEN_MENU_PRODUCT)
+    );
 }
