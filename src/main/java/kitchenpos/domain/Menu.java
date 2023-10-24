0153e0743ba8d329142922eb450cc8adcd29d59a
Ohjintaek <jt950930@gmail.com>
2023-10-24 13:57:23 +0900

    refactor: Menu 도메인에 비즈니스 로직 이관

--- src/main/java/kitchenpos/domain/Menu.java
+++ src/main/java/kitchenpos/domain/Menu.java
@@ -2,6 +2,7 @@ package kitchenpos.domain;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
+import java.util.Objects;
 
 @Entity
 public class Menu {
@@ -28,10 +29,17 @@ public class Menu {
     }
 
     public Menu(final Long id, final String name, final BigDecimal price, final MenuGroup menuGroup) {
+        validatePrice(price);
         this.id = id;
         this.name = name;
         this.price = price;
         this.menuGroup = menuGroup;
+    }
+
+    private void validatePrice(final BigDecimal price) {
+        if (Objects.isNull(price) || price.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException();
+        }
     }
 
     public Long getId() {
