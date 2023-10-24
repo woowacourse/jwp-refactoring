d9ed38c7b70856138fb5fcf52cde6503e3719c2d
zillionme <beatus96@naver.com>
2023-10-24 16:37:10 +0900

    test: MenuServiceTest 수정  MenuProductRepository 추가

--- src/test/java/kitchenpos/fixture/MenuFixture.java
+++ src/test/java/kitchenpos/fixture/MenuFixture.java
@@ -3,16 +3,15 @@ package kitchenpos.fixture;
 import kitchenpos.domain.Menu;
 import kitchenpos.domain.MenuGroup;
 import kitchenpos.domain.MenuProduct;
-import kitchenpos.domain.MenuPrice;
 
 import java.math.BigDecimal;
 import java.util.List;
 
 public class MenuFixture {
-
+    
     public static Menu MENU(String name, Long price, MenuGroup menuGroup, List<MenuProduct> menuProducts) {
-        return new Menu(name,
-                new MenuPrice(BigDecimal.valueOf(price)),
+        return Menu.of(name,
+                BigDecimal.valueOf(price),
                 menuGroup,
                 menuProducts);
     }
