3903c1e21169675505d07ed57557d0980ea9c386
hgo641 <hgo641@gmail.com>
2023-10-24 20:18:49 +0900

    refactor: 동등성 비교 로직 추가

--- src/main/java/kitchenpos/domain/Price.java
+++ src/main/java/kitchenpos/domain/Price.java
@@ -1,6 +1,7 @@
 package kitchenpos.domain;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 import kitchenpos.exception.InvalidPriceException;
 
 public class Price {
@@ -19,5 +20,22 @@ public class Price {
 
     public BigDecimal getValue() {
         return this.value;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Price)) {
+            return false;
+        }
+        final Price price = (Price) o;
+        return Objects.equals(value, price.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
     }
 }
