89e39da819125b0337559cb79a11266fab368694
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-10-24 13:30:39 +0900

    refactor: 서비스에서 MenuRepository를 사용하도록 수정

--- src/test/java/kitchenpos/application/MenuServiceTest.java
+++ src/test/java/kitchenpos/application/MenuServiceTest.java
@@ -33,7 +33,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 @SuppressWarnings("NonAsciiCharacters")
 class MenuServiceTest {
 
-    @InjectMocks
+    @Autowired
     MenuService menuService;
 
     @Autowired
