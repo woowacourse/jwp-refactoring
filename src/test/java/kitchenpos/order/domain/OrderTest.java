3fd36b7455dfaa5890af57c1e4b5028fd71bb75c
Youngho <cyh6099@gmail.com>
2023-10-25 00:09:21 +0900

    test: Order 테스트 작성

--- src/test/java/kitchenpos/order/domain/OrderTest.java
+++ src/test/java/kitchenpos/order/domain/OrderTest.java
@@ -1,40 +1,23 @@
 package kitchenpos.order.domain;
 
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class OrderTest {
+    @Test
+    void 주문의_상태가_COMPLETION_인지_확인한다() {
+        Order order = new Order(1L, LocalDateTime.now());
+        order.changeOrderStatus(OrderStatus.COMPLETION);
+        assertThat(order.isCompleted()).isTrue();
+    }
 
-//    private final Menu pizza = new Menu(1L, "pizza", BigDecimal.TEN, 1L,
-//            List.of(new MenuProduct(1L, 3)));
+    @Test
+    void 주문의_상태가_COMPLETION이_아닌지_확인한다() {
+        Order order = new Order(1L, LocalDateTime.now());
 
-//    @Test
-//    void 빈_테이블이_주문_생성_시_예외_발생() {
-//        assertThatThrownBy(() -> new Order(1L, OrderStatus.COOKING, LocalDateTime.now()))
-//                .isInstanceOf(IllegalArgumentException.class);
-//    }
-//
-//    @Test
-//    void Order의_OrderLineItems_와_주문하려는_메뉴_개수가_다르면_예외발생() {
-//        Order order = new Order(new OrderTable(1L, 3, false), OrderStatus.COOKING, LocalDateTime.now());
-//        order.addOrderLineItem(new OrderLineItem(pizza, 3));
-//        assertThatThrownBy(() -> order.checkEqualMenuCount(2))
-//                .isInstanceOf(IllegalArgumentException.class);
-//    }
-//
-//    @Test
-//    void Order의_OrderLineItems_와_주문하려는_메뉴_개수가_같으면_정상() {
-//        Order order = new Order(new OrderTable(1L, 3, false), OrderStatus.COOKING, LocalDateTime.now());
-//        order.addOrderLineItem(new OrderLineItem(pizza, 3));
-//        assertDoesNotThrow(() -> order.checkEqualMenuCount(1));
-//    }
-//
-//    @Test
-//    void 주문의_상태가_COMPLETION_인지_확인한다() {
-//        Order order = new Order(new OrderTable(1L, 3, false), OrderStatus.COMPLETION, LocalDateTime.now());
-//        assertThat(order.isCompleted()).isTrue();
-//    }
-//
-//    @Test
-//    void 주문의_상태가_COMPLETION이_아닌지_확인한다() {
-//        Order order = new Order(new OrderTable(1L, 3, false), OrderStatus.COOKING, LocalDateTime.now());
-//        assertThat(order.isCompleted()).isFalse();
-//    }
+        assertThat(order.isCompleted()).isFalse();
+    }
 }
