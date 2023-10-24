2873852f08d3e4cbc6344139ab459d48992cf879
zillionme <beatus96@naver.com>
2023-10-24 18:05:30 +0900

    test: TableGroupServiceTest 수정

--- src/test/java/kitchenpos/application/TableGroupServiceTest.java
+++ src/test/java/kitchenpos/application/TableGroupServiceTest.java
@@ -1,22 +1,20 @@
 package kitchenpos.application;
 
-import kitchenpos.dao.OrderDao;
-import kitchenpos.dao.OrderTableDao;
-import kitchenpos.domain.Order;
-import kitchenpos.domain.OrderStatus;
+import kitchenpos.application.dto.TableGroupCreateRequest;
 import kitchenpos.domain.OrderTable;
 import kitchenpos.domain.TableGroup;
+import kitchenpos.domain.exception.InvalidTableGroupException;
+import kitchenpos.domain.exception.NotExistOrderTable;
+import kitchenpos.repository.OrderRepository;
+import kitchenpos.repository.OrderTableRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
-import static kitchenpos.fixture.OrderFixture.ORDER;
 import static kitchenpos.fixture.OrderTableFixture.ORDER_TABLE;
-import static kitchenpos.fixture.TableGroupFixture.TABLE_GROUP;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
@@ -26,10 +24,10 @@ import static org.assertj.core.api.SoftAssertions.assertSoftly;
 class TableGroupServiceTest {
     
     @Autowired
-    private OrderDao orderDao;
+    private OrderRepository orderRepository;
     
     @Autowired
-    private OrderTableDao orderTableDao;
+    private OrderTableRepository orderTableRepository;
     
     @Autowired
     TableGroupService tableGroupService;
@@ -38,13 +36,13 @@ class TableGroupServiceTest {
     void 단체_테이블을_생성할_때_개별_테이블의_개수가_1개_이하이면_예외가_발생한다() {
         //given
         OrderTable orderTable1 = ORDER_TABLE(true, 1);
-        OrderTable savedOrderTable1 = orderTableDao.save(orderTable1);
+        OrderTable savedOrderTable1 = orderTableRepository.save(orderTable1);
         
         //when & then
-        TableGroup tableGroupWithOneTable = TABLE_GROUP(List.of(savedOrderTable1));
+        TableGroupCreateRequest tableGroupCreateRequest = new TableGroupCreateRequest(List.of(savedOrderTable1.getId()));
         
-        assertThatThrownBy(() -> tableGroupService.create(tableGroupWithOneTable))
-                .isInstanceOf(IllegalArgumentException.class)
+        assertThatThrownBy(() -> tableGroupService.create(tableGroupCreateRequest))
+                .isInstanceOf(InvalidTableGroupException.class)
                 .hasMessageContaining("개별 테이블 수가 2개 미만이면 단체 테이블을 만들 수 없습니다");
     }
     
@@ -52,30 +50,31 @@ class TableGroupServiceTest {
     void 단체_테이블을_생성할_때_존재하지_않는_테이블이_있으면_예외가_발생한다() {
         //given
         OrderTable orderTable1 = ORDER_TABLE(true, 1);
-        OrderTable savedOrderTable1 = orderTableDao.save(orderTable1);
-        OrderTable orderTable2 = ORDER_TABLE(true, 1);
+        OrderTable savedOrderTable1 = orderTableRepository.save(orderTable1);
+        Long notExistOrderTableId = 2L;
         
         //when & then
-        TableGroup tableGroupWithNotExistTable = TABLE_GROUP(List.of(savedOrderTable1, orderTable2));
+        TableGroupCreateRequest tableGroupCreateRequest = new TableGroupCreateRequest(List.of(savedOrderTable1.getId(), notExistOrderTableId));
         
-        assertThatThrownBy(() -> tableGroupService.create(tableGroupWithNotExistTable))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("존재하지 않는 테이블이 있습니다");
+        assertThatThrownBy(() -> tableGroupService.create(tableGroupCreateRequest))
+                .isInstanceOf(NotExistOrderTable.class)
+                .hasMessageContaining("존재하지 않는 테이블입니다");
     }
     
     @Test
     void 단체_테이블을_생성할_때_빈_테이블이_아닌_테이블이_있으면_예외가_발생한다() {
         //given
         OrderTable orderTable1 = ORDER_TABLE(true, 1);
-        OrderTable savedOrderTable1 = orderTableDao.save(orderTable1);
+        OrderTable savedOrderTable1 = orderTableRepository.save(orderTable1);
         OrderTable EmptyTable = ORDER_TABLE(false, 1);
-        OrderTable savedEmptyTable = orderTableDao.save(EmptyTable);
+        OrderTable savedEmptyTable2 = orderTableRepository.save(EmptyTable);
         
         //when & then
-        TableGroup tableGroupWithEmptyTable = TABLE_GROUP(List.of(savedOrderTable1, savedEmptyTable));
+        TableGroupCreateRequest tableGroupCreateRequest = new TableGroupCreateRequest(List.of(savedOrderTable1.getId(), savedEmptyTable2.getId()));
         
-        assertThatThrownBy(() -> tableGroupService.create(tableGroupWithEmptyTable))
-                .isInstanceOf(IllegalArgumentException.class)
+        
+        assertThatThrownBy(() -> tableGroupService.create(tableGroupCreateRequest))
+                .isInstanceOf(InvalidTableGroupException.class)
                 .hasMessageContaining("비어있지 않은 테이블은 단체 테이블으로 만들 수 없습니다");
     }
     
@@ -83,21 +82,24 @@ class TableGroupServiceTest {
     void 단체_테이블을_생성할_때_이미_다른_테이블_그룹에_속하는_테이블이_있으면_예외가_발생한다() {
         //given
         OrderTable orderTable1 = ORDER_TABLE(true, 1);
-        OrderTable savedOrderTable1 = orderTableDao.save(orderTable1);
+        OrderTable savedOrderTable1 = orderTableRepository.save(orderTable1);
         
         OrderTable orderTableInAnotherTableGroup1 = ORDER_TABLE(true, 1);
-        OrderTable savedOrderTableInAnotherTableGroup1 = orderTableDao.save(orderTableInAnotherTableGroup1);
+        OrderTable savedOrderTableInAnotherTableGroup1 = orderTableRepository.save(orderTableInAnotherTableGroup1);
         OrderTable orderTableInAnotherTableGroup2 = ORDER_TABLE(true, 1);
-        OrderTable savedOrderTableInAnotherTableGroup2 = orderTableDao.save(orderTableInAnotherTableGroup2);
-        TableGroup anotherTableGroup = tableGroupService.create(TABLE_GROUP(List.of(savedOrderTableInAnotherTableGroup1, savedOrderTableInAnotherTableGroup2)));
+        OrderTable savedOrderTableInAnotherTableGroup2 = orderTableRepository.save(orderTableInAnotherTableGroup2);
+        TableGroupCreateRequest tableGroupCreateRequest = new TableGroupCreateRequest(
+                List.of(savedOrderTableInAnotherTableGroup1.getId(), savedOrderTableInAnotherTableGroup2.getId()));
+        TableGroup anotherTableGroup = tableGroupService.create(tableGroupCreateRequest);
+        orderTableInAnotherTableGroup1.setTableGroup(anotherTableGroup);
+        orderTableInAnotherTableGroup2.setTableGroup(anotherTableGroup);
         
         //when & then
-        OrderTable orderTableInAnotherTableGroup = anotherTableGroup.getOrderTables().get(1);
-        orderTableInAnotherTableGroup.changeEmpty(true);
-        TableGroup tableGroupWithTableInAnotherTableGroup = TABLE_GROUP(List.of(savedOrderTable1, orderTableInAnotherTableGroup));
+        TableGroupCreateRequest tableGroupWithTableInAnotherTableGroupCreateRequest = new TableGroupCreateRequest(
+                List.of(savedOrderTableInAnotherTableGroup1.getId(), savedOrderTable1.getId()));
         
-        assertThatThrownBy(() -> tableGroupService.create(tableGroupWithTableInAnotherTableGroup))
-                .isInstanceOf(IllegalArgumentException.class)
+        assertThatThrownBy(() -> tableGroupService.create(tableGroupWithTableInAnotherTableGroupCreateRequest))
+                .isInstanceOf(InvalidTableGroupException.class)
                 .hasMessageContaining("다른 테이블 그룹에 속한 테이블은 단체 테이블로 만들 수 없습니다");
     }
     
@@ -105,15 +107,16 @@ class TableGroupServiceTest {
     void 단체_테이블을_생성할_때_모든_조건을_만족하면_정상적으로_단체_테이블을_생성한다() {
         //given
         OrderTable orderTable1 = ORDER_TABLE(true, 1);
-        OrderTable savedOrderTable1 = orderTableDao.save(orderTable1);
+        OrderTable savedOrderTable1 = orderTableRepository.save(orderTable1);
         
         OrderTable orderTable2 = ORDER_TABLE(true, 1);
-        OrderTable savedOrderTable2 = orderTableDao.save(orderTable2);
+        OrderTable savedOrderTable2 = orderTableRepository.save(orderTable2);
         
         //when
-        TableGroup tableGroup = TABLE_GROUP(List.of(savedOrderTable1, savedOrderTable2));
-        TableGroup actual = tableGroupService.create(tableGroup);
-        List<OrderTable> actualOrderTablesInOrderGroup = orderTableDao.findAllByTableGroupId(actual.getId());
+        TableGroupCreateRequest tableGroupCreateRequest = new TableGroupCreateRequest(
+                List.of(savedOrderTable1.getId(), savedOrderTable2.getId()));
+        TableGroup actual = tableGroupService.create(tableGroupCreateRequest);
+        List<OrderTable> actualOrderTablesInOrderGroup = orderTableRepository.findAllByTableGroupId(actual.getId());
         
         //then
         assertSoftly(softly -> {
@@ -124,57 +127,4 @@ class TableGroupServiceTest {
         });
     }
     
-    @Test
-    void 단체_테이블을_해제할_때_존재하지않는_테이블이_있으면_예외를_발생한다() {
-        //given
-        OrderTable orderTable = ORDER_TABLE(true, 1);
-        OrderTable savedOrderTable1 = orderTableDao.save(orderTable);
-        
-        OrderTable orderTable2 = ORDER_TABLE(true, 1);
-        OrderTable savedOrderTable2 = orderTableDao.save(orderTable2);
-        
-        List<OrderTable> orderTables = List.of(savedOrderTable1, savedOrderTable2);
-        TableGroup tableGroup = TABLE_GROUP(orderTables);
-        TableGroup savedTableGroup = tableGroupService.create(tableGroup);
-        
-        //when & then
-        Order order1 = ORDER(savedOrderTable1, List.of(), OrderStatus.COOKING);
-        orderDao.save(order1);
-        
-        assertThatThrownBy(() -> tableGroupService.ungroup(savedTableGroup.getId()))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("테이블의 상태가 COOKING 혹은 MEAL이면 단체 테이블을 해제할 수 없습니다");
-    }
-    
-    @Test
-    void 단체_테이블을_해제할_때_모든_조건을_만족하면_개별_테이블들을_해제한다() {
-        //given
-        OrderTable orderTable = ORDER_TABLE(true, 1);
-        OrderTable orderTable1 = orderTableDao.save(orderTable);
-        
-        OrderTable orderTable2 = ORDER_TABLE(true, 1);
-        OrderTable save = orderTableDao.save(orderTable2);
-        
-        List<OrderTable> orderTables = List.of(orderTable1, save);
-        TableGroup tableGroup = TABLE_GROUP(orderTables);
-        TableGroup savedTableGroup = tableGroupService.create(tableGroup);
-        
-        Order order1 = ORDER(orderTable1, List.of(), OrderStatus.COMPLETION);
-        orderDao.save(order1);
-        
-        //when : 저장된 테이블의 id로, 테이블의 현재 단체 테이블 id를 추적한다.
-        List<OrderTable> orderTablesInTableGroup = orderTableDao.findAllByTableGroupId(savedTableGroup.getId());
-        final List<Long> orderTableIds = orderTablesInTableGroup.stream()
-                                                                .map(OrderTable::getId)
-                                                                .collect(Collectors.toList());
-        tableGroupService.ungroup(savedTableGroup.getId());
-        
-        //then : 해제된 모든 개별테이블의 단체테이블id가 null이고, 빈 테이블이 아니다.
-        assertSoftly(softly -> {
-            for (OrderTable tableNotInTableGroup : orderTableDao.findAllByIdIn(orderTableIds)) {
-                softly.assertThat(tableNotInTableGroup.getTableGroup()).isNull();
-                softly.assertThat(tableNotInTableGroup.isEmpty()).isFalse();
-            }
-        });
-    }
 }
