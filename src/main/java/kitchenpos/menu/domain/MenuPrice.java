a2cbbd4514eb87bc9ca84df13731d741f7eda567
parkmuhyeun <pjhg410@gmail.com>
2023-10-26 02:31:06 +0900

    refactor: equals, hashcode 추가

--- src/main/java/kitchenpos/menu/domain/MenuPrice.java
+++ src/main/java/kitchenpos/menu/domain/MenuPrice.java
@@ -18,4 +18,17 @@ public class MenuPrice {
         }
         this.price = price;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MenuPrice menuPrice = (MenuPrice) o;
+        return Objects.equals(price, menuPrice.price);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(price);
+    }
 }
