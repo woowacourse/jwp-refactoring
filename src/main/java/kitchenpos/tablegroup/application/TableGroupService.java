87d12f287d05eced5241835d89870cec946d1850
JunH <tjdtls690@gmail.com>
2023-10-25 10:51:42 +0900

    :art: refactor : TableGroupService의 create 로직의 동작 효율성 개선

--- src/main/java/kitchenpos/tablegroup/application/TableGroupService.java
+++ src/main/java/kitchenpos/tablegroup/application/TableGroupService.java
@@ -10,6 +10,7 @@ import kitchenpos.tablegroup.dto.request.TableGroupCreateRequest;
 import kitchenpos.tablegroup.repository.TableGroupRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.CollectionUtils;
 
 @Service
 public class TableGroupService {
@@ -30,15 +31,26 @@ public class TableGroupService {
     @Transactional
     public Long create(final TableGroupCreateRequest request) {
         final List<Long> orderedTableIds = request.orderTableIds();
-        final List<OrderTable> savedOrderTables = orderTableRepository.findAllByIdIn(orderedTableIds);
+        validateOrderTableCount(orderedTableIds);
 
-        if (orderedTableIds.size() != savedOrderTables.size()) {
-            throw new IllegalArgumentException("[ERROR] 존재하지 않는 주문 테이블이 있습니다.");
-        }
+        final List<OrderTable> savedOrderTables = orderTableRepository.findAllByIdIn(orderedTableIds);
+        validateNotExistOrderTable(orderedTableIds, savedOrderTables);
 
         final TableGroup tableGroup = TableGroup.createWithNowCreatedDate();
         tableGroup.addOrderTables(savedOrderTables);
         return tableGroupRepository.save(tableGroup).id();
+    }
+
+    private void validateOrderTableCount(final List<Long> orderedTableIds) {
+        if (CollectionUtils.isEmpty(orderedTableIds) || orderedTableIds.size() < 2) {
+            throw new IllegalArgumentException("[ERROR] 주문 테이블이 없거나 2개 미만일 수 없습니다.");
+        }
+    }
+
+    private void validateNotExistOrderTable(final List<Long> orderedTableIds, final List<OrderTable> savedOrderTables) {
+        if (orderedTableIds.size() != savedOrderTables.size()) {
+            throw new IllegalArgumentException("존재하지 않는 주문 테이블이 있습니다.");
+        }
     }
 
     @Transactional
