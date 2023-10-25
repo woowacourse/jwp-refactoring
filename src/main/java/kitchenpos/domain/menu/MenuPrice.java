8f8dd2573181467a38e2467dcb6d342cb96f4b37
kevstevie <jianna442@gmail.com>
2023-10-25 14:02:27 +0900

    fix: price 직렬화 문제 수정

--- src/main/java/kitchenpos/domain/menu/MenuPrice.java
+++ src/main/java/kitchenpos/domain/menu/MenuPrice.java
@@ -6,6 +6,10 @@ public class MenuPrice {
 
     private final BigDecimal value;
 
+    public MenuPrice(long value) {
+        this(BigDecimal.valueOf(value));
+    }
+
     public MenuPrice(BigDecimal value) {
         if (value == null || value.compareTo(BigDecimal.ZERO) < 0) {
             throw new IllegalArgumentException("메뉴의 가격은 0원 이상이어야 한다.");
--- src/main/java/kitchenpos/domain/product/ProductPrice.java
+++ src/main/java/kitchenpos/domain/product/ProductPrice.java
@@ -6,6 +6,10 @@ public class ProductPrice {
 
     private final BigDecimal value;
 
+    public ProductPrice(long value) {
+        this(BigDecimal.valueOf(value));
+    }
+
     public ProductPrice(BigDecimal value) {
         if (value == null || value.compareTo(BigDecimal.ZERO) < 0) {
             throw new IllegalArgumentException("상품의 가격은 0원 이상이어야 한다.");
