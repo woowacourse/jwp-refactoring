f38539a917c21173d9043a3a50d29a617fc06ea3
apptie <appti@naver.com>
2023-10-25 12:32:03 +0900

    refactor: 일대다 연관관계 조회 시 fetch join 적용

--- src/main/java/kitchenpos/domain/menu/repository/MenuRepository.java
+++ src/main/java/kitchenpos/domain/menu/repository/MenuRepository.java
@@ -1,7 +1,13 @@
 package kitchenpos.domain.menu.repository;
 
+import java.util.Optional;
 import kitchenpos.domain.menu.Menu;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MenuRepository extends JpaRepository<Menu, Long> {
+
+    @Query("select m from Menu m join fetch m.menuProducts where m.id = :id")
+    Optional<Menu> findById(@Param("id") final Long id);
 }
--- src/main/java/kitchenpos/domain/order/repository/OrderRepository.java
+++ src/main/java/kitchenpos/domain/order/repository/OrderRepository.java
@@ -1,10 +1,16 @@
 package kitchenpos.domain.order.repository;
 
 import java.util.List;
+import java.util.Optional;
 import kitchenpos.domain.order.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    @Query("select o From Order o join fetch o.orderLineItems.values where o.id = :id")
+    Optional<Order> findById(@Param("id") final Long id);
 
     List<Order> findAllByOrderTableId(final Long orderTableId);
 }
