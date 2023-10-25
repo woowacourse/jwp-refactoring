e8953897051e6c92f41395a89f32bb93cd328941
swonny <wonny921@gmail.com>
2023-10-25 20:27:41 +0900

    test: 실패하는 테스트 수정

--- src/test/java/kitchenpos/application/fixture/MenuGroupServiceFixture.java
+++ src/test/java/kitchenpos/application/fixture/MenuGroupServiceFixture.java
@@ -1,25 +1,30 @@
 package kitchenpos.application.fixture;
 
 import kitchenpos.domain.MenuGroup;
+import kitchenpos.infrastructure.persistence.JpaMenuGroupRepository;
 import kitchenpos.ui.dto.CreateMenuGroupRequest;
-import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 @SuppressWarnings("NonAsciiCharacters")
 public class MenuGroupServiceFixture {
 
+    @Autowired
+    private JpaMenuGroupRepository menuGroupRepository;
+
     protected CreateMenuGroupRequest 생성할_메뉴_그룹;
-    protected MenuGroup 저장된_메뉴_그룹;
-    protected List<MenuGroup> 저장된_메뉴_그룹_리스트;
-    protected MenuGroup 저장된_메뉴_그룹_1 = new MenuGroup();
-    protected MenuGroup 저장된_메뉴_그룹_2 = new MenuGroup();
+    protected List<MenuGroup> 저장된_메뉴_그룹;
+    protected MenuGroup 저장한_메뉴_그룹_1;
+    protected MenuGroup 저장한_메뉴_그룹_2;
 
-    @BeforeEach
-    void setUp() {
+    protected void 메뉴_그룹을_생성한다_픽스처_생성() {
         생성할_메뉴_그룹 = new CreateMenuGroupRequest("추천 메뉴");
-        저장된_메뉴_그룹 = new MenuGroup(생성할_메뉴_그룹.getName());
+    }
 
-        저장된_메뉴_그룹_리스트 = List.of(저장된_메뉴_그룹_1, 저장된_메뉴_그룹_2);
+    protected void 메뉴_그룹을_조회한다_픽스처_생성() {
+        저장한_메뉴_그룹_1 = new MenuGroup("저장할_메뉴_그룹_1");
+        저장한_메뉴_그룹_2 = new MenuGroup("저장할_메뉴_그룹_2");
+        저장된_메뉴_그룹 = menuGroupRepository.saveAll(List.of(저장한_메뉴_그룹_1, 저장한_메뉴_그룹_2));
     }
 }
