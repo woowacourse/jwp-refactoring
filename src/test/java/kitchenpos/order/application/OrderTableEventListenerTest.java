06570ab62573ff17c5e1907bdd57ac1133dff3b0
nuyh <hjaehyun25@gmail.com>
2023-10-25 12:00:15 +0900

    test: 주문 테이블 빈 테이블 여부 변경 이벤트 핸들링 테스트

---
+++ src/test/java/kitchenpos/order/application/OrderTableEventListenerTest.java
@@ -0,0 +1,36 @@
+package kitchenpos.order.application;
+
+import kitchenpos.EntityFactory;
+import kitchenpos.ordertable.application.OrderTableService;
+import kitchenpos.ordertable.domain.OrderTable;
+import kitchenpos.ordertable.ui.dto.OrderTableUpdateRequest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@SpringBootTest
+class OrderTableEventListenerTest {
+
+    @Autowired
+    private OrderTableService orderTableService;
+    @Autowired
+    private EntityFactory entityFactory;
+
+    @Test
+    @DisplayName("주문 테이블의 빈 테이블 여부가 변경될 때 주문 상태가 COOKING, MEAL이면 예외가 발생한다")
+    void validateOrderStatus() {
+        //given
+        final OrderTable orderTable = entityFactory.saveOrderTable();
+        entityFactory.saveOrder(orderTable);
+
+        final OrderTableUpdateRequest request = new OrderTableUpdateRequest(4, true);
+
+        //when, then
+        assertThatThrownBy(() -> orderTableService.changeEmpty(orderTable.getId(), request))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("주문 테이블이 조리 중이거나 식사 중입니다.");
+    }
+}
