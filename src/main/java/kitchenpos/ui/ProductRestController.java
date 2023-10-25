f77a8a7b3b8786ebafa5b8bd7cdf3eec31509005
yujamint <woawoa0513@gmail.com>
2023-10-25 19:42:15 +0900

    refactor: Product Response 객체 구현

--- src/main/java/kitchenpos/ui/ProductRestController.java
+++ src/main/java/kitchenpos/ui/ProductRestController.java
@@ -1,15 +1,18 @@
 package kitchenpos.ui;
 
-import java.net.URI;
-import java.util.List;
 import kitchenpos.application.ProductService;
 import kitchenpos.domain.Product;
 import kitchenpos.request.ProductCreateRequest;
+import kitchenpos.response.ProductResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class ProductRestController {
@@ -20,18 +23,17 @@ public class ProductRestController {
     }
 
     @PostMapping("/api/products")
-    public ResponseEntity<Product> create(@RequestBody final ProductCreateRequest request) {
-        final Product created = productService.create(request.getName(), request.getPrice());
-        final URI uri = URI.create("/api/products/" + created.getId());
-        return ResponseEntity.created(uri)
-                .body(created)
-                ;
+    public ResponseEntity<ProductResponse> create(@RequestBody ProductCreateRequest request) {
+        Product created = productService.create(request.getName(), request.getPrice());
+        URI uri = URI.create("/api/products/" + created.getId());
+        return ResponseEntity.created(uri).body(ProductResponse.from(created));
     }
 
     @GetMapping("/api/products")
-    public ResponseEntity<List<Product>> list() {
-        return ResponseEntity.ok()
-                .body(productService.list())
-                ;
+    public ResponseEntity<List<ProductResponse>> list() {
+        List<ProductResponse> response = productService.list().stream()
+                .map(ProductResponse::from)
+                .collect(Collectors.toList());
+        return ResponseEntity.ok().body(response);
     }
 }
