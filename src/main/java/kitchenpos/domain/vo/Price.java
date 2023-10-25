3a8d6c1af96e824df555d353605ec5ef5bded680
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-10-25 16:09:22 +0900

    refactor: price의 동등성 재정의

--- src/main/java/kitchenpos/domain/vo/Price.java
+++ src/main/java/kitchenpos/domain/vo/Price.java
@@ -33,6 +33,19 @@ public class Price {
     }
 
     @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Price price = (Price) o;
+        return Objects.equals(value, price.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+
+    @Override
     public String toString() {
         return "Price{" +
                "value=" + value +
