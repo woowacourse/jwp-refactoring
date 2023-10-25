8ea23faab297fa4b62beee042ed1b0ede4f6cab6
hyunseo <>
2023-10-25 14:32:47 +0900

    refactor: schema 코드 간소화

--- src/main/resources/db/migration/V4__change_schema.sql
+++ src/main/resources/db/migration/V4__change_schema.sql
@@ -6,28 +6,14 @@ ALTER TABLE menu_product
 ALTER TABLE menu_product
     ADD price decimal(19, 2) not null;
 
-insert into menu (name, price, menu_group_id)
-values ('후라이드치킨', 16000, 2);
-insert into menu (name, price, menu_group_id)
-values ('양념치킨', 16000, 2);
-insert into menu (name, price, menu_group_id)
-values ('반반치킨', 16000, 2);
-insert into menu (name, price, menu_group_id)
-values ('통구이', 16000, 2);
-insert into menu (name, price, menu_group_id)
-values ('간장치킨', 17000, 2);
-insert into menu (name, price, menu_group_id)
-values ('순살치킨', 17000, 2);
-
-insert into menu_product (menu_id, product_id, name, price, quantity)
-values (1, 1, '후라이드', 16000, 1);
-insert into menu_product (menu_id, product_id, name, price, quantity)
-values (2, 2, '양념치킨', 16000, 1);
-insert into menu_product (menu_id, product_id, name, price, quantity)
-values (3, 3, '반반치킨', 16000, 1);
-insert into menu_product (menu_id, product_id, name, price, quantity)
-values (4, 4, '통구이', 16000, 1);
-insert into menu_product (menu_id, product_id, name, price, quantity)
-values (5, 5, '간장치킨', 17000, 1);
-insert into menu_product (menu_id, product_id, name, price, quantity)
-values (6,  6, '순살치킨', 17000, 1);
+update menu_product mp
+set (mp.name, mp.price) = (
+    select p.name, p.price
+    from product p
+    where p.id = mp.product_id
+    )
+where exists (
+    select 1
+    from product p
+    where p.id = mp.product_id
+    );
