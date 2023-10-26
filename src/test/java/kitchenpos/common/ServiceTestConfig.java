db89e08a943bd5856e39ab3286954b758245b620
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-10-26 17:04:25 +0900

    refactor: 가독성을 위한 개행 및 메서드 분리 수정

--- src/test/java/kitchenpos/common/ServiceTestConfig.java
+++ src/test/java/kitchenpos/common/ServiceTestConfig.java
@@ -1,11 +1,11 @@
 package kitchenpos.common;
 
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Sql("/truncate.sql")
 @Transactional
+@TestExecutionListeners(value = {AcceptanceTestExecutionListener.class,}, mergeMode = TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS)
 public class ServiceTestConfig {
 }
