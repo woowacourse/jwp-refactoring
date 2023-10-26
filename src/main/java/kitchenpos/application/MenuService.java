f7440e928a0cd3316bb36c1d5ff2267b721a1e63
This2sho <zx00018@naver.com>
2023-10-26 16:57:57 +0900

    refactor: 상품을 각각 조회하는 메서드를 in 절을 통해 한번에 조회하도록 변경

--- src/main/java/kitchenpos/application/MenuService.java
+++ src/main/java/kitchenpos/application/MenuService.java
@@ -1,6 +1,8 @@
 package kitchenpos.application;
 
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import kitchenpos.application.dto.request.MenuCreateRequest;
 import kitchenpos.application.dto.request.MenuProductRequest;
@@ -51,14 +53,31 @@ public class MenuService {
     }
 
     private List<MenuProduct> extractMenuProducts(final List<MenuProductRequest> menuProductRequests) {
+        final List<Long> productIds = extractProductIds(menuProductRequests);
+        final Map<Long, Product> productMap = mapFrom(productRepository.findAllByIdInThrow(productIds));
+
         return menuProductRequests.stream()
-                .map(it -> new MenuProduct(findProductById(it.getProductId()), it.getQuantity()))
+                .map(it -> new MenuProduct(findProductById(productMap, it.getProductId()), it.getQuantity()))
                 .collect(Collectors.toList());
     }
 
-    private Product findProductById(final long productId) {
-        return productRepository.findById(productId)
-                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 상품입니다."));
+    private List<Long> extractProductIds(final List<MenuProductRequest> menuProductRequests) {
+        return menuProductRequests.stream()
+                .map(MenuProductRequest::getProductId)
+                .collect(Collectors.toList());
+    }
+
+    private Map<Long, Product> mapFrom(final List<Product> products) {
+        return products
+                .stream()
+                .collect(Collectors.toMap(Product::getId, Function.identity()));
+    }
+
+    private Product findProductById(Map<Long, Product> productMap, final long productId) {
+        if (productMap.containsKey(productId)) {
+            return productMap.get(productId);
+        }
+        throw new IllegalArgumentException("존재하지 않는 상품입니다.");
     }
 
     public List<MenuResponse> list() {
