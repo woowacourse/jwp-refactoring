5146a98496cf641f12f592e65058ab902d882c40
cpot5620 <cpot5620@gmail.com>
2023-10-26 13:21:52 +0900

    refactor: ProductPrice VO 구현

--- src/main/java/kitchenpos/product/domain/Product.java
+++ src/main/java/kitchenpos/product/domain/Product.java
@@ -1,19 +1,15 @@
 package kitchenpos.product.domain;
 
 import java.math.BigDecimal;
-import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.NotNull;
 
 @Entity
 public class Product {
 
-
-    private static final int MIN_PRICE = 0;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,31 +18,19 @@ public class Product {
     @Embedded
     private ProductName name;
 
-    @Column(precision = 19, scale = 2)
-    @NotNull
-    private BigDecimal price;
+    @Embedded
+    private ProductPrice price;
 
     protected Product() {
     }
 
     private Product(String name, BigDecimal price) {
         this.name = ProductName.from(name);
-        this.price = price;
+        this.price = ProductPrice.from(price);
     }
 
     public static Product create(String name, BigDecimal price) {
-        validatePrice(price);
-
         return new Product(name, price);
-    }
-
-    private static void validatePrice(BigDecimal price) {
-        if (price == null) {
-            throw new NullPointerException("상품 금액은 null일 수 없습니다.");
-        }
-        if (price.doubleValue() < MIN_PRICE) {
-            throw new IllegalArgumentException("상품 금액은 0원 이상이어야 합니다.");
-        }
     }
 
     public Long getId() {
@@ -58,7 +42,7 @@ public class Product {
     }
 
     public BigDecimal getPrice() {
-        return price;
+        return price.getPrice();
     }
 
 }
