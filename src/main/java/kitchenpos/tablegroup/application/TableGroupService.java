f82a6486e7f4dc825ad8032f0080d286ce317c78
aak2075 <lks971003@gmail.com>
2023-10-27 00:45:41 +0900

    refactor: 이벤트 생성 로직 정팩메로 변경

--- src/main/java/kitchenpos/tablegroup/application/TableGroupService.java
+++ src/main/java/kitchenpos/tablegroup/application/TableGroupService.java
@@ -35,7 +35,7 @@ public class TableGroupService {
                 .orElseThrow(IllegalArgumentException::new);
 
         tableGroup.ungroup();
-        eventPublisher.publishEvent(new TableGroupUngroupEvent(tableGroup.getId()));
+        eventPublisher.publishEvent(TableGroupUngroupEvent.from(tableGroup));
         tableGroupDao.save(tableGroup);
     }
 }
--- src/main/java/kitchenpos/tablegroup/dto/TableGroupUngroupEvent.java
+++ src/main/java/kitchenpos/tablegroup/dto/TableGroupUngroupEvent.java
@@ -1,11 +1,17 @@
 package kitchenpos.tablegroup.dto;
 
+import kitchenpos.tablegroup.domain.TableGroup;
+
 public class TableGroupUngroupEvent {
 
     private final Long id;
 
     public TableGroupUngroupEvent(Long id) {
         this.id = id;
+    }
+
+    public static TableGroupUngroupEvent from(TableGroup tableGroup) {
+        return new TableGroupUngroupEvent(tableGroup.getId());
     }
 
     public Long getId() {
