b1efd9e48b119e01a637ad6c8a25a56d9eab567d
splitCoding <voicesplit@daum.net>
2023-10-27 01:27:32 +0900

    feat: 엔티티와 DB 테이블 데이터 간 Converter 인터페이스 구현

---
+++ src/main/java/kitchenpos/domain/repository/converter/Converter.java
@@ -0,0 +1,17 @@
+package kitchenpos.domain.repository.converter;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public interface Converter<E, D> {
+
+    D entityToData(final E entity);
+
+    E dataToEntity(final D data);
+
+    default List<E> dataToEntity(final List<D> data) {
+        return data.stream()
+                .map(this::dataToEntity)
+                .collect(Collectors.toList());
+    }
+}
