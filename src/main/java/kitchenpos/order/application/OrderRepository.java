e248337e98307490c22042805285dd20c25b0407
hong-sile <gurwns9325@naver.com>
2023-10-26 21:19:46 +0900

    feat: getById 추가

--- src/main/java/kitchenpos/order/application/OrderRepository.java
+++ src/main/java/kitchenpos/order/application/OrderRepository.java
@@ -1,8 +1,11 @@
 package kitchenpos.order.application;
 
+import static kitchenpos.order.domain.exception.OrderExceptionType.ORDER_IS_NOT_FOUND;
+
 import java.util.List;
 import java.util.Optional;
 import kitchenpos.order.domain.Order;
+import kitchenpos.order.domain.exception.OrderException;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
@@ -10,4 +13,8 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
     List<Order> findByOrderTableIdIn(List<Long> orderTableIds);
 
     Optional<Order> findByOrderTableId(Long orderTableId);
+
+    default Order getById(final Long id) {
+        return findById(id).orElseThrow(() -> new OrderException(ORDER_IS_NOT_FOUND));
+    }
 }
