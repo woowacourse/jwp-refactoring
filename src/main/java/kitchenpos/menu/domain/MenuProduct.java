25d51c82efde9688f3c6137b0138ce6a8421a519
cpot5620 <cpot5620@gmail.com>
2023-10-26 13:02:10 +0900

    refactor: MenuQuantity VO 구현

--- src/main/java/kitchenpos/menu/domain/MenuProduct.java
+++ src/main/java/kitchenpos/menu/domain/MenuProduct.java
@@ -1,6 +1,7 @@
 package kitchenpos.menu.domain;
 
 import com.sun.istack.NotNull;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -9,14 +10,13 @@ import javax.persistence.Id;
 @Entity
 public class MenuProduct {
 
-    private static final int MIN_QUANTITY = 1;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long seq;
 
-    @NotNull
-    private Long quantity;
+    @Embedded
+    private MenuQuantity quantity;
 
     @NotNull
     private Long productId;
@@ -25,24 +25,14 @@ public class MenuProduct {
     }
 
     private MenuProduct(Long quantity, Long productId) {
-        this.quantity = quantity;
+        this.quantity = MenuQuantity.from(quantity);
         this.productId = productId;
     }
 
     public static MenuProduct create(Long quantity, Long productId) {
-        validateQuantity(quantity);
         validateProduct(productId);
 
         return new MenuProduct(quantity, productId);
-    }
-
-    private static void validateQuantity(Long quantity) {
-        if (quantity == null) {
-            throw new NullPointerException("상품 수량은 null일 수 없습니다.");
-        }
-        if (quantity < MIN_QUANTITY) {
-            throw new IllegalArgumentException("상품 개수는 1개 이상이어야 합니다.");
-        }
     }
 
     private static void validateProduct(Long productId) {
@@ -50,13 +40,13 @@ public class MenuProduct {
             throw new NullPointerException("상품은 null일 수 없습니다.");
         }
     }
-    
+
     public Long getSeq() {
         return seq;
     }
 
     public Long getQuantity() {
-        return quantity;
+        return quantity.getQuantity();
     }
 
     public Long getProductId() {
