a27c663294d561515f9a7d2e955c13aebf0e3e25
hong-sile <gurwns9325@naver.com>
2023-10-26 14:46:28 +0900

    refactor: ExceptionType 분리

--- src/main/java/kitchenpos/table_group/domain/exception/TableGroupExceptionType.java
+++ src/main/java/kitchenpos/table_group/domain/exception/TableGroupExceptionType.java
@@ -4,7 +4,8 @@ import kitchenpos.basic.BasicExceptionType;
 
 public enum TableGroupExceptionType implements BasicExceptionType {
 
-    ORDER_TABLE_SIZE_IS_LOWER_THAN_ZERO_OR_EMPTY("주문 테이블의 사이즈가 2보다 작거나 비어있을 수 없습니다."),
+    ORDER_TABLE_SIZE_IS_LOWER_THAN_TWO("주문 테이블의 사이즈가 2보다 작습니다."),
+    ORDER_TABLE_IS_NOT_EMPTY("주문 테이블들이 비어있지 않습니다."),
     ORDER_TABLE_IS_NOT_EMPTY_OR_ALREADY_GROUPED("테이블이 비어있지 않거나, 다른 그룹에 이미 속해있습니다."),
     TABLE_GROUP_NOT_FOUND("요청하는 테이블 그룹을 찾을 수 없습니다."),
     ORDER_TABLE_IS_NOT_PRESENT_ALL("요청한 테이블들중 저장되지 않은 테이블이 있습니다.");
