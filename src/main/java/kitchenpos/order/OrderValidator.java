14db8e1200a85ff51bbb661fa1b25ea2f87a300f
kiarakim <kiarakim413@gmail.com>
2023-10-26 16:21:29 +0900

    refactor: order와 table의 양방향 의존성 제거

--- src/main/java/kitchenpos/order/OrderValidator.java
+++ src/main/java/kitchenpos/order/OrderValidator.java
@@ -1,54 +1,8 @@
 package kitchenpos.order;
 
-import kitchenpos.menu.MenuRepository;
-import kitchenpos.table.OrderTable;
-import kitchenpos.table.OrderTableRepository;
-import org.springframework.stereotype.Component;
-import org.springframework.util.CollectionUtils;
-
 import java.util.List;
-import java.util.stream.Collectors;
 
-@Component
-public class OrderValidator {
+public interface OrderValidator {
 
-    public OrderValidator(OrderTableRepository orderTableRepository, MenuRepository menuRepository) {
-        this.orderTableRepository = orderTableRepository;
-        this.menuRepository = menuRepository;
-    }
-
-    private final OrderTableRepository orderTableRepository;
-    private final MenuRepository menuRepository;
-
-
-    public void validate(Long orderTableId, List<OrderLineItem> orderLineItems){
-        validateOrderTable(orderTableId);
-        validateOrderLineItem(orderLineItems);
-    }
-
-    private void validateOrderTable(Long orderTableId) {
-        final OrderTable orderTable = orderTableRepository.findById(orderTableId)
-                .orElseThrow(IllegalArgumentException::new);
-        if (orderTable.isEmpty()) {
-            throw new IllegalArgumentException();
-        }
-    }
-
-    private void validateOrderLineItem(List<OrderLineItem> orderLineItems) {
-        validateEmpty(orderLineItems);
-        final List<Long> menuIds = orderLineItems.stream()
-                .map(OrderLineItem::getMenuId)
-                .collect(Collectors.toList());
-        long menuCount = menuRepository.countByIdIn(menuIds);
-
-        if (orderLineItems.size() != menuCount) {
-            throw new IllegalArgumentException();
-        }
-    }
-
-    private void validateEmpty(List<OrderLineItem> orderLineItems) {
-        if (CollectionUtils.isEmpty(orderLineItems)) {
-            throw new IllegalArgumentException();
-        }
-    }
+    void validate(Long orderTableId, List<OrderLineItem> orderLineItems);
 }
