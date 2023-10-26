4f0f82b3785dda06c7ff88f58b73da6cf2cca784
rawfishthelgh <rg970604@naver.com>
2023-10-26 15:44:17 +0900

    refactor: Product - MenuProduct 간 객체참조 제거

---
+++ src/main/java/kitchenpos/domain/menu/MenuProducts.java
@@ -0,0 +1,27 @@
+package kitchenpos.domain.menu;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import java.util.List;
+
+@Embeddable
+public class MenuProducts {
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "menu_id", nullable = false)
+    private List<MenuProduct> menuProducts;
+
+    public MenuProducts() {
+    }
+
+    public MenuProducts(List<MenuProduct> menuProducts) {
+        this.menuProducts = menuProducts;
+    }
+
+    public List<MenuProduct> getMenuProducts() {
+        return menuProducts;
+    }
+
+}
