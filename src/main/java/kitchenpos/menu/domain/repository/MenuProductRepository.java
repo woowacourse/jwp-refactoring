f4cd29f1d939b189316b70505d4cc717bf5f5ffc
ingpyo <juninpyo726@naver.com>
2023-10-27 21:20:09 +0900

    refactor: 사용하지 않는 repository 제거

--- src/main/java/kitchenpos/menu/domain/repository/MenuProductRepository.java
+++
@@ -1,7 +0,0 @@
-package kitchenpos.menu.domain.repository;
-
-import kitchenpos.menu.domain.MenuProduct;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MenuProductRepository extends JpaRepository<MenuProduct, Long> {
-}
