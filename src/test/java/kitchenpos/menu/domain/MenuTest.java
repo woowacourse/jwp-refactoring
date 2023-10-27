9647c6cb8f51ce68f8e25183033fbc23b183f566
jeomxon <jeomxon@gmail.com>
2023-10-27 18:00:29 +0900

    test: MenuValidator 검증 추가

--- src/test/java/kitchenpos/menu/domain/MenuTest.java
+++ src/test/java/kitchenpos/menu/domain/MenuTest.java
@@ -30,19 +30,4 @@ class MenuTest {
                 () -> assertThat(menu.getPrice()).isEqualTo("15000.00")
         );
     }
-
-//    @Test
-//    @DisplayName("금액의 총합이 메뉴의 가격보다 큰 경우 예외가 발생한다.")
-//    void throwsExceptionWhenAmountSumIsLargerThanMenuPrice() {
-//        // given
-//        final Product product = new Product("후라이드치킨", new BigDecimal("15000.00"));
-//        final MenuProduct menuProduct = new MenuProduct(null, product.getId(), 2);
-//        final MenuGroup menuGroup = new MenuGroup("치킨");
-//
-//        // when, then
-//        assertThatThrownBy(
-//                () -> new Menu(menuGroup.getId(), List.of(menuProduct), "후라이드치킨", new BigDecimal("30001.00")))
-//                .isInstanceOf(IllegalArgumentException.class)
-//                .hasMessage("메뉴의 가격은 금액(가격 * 수량)의 합보다 클 수 없습니다.");
-//    }
 }
