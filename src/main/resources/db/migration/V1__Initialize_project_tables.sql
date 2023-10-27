704f3f0bb44615833af6a6658f163081ffe2c16b
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2023-10-27 10:39:32 +0900

    fix: flyway script를 수정한다.

--- src/main/resources/db/migration/V1__Initialize_project_tables.sql
+++ src/main/resources/db/migration/V1__Initialize_project_tables.sql
@@ -1,20 +1,3 @@
-create table product
-(
-    id    bigint         not null auto_increment,
-    name  varchar(255)   not null,
-    price decimal(19, 2) not null,
-    primary key (id)
-);
-
-
-create table menu_group
-(
-    id   bigint       not null auto_increment,
-    name varchar(255) not null,
-    primary key (id)
-);
-
-
 create table menu
 (
     id            bigint         not null auto_increment,
@@ -23,20 +6,70 @@ create table menu
     menu_group_id bigint         not null,
     primary key (id)
 );
+
+create table menu_group
+(
+    id   bigint       not null auto_increment,
+    name varchar(255) not null,
+    primary key (id)
+);
+
+create table menu_product
+(
+    seq        bigint not null auto_increment,
+    quantity   bigint not null,
+    menu_id    bigint not null,
+    product_id bigint not null,
+    primary key (seq)
+);
+
+create table order_line_item
+(
+    seq      bigint not null auto_increment,
+    quantity bigint not null,
+    menu_id  bigint not null,
+    order_id bigint not null,
+    primary key (seq)
+);
+
+create table orders
+(
+    id             bigint       not null auto_increment,
+    order_status   varchar(255) not null,
+    ordered_time   datetime     not null,
+    order_table_id bigint       not null,
+    primary key (id)
+);
+
+create table order_table
+(
+    id               bigint  not null auto_increment,
+    empty            bit     not null,
+    number_of_guests integer not null,
+    table_group_id   bigint,
+    primary key (id)
+);
+
+create table product
+(
+    id    bigint         not null auto_increment,
+    name  varchar(255)   not null,
+    price decimal(19, 2) not null,
+    primary key (id)
+);
+
+create table table_group
+(
+    id           bigint   not null auto_increment,
+    created_date datetime not null,
+    primary key (id)
+);
+
 alter table menu
     add constraint fk_menu_to_menu_group
         foreign key (menu_group_id)
             references menu_group (id);
 
-
-create table menu_product
-(
-    id        bigint not null auto_increment,
-    menu_id    bigint not null,
-    product_id bigint not null,
-    quantity   bigint not null,
-    primary key (id)
-);
 alter table menu_product
     add constraint fk_menu_product_to_menu
         foreign key (menu_id)
@@ -47,52 +80,6 @@ alter table menu_product
         foreign key (product_id)
             references product (id);
 
-
-create table table_group
-(
-    id           bigint   not null auto_increment,
-    created_date datetime not null,
-    primary key (id)
-);
-
-
-create table order_table
-(
-    id               bigint  not null auto_increment,
-    table_group_id   bigint,
-    number_of_guests integer not null,
-    empty            bit     not null,
-    primary key (id)
-);
-
-alter table order_table
-    add constraint fk_order_table_to_table_group
-        foreign key (table_group_id)
-            references table_group (id);
-
-
-create table orders
-(
-    id             bigint       not null auto_increment,
-    order_table_id bigint       not null,
-    order_status   varchar(255) not null,
-    ordered_time   datetime     not null,
-    primary key (id)
-);
-alter table orders
-    add constraint fk_orders_to_order_table
-        foreign key (order_table_id)
-            references order_table (id);
-
-
-create table order_line_item
-(
-    id      bigint not null auto_increment,
-    order_id bigint not null,
-    menu_id  bigint not null,
-    quantity bigint not null,
-    primary key (id)
-);
 alter table order_line_item
     add constraint fk_order_line_item_to_menu
         foreign key (menu_id)
@@ -102,3 +89,13 @@ alter table order_line_item
     add constraint fk_order_line_item_to_orders
         foreign key (order_id)
             references orders (id);
+
+alter table orders
+    add constraint fk_orders_to_order_table
+        foreign key (order_table_id)
+            references order_table (id);
+
+alter table order_table
+    add constraint fk_order_table_to_table_group
+        foreign key (table_group_id)
+            references table_group (id);
