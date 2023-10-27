43603a5e63a0108c091171d4cdf145d77dcfbb8b
jjongwa <troas96@naver.com>
2023-10-27 11:08:45 +0900

    chore: flyway 스크립트 변경

--- src/main/resources/db/migration/V3__Add_MenuName_And_Price_Columns_To_OrderLineItem.sql
+++ src/main/resources/db/migration/V3__Add_MenuName_And_Price_Columns_To_OrderLineItem.sql
@@ -3,18 +3,6 @@ ALTER TABLE order_line_item ADD COLUMN price DECIMAL(19, 2);
 
 UPDATE order_line_item SET menu_name = 'Unknown', price = 0.00 WHERE menu_name IS NULL OR price IS NULL;
 
-UPDATE order_line_item AS oli
-SET menu_name = m.name
-    FROM Menu AS m
-WHERE oli.menu_name = 'Unknown'
-  AND oli.menu_id = m.id;
-
-UPDATE order_line_item AS oli
-SET price = m.price
-    FROM Menu AS m
-WHERE oli.price = 0.00
-  AND oli.menu_id = m.id;
-
 ALTER TABLE order_line_item ALTER COLUMN menu_name SET NOT NULL;
 ALTER TABLE order_line_item ALTER COLUMN price SET NOT NULL;
 
