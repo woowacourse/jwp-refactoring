4a61b21511daa6cdf739e54cc0631b3c3d79f6e8
kdkdhoho <hkim4410@naver.com>
2023-10-27 18:37:11 +0900

    refactor: 최상위 모듈 의존성 설정

---
+++ src/test/java/kitchenpos/ApplicationTest.java
@@ -0,0 +1,12 @@
+package kitchenpos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+}
