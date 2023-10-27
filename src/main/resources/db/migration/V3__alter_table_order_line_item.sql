1d81b2d3f7882373fa48da243b4e511635b0b89b
JunDonghyuk <ehdgur4814@naver.com>
2023-10-27 15:41:28 +0900

    refactor: 제약사항 제거

--- src/main/resources/db/migration/V3__alter_table_order_line_item.sql
+++ src/main/resources/db/migration/V3__alter_table_order_line_item.sql
@@ -8,4 +8,7 @@ UPDATE order_line_item AS oi
     SET (oi.name, oi.price) = (SELECT m.name, m.price FROM menu as m WHERE m.id = oi.menu_id);
 
 ALTER TABLE order_line_item
+DROP CONSTRAINT fk_order_line_item_to_menu;
+
+ALTER TABLE order_line_item
     DROP COLUMN menu_id;
