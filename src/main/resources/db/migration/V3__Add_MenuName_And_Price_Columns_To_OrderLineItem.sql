8997d574eb87524d68d90c120b232c842f6912ae
jjongwa <troas96@naver.com>
2023-10-28 00:48:46 +0900

    chore: flyway 스크립트 변경

--- src/main/resources/db/migration/V3__Add_MenuName_And_Price_Columns_To_OrderLineItem.sql
+++ src/main/resources/db/migration/V3__Add_MenuName_And_Price_Columns_To_OrderLineItem.sql
@@ -1,10 +1,14 @@
-ALTER TABLE order_line_item ADD COLUMN menu_name VARCHAR(255);
-ALTER TABLE order_line_item ADD COLUMN price DECIMAL(19, 2);
+ALTER TABLE order_line_item
+    ADD COLUMN name VARCHAR(255);
 
-UPDATE order_line_item SET menu_name = 'Unknown', price = 0.00 WHERE menu_name IS NULL OR price IS NULL;
+ALTER TABLE order_line_item
+    ADD COLUMN price DECIMAL(19, 2);
 
-ALTER TABLE order_line_item ALTER COLUMN menu_name SET NOT NULL;
-ALTER TABLE order_line_item ALTER COLUMN price SET NOT NULL;
+UPDATE order_line_item AS oi
+SET (oi.name, oi.price) = (SELECT m.name, m.price FROM menu as m WHERE m.id = oi.menu_id);
 
-ALTER TABlE order_line_item DROP CONSTRAINT fk_order_line_item_to_menu;
-ALTER TABlE order_line_item DROP COLUMN menu_id;
+ALTER TABLE order_line_item
+DROP CONSTRAINT fk_order_line_item_to_menu;
+
+ALTER TABLE order_line_item
+DROP COLUMN menu_id;
