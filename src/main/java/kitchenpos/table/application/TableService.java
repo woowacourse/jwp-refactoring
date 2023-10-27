dbd75deca222ac5100f78002a9346e3fee9ae6b2
yenawee <yaena0319@naver.com>
2023-10-27 15:50:01 +0900

    refactor: OrderTable <- Order 단방향 의존되도록 수정

--- src/main/java/kitchenpos/table/application/TableService.java
+++ src/main/java/kitchenpos/table/application/TableService.java
@@ -1,30 +1,27 @@
 package kitchenpos.table.application;
 
-import kitchenpos.order.domain.OrderStatus;
-import kitchenpos.order.domain.repository.OrderRepository;
 import kitchenpos.table.application.request.TableCreateRequest;
 import kitchenpos.table.application.request.TableEmptyUpdateRequest;
 import kitchenpos.table.application.request.TableGuestUpdateRequest;
 import kitchenpos.table.application.response.OrderTableResponse;
+import kitchenpos.table.domain.OrderStatusValidator;
 import kitchenpos.table.domain.OrderTable;
 import kitchenpos.table.domain.repository.OrderTableRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Transactional
 @Service
 public class TableService {
-
-    private OrderRepository orderRepository;
     private OrderTableRepository orderTableRepository;
+    private OrderStatusValidator orderStatusValidator;
 
-    public TableService(OrderRepository orderRepository, OrderTableRepository orderTableRepository) {
-        this.orderRepository = orderRepository;
+    public TableService(OrderTableRepository orderTableRepository, OrderStatusValidator orderStatusValidator) {
         this.orderTableRepository = orderTableRepository;
+        this.orderStatusValidator = orderStatusValidator;
     }
 
     public OrderTableResponse create(final TableCreateRequest tableCreateRequest) {
@@ -46,18 +43,11 @@ public class TableService {
     public OrderTableResponse changeEmpty(final Long orderTableId, TableEmptyUpdateRequest tableEmptyUpdateRequest) {
         OrderTable savedOrderTable = getOrderTable(orderTableId);
 
-        checkIfOrderIsNotCompleted(orderTableId);
+        orderStatusValidator.checkIfOrderIsNotCompleted(orderTableId);
 
         savedOrderTable.changeEmpty(tableEmptyUpdateRequest.getEmpty());
 
         return OrderTableResponse.from(savedOrderTable);
-    }
-
-    private void checkIfOrderIsNotCompleted(Long orderTableId) {
-        if (orderRepository.existsByOrderTableIdAndOrderStatusIn(
-                orderTableId, Arrays.asList(OrderStatus.COOKING, OrderStatus.MEAL))) {
-            throw new IllegalArgumentException();
-        }
     }
 
     public OrderTableResponse changeNumberOfGuests(final Long orderTableId, TableGuestUpdateRequest tableGuestUpdateRequest) {
