1b9adb2a04cce31abda9aaec6fa41dbad75417ed
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2023-10-27 11:11:58 +0900

    fix: Table Group의 이름을 변경하는 sql을 수정한다.

--- src/main/resources/db/migration/V3__refactor_project_tables.sql
+++ src/main/resources/db/migration/V3__refactor_project_tables.sql
@@ -1,3 +1,3 @@
-rename table table_group to order_table_group;
+ALTER TABLE table_group RENAME TO order_table_group;
 ALTER TABLE menu_product RENAME COLUMN seq TO id;
 ALTER TABLE order_line_item RENAME COLUMN seq TO id;
