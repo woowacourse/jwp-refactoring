ffc1b3067fb59e35966ae8de5045b73d51343771
ezzanzzan <cjsdmswjd821@naver.com>
2023-10-27 16:34:41 +0900

    refactor: 불필요한 개행 제거

--- src/main/java/kitchenpos/repository/ProductRepository.java
+++ src/main/java/kitchenpos/repository/ProductRepository.java
@@ -4,5 +4,4 @@ import kitchenpos.domain.product.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
 }
