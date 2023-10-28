2d16ae6a0fde81ad0811d972b0f696e181d722c1
ingpyo <juninpyo726@naver.com>
2023-10-28 16:37:03 +0900

    refactor: MenuProduct validate 추가

--- src/main/java/kitchenpos/menu/domain/MenuProduct.java
+++ src/main/java/kitchenpos/menu/domain/MenuProduct.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+import java.util.Objects;
+
 import static javax.persistence.GenerationType.IDENTITY;
 
 @Entity
@@ -12,25 +14,37 @@ public class MenuProduct {
     @GeneratedValue(strategy = IDENTITY)
     private Long seq;
     private Long productId;
-    private long quantity;
+    private Integer quantity;
 
     protected MenuProduct() {
     }
 
-    private MenuProduct(final Long productId, final long quantity) {
+    private MenuProduct(final Long productId, final Integer quantity) {
         this.productId = productId;
         this.quantity = quantity;
     }
 
-    public static MenuProduct of(final Long productId, final long quantity) {
+    public static MenuProduct of(final Long productId, final Integer quantity) {
+        validate(productId, quantity);
         return new MenuProduct(productId, quantity);
+    }
+
+    private static void validate(final Long productId, final Integer quantity) {
+        Objects.requireNonNull(productId);
+        validateQuantity(quantity);
+    }
+
+    private static void validateQuantity(final Integer quantity) {
+        if (Objects.isNull(quantity) || quantity < 0) {
+            throw new IllegalArgumentException();
+        }
     }
 
     public Long getProductId() {
         return productId;
     }
 
-    public long getQuantity() {
+    public Integer getQuantity() {
         return quantity;
     }
 }
