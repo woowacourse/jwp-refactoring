83ec354fa95496790fc7c960fbc7d228fe3a5243
Eunsol Kim <61370551+Cyma-s@users.noreply.github.com>
2023-10-28 11:57:34 +0900

    refactor: test 모듈에서 web 접근성 제거

--- test/src/test/java/kitchenpos/AcceptanceApplication.java
+++ test/src/test/java/kitchenpos/AcceptanceApplication.java
@@ -2,11 +2,13 @@ package kitchenpos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class AcceptanceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(AcceptanceApplication.class, args);
     }
 }
