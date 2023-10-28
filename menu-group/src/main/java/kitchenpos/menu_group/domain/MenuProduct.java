0c87e9e615babefb9acd0e59f7253a0c334be664
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2023-10-28 15:52:31 +0900

    chore: 와일드 카드를 제거한다.

--- menu-group/src/main/java/kitchenpos/menu_group/domain/MenuProduct.java
+++ menu-group/src/main/java/kitchenpos/menu_group/domain/MenuProduct.java
@@ -2,8 +2,13 @@ package kitchenpos.menu_group.domain;
 
 import kitchenpos.menu.domain.Menu;
 
-import javax.persistence.*;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import static javax.persistence.GenerationType.IDENTITY;
 
 @Entity
