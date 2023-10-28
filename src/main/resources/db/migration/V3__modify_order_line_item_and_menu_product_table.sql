a3c758b7edba661e06a5dc96d89158c1ac4f6184
ingpyo <juninpyo726@naver.com>
2023-10-28 18:10:55 +0900

    refactor: DB 마이그래이션

--- src/main/resources/db/migration/V3__modify_order_line_item_and_menu_product_table.sql
+++ src/main/resources/db/migration/V3__modify_order_line_item_and_menu_product_table.sql
@@ -1,4 +1,6 @@
 ALTER TABLE menu_product ALTER COLUMN menu_id SET NULL;
 ALTER TABLE order_line_item DROP COLUMN menu_id;
+ALTER TABLE order_line_item DROP COLUMN order_id;
 ALTER TABLE order_line_item ADD COLUMN name varchar(255) not null;
-ALTER TABLE order_line_item ADD COLUMN price bigint not null
+ALTER TABLE order_line_item ADD COLUMN price DECIMAL(19, 2) NOT NULL;
+ALTER TABLE table_group RENAME TO order_table_group;
