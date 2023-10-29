c7b1e026a8f7871f019ee237348f060cfe3e36c3
BGuga <tmdwns9913@gmail.com>
2023-10-29 16:53:05 +0900

    chore: gradle 수정

---
+++ core/src/test/java/kitchenpos/Application.java
@@ -0,0 +1,13 @@
+package kitchenpos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class Application {
+
+    @Test
+    void contextLoads(){
+
+    }
+}
