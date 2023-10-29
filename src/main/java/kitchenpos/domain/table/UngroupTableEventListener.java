009dfdb1dca2015fe94dff1f55eaa31d3feb7684
kwonyj1022 <kwonyj1022@naver.com>
2023-10-29 13:25:49 +0900

    refactor: OrderTables 일급컬렉션 제거

--- src/main/java/kitchenpos/domain/table/UngroupTableEventListener.java
+++ src/main/java/kitchenpos/domain/table/UngroupTableEventListener.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class UngroupTableEventListener {
@@ -28,23 +29,47 @@ public class UngroupTableEventListener {
     public void handle(final UngroupTableEvent ungroupTableEvent) {
         final List<OrderTable> findOrderTables =
                 orderTableRepository.findAllByTableGroupId(ungroupTableEvent.getTableGroup().getId());
-        final OrderTables orderTables = new OrderTables(findOrderTables);
 
-        validateCanUngroup(orderTables);
+        validateCanUngroup(findOrderTables);
 
-        orderTables.ungroupOrderTables();
+        ungroupOrderTables(findOrderTables);
     }
 
-    private void validateCanUngroup(final OrderTables orderTables) {
+    private void validateCanUngroup(final List<OrderTable> orderTables) {
         if (containsNotCompleteOrder(orderTables)) {
             throw new IllegalArgumentException("테이블 그룹을 해제하려면 그룹화된 테이블의 모든 주문이 완료 상태이어야 합니다.");
         }
     }
 
-    private boolean containsNotCompleteOrder(final OrderTables orderTables) {
-        final List<Order> findOrders = orderRepository.findAllByOrderTableIdIn(orderTables.getOrderTableIds());
+    private boolean containsNotCompleteOrder(final List<OrderTable> orderTables) {
+        final List<Long> orderTableIds = orderTables.stream()
+                                                    .map(OrderTable::getId)
+                                                    .collect(Collectors.toList());
+        final List<Order> findOrders = orderRepository.findAllByOrderTableIdIn(orderTableIds);
 
         return findOrders.stream()
                          .anyMatch(Order::isNotComplete);
+    }
+
+    private void ungroupOrderTables(final List<OrderTable> orderTables) {
+        validateCanUngroup(orderTables);
+
+        if (containsDifferentId(orderTables)) {
+            throw new IllegalArgumentException("다른 테이블 그룹에 속한 테이블이 포함되어 있습니다.");
+        }
+
+        for (final OrderTable orderTable : orderTables) {
+            orderTable.ungroup();
+        }
+
+    }
+
+    private boolean containsDifferentId(final List<OrderTable> orderTables) {
+        final long orderTableIdCount = orderTables.stream()
+                                                  .map(OrderTable::getTableGroupId)
+                                                  .distinct()
+                                                  .count();
+
+        return orderTableIdCount != 1;
     }
 }
