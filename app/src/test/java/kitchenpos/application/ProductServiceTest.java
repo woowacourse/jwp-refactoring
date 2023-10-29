ed9497f9bd83547fd203592dfc8c233f96731363
Ohjintaek <jt950930@gmail.com>
2023-10-29 16:59:51 +0900

    chore: product, common, app 모듈 분리

---
+++ app/src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -0,0 +1,90 @@
+package kitchenpos.application;
+
+import kitchenpos.domain.Price;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import kitchenpos.product.application.ProductService;
+import kitchenpos.product.application.dto.request.ProductCreateRequest;
+import kitchenpos.product.application.dto.response.ProductResponse;
+import kitchenpos.product.domain.Product;
+import kitchenpos.product.persistence.ProductRepository;
+
+import java.math.BigDecimal;
+import java.util.Collections;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class ProductServiceTest {
+
+    @Mock
+    private ProductRepository productRepository;
+
+    @InjectMocks
+    private ProductService productService;
+
+    @Nested
+    class 상품_생성 {
+        @Test
+        void 상품을_생성한다() {
+            // given
+            final Product savedProduct = new Product(1L, "상품", new Price(BigDecimal.valueOf(1000)));
+            when(productRepository.save(any(Product.class)))
+                    .thenReturn(savedProduct);
+
+            // when
+            final ProductCreateRequest request = new ProductCreateRequest("상품", BigDecimal.valueOf(1000));
+            final ProductResponse result = productService.create(request);
+            final ProductResponse expect = new ProductResponse(1L, "상품", BigDecimal.valueOf(1000));
+
+            // then
+            assertAll(
+                    () -> assertThat(result.getId()).isEqualTo(1),
+                    () -> assertThat(result.getName()).isEqualTo("상품"),
+                    () -> assertThat(result.getPrice()).isEqualTo(BigDecimal.valueOf(1000))
+            );
+        }
+
+        @Test
+        void 상품을_생성할_때_가격이_없으면_실패한다() {
+            // given
+            final ProductCreateRequest request = new ProductCreateRequest("상품", null);
+
+            // when, then
+            assertThatThrownBy(() -> productService.create(request))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        void 상품을_생성할_때_가격이_0보다_작으면_실패한다() {
+            // given
+            final ProductCreateRequest request = new ProductCreateRequest("상품", BigDecimal.valueOf(-1000L));
+
+            // when, then
+            assertThatThrownBy(() -> productService.create(request))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+    }
+
+    @Test
+    void 모든_상품_목록을_반환한다() {
+        // given
+        when(productRepository.findAll())
+                .thenReturn(Collections.emptyList());
+
+        // when
+        final List<ProductResponse> result = productService.list();
+
+        // then
+        assertThat(result).isEmpty();
+    }
+}
---
+++ product/src/main/java/kitchenpos/product/application/ProductService.java
@@ -0,0 +1,37 @@
+package kitchenpos.product.application;
+
+import kitchenpos.domain.Price;
+import kitchenpos.product.application.dto.request.ProductCreateRequest;
+import kitchenpos.product.application.mapper.ProductMapper;
+import kitchenpos.product.domain.Product;
+import kitchenpos.product.persistence.ProductRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import kitchenpos.product.application.dto.response.ProductResponse;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public ProductService(final ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Transactional
+    public ProductResponse create(final ProductCreateRequest request) {
+        final Product product = new Product(request.getName(), new Price(request.getPrice()));
+        final Product savedProduct = productRepository.save(product);
+
+        return ProductMapper.mapToProductResponse(savedProduct);
+    }
+
+    public List<ProductResponse> list() {
+        return productRepository.findAll()
+                .stream()
+                .map(ProductMapper::mapToProductResponse)
+                .collect(Collectors.toList());
+    }
+}
