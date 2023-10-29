cb9024f95ecb073897fa6bdbae80ba91f91a191d
Ethan <kim76744dev@gmail.com>
2023-10-29 14:52:26 +0900

    refactor: ApiApplication으로 @SpringBootApplication 변경

--- api/src/main/java/kitchenpos/ApiApplication.java
+++ api/src/main/java/kitchenpos/ApiApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CoreApplication {
+public class ApiApplication {
     public static void main(final String[] args) {
-        SpringApplication.run(CoreApplication.class, args);
+        SpringApplication.run(ApiApplication.class, args);
     }
 }
