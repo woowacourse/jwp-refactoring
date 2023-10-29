8a221e443f01900a0286423e08e747c339fe309a
Eunsol Kim <61370551+Cyma-s@users.noreply.github.com>
2023-10-29 14:59:02 +0900

    refactor: main 메서드 제거

--- test/src/test/java/kitchenpos/AcceptanceApplication.java
+++ test/src/test/java/kitchenpos/AcceptanceApplication.java
@@ -1,14 +1,9 @@
 package kitchenpos;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
 public class AcceptanceApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(AcceptanceApplication.class, args);
-    }
 }
